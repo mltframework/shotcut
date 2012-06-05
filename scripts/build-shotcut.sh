e2cab912920aff9d99070be81264e6e06f3bbd58
Dan Dennedy <dan@dennedy.org>
2012-06-04 23:57:07 -0700

    disable build of mlt dv module on windows

--- scripts/build-shotcut.sh
+++ scripts/build-shotcut.sh
@@ -455,7 +455,7 @@ function set_globals {
     CONFIG[1]="${CONFIG[1]} --disable-sox"
   fi
   if test "$TARGET_OS" = "Win32" ; then
-    CONFIG[1]="${CONFIG[1]} --disable-kino --disable-vorbis --gtk2-prefix=\"$FINAL_INSTALL_DIR\" --target-os=MinGW --target-arch=i686 --rename-melt=melt.exe"
+    CONFIG[1]="${CONFIG[1]} --disable-dv --disable-kino --disable-vorbis --gtk2-prefix=\"$FINAL_INSTALL_DIR\" --target-os=MinGW --target-arch=i686 --rename-melt=melt.exe"
   fi
   CFLAGS_[1]="-I$FINAL_INSTALL_DIR/include $CFLAGS"
   [ "$TARGET_OS" = "Darwin" ] && CFLAGS_[1]="${CFLAGS_[1]} -DRELOCATABLE"
