mltframework/shotcut

commit 6491fedefdfca7baf1a1e279d42f2fc88bd7290a

Add basic drag-n-drop into timeline.

src/docks/playlistdock.cpp
@@ -352,15 +352,15 @@ void PlaylistDock::onDropped(const QMimeData *data, int row)
             }
         }
     }
-    else if (!data || data->data("application/mlt+xml").isEmpty()) {
+    else if (data && data->hasFormat(Mlt::XmlMimeType)) {
         if (MLT.producer() && MLT.producer()->is_valid()) {
             if (MLT.producer()->type() == playlist_type)
                 emit showStatusMessage(tr("You cannot insert a playlist into a playlist!"));
             else if (MLT.isSeekable()) {
                 if (row == -1)
-                    MAIN.undoStack()->push(new Playlist::AppendCommand(m_model, MLT.saveXML("string")));
+                    MAIN.undoStack()->push(new Playlist::AppendCommand(m_model, data->data(Mlt::XmlMimeType)));
                 else
-                    MAIN.undoStack()->push(new Playlist::InsertCommand(m_model, MLT.saveXML("string"), row));
+                    MAIN.undoStack()->push(new Playlist::InsertCommand(m_model, data->data(Mlt::XmlMimeType), row));
             } else {
                 DurationDialog dialog(this);
                 dialog.setDuration(MLT.profile().fps() * 5);

src/docks/timelinedock.cpp
@@ -173,3 +173,34 @@ void TimelineDock::openClip(int trackIndex, int clipIndex)
         }
     }
 }
+
+void TimelineDock::dragEnterEvent(QDragEnterEvent *event)
+{
+    if (event->mimeData()->hasFormat(Mlt::XmlMimeType)) {
+        event->acceptProposedAction();
+    }
+}
+
+void TimelineDock::dragMoveEvent(QDragMoveEvent *event)
+{
+    emit dragging(event->posF(), event->mimeData()->text().toInt());
+}
+
+void TimelineDock::dragLeaveEvent(QDragLeaveEvent *event)
+{
+    Q_UNUSED(event);
+    emit dropped();
+}
+
+void TimelineDock::dropEvent(QDropEvent *event)
+{
+    if (event->mimeData()->hasFormat(Mlt::XmlMimeType)) {
+        int trackIndex = m_quickView.rootObject()->property("currentTrack").toInt();
+        if (trackIndex >= 0) {
+            MAIN.undoStack()->push(new Timeline::AppendCommand(m_model, trackIndex, event->mimeData()->data(Mlt::XmlMimeType)));
+            event->acceptProposedAction();
+        }
+    }
+    emit dropped();
+}
+

src/docks/timelinedock.h
@@ -46,6 +46,8 @@ signals:
     void seeked(int position);
     void positionChanged();
     void clipOpened(void* producer, int in, int out);
+    void dragging(const QPointF& pos, int duration);
+    void dropped();
 
 public slots:
     void addAudioTrack();
@@ -60,6 +62,12 @@ public slots:
     void releaseKey(int key, Qt::KeyboardModifiers modifiers);
     void selectTrack(int by);
     void openClip(int trackIndex, int clipIndex);
+
+protected:
+    void dragEnterEvent(QDragEnterEvent* event);
+    void dragMoveEvent(QDragMoveEvent* event);
+    void dragLeaveEvent(QDragLeaveEvent* event);
+    void dropEvent(QDropEvent* event);
 
 private:
     Ui::TimelineDock *ui;

src/docks/timelinedock.ui
@@ -10,6 +10,9 @@
     <height>300</height>
    </rect>
   </property>
+  <property name="acceptDrops">
+   <bool>true</bool>
+  </property>
   <property name="windowIcon">
    <iconset theme="view-time-schedule" resource="../../icons/resources.qrc">
     <normaloff>:/icons/oxygen/32x32/actions/view-time-schedule.png</normaloff>:/icons/oxygen/32x32/actions/view-time-schedule.png</iconset>

src/glwidget.cpp
@@ -43,6 +43,7 @@ GLWidget::GLWidget(QWidget *parent)
     , m_threadStartEvent(0)
     , m_threadStopEvent(0)
     , m_image_format(mlt_image_yuv422)
+    , m_lastFrame(0)
 {
     m_texture[0] = m_texture[1] = m_texture[2] = 0;
     setAttribute(Qt::WA_PaintOnScreen);
@@ -66,6 +67,7 @@ GLWidget::~GLWidget()
     delete m_threadStartEvent;
     delete m_threadStopEvent;
     delete m_renderContext;
+    delete m_lastFrame;
 }
 
 QSize GLWidget::minimumSizeHint() const
@@ -162,7 +164,8 @@ void GLWidget::mousePressEvent(QMouseEvent* event)
 {
     if (event->button() == Qt::LeftButton)
         m_dragStart = event->pos();
-    emit dragStarted();
+    if (MLT.isClip())
+        emit dragStarted();
 }
 
 void GLWidget::mouseMoveEvent(QMouseEvent* event)
@@ -177,8 +180,12 @@ void GLWidget::mouseMoveEvent(QMouseEvent* event)
         return;
     QDrag *drag = new QDrag(this);
     QMimeData *mimeData = new QMimeData;
-    mimeData->setData("application/mlt+xml", "");
+    mimeData->setData(Mlt::XmlMimeType, MLT.saveXML("string").toUtf8());
     drag->setMimeData(mimeData);
+    mimeData->setText(QString::number(MLT.producer()->get_playtime()));
+    if (m_lastFrame && !Settings.playerGPU())
+        drag->setPixmap(QPixmap::fromImage(MLT.image(m_lastFrame, 45 * MLT.profile().dar(), 45)).scaledToHeight(45));
+    drag->setHotSpot(QPoint(0, 0));
     drag->exec(Qt::LinkAction);
 }
 
@@ -355,6 +362,8 @@ void GLWidget::showFrame(Mlt::QFrame frame)
                             GL_LUMINANCE, GL_UNSIGNED_BYTE, image + m_image_width * m_image_height + m_image_width/2 * m_image_height/2);
         }
         glDraw();
+        delete m_lastFrame;
+        m_lastFrame = new Mlt::Frame(*frame.frame());
     }
     showFrameSemaphore.release();
 }
@@ -362,6 +371,9 @@ void GLWidget::showFrame(Mlt::QFrame frame)
 int GLWidget::setProducer(Mlt::Producer* producer, bool isMulti)
 {
     int error = Controller::setProducer(producer, isMulti);
+
+    delete m_lastFrame;
+    m_lastFrame = 0;
 
     if (!error) {
         bool reconnect = !m_consumer || !m_consumer->is_valid();
@@ -379,6 +391,9 @@ int GLWidget::setProducer(Mlt::Producer* producer, bool isMulti)
 int GLWidget::reconfigure(bool isMulti)
 {
     int error = 0;
+
+    delete m_lastFrame;
+    m_lastFrame = 0;
 
     // use SDL for audio, OpenGL for video
     QString serviceName = property("mlt_service").toString();

src/glwidget.h
@@ -97,6 +97,7 @@ private:
     Event* m_threadStartEvent;
     Event* m_threadStopEvent;
     mlt_image_format m_image_format;
+    Mlt::Frame* m_lastFrame;
 
 protected:
     void initializeGL();

src/mainwindow.cpp
@@ -1040,7 +1040,7 @@ void MainWindow::dropEvent(QDropEvent *event)
         open(path);
         event->acceptProposedAction();
     }
-    else if (mimeData->hasFormat("application/mlt+xml")) {
+    else if (mimeData->hasFormat(Mlt::XmlMimeType )) {
         m_playlistDock->on_actionOpen_triggered();
         event->acceptProposedAction();
     }

src/mltcontroller.cpp
@@ -27,6 +27,8 @@
 
 namespace Mlt {
 
+const QString XmlMimeType("application/mlt+xml");
+
 QFrame::QFrame(QObject *parent)
     : QObject(parent)
     , m_frame(0)
@@ -415,11 +417,15 @@ bool Controller::isSeekable() const
             seekable = m_producer->get_int("force_seekable");
         } else {
             seekable = m_producer->get_int("seekable");
-            if (!seekable && m_producer->get("mlt_type"))
+            if (!seekable && m_producer->get("mlt_type")) {
+                // XXX what was this for?
                 seekable = !strcmp(m_producer->get("mlt_type"), "mlt_producer");
+            }
             if (!seekable) {
+                // These generators can take an out point to define their length.
+                // TODO: Currently, these max out at 15000 frames, which is arbitrary.
                 QString service(m_producer->get("mlt_service"));
-                seekable = service == "color" || service.startsWith("frei0r.");
+                seekable = (service == "color") || service.startsWith("frei0r.");
             }
         }
     }

commit 71738ad679d03a20999d0d02957a95996b3d39f5

Fix inserting into playlist.
Regression in commit 6491fed.

src/docks/playlistdock.cpp
@@ -167,7 +167,12 @@ void PlaylistDock::on_menuButton_clicked()
 
 void PlaylistDock::on_actionInsertCut_triggered()
 {
-    onDropped(0, ui->tableView->currentIndex().row());
+    if (MLT.isClip() || MLT.savedProducer()) {
+        QMimeData mimeData;
+        mimeData.setData(Mlt::XmlMimeType, MLT.saveXML("string",
+            MLT.isClip()? 0 : MLT.savedProducer()).toUtf8());
+        onDropped(&mimeData, ui->tableView->currentIndex().row());
+    }
 }
 
 void PlaylistDock::on_actionAppendCut_triggered()