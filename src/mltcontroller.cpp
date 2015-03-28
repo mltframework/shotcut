mltframework/shotcut

commit 377eb4f1463645942ed15f8220a2684a83d84e41

Fix crash when using GPU.

src/mltcontroller.cpp
@@ -204,16 +204,15 @@ void Controller::pause()
 {
     if (m_producer && m_producer->get_speed() != 0) {
         if (m_consumer && m_consumer->is_valid()) {
-            // Flush all frames from the video task in the consumer.
-            m_consumer->stop();
             // Disable real_time behavior and buffering for frame accurate seeking.
-            m_consumer->set("real_time", 0);
+            m_consumer->set("real_time", -1);
             m_consumer->set("buffer", 0);
             m_consumer->set("prefill", 0);
         }
         m_producer->set_speed(0);
         m_producer->seek(m_consumer->position() + 1);
         if (m_consumer && m_consumer->is_valid()) {
+            m_consumer->purge();
             m_consumer->start();
         }
     }
@@ -336,10 +335,8 @@ void Controller::seek(int position)
     if (m_producer) {
         // Always pause before seeking (if not already paused).
         if (m_consumer && m_consumer->is_valid() && m_producer->get_speed() != 0) {
-            // Flush all frames from the video task in the consumer.
-            m_consumer->stop();
             // Disable real_time behavior and buffering for frame accurate seeking.
-            m_consumer->set("real_time", 0);
+            m_consumer->set("real_time", -1);
             m_consumer->set("buffer", 0);
             m_consumer->set("prefill", 0);
         }

commit 13200758cbda3ed49a9b88894c56765ce211be0a

Fix performance regression in 377eb4f.

src/mltcontroller.cpp
@@ -189,9 +189,15 @@ void Controller::play(double speed)
         m_producer->set_speed(speed);
     if (m_consumer) {
         // Restore real_time behavior and work-ahead buffering
-        m_consumer->set("real_time", realTime());
-        m_consumer->set("buffer", 25);
-        m_consumer->set("prefill", 1);
+        if (!Settings.playerGPU())
+        if (m_consumer->get_int("real_time") != realTime()) {
+            m_consumer->set("real_time", realTime());
+            m_consumer->set("buffer", 25);
+            m_consumer->set("prefill", 1);
+            // Changes to real_time require a consumer restart if running.
+            if (!m_consumer->is_stopped())
+                m_consumer->stop();
+        }
         m_consumer->start();
         refreshConsumer();
     }
@@ -203,6 +209,7 @@ void Controller::play(double speed)
 void Controller::pause()
 {
     if (m_producer && m_producer->get_speed() != 0) {
+        if (!Settings.playerGPU())
         if (m_consumer && m_consumer->is_valid()) {
             // Disable real_time behavior and buffering for frame accurate seeking.
             m_consumer->set("real_time", -1);
@@ -334,6 +341,7 @@ void Controller::seek(int position)
 {
     if (m_producer) {
         // Always pause before seeking (if not already paused).
+        if (!Settings.playerGPU())
         if (m_consumer && m_consumer->is_valid() && m_producer->get_speed() != 0) {
             // Disable real_time behavior and buffering for frame accurate seeking.
             m_consumer->set("real_time", -1);