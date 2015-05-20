mltframework/shotcut

commit e0c15717cdac85673c615b845c689dc0e859e692

Move all global shortcut handling to MainWindow
In order to have global keyboard shortcuts always work, it is important to keep
strict control on where keyboard events go. This commit moves some timeline
specific keyboard handling from timeline.qml to MainWindow (where it was
already duplicated), and lets the keyevent instead bubble from the qml item all
the way up the parent chain to MainWindow.

The proposed policy is: _all_ global keyboard shorcuts are to be handled in
MainWindow::key{Press,Release}Event. Any widget that accepts keyboard focus is
responsible for event->ignore()ing unhandled keypresses.

src/mainwindow.cpp
@@ -1007,6 +1007,7 @@ void MainWindow::on_actionAbout_Shotcut_triggered()
 
 void MainWindow::keyPressEvent(QKeyEvent* event)
 {
+
     switch (event->key()) {
     case Qt::Key_Home:
         m_player->seek(0);
@@ -1182,6 +1183,9 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
             m_playlistDock->raise();
             m_playlistDock->setIndex(9);
         }
+        else if (m_timelineDock->isVisible()) {
+            m_timelineDock->zoomIn();
+        }
         break;
     case Qt::Key_X: // Avid Extract
         if (event->modifiers() == Qt::ShiftModifier) {
@@ -1219,6 +1223,14 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
             m_timelineDock->raise();
             m_timelineDock->lift(-1, -1);
         }
+        break;
+    case Qt::Key_Minus:
+        if (m_timelineDock->isVisible())
+            m_timelineDock->zoomOut();
+        break;
+    case Qt::Key_Equal:
+        if (m_timelineDock->isVisible())
+            m_timelineDock->zoomIn();
         break;
     case Qt::Key_Enter: // Seek to current playlist item
     case Qt::Key_Return:

src/docks/timelinedock.cpp
@@ -158,6 +158,21 @@ int TimelineDock::currentTrack() const
     return m_quickView.rootObject()->property("currentTrack").toInt();
 }
 
+void TimelineDock::zoomIn()
+{
+    QMetaObject::invokeMethod(m_quickView.rootObject(), "zoomIn");
+}
+
+void TimelineDock::zoomOut()
+{
+    QMetaObject::invokeMethod(m_quickView.rootObject(), "zoomOut");
+}
+
+void TimelineDock::resetZoom()
+{
+    QMetaObject::invokeMethod(m_quickView.rootObject(), "resetZoom");
+}
+
 void TimelineDock::addAudioTrack()
 {
     m_model.addAudioTrack();
@@ -233,18 +248,6 @@ void TimelineDock::lift(int trackIndex, int clipIndex)
         MAIN.undoStack()->push(
             new Timeline::LiftCommand(m_model, trackIndex, clipIndex, position, xml));
     }
-}
-
-void TimelineDock::pressKey(int key, Qt::KeyboardModifiers modifiers)
-{
-    QKeyEvent event(QEvent::KeyPress, key, modifiers);
-    MAIN.keyPressEvent(&event);
-}
-
-void TimelineDock::releaseKey(int key, Qt::KeyboardModifiers modifiers)
-{
-    QKeyEvent event(QEvent::KeyRelease, key, modifiers);
-    MAIN.keyReleaseEvent(&event);
 }
 
 void TimelineDock::selectTrack(int by)

src/docks/timelinedock.h
@@ -51,6 +51,9 @@ public:
     int dockYOffset() const;
     void setCurrentTrack(int currentTrack);
     int currentTrack() const;
+    void zoomIn();
+    void zoomOut();
+    void resetZoom();
 
 signals:
     void currentTrackChanged();
@@ -74,8 +77,6 @@ public slots:
     void append(int trackIndex);
     void remove(int trackIndex, int clipIndex);
     void lift(int trackIndex, int clipIndex);
-    void pressKey(int key, Qt::KeyboardModifiers modifiers);
-    void releaseKey(int key, Qt::KeyboardModifiers modifiers);
     void selectTrack(int by);
     void selectTrackHead(int trackIndex);
     void openClip(int trackIndex, int clipIndex);

src/qml/timeline/timeline.qml
@@ -27,6 +27,24 @@ Rectangle {
     SystemPalette { id: activePalette }
     color: activePalette.window
 
+    function zoomIn() {
+        scaleSlider.value += 0.0625
+        for (var i = 0; i < tracksRepeater.count; i++)
+            tracksRepeater.itemAt(i).redrawWaveforms()
+    }
+
+    function zoomOut() {
+        scaleSlider.value -= 0.0625
+        for (var i = 0; i < tracksRepeater.count; i++)
+            tracksRepeater.itemAt(i).redrawWaveforms()
+    }
+
+    function resetZoom() {
+        scaleSlider.value = 1.0
+        for (var i = 0; i < tracksRepeater.count; i++)
+            tracksRepeater.itemAt(i).redrawWaveforms()
+    }
+
     property int headerWidth: 140
     property int currentTrack: 0
     property int currentClip: -1
@@ -146,30 +164,28 @@ Rectangle {
             focus: true
             hoverEnabled: true
             property bool scim: false
-            onReleased: scrubTimer.stop()
-            onMouseXChanged: {
-                if (scim || pressedButtons === Qt.LeftButton) {
+            onReleased: scim = false
+            onExited: scim = false
+            onPositionChanged: {
+                if (mouse.modifiers === Qt.ShiftModifier || mouse.buttons === Qt.LeftButton) {
                     timeline.position = (scrollView.flickableItem.contentX + mouse.x) / multitrack.scaleFactor
-                    if ((scrollView.flickableItem.contentX > 0 && mouse.x < 50) || (mouse.x > scrollView.width - 50))
-                        scrubTimer.start()
+                    scim = true
                 }
+                else
+                    scim = false
             }
             Timer {
                 id: scrubTimer
                 interval: 25
                 repeat: true
+                running: parent.scim && parent.containsMouse && (parent.mouseX < 50 || parent.mouseX > parent.width - 50)
                 onTriggered: {
-                    if (parent.scim || parent.pressedButtons === Qt.LeftButton) {
-                        if (parent.mouseX < 50)
-                            timeline.position -= 10
-                        else if (parent.mouseX > scrollView.flickableItem.contentX - 50)
-                            timeline.position += 10
-                    }
-                    if (parent.mouseX >= 50 && parent.mouseX <= scrollView.width - 50)
-                        stop()
+                    if (parent.mouseX < 50)
+                        timeline.position -= 10
+                    else
+                        timeline.position += 10
                 }
             }
-        
 
             Column {
                 Flickable {
@@ -371,80 +387,6 @@ Rectangle {
                 timeline.close()
                 scaleSlider.value = 1
             }
-        }
-    }
-
-    Keys.onUpPressed: timeline.selectTrack(-1)
-    Keys.onDownPressed: timeline.selectTrack(1)
-    Keys.onPressed: {
-        tracksArea.scim = (event.modifiers === Qt.ShiftModifier)
-        switch (event.key) {
-        case Qt.Key_B:
-            timeline.overwrite(currentTrack)
-            break;
-        case Qt.Key_C:
-            timeline.append(currentTrack)
-            break;
-        case Qt.Key_S:
-            timeline.splitClip(currentTrack)
-            break;
-        case Qt.Key_V:
-            timeline.insert(currentTrack)
-            break;
-        case Qt.Key_X:
-            timeline.remove(currentClipTrack, currentClip)
-            currentClip = -1
-            break;
-        case Qt.Key_Z:
-            timeline.lift(currentClipTrack, currentClip)
-            currentClip = -1
-            break;
-        case Qt.Key_Delete:
-        case Qt.Key_Backspace:
-            if (event.modifiers & Qt.ShiftModifier)
-                timeline.remove(currentClipTrack, currentClip)
-            else
-                timeline.lift(currentClipTrack, currentClip)
-            currentClip = -1
-            break;
-        case Qt.Key_Equal:
-            scaleSlider.value += 0.0625
-            for (var i = 0; i < tracksRepeater.count; i++)
-                tracksRepeater.itemAt(i).redrawWaveforms()
-            break;
-        case Qt.Key_Minus:
-            scaleSlider.value -= 0.0625
-            for (var i = 0; i < tracksRepeater.count; i++)
-                tracksRepeater.itemAt(i).redrawWaveforms()
-            break;
-        case Qt.Key_0:
-            scaleSlider.value = 1.0
-            for (var i = 0; i < tracksRepeater.count; i++)
-                tracksRepeater.itemAt(i).redrawWaveforms()
-            break;
-        default:
-            timeline.pressKey(event.key, event.modifiers)
-            break;
-        }
-    }
-    Keys.onReleased: {
-        tracksArea.scim = false
-        switch (event.key) {
-        case Qt.Key_B:
-        case Qt.Key_C:
-        case Qt.Key_S:
-        case Qt.Key_V:
-        case Qt.Key_X:
-        case Qt.Key_Z:
-        case Qt.Key_Delete:
-        case Qt.Key_Backspace:
-        case Qt.Key_Equal:
-        case Qt.Key_Minus:
-        case Qt.Key_0:
-            break;
-        default:
-            timeline.releaseKey(event.key, event.modifiers)
-            break;
         }
     }
 

commit de81f05d7775328e249f09cacade23b44cb7b884

Fix reset timeline zoom shortcut regression in e0c15717.

src/mainwindow.cpp
@@ -1250,7 +1250,7 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
             m_playlistDock->setIndex(9);
         }
         else if (m_timelineDock->isVisible()) {
-            m_timelineDock->zoomIn();
+            m_timelineDock->resetZoom();
         }
         break;
     case Qt::Key_X: // Avid Extract