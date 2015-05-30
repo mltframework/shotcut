mltframework/shotcut

commit 245f1b5697708606e993124848961c196e7b6522

Initial proof-of-concept for adding scopes.

src/glwidget.cpp
@@ -51,10 +51,10 @@ GLWidget::GLWidget(QObject *parent)
     , Controller()
     , m_shader(0)
     , m_glslManager(0)
+    , m_initSem(0)
     , m_isInitialized(false)
     , m_threadStartEvent(0)
     , m_threadStopEvent(0)
-    , m_lastFrame(0)
     , m_threadCreateEvent(0)
     , m_threadJoinEvent(0)
     , m_frameRenderer(0)
@@ -95,7 +95,6 @@ GLWidget::~GLWidget()
     delete m_threadStopEvent;
     delete m_threadCreateEvent;
     delete m_threadJoinEvent;
-    delete m_lastFrame;
     if (m_frameRenderer && m_frameRenderer->isRunning()) {
         QMetaObject::invokeMethod(m_frameRenderer, "cleanup");
         m_frameRenderer->quit();
@@ -141,11 +140,11 @@ void GLWidget::initializeGL()
     m_frameRenderer = new FrameRenderer(openglContext());
     openglContext()->makeCurrent(openglContext()->surface());
 
-    connect(this, SIGNAL(frameReceived(Mlt::QFrame)), m_frameRenderer, SLOT(showFrame(Mlt::QFrame)), Qt::QueuedConnection);
+    connect(m_frameRenderer, SIGNAL(frameDisplayed(const SharedFrame&)), this, SIGNAL(frameDisplayed(const SharedFrame&)), Qt::QueuedConnection);
     connect(m_frameRenderer, SIGNAL(textureReady(GLuint,GLuint,GLuint)), SLOT(updateTexture(GLuint,GLuint,GLuint)), Qt::DirectConnection);
     connect(this, SIGNAL(textureUpdated()), SLOT(update()), Qt::QueuedConnection);
 
-    m_condition.wakeAll();
+    m_initSem.release();
     m_isInitialized = true;
     qDebug() << "end";
 }
@@ -376,10 +375,11 @@ void GLWidget::mouseMoveEvent(QMouseEvent* event)
     mimeData->setData(Mlt::XmlMimeType, MLT.XML().toUtf8());
     drag->setMimeData(mimeData);
     mimeData->setText(QString::number(MLT.producer()->get_playtime()));
-    m_mutex.lock();
-    if (m_lastFrame && !m_glslManager)
-        drag->setPixmap(QPixmap::fromImage(MLT.image(m_lastFrame, 45 * MLT.profile().dar(), 45)).scaledToHeight(45));
-    m_mutex.unlock();
+    if (m_frameRenderer && !m_glslManager) {
+        Mlt::Frame displayFrame(m_frameRenderer->getDisplayFrame().clone(false, true));
+        QImage displayImage = MLT.image(&displayFrame, 45 * MLT.profile().dar(), 45).scaledToHeight(45);
+        drag->setPixmap(QPixmap::fromImage(displayImage));
+    }
     drag->setHotSpot(QPoint(0, 0));
     drag->exec(Qt::LinkAction);
 }
@@ -399,9 +399,7 @@ void GLWidget::createThread(RenderThread **thread, thread_function_t function, v
         qApp->processEvents();
 #else
     if (!m_isInitialized) {
-        m_mutex.lock();
-        m_condition.wait(&m_mutex);
-        m_mutex.unlock();
+        m_initSem.acquire();
     }
 #endif
     (*thread) = new RenderThread(function, data, m_glslManager? openglContext() : 0);
@@ -466,11 +464,6 @@ int GLWidget::setProducer(Mlt::Producer* producer, bool isMulti)
 {
     int error = Controller::setProducer(producer, isMulti);
 
-    m_mutex.lock();
-    delete m_lastFrame;
-    m_lastFrame = 0;
-    m_mutex.unlock();
-
     if (!error) {
         error = reconfigure(isMulti);
         if (!error) {
@@ -484,11 +477,6 @@ int GLWidget::setProducer(Mlt::Producer* producer, bool isMulti)
 int GLWidget::reconfigure(bool isMulti)
 {
     int error = 0;
-
-    m_mutex.lock();
-    delete m_lastFrame;
-    m_lastFrame = 0;
-    m_mutex.unlock();
 
     // use SDL for audio, OpenGL for video
     QString serviceName = property("mlt_service").toString();
@@ -588,14 +576,6 @@ int GLWidget::reconfigure(bool isMulti)
     return error;
 }
 
-void GLWidget::setLastFrame(mlt_frame frame)
-{
-    m_mutex.lock();
-    delete m_lastFrame;
-    m_lastFrame = new Mlt::Frame(frame);
-    m_mutex.unlock();
-}
-
 QPoint GLWidget::offset() const
 {
     return QPoint(m_offset.x() - (MLT.profile().width()  * m_zoom -  width()) / 2,
@@ -646,10 +626,11 @@ void GLWidget::on_frame_show(mlt_consumer, void* self, mlt_frame frame_ptr)
 {
     GLWidget* widget = static_cast<GLWidget*>(self);
     int timeout = (widget->consumer()->get_int("real_time") > 0)? 0: 1000;
-    if (widget->frameRenderer() && widget->frameRenderer()->semaphore()->tryAcquire(1, timeout)) {
-        Frame frame(frame_ptr);
-        widget->setLastFrame(frame_ptr);
-        emit widget->frameReceived(Mlt::QFrame(frame));
+    if (widget->m_frameRenderer && widget->m_frameRenderer->semaphore()->tryAcquire(1, timeout)) {
+        Mlt::Frame frame(frame_ptr);
+        if (frame.get_int("rendered")) {
+            QMetaObject::invokeMethod(widget->m_frameRenderer, "showFrame", Qt::QueuedConnection, Q_ARG(Mlt::Frame, frame));
+        }
     }
 }
 
@@ -687,10 +668,10 @@ void RenderThread::run()
 FrameRenderer::FrameRenderer(QOpenGLContext* shareContext)
      : QThread(0)
      , m_semaphore(3)
+     , m_frame()
      , m_context(0)
      , m_surface(0)
      , m_gl32(0)
-     , m_frame(0)
 {
     Q_ASSERT(shareContext);
     m_renderTexture[0] = m_renderTexture[1] = m_renderTexture[2] = 0;
@@ -713,24 +694,23 @@ FrameRenderer::~FrameRenderer()
     qDebug();
     delete m_context;
     delete m_gl32;
-    delete m_frame;
 }
 
-void FrameRenderer::showFrame(Mlt::QFrame frame)
+void FrameRenderer::showFrame(Mlt::Frame frame)
 {
-    if (m_context->isValid() && frame.frame()->get_int("rendered")) {
+    if (m_context->isValid()) {
         int width = 0;
         int height = 0;
 
         m_context->makeCurrent(m_surface);
 
         if (Settings.playerGPU()) {
-            frame.frame()->set("movit.convert.use_texture", 1);
+            frame.set("movit.convert.use_texture", 1);
             mlt_image_format format = mlt_image_glsl_texture;
-            const GLuint* textureId = (GLuint*) frame.frame()->get_image(format, width, height);
+            const GLuint* textureId = (GLuint*) frame.get_image(format, width, height);
 
 #ifdef USE_GL_SYNC
-            GLsync sync = (GLsync) frame.frame()->get_data("movit.convert.fence");
+            GLsync sync = (GLsync) frame.get_data("movit.convert.fence");
             if (sync) {
 #ifdef Q_OS_WIN
                 // On Windows, use QOpenGLFunctions_3_2_Core instead of getProcAddress.
@@ -753,15 +733,11 @@ void FrameRenderer::showFrame(Mlt::QFrame frame)
 #else
             glFinish();
 #endif // USE_GL_FENCE
-
-            // Manage reference to mlt_frame holding reference to GL texture.
-            delete m_frame;
-            m_frame = new Frame(*frame.frame());
             emit textureReady(*textureId);
         }
         else {
             mlt_image_format format = mlt_image_yuv420p;
-            const uint8_t* image = frame.frame()->get_image(format, width, height);
+            const uint8_t* image = frame.get_image(format, width, height);
 
             // Upload each plane of YUV to a texture.
             if (m_renderTexture[0] && m_renderTexture[1] && m_renderTexture[2])
@@ -821,8 +797,20 @@ void FrameRenderer::showFrame(Mlt::QFrame frame)
             emit textureReady(m_displayTexture[0], m_displayTexture[1], m_displayTexture[2]);
         }
         m_context->doneCurrent();
+
+        // Save this frame for future use and to keep a reference to the GL Texture.
+        m_frame = SharedFrame(frame);
+
+        // The frame is now done being modified and can be shared with the rest
+        // of the application.
+        emit frameDisplayed(m_frame);
     }
     m_semaphore.release();
+}
+
+SharedFrame FrameRenderer::getDisplayFrame()
+{
+    return m_frame;
 }
 
 void FrameRenderer::cleanup()

src/controllers/scopecontroller.cpp
@@ -1,0 +1,52 @@
+/*
+ * Copyright (c) 2015 Meltytech, LLC
+ * Author: Brian Matherly <code@brianmatherly.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "scopecontroller.h"
+#include "widgets/scopes/audiowaveformscopewidget.h"
+#include "widgets/scopes/videowaveformscopewidget.h"
+#include "docks/scopedock.h"
+#include "settings.h"
+#include <QDebug>
+#include <QMainWindow>
+#include <QMenu>
+
+ScopeController::ScopeController(QMainWindow* mainWindow, QMenu* menu)
+  : QObject(mainWindow)
+{
+    qDebug() << "begin";
+    QMenu* scopeMenu = menu->addMenu(tr("Scopes"));
+    createScopeDock<AudioWaveformScopeWidget>(mainWindow, scopeMenu);
+    if (!Settings.playerGPU()) {
+        createScopeDock<VideoWaveformScopeWidget>(mainWindow, scopeMenu);
+    }
+    qDebug() << "end";
+}
+
+void ScopeController::onFrameDisplayed(const SharedFrame& frame)
+{
+    emit newFrame(frame);
+}
+
+template<typename ScopeTYPE> void ScopeController::createScopeDock(QMainWindow* mainWindow, QMenu* menu)
+{
+    ScopeWidget* scopeWidget = new ScopeTYPE();
+    ScopeDock* scopeDock = new ScopeDock(this, scopeWidget);
+    scopeDock->hide();
+    menu->addAction(scopeDock->toggleViewAction());
+    mainWindow->addDockWidget(Qt::RightDockWidgetArea, scopeDock);
+}
+

src/controllers/scopecontroller.h
@@ -1,0 +1,48 @@
+/*
+ * Copyright (c) 2015 Meltytech, LLC
+ * Author: Brian Matherly <code@brianmatherly.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SCOPECONTROLLER_H
+#define SCOPECONTROLLER_H
+
+#include <QObject>
+#include <QString>
+#include "sharedframe.h"
+
+class QMainWindow;
+class QMenu;
+class QWidget;
+
+class ScopeController Q_DECL_FINAL : public QObject
+{
+    Q_OBJECT
+
+public:
+    ScopeController(QMainWindow* mainWindow, QMenu* menu);
+
+public slots:
+    void onFrameDisplayed(const SharedFrame& frame);
+
+signals:
+    void newFrame(const SharedFrame& frame);
+
+private:
+    template<typename ScopeTYPE> void createScopeDock(QMainWindow* mainWindow, QMenu* menu);
+
+};
+
+#endif // SCOPECONTROLLER_H

src/dataqueue.h
@@ -1,0 +1,162 @@
+/*
+ * Copyright (c) 2015 Meltytech, LLC
+ * Author: Brian Matherly <code@brianmatherly.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DATAQUEUE_H
+#define DATAQUEUE_H
+
+#include <QList>
+#include <QMutex>
+#include <QWaitCondition>
+#include <QMutexLocker>
+
+/*!
+  \class DataQueue
+  \brief The DataQueue provides a thread save container for passing data between
+  objects.
+
+  \threadsafe
+
+  DataQueue provides a limited size container for passing data between objects.
+  One object can add data to the queue by calling push() while another object
+  can remove items from the queue by calling pop().
+
+  DataQueue provides configurable behavior for handling overflows. It can
+  discard the oldest, discard the newest or block the object calling push()
+  until room has been freed in the queue bu another object calling pop().
+
+  DataQueue is threadsave and is therefore most appropriate when passing data
+  between objects operating in different thread contexts.
+*/
+
+template <class T>
+class DataQueue
+{
+public:
+    //! Overflow behavior modes.
+    typedef enum {
+        OverflowModeDiscardOldest = 0, //!< Discard oldest items
+        OverflowModeDiscardNewest,     //!< Discard newest items
+        OverflowModeWait               //!< Wait for space to be free
+    } OverflowMode;
+
+    /*!
+      Constructs a DataQueue.
+
+      The \a size will be the maximum queue size and the \a mode will dictate
+      overflow behavior.
+    */
+    explicit DataQueue(int maxSize, OverflowMode mode);
+
+    //! Destructs a DataQueue.
+    virtual ~DataQueue();
+
+    /*!
+      Pushes an item into the queue.
+
+      If the queue is full and overflow mode is OverflowModeWait then this
+      function will block until pop() is called.
+    */
+    void push(const T& item);
+
+    /*!
+      Pops an item from the queue.
+
+      If the queue is empty then this  function will block. If blocking is
+      undesired, then check the return of count() before calling pop().
+    */
+    T pop();
+
+    //! Returns the number of items in the queue.
+    int count() const;
+
+private:
+    QList<T> m_queue;
+    int m_maxSize;
+    OverflowMode m_mode;
+    mutable QMutex m_mutex;
+    QWaitCondition m_notEmptyCondition;
+    QWaitCondition m_notFullCondition;
+};
+
+template <class T>
+DataQueue<T>::DataQueue(int maxSize, OverflowMode mode)
+  : m_queue()
+  , m_maxSize(maxSize)
+  , m_mode(mode)
+  , m_mutex(QMutex::NonRecursive)
+  , m_notEmptyCondition()
+  , m_notFullCondition()
+{
+}
+
+template <class T>
+DataQueue<T>::~DataQueue()
+{
+}
+
+template <class T>
+void DataQueue<T>::push(const T& item)
+{
+    m_mutex.lock();
+    if (m_queue.size() == m_maxSize) {
+        switch(m_mode) {
+            case OverflowModeDiscardOldest:
+                m_queue.removeFirst();
+                m_queue.append(item);
+                break;
+            case OverflowModeDiscardNewest:
+                // This item is the newest so discard it and exit
+                break;
+            case OverflowModeWait:
+                m_notFullCondition.wait(&m_mutex);
+                m_queue.append(item);
+                break;
+        }
+    } else {
+        m_queue.append(item);
+        if (m_queue.size() == 1) {
+            m_notEmptyCondition.wakeOne();
+        }
+    }
+    m_mutex.unlock();
+}
+
+template <class T>
+T DataQueue<T>::pop()
+{
+    T retVal;
+    m_mutex.lock();
+    if (m_queue.size() == 0) {
+        m_notEmptyCondition.wait(&m_mutex);
+    }
+    retVal = m_queue.takeFirst();
+    if (m_mode == OverflowModeWait && m_queue.size() == m_maxSize - 1) {
+        m_notFullCondition.wakeOne();
+    }
+    m_mutex.unlock();
+    return retVal;
+}
+
+template <class T>
+int DataQueue<T>::count() const
+{
+    QMutexLocker locker(&m_mutex);
+    return m_queue.size();
+}
+
+#endif // DATAQUEUE_H

src/docks/scopedock.cpp
@@ -1,0 +1,50 @@
+/*
+ * Copyright (c) 2015 Meltytech, LLC
+ * Author: Brian Matherly <code@brianmatherly.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "scopedock.h"
+#include "controllers/scopecontroller.h"
+#include <QDebug>
+#include <QtWidgets/QScrollArea>
+#include <QAction>
+
+ScopeDock::ScopeDock(ScopeController* scopeController, ScopeWidget* scopeWidget) :
+    QDockWidget()
+  , m_scopeController(scopeController)
+  , m_scopeWidget(scopeWidget)
+{
+    qDebug() << "begin";
+    setObjectName(m_scopeWidget->objectName() + "Dock");
+    QScrollArea *scrollArea = new QScrollArea();
+    scrollArea->setFrameShape(QFrame::NoFrame);
+    scrollArea->setWidgetResizable(true);
+    scrollArea->setWidget(m_scopeWidget);
+    QDockWidget::setWidget(scrollArea);
+    QDockWidget::setWindowTitle(m_scopeWidget->getTitle());
+
+    connect(toggleViewAction(), SIGNAL(toggled(bool)), this, SLOT(onActionToggled(bool)));
+    qDebug() << "end";
+}
+
+void ScopeDock::onActionToggled(bool checked)
+{
+    if(checked) {
+        connect(m_scopeController, SIGNAL(newFrame(const SharedFrame&)), m_scopeWidget, SLOT(onNewFrame(const SharedFrame&)));
+    } else {
+        disconnect(m_scopeController, SIGNAL(newFrame(const SharedFrame&)), m_scopeWidget, SLOT(onNewFrame(const SharedFrame&)));
+    }
+}

src/docks/scopedock.h
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2015 Meltytech, LLC
+ * Author: Brian Matherly <code@brianmatherly.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SCOPEDOCK_H
+#define SCOPEDOCK_H
+
+#include "widgets/scopes/scopewidget.h"
+#include <QDockWidget>
+#include <QObject>
+
+class ScopeController;
+
+class ScopeDock Q_DECL_FINAL : public QDockWidget
+{
+   Q_OBJECT
+
+public:
+   explicit ScopeDock(ScopeController* scopeController, ScopeWidget* scopeWidget);
+
+private:
+   ScopeController* m_scopeController;
+   ScopeWidget* m_scopeWidget;
+
+   void setWidget(QWidget * widget); // Private to disallow use
+
+private slots:
+   void onActionToggled(bool checked);
+};
+
+#endif // SCOPEDOCK_H

src/docks/timelinedock.cpp
@@ -57,7 +57,7 @@ TimelineDock::TimelineDock(QWidget *parent) :
     delete ui->scrollAreaWidgetContents;
     ui->scrollArea->setWidget(container);
 
-    connect(MLT.videoWidget(), SIGNAL(frameReceived(Mlt::QFrame)), this, SLOT(onShowFrame(Mlt::QFrame)));
+    connect(MLT.videoWidget(), SIGNAL(frameDisplayed(const SharedFrame&)), this, SLOT(onShowFrame(const SharedFrame&)));
 #ifdef Q_OS_WIN
     onVisibilityChanged(true);
 #else
@@ -159,10 +159,10 @@ void TimelineDock::close()
     hide();
 }
 
-void TimelineDock::onShowFrame(Mlt::QFrame frame)
+void TimelineDock::onShowFrame(const SharedFrame& frame)
 {
     if (MLT.isMultitrack()) {
-        m_position = frame.frame()->get_position();
+        m_position = frame.get_position();
         emit positionChanged();
     }
 }

src/docks/timelinedock.h
@@ -23,7 +23,7 @@
 #include <QQuickView>
 #include <QApplication>
 #include "models/multitrackmodel.h"
-#include "mltcontroller.h"
+#include "sharedframe.h"
 
 namespace Ui {
 class TimelineDock;
@@ -64,7 +64,7 @@ public slots:
     void addAudioTrack();
     void addVideoTrack();
     void close();
-    void onShowFrame(Mlt::QFrame frame);
+    void onShowFrame(const SharedFrame& frame);
     void onSeeked(int position);
     void append(int trackIndex);
     void remove(int trackIndex, int clipIndex);

commit cc5e9a47ffb775decfaa2cccb458f3650d234766

Fix leaking semaphore regression in 245f1b56.
This would cause the video to stop updating, but audio would still play.
And, of course, only when using realtime mode.

src/glwidget.cpp
@@ -616,6 +616,8 @@ void GLWidget::on_frame_show(mlt_consumer, void* self, mlt_frame frame_ptr)
         Mlt::Frame frame(frame_ptr);
         if (frame.get_int("rendered")) {
             QMetaObject::invokeMethod(widget->m_frameRenderer, "showFrame", Qt::QueuedConnection, Q_ARG(Mlt::Frame, frame));
+        } else {
+            widget->m_frameRenderer->semaphore()->release();
         }
     }
 }