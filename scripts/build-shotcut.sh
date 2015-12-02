mltframework/shotcut

commit db04dc0108224b1ffe143ab9990c576cdcecf074

Install QuickWidgets shared lib.

scripts/build-shotcut.sh
@@ -1532,7 +1532,7 @@ function configure_compile_install_subproject {
         cmd cp -a src/qml "$FINAL_INSTALL_DIR"/share/shotcut
         cmd install -p -c "$QTDIR"/translations/qt_*.qm "$FINAL_INSTALL_DIR"/share/shotcut/translations
         cmd install -p -c "$QTDIR"/translations/qtbase_*.qm "$FINAL_INSTALL_DIR"/share/shotcut/translations
-        cmd install -p -c "$QTDIR"/lib/libQt5{Concurrent,Core,Declarative,Gui,Multimedia,MultimediaQuick,MultimediaWidgets,Network,OpenGL,Positioning,PrintSupport,Qml,QmlParticles,Quick,Script,Sensors,Sql,Svg,V8,WebChannel,WebKit,WebKitWidgets,WebSockets,Widgets,Xml,XmlPatterns,X11Extras,DBus,XcbQpa}.so.5 "$FINAL_INSTALL_DIR"/lib
+        cmd install -p -c "$QTDIR"/lib/libQt5{Concurrent,Core,Declarative,Gui,Multimedia,MultimediaQuick,MultimediaWidgets,Network,OpenGL,Positioning,PrintSupport,Qml,QmlParticles,Quick,QuickWidgets,Script,Sensors,Sql,Svg,V8,WebChannel,WebKit,WebKitWidgets,WebSockets,Widgets,Xml,XmlPatterns,X11Extras,DBus,XcbQpa}.so.5 "$FINAL_INSTALL_DIR"/lib
         cmd install -p -c "$QTDIR"/lib/lib{icudata,icui18n,icuuc}.so* "$FINAL_INSTALL_DIR"/lib
         cmd install -d "$FINAL_INSTALL_DIR"/lib/qt5/sqldrivers
         cmd cp -a "$QTDIR"/plugins/{accessible,iconengines,imageformats,mediaservice,platforms,generic,platforminputcontexts,platformthemes,xcbglintegrations} "$FINAL_INSTALL_DIR"/lib/qt5
@@ -1837,7 +1837,7 @@ function deploy_win32
     cmd rm -rf share/doc share/man share/ffmpeg/examples share/aclocal share/glib-2.0 share/gtk-2.0 share/gtk-doc share/themes share/locale
   fi
   cmd mv COPYING COPYING.txt
-  cmd cp -p "$QTDIR"/bin/Qt5{Concurrent,Core,Declarative,Gui,Multimedia,MultimediaQuick_p,MultimediaWidgets,Network,OpenGL,Positioning,PrintSupport,Qml,QuickParticles,Quick,Script,Sensors,Sql,Svg,WebChannel,WebKit,WebKitWidgets,WebSockets,Widgets,Xml,XmlPatterns}.dll .
+  cmd cp -p "$QTDIR"/bin/Qt5{Concurrent,Core,Declarative,Gui,Multimedia,MultimediaQuick_p,MultimediaWidgets,Network,OpenGL,Positioning,PrintSupport,Qml,QuickParticles,Quick,QuickWidgets,Script,Sensors,Sql,Svg,WebChannel,WebKit,WebKitWidgets,WebSockets,Widgets,Xml,XmlPatterns}.dll .
   if [ "$TARGET_OS" = "Win32" ]; then
     cmd cp -p "$QTDIR"/bin/{icudt55,icuin55,icuuc55,libgcc_s_dw2-1,libstdc++-6,libwinpthread-1,libEGL,libGLESv2,opengl32sw,d3dcompiler_47}.dll .
   else
@@ -1993,10 +1993,9 @@ export MLT_DATA="\$INSTALL_DIR/share/mlt"
 export MLT_PROFILES_PATH="\$INSTALL_DIR/share/mlt/profiles"
 export FREI0R_PATH="\$INSTALL_DIR/lib/frei0r-1"
 export MLT_MOVIT_PATH="\$INSTALL_DIR/share/movit"
-cd "\$INSTALL_DIR"
-export QT_PLUGIN_PATH="lib/qt5"
-export QML2_IMPORT_PATH="lib/qml"
-bin/melt "\$@"
+export QT_PLUGIN_PATH="\$INSTALL_DIR/lib/qt5"
+export QML2_IMPORT_PATH="\$INSTALL_DIR/lib/qml"
+"\$INSTALL_DIR/bin/melt" "\$@"
 End-of-melt-wrapper
   if test 0 != $? ; then
     die "Unable to create wrapper script"
@@ -2017,10 +2016,9 @@ export MLT_DATA="\$INSTALL_DIR/share/mlt"
 export MLT_PROFILES_PATH="\$INSTALL_DIR/share/mlt/profiles"
 export FREI0R_PATH="\$INSTALL_DIR/lib/frei0r-1"
 export MLT_MOVIT_PATH="\$INSTALL_DIR/share/movit"
-cd "\$INSTALL_DIR"
-export QT_PLUGIN_PATH="lib/qt5"
-export QML2_IMPORT_PATH="lib/qml"
-bin/shotcut "\$@"
+export QT_PLUGIN_PATH="\$INSTALL_DIR/lib/qt5"
+export QML2_IMPORT_PATH="\$INSTALL_DIR/lib/qml"
+"\$INSTALL_DIR/bin/shotcut" "\$@"
 End-of-shotcut-wrapper
   if test 0 != $? ; then
     die "Unable to create wrapper script"

commit 04ae16fc01edb6fbdba8e35d20938c0cbecaccfd

Fix Qt not locating plugins regression in db04dc01.
This occurs when using extended characters in the path.

scripts/build-shotcut.sh
@@ -2016,9 +2016,10 @@ export MLT_DATA="\$INSTALL_DIR/share/mlt"
 export MLT_PROFILES_PATH="\$INSTALL_DIR/share/mlt/profiles"
 export FREI0R_PATH="\$INSTALL_DIR/lib/frei0r-1"
 export MLT_MOVIT_PATH="\$INSTALL_DIR/share/movit"
-export QT_PLUGIN_PATH="\$INSTALL_DIR/lib/qt5"
-export QML2_IMPORT_PATH="\$INSTALL_DIR/lib/qml"
-"\$INSTALL_DIR/bin/shotcut" "\$@"
+cd "\$INSTALL_DIR"
+export QT_PLUGIN_PATH="lib/qt5"
+export QML2_IMPORT_PATH="lib/qml"
+bin/shotcut "\$@"
 End-of-shotcut-wrapper
   if test 0 != $? ; then
     die "Unable to create wrapper script"