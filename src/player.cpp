08c13940042af516ae114766630a0d4c3f226747
Dan Dennedy <dan@dennedy.org>
2016-09-15 21:07:49 -0700

    Fix #309 external desktop monitor on Windows.

--- src/player.cpp
+++ src/player.cpp
@@ -112,10 +112,6 @@ Player::Player(QWidget *parent)
     m_horizontalScroll = new QScrollBar(Qt::Horizontal);
     glayout->addWidget(m_horizontalScroll, 1, 0);
     m_horizontalScroll->hide();
-#ifdef Q_OS_WIN
-    // Workaround video not showing on Windows with Qt 5.5 upgrade.
-    m_videoWidget->hide();
-#endif
 
     // Add the volume and signal level meter
     m_volumePopup = new QFrame(this, Qt::Popup);
@@ -436,10 +432,6 @@ void Player::seek(int position)
 
 void Player::onProducerOpened(bool play)
 {
-#ifdef Q_OS_WIN
-    // Workaround video not showing on Windows with Qt 5.5 upgrade.
-    m_videoWidget->show();
-#endif
     m_duration = MLT.producer()->get_length();
     m_isSeekable = MLT.isSeekable();
     MLT.producer()->set("ignore_points", 1);
