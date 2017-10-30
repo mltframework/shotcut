mltframework/shotcut

commit a9c2d0f42a6206971e66e7cef3ac1210e36d7906

Do not adjust Timeline zoom until slider is released.
Realtime zoom is causing too much sluggishness and high memory usage on
large projects.

src/qml/timeline/ZoomSlider.qml
@@ -18,6 +18,7 @@
 
 import QtQuick 2.2
 import QtQuick.Controls 1.0
+import 'Timeline.js' as Logic
 
 Rectangle {
     property alias value: slider.value
@@ -41,12 +42,11 @@ Rectangle {
         minimumValue: 0
         maximumValue: 3.0
         value: 1
-        onValueChanged: {
-            if (typeof multitrack.scaleFactor != 'undefined')
-                multitrack.scaleFactor = Math.pow(value, 3) + 0.01
-        }
         onPressedChanged: {
             if (!pressed) {
+                if (typeof multitrack.scaleFactor != 'undefined')
+                    multitrack.scaleFactor = Math.pow(value, 3) + 0.01
+                Logic.scrollIfNeeded()
                 for (var i = 0; i < tracksRepeater.count; i++)
                     tracksRepeater.itemAt(i).redrawWaveforms()
             }

src/qml/timeline/TimelineToolbar.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2016 Meltytech, LLC
+ * Copyright (c) 2013-2017 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -20,7 +20,6 @@ import QtQuick 2.2
 import QtQuick.Controls 1.0
 import QtQuick.Layouts 1.0
 import QtGraphicalEffects 1.0
-import 'Timeline.js' as Logic
 
 ToolBar {
     property alias ripple: rippleButton.checked
@@ -137,7 +136,6 @@ ToolBar {
         }
         ZoomSlider {
             id: scaleSlider
-            onValueChanged: Logic.scrollIfNeeded()
         }
         ToolButton {
             action: zoomInAction

commit f11ebaeed7be2501d706cb55784d2c3471dde2e7

Fix timeline zoom buttons/shortcuts broken in a9c2d0f.
Also, refactor how scrollIfNeeded() is invoked.

src/qml/timeline/ZoomSlider.qml
@@ -18,7 +18,6 @@
 
 import QtQuick 2.2
 import QtQuick.Controls 1.0
-import 'Timeline.js' as Logic
 
 Rectangle {
     property alias value: slider.value
@@ -42,11 +41,16 @@ Rectangle {
         minimumValue: 0
         maximumValue: 3.0
         value: 1
+        function setScaleFactor() {
+            multitrack.scaleFactor = Math.pow(value, 3) + 0.01
+        }
+        onValueChanged: {
+            if (!pressed && typeof multitrack.scaleFactor != 'undefined')
+                setScaleFactor()
+        }
         onPressedChanged: {
             if (!pressed) {
-                if (typeof multitrack.scaleFactor != 'undefined')
-                    multitrack.scaleFactor = Math.pow(value, 3) + 0.01
-                Logic.scrollIfNeeded()
+                setScaleFactor()
                 for (var i = 0; i < tracksRepeater.count; i++)
                     tracksRepeater.itemAt(i).redrawWaveforms()
             }

src/qml/timeline/timeline.qml
@@ -571,6 +571,7 @@ Rectangle {
     Connections {
         target: multitrack
         onLoaded: toolbar.scaleSlider.value = Math.pow(multitrack.scaleFactor - 0.01, 1.0 / 3.0)
+        onScaleFactorChanged: Logic.scrollIfNeeded()
     }
 
     // This provides continuous scrolling at the left/right edges.