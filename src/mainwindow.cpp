mltframework/shotcut

commit 78d3724b3fb743e1fa783cba9e3f82771e17be95

Fix #344 shortcut not working with undocked Timeline.

src/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2017 Meltytech, LLC
+ * Copyright (c) 2011-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -457,8 +457,6 @@ MainWindow::MainWindow()
     connect(leap, SIGNAL(jogLeftSecond()), SLOT(stepLeftOneSecond()));
 
     connect(&m_network, SIGNAL(finished(QNetworkReply*)), SLOT(onUpgradeCheckFinished(QNetworkReply*)));
-
-    m_timelineDock->setFocusPolicy(Qt::StrongFocus);
 
     LOG_DEBUG() << "end";
 }

src/docks/playlistdock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017 Meltytech, LLC
+ * Copyright (c) 2012-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -31,6 +31,7 @@
 #include <QPainter>
 #include <QDebug>
 #include <QHeaderView>
+#include <QKeyEvent>
 
 class TiledItemDelegate : public QStyledItemDelegate
 {
@@ -747,4 +748,18 @@ void PlaylistDock::on_detailsButton_clicked()
     ui->actionTiled->setChecked(false);
     ui->actionIcons->setChecked(false);
     updateViewModeFromActions();
+}
+
+void PlaylistDock::keyPressEvent(QKeyEvent* event)
+{
+    QDockWidget::keyPressEvent(event);
+    if (!event->isAccepted())
+        MAIN.keyPressEvent(event);
+}
+
+void PlaylistDock::keyReleaseEvent(QKeyEvent* event)
+{
+    QDockWidget::keyReleaseEvent(event);
+    if (!event->isAccepted())
+        MAIN.keyReleaseEvent(event);
 }

src/docks/playlistdock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017 Meltytech, LLC
+ * Copyright (c) 2012-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -113,6 +113,10 @@ private slots:
     void on_iconsButton_clicked();
 
     void on_detailsButton_clicked();
+
+protected:
+    void keyPressEvent(QKeyEvent* event);
+    void keyReleaseEvent(QKeyEvent* event);
 
 private:
     void setViewMode(PlaylistModel::ViewMode mode);

src/docks/timelinedock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2017 Meltytech, LLC
+ * Copyright (c) 2013-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -64,7 +64,7 @@ TimelineDock::TimelineDock(QWidget *parent) :
 
     connect(&m_model, SIGNAL(modified()), this, SLOT(clearSelectionIfInvalid()));
 
-    m_quickView.setFocusPolicy(Qt::StrongFocus);
+    m_quickView.setFocusPolicy(Qt::NoFocus);
     setWidget(&m_quickView);
 
     connect(this, SIGNAL(clipMoved(int,int,int,int)), SLOT(onClipMoved(int,int,int,int)), Qt::QueuedConnection);
@@ -969,6 +969,20 @@ bool TimelineDock::event(QEvent *event)
     if (event->type() == QEvent::PaletteChange || event->type() == QEvent::StyleChange)
         load(true);
     return result;
+}
+
+void TimelineDock::keyPressEvent(QKeyEvent* event)
+{
+    QDockWidget::keyPressEvent(event);
+    if (!event->isAccepted())
+        MAIN.keyPressEvent(event);
+}
+
+void TimelineDock::keyReleaseEvent(QKeyEvent* event)
+{
+    QDockWidget::keyReleaseEvent(event);
+    if (!event->isAccepted())
+        MAIN.keyReleaseEvent(event);
 }
 
 void TimelineDock::load(bool force)

src/docks/timelinedock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2017 Meltytech, LLC
+ * Copyright (c) 2013-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -144,6 +144,8 @@ protected:
     void dragLeaveEvent(QDragLeaveEvent* event);
     void dropEvent(QDropEvent* event);
     bool event(QEvent *event);
+    void keyPressEvent(QKeyEvent* event);
+    void keyReleaseEvent(QKeyEvent* event);
 
 private:
     bool isBlank(int trackIndex, int clipIndex);

commit 7a9b61e2511583e040f1909d7b7c646bee536e65

Fix multiply handled KeyEvent regression in 78d3724.
https://forum.shotcut.org/t/shotcut-18-03-02-video-and-audio-track-bug/
4475

src/mainwindow.cpp
@@ -1424,7 +1424,6 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
 {
     bool handled = true;
 
-
     switch (event->key()) {
     case Qt::Key_Home:
         m_player->seek(0);
@@ -1773,19 +1772,24 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
         LOG_DEBUG() << "Current focusWindow:" << QApplication::focusWindow();
         break;
     default:
+        handled = false;
         break;
     }
 
-    if (!handled)
+    if (handled)
+        event->setAccepted(handled);
+    else
         QMainWindow::keyPressEvent(event);
 }
 
 void MainWindow::keyReleaseEvent(QKeyEvent* event)
 {
-    if (event->key() == Qt::Key_K)
+    if (event->key() == Qt::Key_K) {
         m_isKKeyPressed = false;
-    else
+        event->setAccepted(true);
+    } else {
         QMainWindow::keyReleaseEvent(event);
+    }
 }
 
 void MainWindow::hideSetDataDirectory()