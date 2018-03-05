mltframework/shotcut

commit 78d3724b3fb743e1fa783cba9e3f82771e17be95

Fix #344 shortcut not working with undocked Timeline.

src/docks/timelinedock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2017 Meltytech, LLC
+ * Copyright (c) 2013-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -64,7 +64,7 @@ TimelineDock::TimelineDock(QWidget *parent) :
 
     connect(&m_model, SIGNAL(modified()), this, SLOT(clearSelectionIfInvalid()));
 
-    m_quickView.setFocusPolicy(Qt::StrongFocus);
+    m_quickView.setFocusPolicy(Qt::NoFocus);
     setWidget(&m_quickView);
 
     connect(this, SIGNAL(clipMoved(int,int,int,int)), SLOT(onClipMoved(int,int,int,int)), Qt::QueuedConnection);
@@ -969,6 +969,20 @@ bool TimelineDock::event(QEvent *event)
     if (event->type() == QEvent::PaletteChange || event->type() == QEvent::StyleChange)
         load(true);
     return result;
+}
+
+void TimelineDock::keyPressEvent(QKeyEvent* event)
+{
+    QDockWidget::keyPressEvent(event);
+    if (!event->isAccepted())
+        MAIN.keyPressEvent(event);
+}
+
+void TimelineDock::keyReleaseEvent(QKeyEvent* event)
+{
+    QDockWidget::keyReleaseEvent(event);
+    if (!event->isAccepted())
+        MAIN.keyReleaseEvent(event);
 }
 
 void TimelineDock::load(bool force)

src/docks/timelinedock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2017 Meltytech, LLC
+ * Copyright (c) 2013-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -144,6 +144,8 @@ protected:
     void dragLeaveEvent(QDragLeaveEvent* event);
     void dropEvent(QDropEvent* event);
     bool event(QEvent *event);
+    void keyPressEvent(QKeyEvent* event);
+    void keyReleaseEvent(QKeyEvent* event);
 
 private:
     bool isBlank(int trackIndex, int clipIndex);

src/docks/playlistdock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017 Meltytech, LLC
+ * Copyright (c) 2012-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -31,6 +31,7 @@
 #include <QPainter>
 #include <QDebug>
 #include <QHeaderView>
+#include <QKeyEvent>
 
 class TiledItemDelegate : public QStyledItemDelegate
 {
@@ -747,4 +748,18 @@ void PlaylistDock::on_detailsButton_clicked()
     ui->actionTiled->setChecked(false);
     ui->actionIcons->setChecked(false);
     updateViewModeFromActions();
+}
+
+void PlaylistDock::keyPressEvent(QKeyEvent* event)
+{
+    QDockWidget::keyPressEvent(event);
+    if (!event->isAccepted())
+        MAIN.keyPressEvent(event);
+}
+
+void PlaylistDock::keyReleaseEvent(QKeyEvent* event)
+{
+    QDockWidget::keyReleaseEvent(event);
+    if (!event->isAccepted())
+        MAIN.keyReleaseEvent(event);
 }

src/docks/playlistdock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017 Meltytech, LLC
+ * Copyright (c) 2012-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -113,6 +113,10 @@ private slots:
     void on_iconsButton_clicked();
 
     void on_detailsButton_clicked();
+
+protected:
+    void keyPressEvent(QKeyEvent* event);
+    void keyReleaseEvent(QKeyEvent* event);
 
 private:
     void setViewMode(PlaylistModel::ViewMode mode);

src/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2017 Meltytech, LLC
+ * Copyright (c) 2011-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -457,8 +457,6 @@ MainWindow::MainWindow()
     connect(leap, SIGNAL(jogLeftSecond()), SLOT(stepLeftOneSecond()));
 
     connect(&m_network, SIGNAL(finished(QNetworkReply*)), SLOT(onUpgradeCheckFinished(QNetworkReply*)));
-
-    m_timelineDock->setFocusPolicy(Qt::StrongFocus);
 
     LOG_DEBUG() << "end";
 }

commit 9350252285cac3b98d41dd98a9c3a57dd8af5c17

Fix track name not editable regression in 78d3724.
This is an unfortunate workaround that prevents being able to edit the
track name when the Timeline dock is floating. I could not come up with
anything better using sendEvent in the KeyPress handler or an event
filter on the QQuickWidget. Things might be different with a newer
version of Qt.

src/docks/timelinedock.cpp
@@ -64,16 +64,12 @@ TimelineDock::TimelineDock(QWidget *parent) :
 
     connect(&m_model, SIGNAL(modified()), this, SLOT(clearSelectionIfInvalid()));
 
-    m_quickView.setFocusPolicy(Qt::NoFocus);
     setWidget(&m_quickView);
 
     connect(this, SIGNAL(clipMoved(int,int,int,int)), SLOT(onClipMoved(int,int,int,int)), Qt::QueuedConnection);
     connect(MLT.videoWidget(), SIGNAL(frameDisplayed(const SharedFrame&)), this, SLOT(onShowFrame(const SharedFrame&)));
-#ifdef Q_OS_WIN
-    onVisibilityChanged(true);
-#else
-    connect(this, &QDockWidget::visibilityChanged, this, &TimelineDock::load);
-#endif
+    connect(this, SIGNAL(visibilityChanged(bool)), this, SLOT(load(bool)));
+    connect(this, SIGNAL(topLevelChanged(bool)), this, SLOT(onTopLevelChanged(bool)));
     LOG_DEBUG() << "end";
 }
 
@@ -990,6 +986,7 @@ void TimelineDock::load(bool force)
     if (m_quickView.source().isEmpty() || force) {
         QDir sourcePath = QmlUtilities::qmlDir();
         sourcePath.cd("timeline");
+        m_quickView.setFocusPolicy(isFloating()? Qt::NoFocus : Qt::StrongFocus);
         m_quickView.setSource(QUrl::fromLocalFile(sourcePath.filePath("timeline.qml")));
         disconnect(this, SIGNAL(visibilityChanged(bool)), this, SLOT(onVisibilityChanged(bool)));
         connect(m_quickView.rootObject(), SIGNAL(currentTrackChanged()),
@@ -1003,8 +1000,7 @@ void TimelineDock::load(bool force)
     }
 }
 
-void TimelineDock::onVisibilityChanged(bool visible)
+void TimelineDock::onTopLevelChanged(bool floating)
 {
-    if (visible)
-        load();
+    m_quickView.setFocusPolicy(floating? Qt::NoFocus : Qt::StrongFocus);
 }

src/docks/timelinedock.h
@@ -150,7 +150,6 @@ protected:
 private:
     bool isBlank(int trackIndex, int clipIndex);
     void pulseLockButtonOnTrack(int trackIndex);
-    void load(bool force = false);
 
     Ui::TimelineDock *ui;
     QQuickWidget m_quickView;
@@ -170,7 +169,8 @@ private:
     int m_trimDelta;
 
 private slots:
-    void onVisibilityChanged(bool visible);
+    void load(bool force = false);
+    void onTopLevelChanged(bool floating);
 };
 
 #endif // TIMELINEDOCK_H

src/qml/timeline/TrackHead.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2017 Meltytech, LLC
+ * Copyright (c) 2013-2018 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -100,15 +100,15 @@ Rectangle {
             color: 'transparent'
             width: trackHeadRoot.width - trackHeadColumn.anchors.margins * 2
             radius: 2
-            border.color: trackNameMouseArea.containsMouse? activePalette.shadow : 'transparent'
+            border.color: (!timeline.floating && trackNameMouseArea.containsMouse)? activePalette.shadow : 'transparent'
             height: nameEdit.height
             MouseArea {
                 id: trackNameMouseArea
                 height: parent.height
                 width: nameEdit.width
                 hoverEnabled: true
-                onClicked: {
-                    nameEdit.visible = true
+                onClicked: if (!timeline.floating) {
+                    nameEdit.focus = true
                     nameEdit.selectAll()
                 }
             }
@@ -122,14 +122,13 @@ Rectangle {
             }
             TextField {
                 id: nameEdit
-                visible: false
+                visible: focus
                 width: trackHeadRoot.width - trackHeadColumn.anchors.margins * 2
                 text: trackName
                 onAccepted: {
                     timeline.setTrackName(index, text)
-                    visible = false
+                    focus = false
                 }
-                onFocusChanged: visible = focus
             }
         }
         RowLayout {