mltframework/shotcut

commit ccb4a08046cfb9e4afa0578258e5ad0fcf756ba2

qmake: install translations

translations/translations.pro
@@ -1,0 +1,97 @@
+# copied from https://raw.githubusercontent.com/bjorn/tiled/master/translations/translations.pro
+
+# This file is largely based on the way translations are compiled in
+# translations.pro from Qt Creator.
+#
+# Translation howto:
+# - Translations are mentioned in the LANGUAGES variable below
+# - To update the .ts files, cd into translations and run 'make ts'
+# - The .qm files are created as part of a regular make command
+#
+
+# The list of supported translations
+LANGUAGES = \
+    ca \
+    cs \
+    da \
+    de \
+    el \
+    en \
+    es \
+    et \
+    fi \
+    fr \
+    gd \
+    hu \
+    it \
+    ja \
+    nb \
+    ne \
+    nl \
+    oc \
+    pl \
+    pt_BR \
+    pt_PT \
+    ru \
+    sk \
+    sl \
+    tr \
+    uk \
+    zh_CN \
+    zh_TW
+
+# Disabled languages because they're too outdated
+#    lv \
+
+# Helper function to adapt the slashes in a path to the system
+defineReplace(fixSlashes) {
+    win32:1 ~= s|/|\\|g
+    return($$1)
+}
+
+# Helper function prepending and appending text to all values
+# Usage: var, prepend, append
+defineReplace(prependAppend) {
+    for(a,$$1):result += $$2$${a}$$3
+    return($$result)
+}
+
+# Large hack to make sure this pro file does not try to compile an application
+TEMPLATE = app
+TARGET = phony_target
+CONFIG -= qt sdk separate_debug_info gdb_dwarf_index
+QT =
+LIBS =
+QMAKE_LINK = @: IGNORE THIS LINE
+OBJECTS_DIR =
+win32:CONFIG -= embed_manifest_exe
+
+TRANSLATIONS = $$prependAppend(LANGUAGES, $$PWD/shotcut_, .ts)
+LUPDATE = $$fixSlashes($$[QT_INSTALL_BINS]/lupdate) -locations relative -no-obsolete
+LRELEASE = $$QMAKE_LRELEASE
+isEmpty(LRELEASE):LRELEASE = $$fixSlashes($$[QT_INSTALL_BINS]/lrelease)
+
+ts.commands = cd $$PWD/.. && $$LUPDATE src -ts $$TRANSLATIONS
+QMAKE_EXTRA_TARGETS += ts
+
+win32 {
+    TARGET_DIR = .
+} else:macx {
+    TARGET_DIR = ../bin/shotcut.app/Contents/Translations
+} else {
+    TARGET_DIR = ../share/shotcut/translations
+}
+
+updateqm.input = TRANSLATIONS
+updateqm.output = $$OUT_PWD/$$TARGET_DIR/${QMAKE_FILE_BASE}.qm
+isEmpty(vcproj):updateqm.variable_out = PRE_TARGETDEPS
+updateqm.commands = $$LRELEASE ${QMAKE_FILE_IN} -qm ${QMAKE_FILE_OUT}
+updateqm.name = LRELEASE ${QMAKE_FILE_IN}
+updateqm.CONFIG += no_link
+QMAKE_EXTRA_COMPILERS += updateqm
+
+# Install rule for translations
+qmfiles.files = $$prependAppend(LANGUAGES, $$OUT_PWD/$$TARGET_DIR/shotcut_, .qm)
+qmfiles.path = $${PREFIX}/share/shotcut/translations
+qmfiles.CONFIG += no_check_exist
+INSTALLS += qmfiles

shotcut.pro
@@ -1,4 +1,4 @@
 TEMPLATE = subdirs
-SUBDIRS = CuteLogger mvcp src
+SUBDIRS = CuteLogger mvcp src translations
 cache()
 src.depends = CuteLogger mvcp

src/src.pro
@@ -295,37 +295,6 @@ OTHER_FILES += \
     ../packaging/linux/org.shotcut.Shotcut.desktop \
     ../packaging/linux/org.shotcut.Shotcut.xml
 
-TRANSLATIONS += \
-    ../translations/shotcut_ca.ts \
-    ../translations/shotcut_cs.ts \
-    ../translations/shotcut_da.ts \
-    ../translations/shotcut_de.ts \
-    ../translations/shotcut_el.ts \
-    ../translations/shotcut_en.ts \
-    ../translations/shotcut_es.ts \
-    ../translations/shotcut_et.ts \
-    ../translations/shotcut_fi.ts \
-    ../translations/shotcut_fr.ts \
-    ../translations/shotcut_gd.ts \
-    ../translations/shotcut_hu.ts \
-    ../translations/shotcut_it.ts \
-    ../translations/shotcut_ja.ts \
-    ../translations/shotcut_nb.ts \
-    ../translations/shotcut_ne.ts \
-    ../translations/shotcut_nl.ts \
-    ../translations/shotcut_oc.ts \
-    ../translations/shotcut_pl.ts \
-    ../translations/shotcut_pt_BR.ts \
-    ../translations/shotcut_pt_PT.ts \
-    ../translations/shotcut_ru.ts \
-    ../translations/shotcut_sk.ts \
-    ../translations/shotcut_sl.ts \
-    ../translations/shotcut_tr.ts \
-    ../translations/shotcut_uk.ts \
-    ../translations/shotcut_zh_CN.ts \
-    ../translations/shotcut_zh_TW.ts
-
-
 INCLUDEPATH = ../CuteLogger/include ../mvcp
 
 debug_and_release {

commit 73ca1f9654268681723ed4a821337b936b1218dd

Fix cross-compile Windows on Linux broken by ccb4a080.
It is not necessary to convert the slashes when running mingw32-make as
used in Qt Creator, which is all I care about now to build on Windows
directly.

translations/translations.pro
@@ -40,15 +40,6 @@ LANGUAGES = \
     zh_CN \
     zh_TW
 
-# Disabled languages because they're too outdated
-#    lv \
-
-# Helper function to adapt the slashes in a path to the system
-defineReplace(fixSlashes) {
-    win32:1 ~= s|/|\\|g
-    return($$1)
-}
-
 # Helper function prepending and appending text to all values
 # Usage: var, prepend, append
 defineReplace(prependAppend) {
@@ -67,9 +58,9 @@ OBJECTS_DIR =
 win32:CONFIG -= embed_manifest_exe
 
 TRANSLATIONS = $$prependAppend(LANGUAGES, $$PWD/shotcut_, .ts)
-LUPDATE = $$fixSlashes($$[QT_INSTALL_BINS]/lupdate) -locations relative -no-obsolete
+LUPDATE = $$[QT_INSTALL_BINS]/lupdate -locations relative -no-obsolete
 LRELEASE = $$QMAKE_LRELEASE
-isEmpty(LRELEASE):LRELEASE = $$fixSlashes($$[QT_INSTALL_BINS]/lrelease)
+isEmpty(LRELEASE):LRELEASE = $$[QT_INSTALL_BINS]/lrelease
 
 ts.commands = cd $$PWD/.. && $$LUPDATE src -ts $$TRANSLATIONS
 QMAKE_EXTRA_TARGETS += ts