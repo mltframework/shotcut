mltframework/shotcut

commit 29fad9b192177007b35243109b864e06efd02115

Fix in/out points when changing profile.
The in/out points would get changed by the ratio of the old frame
rate to the new frame rate. For example, a 60 second video would
have an out point of 30 seconds when switching from 30fps to 60fps.

src/mainwindow.cpp
@@ -2953,8 +2953,10 @@ void MainWindow::onKeyerTriggered(QAction *action)
 void MainWindow::onProfileTriggered(QAction *action)
 {
     Settings.setPlayerProfile(action->data().toString());
+    // Save the XML to get correct in/out points before profile is changed.
+    QString xml = MLT.XML();
     setProfile(action->data().toString());
-    MLT.restart();
+    MLT.restart(xml);
 }
 
 void MainWindow::onProfileChanged()

src/mltcontroller.cpp
@@ -735,15 +735,17 @@ void Controller::setOut(int out)
     }
 }
 
-void Controller::restart()
+void Controller::restart(const QString& xml)
 {
     if (!m_consumer || !m_consumer->is_valid() || !m_producer || !m_producer->is_valid())
         return;
     const char* position = m_consumer->frames_to_time(m_consumer->position());
     double speed = m_producer->get_speed();
-    QString xml = XML();
+    QString loadXml = xml;
+    if (loadXml.isEmpty())
+        loadXml = XML();
     stop();
-    if (!setProducer(new Mlt::Producer(profile(), "xml-string", xml.toUtf8().constData()))) {
+    if (!setProducer(new Mlt::Producer(profile(), "xml-string", loadXml.toUtf8().constData()))) {
 #ifdef Q_OS_WIN
         play(speed);
         if (m_producer && m_producer->is_valid())

src/mltcontroller.h
@@ -96,7 +96,7 @@ public:
     void next(int currentPosition);
     void setIn(int);
     void setOut(int);
-    void restart();
+    void restart(const QString& xml = "");
     void resetURL();
     QImage image(Frame *frame, int width, int height);
     QImage image(Mlt::Producer& producer, int frameNumber, int width, int height);

commit 3b654bfe7aa25f78c274feb6aed13f5009c6d237

Fix crash changing Video Mode with nothing loaded.
Regression in 29fad9b.

src/mainwindow.cpp
@@ -2964,10 +2964,12 @@ void MainWindow::onKeyerTriggered(QAction *action)
 void MainWindow::onProfileTriggered(QAction *action)
 {
     Settings.setPlayerProfile(action->data().toString());
-    // Save the XML to get correct in/out points before profile is changed.
-    QString xml = MLT.XML();
-    setProfile(action->data().toString());
-    MLT.restart(xml);
+    if (MLT.producer() && MLT.producer()->is_valid()) {
+        // Save the XML to get correct in/out points before profile is changed.
+        QString xml = MLT.XML();
+        setProfile(action->data().toString());
+        MLT.restart(xml);
+    }
 }
 
 void MainWindow::onProfileChanged()