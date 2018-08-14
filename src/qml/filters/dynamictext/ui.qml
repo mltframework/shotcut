mltframework/shotcut

commit 5d0ba623cbccf5cdc19ffdac74323fbdcc080a02

Fix text may not appear after adding filter.

src/qml/filters/dynamictext/ui.qml
@@ -139,7 +139,7 @@ Item {
         filter.blockSignals = false
         setControls()
         setKeyframedControls()
-        filter.changed()
+        filter.set(rectProperty, filter.getRect(rectProperty))
     }
 
     function getPosition() {

commit 02ae84c6417499103812fbe9ce08dbd478e01e23

Fix loading existing Text filter with animation.
Regression in 5d0ba62.

src/qml/filters/dynamictext/ui.qml
@@ -139,7 +139,10 @@ Item {
         filter.blockSignals = false
         setControls()
         setKeyframedControls()
-        filter.set(rectProperty, filter.getRect(rectProperty))
+        if (filter.isNew)
+            filter.set(rectProperty, filter.getRect(rectProperty))
+        else
+            filter.changed()
     }
 
     function getPosition() {