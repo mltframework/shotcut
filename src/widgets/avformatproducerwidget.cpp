mltframework/shotcut

commit a39b0de26acdd85deeebbcb73f6b26f8cd4012e4

Fix making project modified when adding comments or changing sync.

src/widgets/avformatproducerwidget.cpp
@@ -562,8 +562,11 @@ void AvformatProducerWidget::on_speedSpinBox_editingFinished()
 
 void AvformatProducerWidget::on_syncSlider_valueChanged(int value)
 {
-    if (m_producer)
-        m_producer->set("video_delay", double(value) / 1000);
+    double delay = double(value) / 1000.0;
+    if (m_producer && m_producer->get_double("video_delay") != delay) {
+        m_producer->set("video_delay", delay);
+        emit modified();
+    }
 }
 
 void AvformatProducerWidget::on_actionOpenFolder_triggered()
@@ -591,7 +594,11 @@ void AvformatProducerWidget::on_actionCopyFullFilePath_triggered()
 
 void AvformatProducerWidget::on_notesTextEdit_textChanged()
 {
-    m_producer->set(kCommentProperty, ui->notesTextEdit->toPlainText().toUtf8().constData());
+    const char* text = ui->notesTextEdit->toPlainText().toUtf8().constData();
+    if (qstrcmp(m_producer->get(kCommentProperty), text)) {
+        m_producer->set(kCommentProperty, text);
+        emit modified();
+    }
 }
 
 void AvformatProducerWidget::on_actionFFmpegInfo_triggered()

src/mainwindow.cpp
@@ -2334,6 +2334,14 @@ void MainWindow::onCutModified()
         m_playlistDock->setUpdateButtonEnabled(true);
 }
 
+void MainWindow::onProducerModified()
+{
+    setWindowModified(true);
+    updateAutoSave();
+    if (playlist())
+        m_playlistDock->setUpdateButtonEnabled(true);
+}
+
 void MainWindow::onFilterModelChanged()
 {
     MLT.refreshConsumer();
@@ -2533,6 +2541,9 @@ QWidget *MainWindow::loadProducerWidget(Mlt::Producer* producer)
             connect(w, SIGNAL(producerChanged(Mlt::Producer*)), m_filterController, SLOT(setProducer(Mlt::Producer*)));
             if (producer->get(kMultitrackItemProperty))
                 connect(w, SIGNAL(producerChanged(Mlt::Producer*)), m_timelineDock, SLOT(onProducerChanged(Mlt::Producer*)));
+        }
+        if (-1 != w->metaObject()->indexOfSignal("modified()")) {
+            connect(w, SIGNAL(modified()), SLOT(onProducerModified()));
         }
         scrollArea->setWidget(w);
         onProducerChanged();

src/mainwindow.h
@@ -207,6 +207,7 @@ private slots:
     void onMultitrackModified();
     void onMultitrackDurationChanged();
     void onCutModified();
+    void onProducerModified();
     void onFilterModelChanged();
     void updateMarkers();
     void updateThumbnails();

src/widgets/avformatproducerwidget.h
@@ -43,6 +43,7 @@ public:
 signals:
     void producerChanged(Mlt::Producer*);
     void producerReopened();
+    void modified();
 
 protected:
     void keyPressEvent(QKeyEvent *event);

commit 7ce1ab153373362f3975e5cb58d2c1603c50d9f9

Fix selecting clip with speed change corrupts XML.
This is a regression in a39b0de because changing speed removes the
shotcut:comment property (another bug). Then, selecting the clip thinks
the text has changed (NULL to empty) and then sets the property with a
bad QString data pointer.

src/widgets/avformatproducerwidget.cpp
@@ -593,9 +593,9 @@ void AvformatProducerWidget::on_actionCopyFullFilePath_triggered()
 
 void AvformatProducerWidget::on_notesTextEdit_textChanged()
 {
-    const char* text = ui->notesTextEdit->toPlainText().toUtf8().constData();
-    if (qstrcmp(m_producer->get(kCommentProperty), text)) {
-        m_producer->set(kCommentProperty, text);
+    QString existing = QString::fromUtf8(m_producer->get(kCommentProperty));
+    if (ui->notesTextEdit->toPlainText() != existing) {
+        m_producer->set(kCommentProperty, ui->notesTextEdit->toPlainText().toUtf8().constData());
         emit modified();
     }
 }