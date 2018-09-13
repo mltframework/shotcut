mltframework/shotcut

commit 8d5dca3f7c50bc2dd7f5d89cc385a1b3fd2b17e7

Fix Export may change frame rate and cause timing errors.
https://forum.shotcut.org/t/problem-with-crop-filter/7114

src/docks/encodedock.cpp
@@ -757,24 +757,30 @@ Mlt::Properties* EncodeDock::collectProperties(int realtime)
                 setIfNotSet(p, "rescale", "hyper");
                 break;
             }
-            if (qFloor(ui->fpsSpinner->value() * 10.0) == 239) {
-                setIfNotSet(p, "frame_rate_num", 24000);
-                setIfNotSet(p, "frame_rate_den", 1001);
+            // If the frame rate is not specified in Other.
+            if (!p->get("r") && !(p->get("frame_rate_num") && p->get("frame_rate_den"))
+                // Only if the frame rate spinner does not match the profile.
+                && qRound(ui->fpsSpinner->value() * 1000000.0) != qRound(MLT.profile().fps() * 1000000.0))
+            {
+                // Convert some common non-integer frame rates to fractions.
+                if (qRound(ui->fpsSpinner->value() * 1000000.0) == 23976024) {
+                    p->set("frame_rate_num", 24000);
+                    p->set("frame_rate_den", 1001);
+                } else if (qFloor(ui->fpsSpinner->value() * 1000000.0) == 299700299) {
+                    p->set("frame_rate_num", 30000);
+                    p->set("frame_rate_den", 1001);
+                } else if (qRound(ui->fpsSpinner->value() * 1000000.0) == 47952048) {
+                    p->set("frame_rate_num", 48000);
+                    p->set("frame_rate_den", 1001);
+                } else if (qFloor(ui->fpsSpinner->value() * 1000000.0) == 599400599) {
+                    p->set("frame_rate_num", 60000);
+                    p->set("frame_rate_den", 1001);
+                } else {
+                    // Workaround storing QDoubleSpinBox::value() loses precison.
+                    p->set("frame_rate_num", qRound(ui->fpsSpinner->value() * 1000000.0));
+                    p->set("frame_rate_den", 1000000);
+                }
             }
-            else if (qFloor(ui->fpsSpinner->value() * 10.0) == 299) {
-                setIfNotSet(p, "frame_rate_num", 30000);
-                setIfNotSet(p, "frame_rate_den", 1001);
-            }
-            else if (qFloor(ui->fpsSpinner->value() * 10.0) == 479) {
-                setIfNotSet(p, "frame_rate_num", 48000);
-                setIfNotSet(p, "frame_rate_den", 1001);
-            }
-            else if (qFloor(ui->fpsSpinner->value() * 10.0) == 599) {
-                setIfNotSet(p, "frame_rate_num", 60000);
-                setIfNotSet(p, "frame_rate_den", 1001);
-            }
-            else
-                setIfNotSet(p, "r", ui->fpsSpinner->value());
             if (ui->formatCombo->currentText() == "image2")
                 setIfNotSet(p, "threads", 1);
             else if (ui->videoCodecThreadsSpinner->value() == 0

src/docks/encodedock.ui
@@ -391,7 +391,7 @@
                  <item>
                   <widget class="QDoubleSpinBox" name="fpsSpinner">
                    <property name="decimals">
-                    <number>3</number>
+                    <number>6</number>
                    </property>
                    <property name="minimum">
                     <double>1.000000000000000</double>

commit ed558f80b3d5ec49f9a6f3db44e044ee82b02a5e

Fix export frame rate regression in 8d5dca3.

src/docks/encodedock.cpp
@@ -765,13 +765,13 @@ Mlt::Properties* EncodeDock::collectProperties(int realtime)
                 if (qRound(ui->fpsSpinner->value() * 1000000.0) == 23976024) {
                     p->set("frame_rate_num", 24000);
                     p->set("frame_rate_den", 1001);
-                } else if (qFloor(ui->fpsSpinner->value() * 1000000.0) == 299700299) {
+                } else if (qRound(ui->fpsSpinner->value() * 100000.0) == 2997003) {
                     p->set("frame_rate_num", 30000);
                     p->set("frame_rate_den", 1001);
                 } else if (qRound(ui->fpsSpinner->value() * 1000000.0) == 47952048) {
                     p->set("frame_rate_num", 48000);
                     p->set("frame_rate_den", 1001);
-                } else if (qFloor(ui->fpsSpinner->value() * 1000000.0) == 599400599) {
+                } else if (qRound(ui->fpsSpinner->value() * 100000.0) == 5994006) {
                     p->set("frame_rate_num", 60000);
                     p->set("frame_rate_den", 1001);
                 } else {