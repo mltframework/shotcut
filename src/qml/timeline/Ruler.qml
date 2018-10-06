mltframework/shotcut

commit ace1c30225ed072e2395b9ea481dfe8f280cfc42

Fix timeline rulers inaccurate after video mode fps change.

src/qml/timeline/Ruler.qml
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2013-2017 Meltytech, LLC
- * Author: Dan Dennedy <dan@dennedy.org>
+ * Copyright (c) 2013-2018 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -49,6 +48,15 @@ Rectangle {
                 text: application.timecode(index * stepSize / timeScale)
                 font.pointSize: 7.5
             }
+        }
+    }
+
+    Connections {
+        target: profile
+        onProfileChanged: {
+            // Force a repeater model change to update the labels.
+            ++stepSize
+            --stepSize
         }
     }
 }

src/mainwindow.cpp
@@ -74,6 +74,7 @@
 #include "models/keyframesmodel.h"
 #include "dialogs/listselectiondialog.h"
 #include "widgets/textproducerwidget.h"
+#include "qmltypes/qmlprofile.h"
 
 #include <QtWidgets>
 #include <Logger.h>
@@ -393,6 +394,7 @@ MainWindow::MainWindow()
     connect(m_encodeDock, SIGNAL(captureStateChanged(bool)), m_historyDock, SLOT(setDisabled(bool)));
     connect(this, SIGNAL(profileChanged()), m_encodeDock, SLOT(onProfileChanged()));
     connect(this, SIGNAL(profileChanged()), SLOT(onProfileChanged()));
+    connect(this, SIGNAL(profileChanged()), &QmlProfile::singleton(), SIGNAL(profileChanged()));
     connect(this, SIGNAL(audioChannelsChanged()), m_encodeDock, SLOT(onAudioChannelsChanged()));
     connect(m_playlistDock->model(), SIGNAL(modified()), m_encodeDock, SLOT(onProducerOpened()));
     connect(m_timelineDock, SIGNAL(clipCopied()), m_encodeDock, SLOT(onProducerOpened()));

src/qmltypes/qmlprofile.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2014 Meltytech, LLC
- * Author: Brian Matherly <pez4brian@yahoo.com>
+ * Copyright (c) 2014-2018 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,6 +35,9 @@ public:
     int height() const;
     double aspectRatio() const;
     double fps() const;
+
+signals:
+    void profileChanged();
 
 private:
     explicit QmlProfile();

commit c770ad4910011403a6682a87c13452966b7606de

Fix timeline performance regression in ace1c30.

src/qml/timeline/Ruler.qml
@@ -21,7 +21,8 @@ import QtQuick.Controls 1.0
 Rectangle {
     property int index: 0
     property real timeScale: 1.0
-    property int stepSize: Math.round(136 * Math.max(1.0, timeScale))
+    property int adjustment: 0
+    property int stepSize: Math.round(136 * Math.max(1.0, timeScale)) + adjustment
 
     SystemPalette { id: activePalette }
 
@@ -55,8 +56,8 @@ Rectangle {
         target: profile
         onProfileChanged: {
             // Force a repeater model change to update the labels.
-            ++stepSize
-            --stepSize
+            ++adjustment
+            --adjustment
         }
     }
 }