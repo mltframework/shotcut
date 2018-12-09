mltframework/shotcut

commit 63dda96d9ee46ff87379bd2002742acac9b17033

Add New Project / Recent Projects screen.

src/player.cpp
@@ -22,6 +22,8 @@
 #include "widgets/audioscale.h"
 #include "settings.h"
 #include "util.h"
+#include "widgets/newprojectfolder.h"
+
 #include <QtWidgets>
 #include <limits>
 
@@ -97,7 +99,6 @@ Player::Player(QWidget *parent)
     m_videoLayout->setSpacing(4);
     m_videoLayout->setContentsMargins(0, 0, 0, 0);
     vlayout->addLayout(m_videoLayout, 10);
-    vlayout->addStretch();
     m_videoScrollWidget = new QWidget;
     m_videoLayout->addWidget(m_videoScrollWidget, 10);
     m_videoLayout->addStretch();
@@ -116,6 +117,12 @@ Player::Player(QWidget *parent)
     m_horizontalScroll = new QScrollBar(Qt::Horizontal);
     glayout->addWidget(m_horizontalScroll, 1, 0);
     m_horizontalScroll->hide();
+
+    // Add the new project widget.
+    m_videoWidget->hide();
+    m_projectWidget = new NewProjectFolder(this);
+    vlayout->addWidget(m_projectWidget);
+    vlayout->addStretch();
 
     // Add the volume and signal level meter
     m_volumePopup = new QFrame(this, Qt::Popup);
@@ -506,10 +513,12 @@ void Player::reset()
     actionRewind->setDisabled(true);
     actionFastForward->setDisabled(true);
     m_videoWidget->hide();
+    m_projectWidget->show();
 }
 
 void Player::onProducerOpened(bool play)
 {
+    m_projectWidget->hide();
     m_videoWidget->show();
     m_duration = MLT.producer()->get_length();
     m_isSeekable = MLT.isSeekable();

src/mainwindow.cpp
@@ -540,58 +540,7 @@ void MainWindow::setupSettingsMenu()
     m_profileGroup = new QActionGroup(this);
     m_profileGroup->addAction(ui->actionProfileAutomatic);
     ui->actionProfileAutomatic->setData(QString());
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 720p 50 fps", "atsc_720p_50"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 720p 59.94 fps", "atsc_720p_5994"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 720p 60 fps", "atsc_720p_60"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 1080i 25 fps", "atsc_1080i_50"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 1080i 29.97 fps", "atsc_1080i_5994"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 1080p 23.98 fps", "atsc_1080p_2398"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 1080p 24 fps", "atsc_1080p_24"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 1080p 25 fps", "atsc_1080p_25"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 1080p 29.97 fps", "atsc_1080p_2997"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 1080p 30 fps", "atsc_1080p_30"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 1080p 59.94 fps", "atsc_1080p_5994"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "HD 1080p 60 fps", "atsc_1080p_60"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "SD NTSC", "dv_ntsc"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "SD PAL", "dv_pal"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "UHD 2160p 23.98 fps", "uhd_2160p_2398"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "UHD 2160p 24 fps", "uhd_2160p_24"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "UHD 2160p 25 fps", "uhd_2160p_25"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "UHD 2160p 29.97 fps", "uhd_2160p_2997"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "UHD 2160p 30 fps", "uhd_2160p_30"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "UHD 2160p 50 fps", "uhd_2160p_50"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "UHD 2160p 59.94 fps", "uhd_2160p_5994"));
-    ui->menuProfile->addAction(addProfile(m_profileGroup, "UHD 2160p 60 fps", "uhd_2160p_60"));
-    QMenu* menu = ui->menuProfile->addMenu(tr("Non-Broadcast"));
-    menu->addAction(addProfile(m_profileGroup, "HD 720p 23.98 fps", "atsc_720p_2398"));
-    menu->addAction(addProfile(m_profileGroup, "HD 720p 24 fps", "atsc_720p_24"));
-    menu->addAction(addProfile(m_profileGroup, "HD 720p 25 fps", "atsc_720p_25"));
-    menu->addAction(addProfile(m_profileGroup, "HD 720p 29.97 fps", "atsc_720p_2997"));
-    menu->addAction(addProfile(m_profileGroup, "HD 720p 30 fps", "atsc_720p_30"));
-    menu->addAction(addProfile(m_profileGroup, "HD 1080i 60 fps", "atsc_1080i_60"));
-    menu->addAction(addProfile(m_profileGroup, "HDV 1080i 25 fps", "hdv_1080_50i"));
-    menu->addAction(addProfile(m_profileGroup, "HDV 1080i 29.97 fps", "hdv_1080_60i"));
-    menu->addAction(addProfile(m_profileGroup, "HDV 1080p 25 fps", "hdv_1080_25p"));
-    menu->addAction(addProfile(m_profileGroup, "HDV 1080p 29.97 fps", "hdv_1080_30p"));
-    menu->addAction(addProfile(m_profileGroup, tr("DVD Widescreen NTSC"), "dv_ntsc_wide"));
-    menu->addAction(addProfile(m_profileGroup, tr("DVD Widescreen PAL"), "dv_pal_wide"));
-    menu->addAction(addProfile(m_profileGroup, "640x480 4:3 NTSC", "square_ntsc"));
-    menu->addAction(addProfile(m_profileGroup, "768x576 4:3 PAL", "square_pal"));
-    menu->addAction(addProfile(m_profileGroup, "854x480 16:9 NTSC", "square_ntsc_wide"));
-    menu->addAction(addProfile(m_profileGroup, "1024x576 16:9 PAL", "square_pal_wide"));
-    m_customProfileMenu = ui->menuProfile->addMenu(tr("Custom"));
-    m_customProfileMenu->addAction(ui->actionAddCustomProfile);
-    // Load custom profiles
-    QDir dir(Settings.appDataLocation());
-    if (dir.cd("profiles")) {
-        QStringList profiles = dir.entryList(QDir::Files | QDir::NoDotAndDotDot | QDir::Readable);
-        if (profiles.length() > 0) {
-            m_customProfileMenu->addAction(ui->actionProfileRemove);
-            m_customProfileMenu->addSeparator();
-        }
-        foreach (QString name, profiles)
-            m_customProfileMenu->addAction(addProfile(m_profileGroup, name, dir.filePath(name)));
-    }
+    buildVideoModeMenu(ui->menuProfile, m_customProfileMenu, m_profileGroup, ui->actionAddCustomProfile, ui->actionProfileRemove);
 
     // Add the SDI and HDMI devices to the Settings menu.
     m_externalGroup = new QActionGroup(this);
@@ -2001,6 +1950,137 @@ void MainWindow::hideSetDataDirectory()
     delete ui->actionAppDataSet;
 }
 
+QAction *MainWindow::actionAddCustomProfile() const
+{
+    return ui->actionAddCustomProfile;
+}
+
+QAction *MainWindow::actionProfileRemove() const
+{
+    return ui->actionProfileRemove;
+}
+
+void MainWindow::buildVideoModeMenu(QMenu* topMenu, QMenu*& customMenu, QActionGroup* group, QAction* addAction, QAction* removeAction)
+{
+    topMenu->addAction(addProfile(group, "HD 720p 50 fps", "atsc_720p_50"));
+    topMenu->addAction(addProfile(group, "HD 720p 59.94 fps", "atsc_720p_5994"));
+    topMenu->addAction(addProfile(group, "HD 720p 60 fps", "atsc_720p_60"));
+    topMenu->addAction(addProfile(group, "HD 1080i 25 fps", "atsc_1080i_50"));
+    topMenu->addAction(addProfile(group, "HD 1080i 29.97 fps", "atsc_1080i_5994"));
+    topMenu->addAction(addProfile(group, "HD 1080p 23.98 fps", "atsc_1080p_2398"));
+    topMenu->addAction(addProfile(group, "HD 1080p 24 fps", "atsc_1080p_24"));
+    topMenu->addAction(addProfile(group, "HD 1080p 25 fps", "atsc_1080p_25"));
+    topMenu->addAction(addProfile(group, "HD 1080p 29.97 fps", "atsc_1080p_2997"));
+    topMenu->addAction(addProfile(group, "HD 1080p 30 fps", "atsc_1080p_30"));
+    topMenu->addAction(addProfile(group, "HD 1080p 59.94 fps", "atsc_1080p_5994"));
+    topMenu->addAction(addProfile(group, "HD 1080p 60 fps", "atsc_1080p_60"));
+    topMenu->addAction(addProfile(group, "SD NTSC", "dv_ntsc"));
+    topMenu->addAction(addProfile(group, "SD PAL", "dv_pal"));
+    topMenu->addAction(addProfile(group, "UHD 2160p 23.98 fps", "uhd_2160p_2398"));
+    topMenu->addAction(addProfile(group, "UHD 2160p 24 fps", "uhd_2160p_24"));
+    topMenu->addAction(addProfile(group, "UHD 2160p 25 fps", "uhd_2160p_25"));
+    topMenu->addAction(addProfile(group, "UHD 2160p 29.97 fps", "uhd_2160p_2997"));
+    topMenu->addAction(addProfile(group, "UHD 2160p 30 fps", "uhd_2160p_30"));
+    topMenu->addAction(addProfile(group, "UHD 2160p 50 fps", "uhd_2160p_50"));
+    topMenu->addAction(addProfile(group, "UHD 2160p 59.94 fps", "uhd_2160p_5994"));
+    topMenu->addAction(addProfile(group, "UHD 2160p 60 fps", "uhd_2160p_60"));
+    QMenu* menu = topMenu->addMenu(tr("Non-Broadcast"));
+    menu->addAction(addProfile(group, "HD 720p 23.98 fps", "atsc_720p_2398"));
+    menu->addAction(addProfile(group, "HD 720p 24 fps", "atsc_720p_24"));
+    menu->addAction(addProfile(group, "HD 720p 25 fps", "atsc_720p_25"));
+    menu->addAction(addProfile(group, "HD 720p 29.97 fps", "atsc_720p_2997"));
+    menu->addAction(addProfile(group, "HD 720p 30 fps", "atsc_720p_30"));
+    menu->addAction(addProfile(group, "HD 1080i 60 fps", "atsc_1080i_60"));
+    menu->addAction(addProfile(group, "HDV 1080i 25 fps", "hdv_1080_50i"));
+    menu->addAction(addProfile(group, "HDV 1080i 29.97 fps", "hdv_1080_60i"));
+    menu->addAction(addProfile(group, "HDV 1080p 25 fps", "hdv_1080_25p"));
+    menu->addAction(addProfile(group, "HDV 1080p 29.97 fps", "hdv_1080_30p"));
+    menu->addAction(addProfile(group, tr("DVD Widescreen NTSC"), "dv_ntsc_wide"));
+    menu->addAction(addProfile(group, tr("DVD Widescreen PAL"), "dv_pal_wide"));
+    menu->addAction(addProfile(group, "640x480 4:3 NTSC", "square_ntsc"));
+    menu->addAction(addProfile(group, "768x576 4:3 PAL", "square_pal"));
+    menu->addAction(addProfile(group, "854x480 16:9 NTSC", "square_ntsc_wide"));
+    menu->addAction(addProfile(group, "1024x576 16:9 PAL", "square_pal_wide"));
+    customMenu = topMenu->addMenu(tr("Custom"));
+    customMenu->addAction(addAction);
+    // Load custom profiles
+    QDir dir(Settings.appDataLocation());
+    if (dir.cd("profiles")) {
+        QStringList profiles = dir.entryList(QDir::Files | QDir::NoDotAndDotDot | QDir::Readable);
+        if (profiles.length() > 0) {
+            customMenu->addAction(removeAction);
+            customMenu->addSeparator();
+        }
+        foreach (QString name, profiles)
+            customMenu->addAction(addProfile(group, name, dir.filePath(name)));
+    }
+}
+
+void MainWindow::newProject(const QString &filename, bool isProjectFolder)
+{
+    saveXML(filename);
+    if (m_autosaveFile)
+        m_autosaveFile->changeManagedFile(filename);
+    else
+        m_autosaveFile.reset(new AutoSaveFile(filename));
+    setCurrentFile(filename);
+    setWindowModified(false);
+    if (MLT.producer())
+        showStatusMessage(tr("Saved %1").arg(m_currentFile));
+    m_undoStack->setClean();
+    m_recentDock->add(filename);
+
+    if (isProjectFolder) {
+        QFileInfo info(filename);
+        m_projectFolder = info.absolutePath();
+    }
+}
+
+void MainWindow::addCustomProfile(const QString &name, QMenu *menu, QAction *action, QActionGroup *group)
+{
+    // Add new profile to the menu.
+    QDir dir(Settings.appDataLocation());
+    if (dir.cd("profiles")) {
+        QStringList profiles = dir.entryList(QDir::Files | QDir::NoDotAndDotDot | QDir::Readable);
+        if (profiles.length() == 1) {
+            menu->addAction(action);
+            menu->addSeparator();
+        }
+        action = addProfile(group, name, dir.filePath(name));
+        action->setChecked(true);
+        menu->addAction(action);
+        Settings.setPlayerProfile(dir.filePath(name));
+        Settings.sync();
+    }
+}
+
+void MainWindow::removeCustomProfiles(const QStringList &profiles, QDir& dir, QMenu *menu, QAction *action)
+{
+    foreach(const QString& profile, profiles) {
+        // Remove the file.
+        dir.remove(profile);
+        // Locate the menu item.
+        foreach (QAction* a, menu->actions()) {
+            if (a->text() == profile) {
+                // Remove the menu item.
+                delete a;
+                break;
+            }
+        }
+    }
+    // If no more custom video modes.
+    if (menu->actions().size() == 3) {
+        // Remove the Remove action and separator.
+        menu->removeAction(action);
+        foreach (QAction* a, menu->actions()) {
+            if (a->isSeparator()) {
+                delete a;
+                break;
+            }
+        }
+    }
+}
+
 // Drag-n-drop events
 
 bool MainWindow::eventFilter(QObject* target, QEvent* event)
@@ -2209,18 +2289,7 @@ bool MainWindow::on_actionSave_As_triggered()
 
         if (Util::warnIfNotWritable(filename, this, caption))
             return false;
-
-        saveXML(filename);
-        if (m_autosaveFile)
-            m_autosaveFile->changeManagedFile(filename);
-        else
-            m_autosaveFile.reset(new AutoSaveFile(filename));
-        setCurrentFile(filename);
-        setWindowModified(false);
-        if (MLT.producer())
-            showStatusMessage(tr("Saved %1").arg(m_currentFile));
-        m_undoStack->setClean();
-        m_recentDock->add(filename);
+        newProject(filename);
     }
     return !filename.isEmpty();
 }
@@ -3129,20 +3198,7 @@ void MainWindow::on_actionAddCustomProfile_triggered()
     if (dialog.exec() == QDialog::Accepted) {
         QString name = dialog.profileName();
         if (!name.isEmpty()) {
-            // Add new profile to the menu.
-            QDir dir(Settings.appDataLocation());
-            if (dir.cd("profiles")) {
-                QStringList profiles = dir.entryList(QDir::Files | QDir::NoDotAndDotDot | QDir::Readable);
-                if (profiles.length() == 1) {
-                    m_customProfileMenu->addAction(ui->actionProfileRemove);
-                    m_customProfileMenu->addSeparator();
-                }
-                QAction* action = addProfile(m_profileGroup, name, dir.filePath(name));
-                action->setChecked(true);
-                m_customProfileMenu->addAction(action);
-                Settings.setPlayerProfile(dir.filePath(name));
-                Settings.sync();
-            }
+            addCustomProfile(name, customProfileMenu(), actionProfileRemove(), profileGroup());
         } else if (m_profileGroup->checkedAction()) {
             m_profileGroup->checkedAction()->setChecked(false);
         }
@@ -3355,6 +3411,7 @@ void MainWindow::on_actionClose_triggered()
 {
     if (continueModified()) {
         LOG_DEBUG() << "";
+        m_projectFolder = "";
         if (multitrack())
             m_timelineDock->model()->close();
         if (playlist())
@@ -3671,29 +3728,7 @@ void MainWindow::on_actionProfileRemove_triggered()
 
         // Show the dialog.
         if (dialog.exec() == QDialog::Accepted) {
-            foreach(const QString& profile, dialog.selection()) {
-                // Remove the file.
-                dir.remove(profile);
-                // Locate the menu item.
-                foreach (QAction* a, m_customProfileMenu->actions()) {
-                    if (a->text() == profile) {
-                        // Remove the menu item.
-                        delete a;
-                        break;
-                    }
-                }
-            }
-            // If no more custom video modes.
-            if (m_customProfileMenu->actions().size() == 3) {
-                // Remove the Remove action and separator.
-                m_customProfileMenu->removeAction(ui->actionProfileRemove);
-                foreach (QAction* a, m_customProfileMenu->actions()) {
-                    if (a->isSeparator()) {
-                        delete a;
-                        break;
-                    }
-                }
-            }
+            removeCustomProfiles(dialog.selection(), dir, customProfileMenu(), actionProfileRemove());
         }
     }
 }

src/mainwindow.h
@@ -101,6 +101,14 @@ public:
     void keyPressEvent(QKeyEvent*);
     void keyReleaseEvent(QKeyEvent *);
     void hideSetDataDirectory();
+    QMenu* customProfileMenu() const { return m_customProfileMenu; }
+    QAction* actionAddCustomProfile() const;
+    QAction* actionProfileRemove() const;
+    QActionGroup* profileGroup() const { return m_profileGroup; }
+    void buildVideoModeMenu(QMenu *topMenu, QMenu *&customMenu, QActionGroup* group, QAction *addAction, QAction *removeAction);
+    void newProject(const QString& filename, bool isProjectFolder = false);
+    void addCustomProfile(const QString& name, QMenu* menu, QAction* action, QActionGroup* group);
+    void removeCustomProfiles(const QStringList& profiles, QDir &dir, QMenu* menu, QAction* action);
 
 signals:
     void audioChannelsChanged();
@@ -169,6 +177,7 @@ private:
     QNetworkAccessManager m_network;
     QString m_upgradeUrl;
     KeyframesDock* m_keyframesDock;
+    QString m_projectFolder;
 
 #ifdef WITH_LIBLEAP
     LeapListener m_leapListener;

src/player.h
@@ -164,6 +164,7 @@ private:
     QPropertyAnimation* m_statusFadeOut;
     QTimer m_statusTimer;
     QMenu* m_zoomMenu;
+    QWidget* m_projectWidget;
 
 private slots:
     void updateSelection();

src/settings.cpp
@@ -672,3 +672,13 @@ void ShotcutSettings::setClearRecent(bool b)
 {
     settings.setValue("clearRecent", b);
 }
+
+QString ShotcutSettings::projectsFolder() const
+{
+    return settings.value("projectsFolder", QStandardPaths::standardLocations(QStandardPaths::MoviesLocation)).toString();
+}
+
+void ShotcutSettings::setProjectsFolder(const QString &path)
+{
+    settings.setValue("projectsFolder", path);
+}

src/settings.h
@@ -175,6 +175,9 @@ public:
     bool clearRecent() const;
     void setClearRecent(bool);
 
+    QString projectsFolder() const;
+    void setProjectsFolder(const QString& path);
+
 signals:
     void openPathChanged();
     void savePathChanged();

src/src.pro
@@ -112,7 +112,8 @@ SOURCES += main.cpp\
     qmltypes/qmlproducer.cpp \
     models/keyframesmodel.cpp \
     widgets/textproducerwidget.cpp \
-    dialogs/listselectiondialog.cpp
+    dialogs/listselectiondialog.cpp \
+    widgets/newprojectfolder.cpp
 
 
 HEADERS  += mainwindow.h \
@@ -221,7 +222,8 @@ HEADERS  += mainwindow.h \
     qmltypes/qmlproducer.h \
     models/keyframesmodel.h \
     widgets/textproducerwidget.h \
-    dialogs/listselectiondialog.h
+    dialogs/listselectiondialog.h \
+    widgets/newprojectfolder.h
 
 FORMS    += mainwindow.ui \
     openotherdialog.ui \
@@ -264,7 +266,8 @@ FORMS    += mainwindow.ui \
     dialogs/unlinkedfilesdialog.ui \
     dialogs/transcodedialog.ui \
     widgets/textproducerwidget.ui \
-    dialogs/listselectiondialog.ui
+    dialogs/listselectiondialog.ui \
+    widgets/newprojectfolder.ui
 
 RESOURCES += \
     ../icons/resources.qrc \

src/widgets/newprojectfolder.cpp
@@ -1,0 +1,229 @@
+/*
+ * Copyright (c) 2018 Meltytech, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "newprojectfolder.h"
+#include "ui_newprojectfolder.h"
+#include "settings.h"
+#include "mainwindow.h"
+#include "util.h"
+#include "dialogs/customprofiledialog.h"
+#include "dialogs/listselectiondialog.h"
+#include "qmltypes/qmlapplication.h"
+#include <Logger.h>
+
+#include <QDir>
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QMessageBox>
+#include <QListWidgetItem>
+
+NewProjectFolder::NewProjectFolder(QWidget* parent) :
+    QWidget(parent),
+    ui(new Ui::NewProjectFolder)
+{
+    ui->setupUi(this);
+    Util::setColorsToHighlight(ui->newProjectLabel);
+    Util::setColorsToHighlight(ui->newProjectLabel_2);
+    ui->projectsFolderButton->setText(Settings.projectsFolder());
+    ui->actionProfileAutomatic->setData(QString());
+    ui->recentListView->setModel(&m_model);
+    m_profileGroup = new QActionGroup(this);
+    connect(m_profileGroup, SIGNAL(triggered(QAction*)), SLOT(onProfileTriggered(QAction*)));
+}
+
+NewProjectFolder::~NewProjectFolder()
+{
+    delete ui;
+}
+
+void NewProjectFolder::showEvent(QShowEvent*)
+{
+    m_profile = Settings.playerProfile();
+
+    // Automatic not permitted for SDI/HDMI
+    if (!Settings.playerExternal().isEmpty() && m_profile.isEmpty())
+        m_profile = "atsc_720p_50";
+    bool found = false;
+    foreach (QAction* a, MAIN.profileGroup()->actions()) {
+        if (a->data().toString() == m_profile) {
+            ui->videoModeButton->setText(a->text());
+            found = true;
+            break;
+        }
+    }
+    if (!found)
+        ui->videoModeButton->setText(ui->actionProfileAutomatic->text());
+
+    // Update Video Mode menu.
+    m_videoModeMenu.clear();
+    QString external = Settings.playerExternal();
+    bool ok = false;
+    external.toInt(&ok);
+    if (external.isEmpty() || ok) {
+        m_profileGroup->addAction(ui->actionProfileAutomatic);
+        m_videoModeMenu.addAction(ui->actionProfileAutomatic);
+    }
+    MAIN.buildVideoModeMenu(&m_videoModeMenu, m_customProfileMenu, m_profileGroup, ui->actionAddCustomProfile, ui->actionProfileRemove);
+
+    // Check the current menu item.
+    foreach (QAction* a, m_profileGroup->actions()) {
+        if (a->data().toString() == m_profile) {
+            a->setChecked(true);
+            break;
+        }
+    }
+
+    // Update recent projects.
+    m_model.clear();
+    foreach (QString s, Settings.recent()) {
+        if (s.endsWith(".mlt")) {
+            QStandardItem* item = new QStandardItem(Util::baseName(s));
+            item->setToolTip(s);
+            m_model.appendRow(item);
+        }
+    }
+
+}
+
+void NewProjectFolder::hideEvent(QHideEvent*)
+{
+    ui->projectNameLineEdit->setText(QString());
+}
+
+void NewProjectFolder::on_projectsFolderButton_clicked()
+{
+    QString dirName = QFileDialog::getExistingDirectory(this, tr("Projects Folder"), Settings.projectsFolder());
+    if (!dirName.isEmpty()) {
+        ui->projectsFolderButton->setText(dirName);
+        Settings.setProjectsFolder(dirName);
+    }
+}
+
+void NewProjectFolder::on_videoModeButton_clicked()
+{
+    m_videoModeMenu.exec(ui->videoModeButton->mapToGlobal(QPoint(0, 0)));
+}
+
+void NewProjectFolder::onProfileTriggered(QAction *action)
+{
+    m_profile = action->data().toString();
+    ui->videoModeButton->setText(action->text());
+}
+
+void NewProjectFolder::on_actionAddCustomProfile_triggered()
+{
+    CustomProfileDialog dialog(this);
+    dialog.setWindowModality(QmlApplication::dialogModality());
+    if (dialog.exec() == QDialog::Accepted) {
+        QString name = dialog.profileName();
+        if (!name.isEmpty()) {
+            ui->videoModeButton->setText(name);
+            MAIN.addCustomProfile(name, m_customProfileMenu, ui->actionProfileRemove, m_profileGroup);
+            MAIN.addCustomProfile(name, MAIN.customProfileMenu(), MAIN.actionProfileRemove(), MAIN.profileGroup());
+        } else if (m_profileGroup->checkedAction()) {
+            ui->videoModeButton->setText(tr("Custom"));
+            m_profileGroup->checkedAction()->setChecked(false);
+            MAIN.profileGroup()->checkedAction()->setChecked(false);
+        }
+        // Use the new profile.
+        emit MAIN.profileChanged();
+    }
+}
+
+void NewProjectFolder::on_actionProfileRemove_triggered()
+{
+    QDir dir(Settings.appDataLocation());
+    if (dir.cd("profiles")) {
+        // Setup the dialog.
+        QStringList profiles = dir.entryList(QDir::Files | QDir::NoDotAndDotDot | QDir::Readable);
+        ListSelectionDialog dialog(profiles, this);
+        dialog.setWindowModality(QmlApplication::dialogModality());
+        dialog.setWindowTitle(tr("Remove Video Mode"));
+
+        // Show the dialog.
+        if (dialog.exec() == QDialog::Accepted) {
+            MAIN.removeCustomProfiles(dialog.selection(), dir, m_customProfileMenu, ui->actionProfileRemove);
+            MAIN.removeCustomProfiles(dialog.selection(), dir, MAIN.customProfileMenu(), MAIN.actionProfileRemove());
+            if (dialog.selection().indexOf(ui->videoModeButton->text()) >= 0) {
+                ui->actionProfileAutomatic->setChecked(true);
+                ui->videoModeButton->setText(ui->actionProfileAutomatic->text());
+            }
+        }
+    }
+}
+
+void NewProjectFolder::on_startButton_clicked()
+{
+    QDir dir(ui->projectsFolderButton->text());
+    QString projectName = ui->projectNameLineEdit->text();
+    QString fileName = projectName;
+    if (projectName.endsWith(".mlt"))
+        projectName = projectName.mid(0, projectName.length() - 4);
+    else
+        fileName += ".mlt";
+
+    // Check if the project exists.
+    if (dir.cd(ui->projectNameLineEdit->text())) {
+        if (dir.exists(fileName)) {
+            QMessageBox::warning(this, ui->newProjectLabel->text(),
+                                 tr("There is already a project with that name.\n"
+                                    "Try again with a different name."));
+            return;
+        }
+    } else {
+        // Create the project folder if needed.
+        if (!dir.mkdir(projectName)) {
+            QMessageBox::warning(this, ui->newProjectLabel->text(),
+                                 tr("Unable to create folder %1\n"
+                                    "Perhaps you do not have permission.\n"
+                                    "Try again with a different folder.").arg(projectName));
+            return;
+        }
+        dir.cd(projectName);
+    }
+    Settings.setSavePath(dir.absolutePath());
+//    Settings.setEncodePath(dir.absolutePath());
+
+    // Create the project file.
+    QFileInfo info(dir.absolutePath(), fileName);
+    if (Util::warnIfNotWritable(info.absoluteFilePath(), this, ui->newProjectLabel->text()))
+        return;
+    MAIN.newProject(info.absoluteFilePath(), true);
+
+    // Change the video mode.
+    if (m_profileGroup->checkedAction()) {
+        Settings.setPlayerProfile(m_profile);
+        MAIN.setProfile(m_profile);
+        foreach (QAction* a, MAIN.profileGroup()->actions()) {
+            if (a->data().toString() == m_profile) {
+                a->setChecked(true);
+                break;
+            }
+        }
+    }
+    hide();
+}
+
+void NewProjectFolder::on_projectNameLineEdit_textChanged(const QString& arg1)
+{
+    ui->startButton->setDisabled(arg1.isEmpty());
+}
+
+void NewProjectFolder::on_recentListView_clicked(const QModelIndex& index)
+{
+    MAIN.open(m_model.itemData(index)[Qt::ToolTipRole].toString());
+}

commit f642bfd67f97feb382de75e96d19ffb22a9803fd

Fix external monitoring on Windows screen.
Regression in 63dda96

src/player.cpp
@@ -109,7 +109,6 @@ Player::Player(QWidget *parent)
     // Add the video widgets.
     m_videoWidget = qobject_cast<QWidget*>(MLT.videoWidget());
     Q_ASSERT(m_videoWidget);
-    m_videoWidget->setMinimumSize(QSize(320, 180));
     glayout->addWidget(m_videoWidget, 0, 0);
     m_verticalScroll = new QScrollBar(Qt::Vertical);
     glayout->addWidget(m_verticalScroll, 0, 1);
@@ -119,7 +118,6 @@ Player::Player(QWidget *parent)
     m_horizontalScroll->hide();
 
     // Add the new project widget.
-    m_videoWidget->hide();
     m_projectWidget = new NewProjectFolder(this);
     vlayout->addWidget(m_projectWidget, 10);
     vlayout->addStretch();