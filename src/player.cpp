mltframework/shotcut

commit f642bfd67f97feb382de75e96d19ffb22a9803fd

Fix external monitoring on Windows screen.
Regression in 63dda96

src/player.cpp
@@ -109,7 +109,6 @@ Player::Player(QWidget *parent)
     // Add the video widgets.
     m_videoWidget = qobject_cast<QWidget*>(MLT.videoWidget());
     Q_ASSERT(m_videoWidget);
-    m_videoWidget->setMinimumSize(QSize(320, 180));
     glayout->addWidget(m_videoWidget, 0, 0);
     m_verticalScroll = new QScrollBar(Qt::Vertical);
     glayout->addWidget(m_verticalScroll, 0, 1);
@@ -119,7 +118,6 @@ Player::Player(QWidget *parent)
     m_horizontalScroll->hide();
 
     // Add the new project widget.
-    m_videoWidget->hide();
     m_projectWidget = new NewProjectFolder(this);
     vlayout->addWidget(m_projectWidget, 10);
     vlayout->addStretch();

commit 1f384768f51a69d4964061c00ec2e1108df8ee77

Fix external monitoring on Linux screen.
Regression in f642bfd

src/player.cpp
@@ -113,6 +113,7 @@ Player::Player(QWidget *parent)
     // Add the video widgets.
     m_videoWidget = qobject_cast<QWidget*>(MLT.videoWidget());
     Q_ASSERT(m_videoWidget);
+    m_videoWidget->setMinimumSize(QSize(1, 1));
     glayout->addWidget(m_videoWidget, 0, 0);
     m_verticalScroll = new QScrollBar(Qt::Vertical);
     glayout->addWidget(m_verticalScroll, 0, 1);