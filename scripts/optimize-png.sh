8e22f6a108e482f0d9616eec38471383acc365de
Brian Matherly <code@brianmatherly.com>
2019-03-04 21:24:52 -0600

    Add script to optimize png files.

---
+++ scripts/optimize-png.sh
@@ -0,0 +1,11 @@
+# Script to optimize and compress png impage files to reduce size.
+# All operations are lossless
+# Requires optipng and advpng
+# In Ubuntu: "sudo apt-get install advancecomp optipng"
+
+files=`find . -name '*.PNG' -o -name '*.png'`
+
+optipng -o 7 $files
+advpng -z -4 -i 25 $files
+# Sometimes 7z is actually smaller than zopfli
+advpng -z -3 -i 25 $files
