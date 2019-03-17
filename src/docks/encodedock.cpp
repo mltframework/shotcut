mltframework/shotcut

commit f9da6d4146d02f1ac188917f6acfaf161730bdd4

Verify saving well-formed XML.

src/mltcontroller.cpp
@@ -22,6 +22,7 @@
 #include <QFileInfo>
 #include <QUuid>
 #include <QTemporaryFile>
+#include <QXmlStreamReader>
 #include <Logger.h>
 #include <Mlt.h>
 #include <math.h>
@@ -410,10 +411,17 @@ void Controller::refreshConsumer(bool scrubAudio)
     }
 }
 
-void Controller::saveXML(const QString& filename, Service* service, bool withRelativePaths)
+bool Controller::saveXML(const QString& filename, Service* service, bool withRelativePaths)
 {
     QMutexLocker locker(&m_saveXmlMutex);
-    Consumer c(profile(), "xml", filename.toUtf8().constData());
+    // First, write to a temp file.
+    QFileInfo fi(filename);
+    QTemporaryFile tmp(fi.absolutePath().append("/shotcut-XXXXXX.mlt"));
+    tmp.open();
+    tmp.close();
+    QString tmpFileName = tmp.fileName();
+    LOG_DEBUG() << "writing temporary XML file" << tmpFileName;
+    Consumer c(profile(), "xml", tmpFileName.toUtf8().constData());
     Service s(service? service->get_service() : m_producer->get_service());
     if (s.is_valid()) {
         s.set(kShotcutProjectAudioChannels, m_audioChannels);
@@ -425,7 +433,7 @@ void Controller::saveXML(const QString& filename, Service* service, bool withRel
         c.set("no_meta", 1);
         c.set("store", "shotcut");
         if (withRelativePaths) {
-            c.set("root", QFileInfo(filename).absolutePath().toUtf8().constData());
+            c.set("root", fi.absolutePath().toUtf8().constData());
             c.set("no_root", 1);
         }
         c.set("title", QString("Shotcut version ").append(SHOTCUT_VERSION).toUtf8().constData());
@@ -433,7 +441,22 @@ void Controller::saveXML(const QString& filename, Service* service, bool withRel
         c.start();
         if (ignore)
             s.set("ignore_points", ignore);
+
+        // Next, check if the temp file is well-formed XML.
+        tmp.open();
+        QXmlStreamReader xml(&tmp);
+        while (!xml.atEnd())
+            xml.readNext();
+        tmp.close();
+        if (!xml.hasError()) {
+            // If the file is good, then move it into place.
+            LOG_DEBUG() << "rename" << tmpFileName << filename;
+            tmp.setAutoRemove(false);
+            tmp.rename(filename);
+            return true;
+        }
     }
+    return false;
 }
 
 QString Controller::XML(Service* service, bool withProfile, bool withMetadata)

src/mltcontroller.h
@@ -79,7 +79,7 @@ public:
     void onWindowResize();
     virtual void seek(int position);
     void refreshConsumer(bool scrubAudio = false);
-    void saveXML(const QString& filename, Service* service = 0, bool withRelativePaths = true);
+    bool saveXML(const QString& filename, Service* service = 0, bool withRelativePaths = true);
     QString XML(Service* service = 0, bool withProfile = false, bool withMetadata = false);
     int consumerChanged();
     void setProfile(const QString& profile_name);

src/mainwindow.cpp
@@ -1022,9 +1022,11 @@ void MainWindow::doAutosave()
 {
     QMutexLocker locker(&m_autosaveMutex);
     if (m_autosaveFile) {
+        bool success = false;
         if (m_autosaveFile->isOpen() || m_autosaveFile->open(QIODevice::ReadWrite)) {
-            saveXML(m_autosaveFile->fileName(), false /* without relative paths */);
-        } else {
+            success = saveXML(m_autosaveFile->fileName(), false /* without relative paths */);
+        }
+        if (!success) {
             LOG_ERROR() << "failed to open autosave file for writing" << m_autosaveFile->fileName();
         }
     }
@@ -1130,6 +1132,19 @@ void MainWindow::setAudioChannels(int channels)
     else if (channels == 6)
         ui->actionChannels6->setChecked(true);
     emit audioChannelsChanged();
+}
+
+void MainWindow::showSaveError()
+{
+    QMessageBox dialog(QMessageBox::Critical,
+                       qApp->applicationName(),
+                       tr("There was an error saving. Please try again."),
+                       QMessageBox::Ok,
+                       this);
+    dialog.setDefaultButton(QMessageBox::Ok);
+    dialog.setEscapeButton(QMessageBox::Ok);
+    dialog.setWindowModality(QmlApplication::dialogModality());
+    dialog.exec();
 }
 
 static void autosaveTask(MainWindow* p)
@@ -2034,18 +2049,21 @@ void MainWindow::newProject(const QString &filename, bool isProjectFolder)
         QFileInfo info(filename);
         MLT.setProjectFolder(info.absolutePath());
     }
-    saveXML(filename);
-    QMutexLocker locker(&m_autosaveMutex);
-    if (m_autosaveFile)
-        m_autosaveFile->changeManagedFile(filename);
-    else
-        m_autosaveFile.reset(new AutoSaveFile(filename));
-    setCurrentFile(filename);
-    setWindowModified(false);
-    if (MLT.producer())
-        showStatusMessage(tr("Saved %1").arg(m_currentFile));
-    m_undoStack->setClean();
-    m_recentDock->add(filename);
+    if (saveXML(filename)) {
+        QMutexLocker locker(&m_autosaveMutex);
+        if (m_autosaveFile)
+            m_autosaveFile->changeManagedFile(filename);
+        else
+            m_autosaveFile.reset(new AutoSaveFile(filename));
+        setCurrentFile(filename);
+        setWindowModified(false);
+        if (MLT.producer())
+            showStatusMessage(tr("Saved %1").arg(m_currentFile));
+        m_undoStack->setClean();
+        m_recentDock->add(filename);
+    } else {
+        showSaveError();
+    }
 }
 
 void MainWindow::addCustomProfile(const QString &name, QMenu *menu, QAction *action, QActionGroup *group)
@@ -2280,12 +2298,16 @@ bool MainWindow::on_actionSave_triggered()
     } else {
         if (Util::warnIfNotWritable(m_currentFile, this, tr("Save XML")))
             return false;
-        saveXML(m_currentFile);
+        bool success = saveXML(m_currentFile);
         QMutexLocker locker(&m_autosaveMutex);
         m_autosaveFile.reset(new AutoSaveFile(m_currentFile));
         setCurrentFile(m_currentFile);
         setWindowModified(false);
-        showStatusMessage(tr("Saved %1").arg(m_currentFile));
+        if (success) {
+            showStatusMessage(tr("Saved %1").arg(m_currentFile));
+        } else {
+            showSaveError();
+        }
         m_undoStack->setClean();
         return true;
     }
@@ -2615,23 +2637,25 @@ void MainWindow::on_actionForum_triggered()
     QDesktopServices::openUrl(QUrl("https://forum.shotcut.org/"));
 }
 
-void MainWindow::saveXML(const QString &filename, bool withRelativePaths)
+bool MainWindow::saveXML(const QString &filename, bool withRelativePaths)
 {
+    bool result;
     if (m_timelineDock->model()->rowCount() > 0) {
-        MLT.saveXML(filename, multitrack(), withRelativePaths);
+        result = MLT.saveXML(filename, multitrack(), withRelativePaths);
     } else if (m_playlistDock->model()->rowCount() > 0) {
         int in = MLT.producer()->get_in();
         int out = MLT.producer()->get_out();
         MLT.producer()->set_in_and_out(0, MLT.producer()->get_length() - 1);
-        MLT.saveXML(filename, playlist(), withRelativePaths);
+        result = MLT.saveXML(filename, playlist(), withRelativePaths);
         MLT.producer()->set_in_and_out(in, out);
     } else if (MLT.producer()) {
-        MLT.saveXML(filename, (MLT.isMultitrack() || MLT.isPlaylist())? MLT.savedProducer() : 0, withRelativePaths);
+        result = MLT.saveXML(filename, (MLT.isMultitrack() || MLT.isPlaylist())? MLT.savedProducer() : 0, withRelativePaths);
     } else {
         // Save an empty playlist, which is accepted by both MLT and Shotcut.
         Mlt::Playlist playlist(MLT.profile());
-        MLT.saveXML(filename, &playlist, withRelativePaths);
+        result = MLT.saveXML(filename, &playlist, withRelativePaths);
     }
+    return result;
 }
 
 void MainWindow::changeTheme(const QString &theme)

src/mainwindow.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2018 Meltytech, LLC
+ * Copyright (c) 2011-2019 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -79,7 +79,7 @@ public:
     bool continueModified();
     bool continueJobsRunning();
     QUndoStack* undoStack() const;
-    void saveXML(const QString& filename, bool withRelativePaths = true);
+    bool saveXML(const QString& filename, bool withRelativePaths = true);
     static void changeTheme(const QString& theme);
     PlaylistDock* playlistDock() const { return m_playlistDock; }
     FilterController* filterController() const { return m_filterController; }
@@ -142,6 +142,7 @@ private:
     void stepLeftBySeconds(int sec);
     bool saveRepairedXmlFile(MltXmlChecker& checker, QString& fileName);
     void setAudioChannels(int channels);
+    void showSaveError();
 
     Ui::MainWindow* ui;
     Player* m_player;

commit c25f8c1b2bb3e985fb951d19104b13ec396edb7c

Fix export regression in f9da6d4.

src/docks/encodedock.cpp
@@ -884,7 +884,7 @@ MeltJob* EncodeDock::createMeltJob(Mlt::Producer* service, const QString& target
     QTemporaryFile tmp;
     tmp.open();
     tmp.close();
-    MLT.saveXML(tmp.fileName(), service, false /* without relative paths */);
+    MLT.saveXML(tmp.fileName(), service, false /* without relative paths */, false /* without verify */);
 
     // parse xml
     QFile f1(tmp.fileName());

src/mltcontroller.cpp
@@ -411,17 +411,21 @@ void Controller::refreshConsumer(bool scrubAudio)
     }
 }
 
-bool Controller::saveXML(const QString& filename, Service* service, bool withRelativePaths)
+bool Controller::saveXML(const QString& filename, Service* service, bool withRelativePaths, bool verify)
 {
     QMutexLocker locker(&m_saveXmlMutex);
-    // First, write to a temp file.
     QFileInfo fi(filename);
-    QTemporaryFile tmp(fi.absolutePath().append("/shotcut-XXXXXX.mlt"));
-    tmp.open();
-    tmp.close();
-    QString tmpFileName = tmp.fileName();
-    LOG_DEBUG() << "writing temporary XML file" << tmpFileName;
-    Consumer c(profile(), "xml", tmpFileName.toUtf8().constData());
+    QTemporaryFile tmp;
+    QString mltFileName = filename;
+    // First, write to a temp file.
+    if (verify) {
+        tmp.setFileTemplate(fi.absolutePath().append("/shotcut-XXXXXX.mlt"));
+        tmp.open();
+        tmp.close();
+        mltFileName = tmp.fileName();
+        LOG_DEBUG() << "writing temporary XML file" << mltFileName;
+    }
+    Consumer c(profile(), "xml", mltFileName.toUtf8().constData());
     Service s(service? service->get_service() : m_producer->get_service());
     if (s.is_valid()) {
         s.set(kShotcutProjectAudioChannels, m_audioChannels);
@@ -442,17 +446,21 @@ bool Controller::saveXML(const QString& filename, Service* service, bool withRel
         if (ignore)
             s.set("ignore_points", ignore);
 
-        // Next, check if the temp file is well-formed XML.
-        tmp.open();
-        QXmlStreamReader xml(&tmp);
-        while (!xml.atEnd())
-            xml.readNext();
-        tmp.close();
-        if (!xml.hasError()) {
-            // If the file is good, then move it into place.
-            LOG_DEBUG() << "rename" << tmpFileName << filename;
-            tmp.setAutoRemove(false);
-            tmp.rename(filename);
+        if (verify) {
+            // Check if the temp file is well-formed XML.
+            tmp.open();
+            QXmlStreamReader xml(&tmp);
+            while (!xml.atEnd())
+                xml.readNext();
+            tmp.close();
+            if (!xml.hasError()) {
+                // If the file is good, then move it into place.
+                LOG_DEBUG() << "rename" << mltFileName << filename;
+                tmp.setAutoRemove(false);
+                tmp.rename(filename);
+                return true;
+            }
+        } else {
             return true;
         }
     }

src/mltcontroller.h
@@ -79,7 +79,7 @@ public:
     void onWindowResize();
     virtual void seek(int position);
     void refreshConsumer(bool scrubAudio = false);
-    bool saveXML(const QString& filename, Service* service = 0, bool withRelativePaths = true);
+    bool saveXML(const QString& filename, Service* service = 0, bool withRelativePaths = true, bool verify = true);
     QString XML(Service* service = 0, bool withProfile = false, bool withMetadata = false);
     int consumerChanged();
     void setProfile(const QString& profile_name);

src/qmltypes/qmlfilter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2018 Meltytech, LLC
+ * Copyright (c) 2013-2019 Meltytech, LLC
  * Author: Dan Dennedy <dan@dennedy.org>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -298,7 +298,7 @@ void QmlFilter::analyze(bool isAudio)
     m_filter.set("results", NULL, 0);
     int disable = m_filter.get_int("disable");
     m_filter.set("disable", 0);
-    MLT.saveXML(tmp.fileName(), &service);
+    MLT.saveXML(tmp.fileName(), &service, false, false);
     m_filter.set("disable", disable);
 
     // get temp filename for output xml