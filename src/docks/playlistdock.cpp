mltframework/shotcut

commit 45aa48f83cccc0a5821a49a626a820a9c5efe02d

Add trim support to an opened playlist item.

src/docks/playlistdock.cpp
@@ -450,6 +450,30 @@ void PlaylistDock::onProducerOpened()
         ui->addButton->setEnabled(true);
 }
 
+void PlaylistDock::onInChanged()
+{
+    int index = MLT.producer()->get_int(kPlaylistIndexProperty) - 1;
+    QScopedPointer<Mlt::ClipInfo> info(m_model.playlist()->clip_info(index));
+    if (info && info->producer
+             && info->producer->get_producer() == MLT.producer()->get_producer()
+             && info->frame_in != MLT.producer()->get_in()) {
+        MAIN.undoStack()->push(new Playlist::TrimClipInCommand(m_model, index, MLT.producer()->get_in()));
+        setUpdateButtonEnabled(false);
+    }
+}
+
+void PlaylistDock::onOutChanged()
+{
+    int index = MLT.producer()->get_int(kPlaylistIndexProperty) - 1;
+    QScopedPointer<Mlt::ClipInfo> info(m_model.playlist()->clip_info(index));
+    if (info && info->producer
+             && info->producer->get_producer() == MLT.producer()->get_producer()
+             && info->frame_out != MLT.producer()->get_out()) {
+        MAIN.undoStack()->push(new Playlist::TrimClipOutCommand(m_model, index, MLT.producer()->get_out()));
+        setUpdateButtonEnabled(false);
+    }
+}
+
 void PlaylistDock::on_actionOpen_triggered()
 {
     QModelIndex index = m_view->currentIndex();

src/commands/playlistcommands.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2018 Meltytech, LLC
+ * Copyright (c) 2013-2019 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -89,6 +89,16 @@ void UpdateCommand::undo()
     LOG_DEBUG() << "row" << m_row;
     Mlt::Producer producer(MLT.profile(), "xml-string", m_oldXml.toUtf8().constData());
     m_model.update(m_row, producer);
+}
+
+bool UpdateCommand::mergeWith(const QUndoCommand *other)
+{
+    const UpdateCommand* that = static_cast<const UpdateCommand*>(other);
+    LOG_DEBUG() << "this row" << m_row << "that row" << that->m_row;
+    if (that->id() != id() || that->m_row != m_row)
+        return false;
+    m_newXml = that->m_newXml;
+    return true;
 }
 
 RemoveCommand::RemoveCommand(PlaylistModel& model, int row, QUndoCommand *parent)
@@ -195,6 +205,82 @@ void SortCommand::undo()
     } else {
         LOG_ERROR() << "failed to restore playlist from XML";
     }
+}
+
+TrimClipInCommand::TrimClipInCommand(PlaylistModel& model, int row, int in, QUndoCommand *parent)
+    : QUndoCommand(parent)
+    , m_model(model)
+    , m_row(row)
+    , m_oldIn(in)
+    , m_newIn(in)
+    , m_out(-1)
+{
+    setText(QObject::tr("Trim playlist item %1 in").arg(row + 1));
+    QScopedPointer<Mlt::ClipInfo> info(m_model.playlist()->clip_info(row));
+    if (info) {
+        m_oldIn = info->frame_in;
+        m_out = info->frame_out;
+    }
+}
+
+void TrimClipInCommand::redo()
+{
+    LOG_DEBUG() << "row" << m_row << "in" << m_newIn;
+    m_model.setInOut(m_row, m_newIn, m_out);
+}
+
+void TrimClipInCommand::undo()
+{
+    LOG_DEBUG() << "row" << m_row << "in" << m_oldIn;
+    m_model.setInOut(m_row, m_oldIn, m_out);
+}
+
+bool TrimClipInCommand::mergeWith(const QUndoCommand *other)
+{
+    const TrimClipInCommand* that = static_cast<const TrimClipInCommand*>(other);
+    LOG_DEBUG() << "this row" << m_row << "that row" << that->m_row;
+    if (that->id() != id() || that->m_row != m_row)
+        return false;
+    m_newIn = that->m_newIn;
+    return true;
+}
+
+TrimClipOutCommand::TrimClipOutCommand(PlaylistModel& model, int row, int out, QUndoCommand *parent)
+    : QUndoCommand(parent)
+    , m_model(model)
+    , m_row(row)
+    , m_in(-1)
+    , m_oldOut(out)
+    , m_newOut(out)
+{
+    setText(QObject::tr("Trim playlist item %1 out").arg(row + 1));
+    QScopedPointer<Mlt::ClipInfo> info(m_model.playlist()->clip_info(row));
+    if (info) {
+        m_in = info->frame_in;
+        m_oldOut = info->frame_out;
+    }
+}
+
+void TrimClipOutCommand::redo()
+{
+    LOG_DEBUG() << "row" << m_row << "out" << m_newOut;
+    m_model.setInOut(m_row, m_in, m_newOut);
+}
+
+void TrimClipOutCommand::undo()
+{
+    LOG_DEBUG() << "row" << m_row << "out" << m_oldOut;
+    m_model.setInOut(m_row, m_in, m_oldOut);
+}
+
+bool TrimClipOutCommand::mergeWith(const QUndoCommand *other)
+{
+    const TrimClipOutCommand* that = static_cast<const TrimClipOutCommand*>(other);
+    LOG_DEBUG() << "this row" << m_row << "that row" << that->m_row;
+    if (that->id() != id() || that->m_row != m_row)
+        return false;
+    m_newOut = that->m_newOut;
+    return true;
 }
 
 } // namespace Playlist

src/commands/playlistcommands.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2018 Meltytech, LLC
+ * Copyright (c) 2013-2019 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,6 +24,12 @@
 
 namespace Playlist
 {
+
+enum {
+    UndoIdTrimClipIn,
+    UndoIdTrimClipOut,
+    UndoIdUpdate
+};
 
 class AppendCommand : public QUndoCommand
 {
@@ -55,6 +61,9 @@ public:
     UpdateCommand(PlaylistModel& model, const QString& xml, int row, QUndoCommand * parent = 0);
     void redo();
     void undo();
+protected:
+    int id() const { return UndoIdUpdate; }
+    bool mergeWith(const QUndoCommand *other);
 private:
     PlaylistModel& m_model;
     QString m_newXml;
@@ -108,6 +117,40 @@ private:
     int m_column;
     Qt::SortOrder m_order;
     QString m_xml;
+};
+
+class TrimClipInCommand : public QUndoCommand
+{
+public:
+    TrimClipInCommand(PlaylistModel& model, int row, int in, QUndoCommand * parent = nullptr);
+    void redo();
+    void undo();
+protected:
+    int id() const { return UndoIdTrimClipIn; }
+    bool mergeWith(const QUndoCommand *other);
+private:
+    PlaylistModel& m_model;
+    int m_row;
+    int m_oldIn;
+    int m_newIn;
+    int m_out;
+};
+
+class TrimClipOutCommand : public QUndoCommand
+{
+public:
+    TrimClipOutCommand(PlaylistModel& model, int row, int out, QUndoCommand * parent = nullptr);
+    void redo();
+    void undo();
+protected:
+    int id() const { return UndoIdTrimClipOut; }
+    bool mergeWith(const QUndoCommand *other);
+private:
+    PlaylistModel& m_model;
+    int m_row;
+    int m_in;
+    int m_oldOut;
+    int m_newOut;
 };
 
 }

src/docks/playlistdock.h
@@ -59,6 +59,8 @@ public slots:
     void on_removeButton_clicked();
     void setUpdateButtonEnabled(bool modified);
     void onProducerOpened();
+    void onInChanged();
+    void onOutChanged();
 
 private slots:
     void on_menuButton_clicked();

src/mainwindow.cpp
@@ -288,6 +288,10 @@ MainWindow::MainWindow()
     connect(this, SIGNAL(producerOpened()), m_playlistDock, SLOT(onProducerOpened()));
     if (!Settings.playerGPU())
         connect(m_playlistDock->model(), SIGNAL(loaded()), this, SLOT(updateThumbnails()));
+    connect(m_player, &Player::inChanged, m_playlistDock, &PlaylistDock::onInChanged);
+    connect(m_player, &Player::outChanged, m_playlistDock, &PlaylistDock::onOutChanged);
+    connect(m_playlistDock->model(), &PlaylistModel::inChanged, m_player, &Player::setIn);
+    connect(m_playlistDock->model(), &PlaylistModel::outChanged, m_player, &Player::setOut);
 
     m_timelineDock = new TimelineDock(this);
     m_timelineDock->hide();

src/models/playlistmodel.cpp
@@ -731,3 +731,23 @@ void PlaylistModel::setPlaylist(Mlt::Playlist& playlist)
         emit loaded();
     }
 }
+
+void PlaylistModel::setInOut(int row, int in, int out)
+{
+    if (!m_playlist || row < 0 || row >= m_playlist->count()) return;
+    bool inChanged = false, outChanged = false;
+    QScopedPointer<Mlt::ClipInfo> info(m_playlist->clip_info(row));
+    if (info && info->producer && info->producer->is_valid()) {
+        if (MLT.producer()->get_producer() == info->producer->get_producer()) {
+            inChanged = info->frame_in != in;
+            outChanged = info->frame_out != out;
+        }
+        m_playlist->resize_clip(row, in, out);
+        QThreadPool::globalInstance()->start(
+            new UpdateThumbnailTask(this, *info->producer, in, out, row), 1);
+        emit dataChanged(createIndex(row, COLUMN_IN), createIndex(row, COLUMN_START));
+        emit modified();
+        if (inChanged) emit this->inChanged(in);
+        if (outChanged) emit this->outChanged(out);
+    }
+}

src/models/playlistmodel.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2012 Meltytech, LLC
- * Author: Dan Dennedy <dan@dennedy.org>
+ * Copyright (c) 2012-2019 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -87,6 +86,7 @@ public:
     void refreshThumbnails();
     Mlt::Playlist* playlist() { return m_playlist; }
     void setPlaylist(Mlt::Playlist& playlist);
+    void setInOut(int row, int in, int out);
 
     ViewMode viewMode() const;
     void setViewMode(ViewMode mode);
@@ -99,6 +99,8 @@ signals:
     void loaded();
     void dropped(const QMimeData *data, int row);
     void moveClip(int from, int to);
+    void inChanged(int in);
+    void outChanged(int out);
 
 public slots:
     void clear();

commit ce2cabeda75b93b9cf540188b69b315f32b7e08e

Fix crash regression in 45aa48f.

src/docks/playlistdock.cpp
@@ -453,6 +453,8 @@ void PlaylistDock::onProducerOpened()
 void PlaylistDock::onInChanged()
 {
     int index = MLT.producer()->get_int(kPlaylistIndexProperty) - 1;
+    if (index < 0 || !m_model.playlist() || !m_model.playlist()->is_valid())
+        return;
     QScopedPointer<Mlt::ClipInfo> info(m_model.playlist()->clip_info(index));
     if (info && info->producer
              && info->producer->get_producer() == MLT.producer()->get_producer()
@@ -465,6 +467,8 @@ void PlaylistDock::onInChanged()
 void PlaylistDock::onOutChanged()
 {
     int index = MLT.producer()->get_int(kPlaylistIndexProperty) - 1;
+    if (index < 0 || !m_model.playlist() || !m_model.playlist()->is_valid())
+        return;
     QScopedPointer<Mlt::ClipInfo> info(m_model.playlist()->clip_info(index));
     if (info && info->producer
              && info->producer->get_producer() == MLT.producer()->get_producer()