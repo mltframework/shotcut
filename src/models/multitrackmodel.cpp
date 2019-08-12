mltframework/shotcut

commit 7c77b49a132be0ecec6ffa36d4df7109b1979089

Fix filters added after transitions.
Previously, adding a filter to a clip involved in a transition did not
extend that filter into the time range of the transition, and the
Keyframes filter trimming did not make it possible to extend it either.
Now, it works as expected. Also, there were some general fixes for
filter in/out with undo.

https://forum.shotcut.org/t/error-in-transition-between-pictures-when-
working-with-keyframes/11540

src/models/multitrackmodel.cpp
@@ -453,7 +453,9 @@ int MultitrackModel::trimClipIn(int trackIndex, int clipIndex, int delta, bool r
         playlist.resize_clip(clipIndex, info->frame_in + delta, info->frame_out);
 
         // Adjust filters.
-        adjustClipFilters(*info->producer, info->frame_in, info->frame_out, delta, 0);
+        int in = info->producer->get(kFilterInProperty)? info->producer->get_int(kFilterInProperty) : info->frame_in;
+        int out = info->producer->get(kFilterOutProperty)? info->producer->get_int(kFilterOutProperty) : info->frame_out;
+        adjustClipFilters(*info->producer, in, out, delta, 0);
 
         QModelIndex modelIndex = createIndex(clipIndex, 0, i);
         QVector<int> roles;
@@ -628,7 +630,9 @@ int MultitrackModel::trimClipOut(int trackIndex, int clipIndex, int delta, bool 
         playlist.resize_clip(clipIndex, info->frame_in, info->frame_out - delta);
 
         // Adjust filters.
-        adjustClipFilters(*info->producer, info->frame_in, info->frame_out, 0, delta);
+        int in = info->producer->get(kFilterInProperty)? info->producer->get_int(kFilterInProperty) : info->frame_in;
+        int out = info->producer->get(kFilterOutProperty)? info->producer->get_int(kFilterOutProperty) : info->frame_out;
+        adjustClipFilters(*info->producer, in, out, 0, delta);
 
         QModelIndex index = createIndex(clipIndex, 0, i);
         QVector<int> roles;

src/commands/undohelper.cpp
@@ -118,6 +118,8 @@ void UndoHelper::recordAfterState()
                 if (info.frame_in != newInfo.frame_in || info.frame_out != newInfo.frame_out) {
                     UNDOLOG << "In/out changed:" << uid;
                     info.changes |= ClipInfoModified;
+                    info.in_delta = info.frame_in - newInfo.frame_in;
+                    info.out_delta = newInfo.frame_out - info.frame_out;
                 }
             }
             clipsRemoved.removeOne(uid);
@@ -205,14 +207,25 @@ void UndoHelper::undoChanges()
             UNDOLOG << "resizing clip at" << currentIndex;
             playlist.resize_clip(currentIndex, info.frame_in, info.frame_out);
 
+            QScopedPointer<Mlt::Producer> clip(playlist.get_clip(currentIndex));
+            int in = info.frame_in;
+            int out = info.frame_out;
+            if (clip && clip->is_valid()) {
+                if (clip->parent().get(kFilterInProperty))
+                    in = clip->parent().get_int(kFilterInProperty);
+                if (clip->parent().get(kFilterOutProperty))
+                    out = clip->parent().get_int(kFilterOutProperty);
+                m_model.adjustClipFilters(clip->parent(), in, out, info.in_delta, info.out_delta);
+            }
+
             QModelIndex modelIndex = m_model.createIndex(currentIndex, 0, info.oldTrackIndex);
             QVector<int> roles;
             roles << MultitrackModel::InPointRole;
             roles << MultitrackModel::OutPointRole;
             roles << MultitrackModel::DurationRole;
             emit m_model.dataChanged(modelIndex, modelIndex, roles);
-            QScopedPointer<Mlt::Producer> clip(playlist.get_clip(currentIndex));
-            AudioLevelsTask::start(clip->parent(), &m_model, modelIndex);
+            if (clip && clip->is_valid())
+                AudioLevelsTask::start(clip->parent(), &m_model, modelIndex);
         }
     }
 

src/commands/undohelper.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2015 Meltytech, LLC
- * Author: Harald Hvaal <harald.hvaal@gmail.com>
+ * Copyright (c) 2015-2019 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -60,6 +59,8 @@ private:
         QString xml;
         int frame_in;
         int frame_out;
+        int in_delta;
+        int out_delta;
 
         int changes;
         Info()
@@ -70,6 +71,8 @@ private:
             , isBlank(false)
             , frame_in(-1)
             , frame_out(-1)
+            , in_delta(0)
+            , out_delta(0)
             , changes(NoChange)
         {}
     };

src/docks/timelinedock.cpp
@@ -648,9 +648,24 @@ void TimelineDock::emitSelectedFromSelection()
         // We need to set these special properties so time-based filters
         // can get information about the cut while still applying filters
         // to the cut parent.
-        info->producer->set(kFilterInProperty, info->frame_in);
-        info->producer->set(kFilterOutProperty, info->frame_out);
-        info->producer->set(kPlaylistStartProperty, info->start);
+        QScopedPointer<Mlt::ClipInfo> info2(getClipInfo(trackIndex, clipIndex - 1));
+        if (info2 && info2->producer && info2->producer->is_valid()
+                  && info2->producer->get(kShotcutTransitionProperty)) {
+            // Factor in a transition left of the clip.
+            info->producer->set(kFilterInProperty, info->frame_in - info2->frame_count);
+            info->producer->set(kPlaylistStartProperty, info2->start);
+        } else {
+            info->producer->set(kFilterInProperty, info->frame_in);
+            info->producer->set(kPlaylistStartProperty, info->start);
+        }
+        info2.reset(getClipInfo(trackIndex, clipIndex + 1));
+        if (info2 && info2->producer && info2->producer->is_valid()
+                  && info2->producer->get(kShotcutTransitionProperty)) {
+            // Factor in a transition right of the clip.
+            info->producer->set(kFilterOutProperty, info->frame_out + info2->frame_count);
+        } else {
+            info->producer->set(kFilterOutProperty, info->frame_out);
+        }
         info->producer->set(kMultitrackItemProperty, QString("%1:%2").arg(clipIndex).arg(trackIndex).toLatin1().constData());
         m_ignoreNextPositionChange = true;
         emit selected(info->producer);

src/mainwindow.cpp
@@ -2596,18 +2596,35 @@ void MainWindow::onMultitrackModified()
 
     // Reflect this playlist info onto the producer for keyframes dock.
     if (!m_timelineDock->selection().isEmpty()) {
+        int trackIndex = m_timelineDock->currentTrack();
         int clipIndex = m_timelineDock->selection().first();
-        QScopedPointer<Mlt::ClipInfo> info(m_timelineDock->getClipInfo(m_timelineDock->currentTrack(), clipIndex));
+        QScopedPointer<Mlt::ClipInfo> info(m_timelineDock->getClipInfo(trackIndex, clipIndex));
         if (info && info->producer && info->producer->is_valid()) {
-            info->producer->set(kPlaylistStartProperty, info->start);
-            if (info->frame_in != info->producer->get_int(kFilterInProperty)) {
-                info->producer->set(kFilterInProperty, info->frame_in);
-                int delta = info->frame_in - info->producer->get_int(kFilterInProperty);
+            int expected = info->frame_in;
+            QScopedPointer<Mlt::ClipInfo> info2(m_timelineDock->getClipInfo(trackIndex, clipIndex - 1));
+            if (info2 && info2->producer && info2->producer->is_valid()
+                      && info2->producer->get(kShotcutTransitionProperty)) {
+                // Factor in a transition left of the clip.
+                expected -= info2->frame_count;
+                info->producer->set(kPlaylistStartProperty, info2->start);
+            } else {
+                info->producer->set(kPlaylistStartProperty, info->start);
+            }
+            if (expected != info->producer->get_int(kFilterInProperty)) {
+                int delta = expected - info->producer->get_int(kFilterInProperty);
+                info->producer->set(kFilterInProperty, expected);
                 emit m_filtersDock->producerInChanged(delta);
             }
-            if (info->frame_out != info->producer->get_int(kFilterOutProperty)) {
-                info->producer->set(kFilterOutProperty, info->frame_out);
-                int delta = info->frame_out - info->producer->get_int(kFilterOutProperty);
+            expected = info->frame_out;
+            info2.reset(m_timelineDock->getClipInfo(trackIndex, clipIndex + 1));
+            if (info2 && info2->producer && info2->producer->is_valid()
+                      && info2->producer->get(kShotcutTransitionProperty)) {
+                // Factor in a transition right of the clip.
+                expected += info2->frame_count;
+            }
+            if (expected != info->producer->get_int(kFilterOutProperty)) {
+                int delta = expected - info->producer->get_int(kFilterOutProperty);
+                info->producer->set(kFilterOutProperty, expected);
                 emit m_filtersDock->producerOutChanged(delta);
             }
         }

src/models/multitrackmodel.h
@@ -108,6 +108,7 @@ public:
     void insertTrack(int trackIndex, TrackType type = VideoTrackType);
     void insertOrAdjustBlankAt(QList<int> tracks, int position, int length);
     bool mergeClipWithNext(int trackIndex, int clipIndex, bool dryrun);
+    void adjustClipFilters(Mlt::Producer& producer, int in, int out, int inDelta, int outDelta);
 
 signals:
     void created();
@@ -201,7 +202,6 @@ private:
 private slots:
     void adjustBackgroundDuration();
     void adjustTrackFilters();
-    void adjustClipFilters(Mlt::Producer& producer, int in, int out, int inDelta, int outDelta);
 };
 
 #endif // MULTITRACKMODEL_H

commit caca2b58a0fdef3bd1657aa05c9b327916680582

Fix adjusting filter in/out on unselected clips.
Regression in 7c77b49a132be0ecec6ffa36d4df7109b1979089

https://forum.shotcut.org/t/in-ver-19-07-15-filters-become-affected-by-
resizing-clip/12460

src/models/multitrackmodel.cpp
@@ -456,6 +456,8 @@ int MultitrackModel::trimClipIn(int trackIndex, int clipIndex, int delta, bool r
         int in = info->producer->get(kFilterInProperty)? info->producer->get_int(kFilterInProperty) : info->frame_in;
         int out = info->producer->get(kFilterOutProperty)? info->producer->get_int(kFilterOutProperty) : info->frame_out;
         adjustClipFilters(*info->producer, in, out, delta, 0);
+        if (info->producer->get(kFilterInProperty))
+            info->producer->set(kFilterInProperty, in + delta);
 
         QModelIndex modelIndex = createIndex(clipIndex, 0, i);
         QVector<int> roles;
@@ -634,6 +636,8 @@ int MultitrackModel::trimClipOut(int trackIndex, int clipIndex, int delta, bool 
         int in = info->producer->get(kFilterInProperty)? info->producer->get_int(kFilterInProperty) : info->frame_in;
         int out = info->producer->get(kFilterOutProperty)? info->producer->get_int(kFilterOutProperty) : info->frame_out;
         adjustClipFilters(*info->producer, in, out, 0, delta);
+        if (info->producer->get(kFilterOutProperty))
+            info->producer->set(kFilterOutProperty, out - delta);
 
         QModelIndex index = createIndex(clipIndex, 0, i);
         QVector<int> roles;