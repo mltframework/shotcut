mltframework/shotcut

commit a078f3e1476d975f9f69df2e3545936ed223b932

Refactor to isPaused().

src/mltcontroller.cpp
@@ -222,9 +222,14 @@ void Controller::play(double speed)
     setVolume(m_volume);
 }
 
+bool Controller::isPaused() const
+{
+    return m_producer && qAbs(m_producer->get_speed()) < 0.1;
+}
+
 void Controller::pause()
 {
-    if (m_producer && m_producer->get_speed() != 0) {
+    if (m_producer && !isPaused()) {
         m_producer->set_speed(0);
         if (m_consumer && m_consumer->is_valid()) {
             m_producer->seek(m_consumer->position() + 1);
@@ -285,7 +290,7 @@ void Controller::onJackStopped(int position)
         --m_skipJackEvents;
     } else {
         if (m_producer) {
-            if (m_producer->get_speed() != 0) {
+            if (!isPaused()) {
                 Event *event = m_consumer->setup_wait_for("consumer-sdl-paused");
                 int result = m_producer->set_speed(0);
                 if (result == 0 && m_consumer->is_valid() && !m_consumer->is_stopped())
@@ -380,7 +385,7 @@ void Controller::setVolume(double volume, bool muteOnPause)
     m_volume = volume;
 
     // Keep the consumer muted when paused
-    if (muteOnPause && m_producer && m_producer->get_speed() == 0) {
+    if (muteOnPause && !isPaused()) {
         volume = 0.0;
     }
 

src/mltcontroller.h
@@ -71,6 +71,7 @@ public:
 
     void closeConsumer();
     virtual void play(double speed = 1.0);
+    bool isPaused() const;
     virtual void pause();
     void stop();
     bool enableJack(bool enable = true);

commit 78004ce7ec4bcb2e1d5fb2654a3202a284e86bc3

Fix audio playback regression in a078f3e1.

src/mltcontroller.cpp
@@ -382,7 +382,7 @@ void Controller::setVolume(double volume, bool muteOnPause)
     m_volume = volume;
 
     // Keep the consumer muted when paused
-    if (muteOnPause && !isPaused()) {
+    if (muteOnPause && isPaused()) {
         volume = 0.0;
     }
 