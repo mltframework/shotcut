mltframework/shotcut

commit ad9709b0005d5f076b18ba958f549890a8a32b10

Fix rectangle control when switching filters that both have VUI.
See #4 here https://forum.shotcut.org/t/v19-08-beta-now-available/
12358/2

src/qml/filters/size_position/SizePositionVUI.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2018 Meltytech, LLC
+ * Copyright (c) 2014-2019 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@ VuiBase {
     property string halignProperty
     property string valignProperty
     property real zoom: (video.zoom > 0)? video.zoom : 1.0
-    property rect filterRect
+    property rect filterRect: Qt.rect(-1, -1, -1, -1)
     property bool blockUpdate: false
     property string startValue: '_shotcut:startValue'
     property string middleValue: '_shotcut:middleValue'
@@ -47,11 +47,11 @@ VuiBase {
     function setRectangleControl() {
         if (blockUpdate) return
         var position = getPosition()
-        var newValue = filter.getRect(rectProperty, position)
-        if (filterRect !== newValue) {
-            filterRect = newValue
-            rectangle.setHandles(filterRect)
-        }
+            var newValue = filter.getRect(rectProperty, position)
+            if (filterRect !== newValue) {
+                filterRect = newValue
+                rectangle.setHandles(filterRect)
+            }
         rectangle.enabled = position <= 0 || (position >= (filter.animateIn - 1) && position <= (filter.duration - filter.animateOut)) || position >= (filter.duration - 1)
     }
 

src/qml/modules/Shotcut/Controls/RectangleControl.qml
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2014-2018 Meltytech, LLC
- * Author: Dan Dennedy <dan@dennedy.org>
+ * Copyright (c) 2014-2019 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -34,6 +33,8 @@ Item {
     signal rectChanged(Rectangle rect)
 
     function setHandles(rect) {
+        if (rect.x < 0 || rect.y < 0 || rect.width < 0 || rect.height < 0)
+            return
         topLeftHandle.x = (rect.x * widthScale)
         topLeftHandle.y = (rect.y * heightScale)
         if (aspectRatio === 0.0) {

src/qml/modules/Shotcut/Controls/TextFilterVui.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2018 Meltytech, LLC
+ * Copyright (c) 2014-2019 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,15 +24,13 @@ VuiBase {
     property string valignProperty: 'halign'
     property string useFontSizeProperty: 'shotcut:usePointSize'
     property real zoom: (video.zoom > 0)? video.zoom : 1.0
-    property rect filterRect
+    property rect filterRect: Qt.rect(-1, -1, -1, -1)
     property bool blockUpdate: false
     property string startValue: '_shotcut:startValue'
     property string middleValue: '_shotcut:middleValue'
     property string endValue:  '_shotcut:endValue'
 
     Component.onCompleted: {
-        filterRect = filter.getRect(rectProperty, getPosition())
-        rectangle.setHandles(filterRect)
         setRectangleControl()
     }
 
@@ -43,11 +41,11 @@ VuiBase {
     function setRectangleControl() {
         if (blockUpdate) return
         var position = getPosition()
-        var newValue = filter.getRect(rectProperty, position)
-        if (filterRect !== newValue) {
-            filterRect = newValue
-            rectangle.setHandles(filterRect)
-        }
+            var newValue = filter.getRect(rectProperty, position)
+            if (filterRect !== newValue) {
+                filterRect = newValue
+                rectangle.setHandles(filterRect)
+            }
         rectangle.enabled = position <= 0 || (position >= (filter.animateIn - 1) && position <= (filter.duration - filter.animateOut)) || position >= (filter.duration - 1)
     }
 

src/controllers/filtercontroller.cpp
@@ -143,6 +143,7 @@ void FilterController::setCurrentFilter(int attachedIndex, bool isNew)
     QmlMetadata* meta = m_attachedModel.getMetadata(m_currentFilterIndex);
     QmlFilter* filter = 0;
     if (meta) {
+        emit currentFilterChanged(nullptr, nullptr, -1);
         m_mltFilter = m_attachedModel.getFilter(m_currentFilterIndex);
         filter = new QmlFilter(*m_mltFilter, meta);
         filter->setIsNew(isNew);

commit 66d692b684c4b46b91d19a4b183612259c0e7b17

Fix rectangle VUI regression in ad9709b.
https://forum.shotcut.org/t/filters-with-rectangle-control-bug/12717

src/qml/filters/size_position/SizePositionVUI.qml
@@ -47,11 +47,11 @@ VuiBase {
     function setRectangleControl() {
         if (blockUpdate) return
         var position = getPosition()
-            var newValue = filter.getRect(rectProperty, position)
-            if (filterRect !== newValue) {
-                filterRect = newValue
-                rectangle.setHandles(filterRect)
-            }
+        var newValue = filter.getRect(rectProperty, position)
+        if (filterRect !== newValue) {
+            filterRect = newValue
+            rectangle.setHandles(filterRect)
+        }
         rectangle.enabled = position <= 0 || (position >= (filter.animateIn - 1) && position <= (filter.duration - filter.animateOut)) || position >= (filter.duration - 1)
     }
 

src/qml/modules/Shotcut/Controls/RectangleControl.qml
@@ -33,7 +33,7 @@ Item {
     signal rectChanged(Rectangle rect)
 
     function setHandles(rect) {
-        if (rect.x < 0 || rect.y < 0 || rect.width < 0 || rect.height < 0)
+        if ( rect.width < 0 || rect.height < 0)
             return
         topLeftHandle.x = (rect.x * widthScale)
         topLeftHandle.y = (rect.y * heightScale)

src/qml/modules/Shotcut/Controls/TextFilterVui.qml
@@ -41,11 +41,11 @@ VuiBase {
     function setRectangleControl() {
         if (blockUpdate) return
         var position = getPosition()
-            var newValue = filter.getRect(rectProperty, position)
-            if (filterRect !== newValue) {
-                filterRect = newValue
-                rectangle.setHandles(filterRect)
-            }
+        var newValue = filter.getRect(rectProperty, position)
+        if (filterRect !== newValue) {
+            filterRect = newValue
+            rectangle.setHandles(filterRect)
+        }
         rectangle.enabled = position <= 0 || (position >= (filter.animateIn - 1) && position <= (filter.duration - filter.animateOut)) || position >= (filter.duration - 1)
     }
 