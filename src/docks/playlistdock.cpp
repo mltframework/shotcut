mltframework/shotcut

commit ccab283cc201be78cd07e866edd16e5ccabe6998

fix changing playlist item properties not updating playlist
For example, image duration, which also changes when clicking the image
sequence checkbox. This addresses the use case illustrated in #784, but
also other Properties widgets such as AvformatProducerWidget may create
a new MLT producer on some changes without informing the playlist.

src/docks/playlistdock.cpp
@@ -261,15 +261,19 @@ void PlaylistDock::setIndex(int row)
 void PlaylistDock::moveClipUp()
 {
     int row = m_view->currentIndex().row();
-    if (row > 0)
+    if (row > 0) {
         MAIN.undoStack()->push(new Playlist::MoveCommand(m_model, row, row - 1));
+        static_cast<Mlt::Properties>(MLT.producer()).clear(kPlaylistIndexProperty);
+    }
 }
 
 void PlaylistDock::moveClipDown()
 {
     int row = m_view->currentIndex().row();
-    if (row + 1 < m_model.rowCount())
+    if (row + 1 < m_model.rowCount()) {
         MAIN.undoStack()->push(new Playlist::MoveCommand(m_model, row, row + 1));
+        static_cast<Mlt::Properties>(MLT.producer()).clear(kPlaylistIndexProperty);
+    }
 }
 
 void PlaylistDock::on_menuButton_clicked()
@@ -341,7 +345,7 @@ void PlaylistDock::on_actionAppendCut_triggered()
                 new Playlist::AppendCommand(m_model,
                     MLT.XML(MLT.isClip()? nullptr : MLT.savedProducer())));
             MLT.producer()->set(kPlaylistIndexProperty, m_model.playlist()->count());
-            setUpdateButtonEnabled(false);
+            setUpdateButtonEnabled(true);
         } else {
             DurationDialog dialog(this);
             dialog.setDuration(MLT.profile().fps() * 5);
@@ -350,6 +354,8 @@ void PlaylistDock::on_actionAppendCut_triggered()
                 if (MLT.producer()->get("mlt_service") && !strcmp(MLT.producer()->get("mlt_service"), "avformat"))
                     MLT.producer()->set("mlt_service", "avformat-novalidate");
                 MAIN.undoStack()->push(new Playlist::AppendCommand(m_model, MLT.XML()));
+                MLT.producer()->set(kPlaylistIndexProperty, m_model.playlist()->count());
+                setUpdateButtonEnabled(true);
             }
         }
     }
@@ -397,6 +403,8 @@ void PlaylistDock::on_actionUpdate_triggered()
                 if (MLT.producer()->get("mlt_service") && !strcmp(MLT.producer()->get("mlt_service"), "avformat"))
                     MLT.producer()->set("mlt_service", "avformat-novalidate");
                 MAIN.undoStack()->push(new Playlist::UpdateCommand(m_model, MLT.XML(), index.row()));
+                MLT.producer()->set(kPlaylistIndexProperty, index.row() + 1);
+                setUpdateButtonEnabled(true);
             }
         }
     }
@@ -432,7 +440,7 @@ void PlaylistDock::on_removeButton_clicked()
         MAIN.undoStack()->endMacro();
     if (rowsRemoved.contains(MLT.producer()->get_int(kPlaylistIndexProperty))) {
         // Remove the playlist index property on the producer.
-        MLT.producer()->set(kPlaylistIndexProperty, nullptr, 0);
+        static_cast<Mlt::Properties>(MLT.producer()).clear(kPlaylistIndexProperty);
         setUpdateButtonEnabled(false);
     }
 }
@@ -508,15 +516,8 @@ void PlaylistDock::viewCustomContextMenuRequested(const QPoint &pos)
         menu.addAction(ui->actionCopy);
         if (MLT.isClip())
             menu.addAction(ui->actionInsertCut);
-
-        QScopedPointer<Mlt::ClipInfo> info(m_model.playlist()->clip_info(index.row()));
-        if (info && MLT.producer()->get_int(kPlaylistIndexProperty) == index.row() + 1) {
-            if (MLT.producer()->get_in() != info->frame_in || MLT.producer()->get_out() != info->frame_out)
-                menu.addAction(ui->actionUpdate);
-        }
-
+        menu.addAction(ui->actionUpdate);
         menu.addAction(ui->actionSetFileDate);
-
         menu.exec(mapToGlobal(pos));
     }
 }
@@ -551,16 +552,19 @@ void PlaylistDock::on_actionGoto_triggered()
 
 void PlaylistDock::on_actionRemoveAll_triggered()
 {
+    static_cast<Mlt::Properties>(MLT.producer()).clear(kPlaylistIndexProperty);
     MAIN.undoStack()->push(new Playlist::ClearCommand(m_model));
 }
 
 void PlaylistDock::on_actionSortByName_triggered()
 {
+    static_cast<Mlt::Properties>(MLT.producer()).clear(kPlaylistIndexProperty);
     MAIN.undoStack()->push(new Playlist::SortCommand(m_model, PlaylistModel::COLUMN_RESOURCE, Qt::AscendingOrder));
 }
 
 void PlaylistDock::on_actionSortByDate_triggered()
 {
+    static_cast<Mlt::Properties>(MLT.producer()).clear(kPlaylistIndexProperty);
     MAIN.undoStack()->push(new Playlist::SortCommand(m_model, PlaylistModel::COLUMN_DATE, Qt::AscendingOrder));
 }
 
@@ -685,12 +689,14 @@ void PlaylistDock::onDropped(const QMimeData *data, int row)
             }
         }
     }
+    static_cast<Mlt::Properties>(MLT.producer()).clear(kPlaylistIndexProperty);
 }
 
 void PlaylistDock::onMoveClip(int from, int to)
 {
     MAIN.undoStack()->push(new Playlist::MoveCommand(m_model, from, to));
     m_view->clearSelection();
+    static_cast<Mlt::Properties>(MLT.producer()).clear(kPlaylistIndexProperty);
 }
 
 void PlaylistDock::onPlayerDragStarted()
@@ -788,6 +794,16 @@ void PlaylistDock::on_updateButton_clicked()
         setUpdateButtonEnabled(false);
     }
 }
+
+void PlaylistDock::onProducerChanged(Mlt::Producer* producer)
+{
+    int index = producer->get_int(kPlaylistIndexProperty) - 1;
+    if (index < 0 || !m_model.playlist() || !m_model.playlist()->is_valid() || index >= m_model.playlist()->count())
+        return;
+    MAIN.undoStack()->push(new Playlist::UpdateCommand(m_model, MLT.XML(producer), index));
+    setUpdateButtonEnabled(false);
+}
+
 
 void PlaylistDock::updateViewModeFromActions()
 {

src/docks/playlistdock.h
@@ -65,6 +65,7 @@ public slots:
     void on_actionCopy_triggered();
     void on_actionSelectAll_triggered();
     void on_actionSelectNone_triggered();
+    void onProducerChanged(Mlt::Producer* producer);
 
 private slots:
     void on_menuButton_clicked();

src/mainwindow.cpp
@@ -2953,6 +2953,7 @@ QWidget *MainWindow::loadProducerWidget(Mlt::Producer* producer)
         if (-1 != w->metaObject()->indexOfSignal("producerChanged(Mlt::Producer*)")) {
             connect(w, SIGNAL(producerChanged(Mlt::Producer*)), SLOT(onProducerChanged()));
             connect(w, SIGNAL(producerChanged(Mlt::Producer*)), m_filterController, SLOT(setProducer(Mlt::Producer*)));
+            connect(w, SIGNAL(producerChanged(Mlt::Producer*)), m_playlistDock, SLOT(onProducerChanged(Mlt::Producer*)));
             if (producer->get(kMultitrackItemProperty))
                 connect(w, SIGNAL(producerChanged(Mlt::Producer*)), m_timelineDock, SLOT(onProducerChanged(Mlt::Producer*)));
         }

commit e137bf3f73eb6897301ca8f2c893948e14e6ce0c

fix #800 crash regression in ccab283

src/docks/playlistdock.cpp
@@ -874,7 +874,7 @@ void PlaylistDock::setViewMode(PlaylistModel::ViewMode mode)
 void PlaylistDock::resetPlaylistIndex()
 {
     if (MLT.producer())
-        static_cast<Mlt::Properties>(MLT.producer()).clear(kPlaylistIndexProperty);
+        MLT.producer()->set(kPlaylistIndexProperty, nullptr, 0);
 }
 
 #include "playlistdock.moc"