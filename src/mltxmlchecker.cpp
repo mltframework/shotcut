mltframework/shotcut

commit f2bcc8c647fdac53135780144fc3f3b073103adf

add alpha_over=1 property to luma transitions
This addresses with a MLT change that added a property named
`alpha_over` to fix a behavior regression on the alpha blending. The
property switches it to using the over mode of blending for the alpha
channel in addition to the color channels that Shotcut prefers.

src/mltxmlchecker.cpp
@@ -56,6 +56,7 @@ MltXmlChecker::MltXmlChecker()
     , m_needsCPU(false)
     , m_hasEffects(false)
     , m_isCorrected(false)
+    , m_isUpdated(false)
     , m_usesLocale(false)
     , m_decimalPoint('.')
     , m_numericValueChanged(false)
@@ -245,6 +246,7 @@ void MltXmlChecker::processProperties()
         checkCpuEffects(mlt_service);
         checkUnlinkedFile(mlt_service);
         checkIncludesSelf(newProperties);
+        checkLumaAlphaOver(mlt_service, newProperties);
 
         // Second pass: amend property values.
         m_properties = newProperties;
@@ -481,5 +483,21 @@ void MltXmlChecker::checkIncludesSelf(QVector<MltProperty>& properties)
         }
         properties << MltProperty(kShotcutCaptionProperty, "INVALID");
         m_isCorrected = true;
+    }
+}
+
+void MltXmlChecker::checkLumaAlphaOver(const QString& mlt_service, QVector<MltXmlChecker::MltProperty>& properties)
+{
+    if (mlt_service == "luma") {
+        bool found = false;
+        for (auto& p : properties) {
+            if (p.first == "alpha_over") {
+                found = true;
+            }
+        }
+        if (!found) {
+            properties << MltProperty("alpha_over", "1");
+            m_isUpdated = true;
+        }
     }
 }

src/mainwindow.cpp
@@ -1262,7 +1262,8 @@ void MainWindow::open(QString url, const Mlt::Properties* properties)
         checker.setLocale();
         LOG_INFO() << "decimal point" << MLT.decimalPoint();
     }
-    if (!MLT.open(QDir::fromNativeSeparators(url))) {
+    QString urlToOpen = checker.isUpdated()? checker.tempFileName() : url;
+    if (!MLT.open(QDir::fromNativeSeparators(urlToOpen), QDir::fromNativeSeparators(url))) {
         Mlt::Properties* props = const_cast<Mlt::Properties*>(properties);
         if (props && props->is_valid())
             mlt_properties_inherit(MLT.producer()->get_properties(), props->get_properties());

src/mltcontroller.cpp
@@ -99,7 +99,7 @@ static bool isFpsDifferent(double a, double b)
     return qAbs(a - b) > 0.001;
 }
 
-int Controller::open(const QString &url)
+int Controller::open(const QString &url, const QString& urlToSave)
 {
     int error = 0;
 
@@ -143,7 +143,7 @@ int Controller::open(const QString &url)
         if (m_url.isEmpty() && QString(m_producer->get("xml")) == "was here") {
             if (m_producer->get_int("_original_type") != tractor_type ||
                (m_producer->get_int("_original_type") == tractor_type && m_producer->get(kShotcutXmlProperty)))
-                m_url = url;
+                m_url = urlToSave;
         }
         setImageDurationFromDefault(m_producer.data());
         lockCreationTime(m_producer.data());

src/mltcontroller.h
@@ -72,7 +72,7 @@ public:
 
     virtual QObject* videoWidget() = 0;
     virtual int setProducer(Mlt::Producer*, bool isMulti = false);
-    virtual int open(const QString& url);
+    virtual int open(const QString& url, const QString& urlToSave);
     bool openXML(const QString& filename);
     virtual void close();
     virtual int displayWidth() const = 0;

src/mltxmlchecker.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2014-2016 Meltytech, LLC
- * Author: Dan Dennedy <dan@dennedy.org>
+ * Copyright (c) 2014-2019 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -51,6 +50,7 @@ public:
     bool needsCPU() const { return m_needsCPU; }
     bool hasEffects() const { return m_hasEffects; }
     bool isCorrected() const { return m_isCorrected; }
+    bool isUpdated() const { return m_isUpdated; }
     QString tempFileName() const { return m_tempFile->fileName(); }
     QStandardItemModel& unlinkedFilesModel() { return m_unlinkedFilesModel; }
     void setLocale();
@@ -72,6 +72,7 @@ private:
     void fixStreamIndex(QString& value);
     bool fixVersion1701WindowsPathBug(QString& value);
     void checkIncludesSelf(QVector<MltProperty>& properties);
+    void checkLumaAlphaOver(const QString& mlt_service, QVector<MltProperty>& properties);
 
     QXmlStreamReader m_xml;
     QXmlStreamWriter m_newXml;
@@ -79,6 +80,7 @@ private:
     bool m_needsCPU;
     bool m_hasEffects;
     bool m_isCorrected;
+    bool m_isUpdated;
     bool m_usesLocale;
     QChar m_decimalPoint;
     QScopedPointer<QTemporaryFile> m_tempFile;

src/models/multitrackmodel.cpp
@@ -1619,6 +1619,7 @@ int MultitrackModel::addTransition(int trackIndex, int clipIndex, int position, 
             // Add transitions
             Mlt::Transition dissolve(MLT.profile(), Settings.playerGPU()? "movit.luma_mix" : "luma");
             Mlt::Transition crossFade(MLT.profile(), "mix:-1");
+            if (!Settings.playerGPU()) dissolve.set("alpha_over", 1);
             playlist.mix_add(targetIndex + 1, &dissolve);
             playlist.mix_add(targetIndex + 1, &crossFade);
 
@@ -1942,6 +1943,7 @@ void MultitrackModel::addTransitionByTrimIn(int trackIndex, int clipIndex, int d
             // Add transitions.
             Mlt::Transition dissolve(MLT.profile(), Settings.playerGPU()? "movit.luma_mix" : "luma");
             Mlt::Transition crossFade(MLT.profile(), "mix:-1");
+            if (!Settings.playerGPU()) dissolve.set("alpha_over", 1);
             playlist.mix_add(clipIndex, &dissolve);
             playlist.mix_add(clipIndex, &crossFade);
 
@@ -2017,6 +2019,7 @@ void MultitrackModel::addTransitionByTrimOut(int trackIndex, int clipIndex, int 
             // Add transitions.
             Mlt::Transition dissolve(MLT.profile(), Settings.playerGPU()? "movit.luma_mix" : "luma");
             Mlt::Transition crossFade(MLT.profile(), "mix:-1");
+            if (!Settings.playerGPU()) dissolve.set("alpha_over", 1);
             playlist.mix_add(clipIndex + 1, &dissolve);
             playlist.mix_add(clipIndex + 1, &crossFade);
 

commit 97ae6c8f27e4d56431e306297a2830983c611fa8

fix loading project with relative paths
regression in f2bcc8c

src/mltxmlchecker.cpp
@@ -69,7 +69,8 @@ bool MltXmlChecker::check(const QString& fileName)
     LOG_DEBUG() << "begin";
 
     QFile file(fileName);
-    m_tempFile.reset(Util::writableTemporaryFile("", "shotcut-XXXXXX.mlt"));
+    m_tempFile.reset(new QTemporaryFile(
+        QFileInfo(fileName).dir().filePath("shotcut-XXXXXX.mlt")));
     if (file.open(QIODevice::ReadOnly | QIODevice::Text) && m_tempFile->open()) {
         m_tempFile->resize(0);
         m_fileInfo = QFileInfo(fileName);