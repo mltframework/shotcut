mltframework/shotcut

commit a2acc0d367e1d4edcc890c94b44bd47ca74ce80a

Add mult-select for timeline.
This is currently limited to lift and ripple delete, but more is
planned. Also, range selection (shift) is limited to a single track but
not extended/toggle selection (ctrl).

src/qml/views/timeline/Clip.qml
@@ -46,7 +46,7 @@ Rectangle {
     property string audioIndex: ''
     property bool isTrackMute: false
 
-    signal clicked(var clip)
+    signal clicked(var clip, var mouse)
     signal moved(var clip)
     signal dragged(var clip, var mouse)
     signal dropped(var clip)
@@ -302,7 +302,7 @@ Rectangle {
             originalClipIndex = index
             startX = parent.x
             clipRoot.forceActiveFocus();
-            clipRoot.clicked(clipRoot)
+            clipRoot.clicked(clipRoot, mouse)
         }
         onPositionChanged: {
             if (mouse.y < 0 && trackIndex > 0)
@@ -347,7 +347,7 @@ Rectangle {
             onClicked: {
                 timeline.position = timeline.position // pause
                 clipRoot.forceActiveFocus();
-                clipRoot.clicked(clipRoot)
+                clipRoot.clicked(clipRoot, mouse)
                 menu.show()
             }
         }
@@ -680,7 +680,7 @@ Rectangle {
             text: qsTr('Properties')
             onTriggered: {
                 clipRoot.forceActiveFocus()
-                clipRoot.clicked(clipRoot)
+                clipRoot.clicked(clipRoot, mouse)
                 timeline.openProperties()
             }
         }

src/qml/views/timeline/timeline.qml
@@ -81,8 +81,6 @@ Rectangle {
     property bool stopScrolling: false
     property color shotcutBlue: Qt.rgba(23/255, 92/255, 118/255, 1.0)
 
-    onCurrentTrackChanged: timeline.selection = []
-
     MouseArea {
         anchors.fill: parent
         acceptedButtons: Qt.RightButton
@@ -291,14 +289,14 @@ Rectangle {
             CornerSelectionShadow {
                 y: tracksRepeater.count ? tracksRepeater.itemAt(currentTrack).y + ruler.height - scrollView.flickableItem.contentY : 0
                 clip: timeline.selection.length ?
-                        tracksRepeater.itemAt(currentTrack).clipAt(timeline.selection[0]) : null
+                        tracksRepeater.itemAt(timeline.selection[0].y).clipAt(timeline.selection[0].x) : null
                 opacity: clip && clip.x + clip.width < scrollView.flickableItem.contentX ? 1 : 0
             }
 
             CornerSelectionShadow {
                 y: tracksRepeater.count ? tracksRepeater.itemAt(currentTrack).y + ruler.height - scrollView.flickableItem.contentY : 0
                 clip: timeline.selection.length ?
-                        tracksRepeater.itemAt(currentTrack).clipAt(timeline.selection[timeline.selection.length - 1]) : null
+                        tracksRepeater.itemAt(timeline.selection[timeline.selection.length - 1].y).clipAt(timeline.selection[timeline.selection.length - 1].x) : null
                 opacity: clip && clip.x > scrollView.flickableItem.contentX + scrollView.width ? 1 : 0
                 anchors.right: parent.right
                 mirrorGradient: true
@@ -507,10 +505,16 @@ Rectangle {
             isMute: mute
             isCurrentTrack: currentTrack === index
             timeScale: multitrack.scaleFactor
-            selection: timeline.selection
             onClipClicked: {
-                currentTrack = track.DelegateModel.itemsIndex
-                timeline.selection = [ clip.DelegateModel.itemsIndex ];
+                var trackIndex = track.DelegateModel.itemsIndex
+                var clipIndex = clip.DelegateModel.itemsIndex
+                currentTrack = trackIndex
+                if (mouse.modifiers & Qt.ControlModifier)
+                    timeline.selection = Logic.toggleSelection(trackIndex, clipIndex)
+                else if (mouse.modifiers & Qt.ShiftModifier)
+                    timeline.selection = Logic.selectRange(trackIndex, clipIndex)
+                else // select one
+                    timeline.selection = [Qt.point(clipIndex, trackIndex)]
                 root.clipClicked()
             }
             onClipDragged: {

src/docks/playlistdock.cpp
@@ -405,7 +405,7 @@ void PlaylistDock::on_removeButton_clicked()
     if (!m_model.playlist() || !m_view->selectionModel()) return;
     QList<int> rowsRemoved;
     int n = m_view->selectionModel()->selectedIndexes().size();
-    if (n > 0)
+    if (n > 1)
         MAIN.undoStack()->beginMacro(tr("Remove %1 playlist items").arg(n));
     foreach (auto index, m_view->selectionModel()->selectedIndexes()) {
         int row = index.row();
@@ -421,7 +421,7 @@ void PlaylistDock::on_removeButton_clicked()
                 MAIN.undoStack()->push(new Playlist::RemoveCommand(m_model, row));
         }
     }
-    if (n > 0)
+    if (n > 1)
         MAIN.undoStack()->endMacro();
     if (rowsRemoved.contains(MLT.producer()->get_int(kPlaylistIndexProperty))) {
         // Remove the playlist index property on the producer.

src/docks/timelinedock.cpp
@@ -98,7 +98,7 @@ void TimelineDock::setPosition(int position)
 
 Mlt::ClipInfo *TimelineDock::getClipInfo(int trackIndex, int clipIndex)
 {
-    Mlt::ClipInfo* result = 0;
+    Mlt::ClipInfo* result = nullptr;
     if (clipIndex >= 0 && trackIndex >= 0) {
         int i = m_model.trackList().at(trackIndex).mlt_index;
         QScopedPointer<Mlt::Producer> track(m_model.tractor()->track(i));
@@ -112,7 +112,7 @@ Mlt::ClipInfo *TimelineDock::getClipInfo(int trackIndex, int clipIndex)
 
 Mlt::Producer *TimelineDock::producerForClip(int trackIndex, int clipIndex)
 {
-    Mlt::Producer* result = 0;
+    Mlt::Producer* result = nullptr;
     Mlt::ClipInfo* info = getClipInfo(trackIndex, clipIndex);
     if (info) {
         result = new Mlt::Producer(info->producer);
@@ -243,7 +243,15 @@ void TimelineDock::makeTracksTaller()
     QMetaObject::invokeMethod(m_quickView.rootObject(), "makeTracksTaller");
 }
 
-void TimelineDock::setSelection(QList<int> newSelection, int trackIndex, bool isMultitrack)
+void TimelineDock::setSelectionFromJS(const QVariantList& list)
+{
+    QList<QPoint> points;
+    foreach (auto v, list)
+        points << v.toPoint();
+    setSelection(points);
+}
+
+void TimelineDock::setSelection(QList<QPoint> newSelection, int trackIndex, bool isMultitrack)
 {
     if (newSelection != selection()
             || trackIndex != m_selection.selectedTrack
@@ -257,21 +265,29 @@ void TimelineDock::setSelection(QList<int> newSelection, int trackIndex, bool is
         if (!m_selection.selectedClips.isEmpty())
             emitSelectedFromSelection();
         else
-            emit selected(0);
+            emit selected(nullptr);
     }
 }
 
-QList<int> TimelineDock::selection() const
+QVariantList TimelineDock::selectionForJS() const
+{
+    QVariantList result;
+    foreach (auto point, selection())
+        result << QVariant(point);
+    return result;
+}
+
+QList<QPoint> TimelineDock::selection() const
 {
     if (!m_quickView.rootObject())
-        return QList<int>();
+        return QList<QPoint>();
     return m_selection.selectedClips;
 }
 
 void TimelineDock::saveAndClearSelection()
 {
     m_savedSelection = m_selection;
-    m_selection.selectedClips = QList<int>();
+    m_selection.selectedClips = QList<QPoint>();
     m_selection.selectedTrack = -1;
     m_selection.isMultitrackSelected = false;
     emit selectionChanged();
@@ -297,12 +313,12 @@ void TimelineDock::selectClipUnderPlayhead()
         if (idx == -1)
             setSelection();
         else
-            setSelection(QList<int>() << idx);
+            setSelection(QList<QPoint>() << QPoint(idx, track));
         return;
     }
 
     setCurrentTrack(track);
-    setSelection(QList<int>() << clip);
+    setSelection(QList<QPoint>() << QPoint(clip, track));
 }
 
 int TimelineDock::centerOfClip(int trackIndex, int clipIndex)
@@ -382,12 +398,12 @@ void TimelineDock::clearSelectionIfInvalid()
 {
     int count = clipCount(currentTrack());
 
-    QList<int> newSelection;
-    foreach (int index, selection()) {
-        if (index >= count)
+    QList<QPoint> newSelection;
+    foreach (auto clip, selection()) {
+        if (clip.x() >= count)
             continue;
 
-        newSelection << index;
+        newSelection << QPoint(clip.x(), clip.y());
     }
     setSelection(newSelection);
     emit selectionChanged();
@@ -436,7 +452,7 @@ void TimelineDock::onProducerChanged(Mlt::Producer* after)
         // Ensure the new XML has same in/out point as selected clip by making
         // a copy of the changed producer and copying the in/out from timeline.
         Mlt::Playlist playlist(*track);
-        int clipIndex = selection().first();
+        int clipIndex = selection().first().x();
         QScopedPointer<Mlt::ClipInfo> info(playlist.clip_info(clipIndex));
         if (info) {
             double oldSpeed = qstrcmp("timewarp", info->producer->get("mlt_service")) ? 1.0 : info->producer->get_double("warp_speed");
@@ -510,7 +526,7 @@ void TimelineDock::append(int trackIndex)
     if (MLT.isSeekableClip() || MLT.savedProducer()) {
         MAIN.undoStack()->push(
             new Timeline::AppendCommand(m_model, trackIndex,
-                MLT.XML(MLT.isClip()? 0 : MLT.savedProducer())));
+                MLT.XML(MLT.isClip()? nullptr : MLT.savedProducer())));
         selectClipUnderPlayhead();
     } else if (!MLT.isSeekableClip()) {
         emit showStatusMessage(kNonSeekableWarning);
@@ -566,10 +582,33 @@ void TimelineDock::removeSelection(bool withCopy)
     if (selection().isEmpty() || currentTrack() < 0)
         return;
 
-    if (withCopy)
-        copyClip(currentTrack(), selection().first());
-    foreach (int index, selection())
-        remove(currentTrack(), index);
+    // Cut
+    if (withCopy) {
+        auto clip = selection().first();
+        copyClip(clip.y(), clip.x());
+        remove(clip.y(), clip.x());
+        return;
+    }
+
+    // Ripple delete
+    int n = selection().size();
+    if (n > 1)
+        MAIN.undoStack()->beginMacro(tr("Remove %1 from timeline").arg(n));
+    QList<QPoint> clipsRemoved;
+    foreach (auto clip, selection()) {
+        if (!clipsRemoved.contains(clip)) {
+            int adjustment = 0;
+            foreach (auto i, clipsRemoved) {
+                if (clip.y() == i.y() && clip.x() > i.x())
+                    --adjustment;
+            }
+            clipsRemoved << clip;
+            clip.setX(clip.x() + adjustment);
+            remove(clip.y(), clip.x());
+        }
+    }
+    if (n > 1)
+        MAIN.undoStack()->endMacro();
 }
 
 void TimelineDock::liftSelection()
@@ -582,8 +621,13 @@ void TimelineDock::liftSelection()
         selectClipUnderPlayhead();
     if (selection().isEmpty())
         return;
-    foreach (int index, selection())
-        lift(currentTrack(), index);
+    int n = selection().size();
+    if (n > 1)
+        MAIN.undoStack()->beginMacro(tr("Lift %1 from timeline").arg(n));
+    foreach (auto clip, selection())
+        lift(clip.y(), clip.x());
+    if (n > 1)
+        MAIN.undoStack()->endMacro();
 }
 
 void TimelineDock::selectTrack(int by)
@@ -599,7 +643,7 @@ void TimelineDock::selectTrack(int by)
 void TimelineDock::selectTrackHead(int trackIndex)
 {
     if (trackIndex >= 0) {
-        setSelection(QList<int>(), trackIndex);
+        setSelection(QList<QPoint>(), trackIndex);
         int i = m_model.trackList().at(trackIndex).mlt_index;
         Mlt::Producer* producer = m_model.tractor()->track(i);
         if (producer && producer->is_valid())
@@ -610,7 +654,7 @@ void TimelineDock::selectTrackHead(int trackIndex)
 
 void TimelineDock::selectMultitrack()
 {
-    setSelection(QList<int>(), -1, true);
+    setSelection(QList<QPoint>(), -1, true);
     QMetaObject::invokeMethod(m_quickView.rootObject(), "selectMultitrack");
     emit selected(m_model.tractor());
 }
@@ -636,12 +680,12 @@ void TimelineDock::emitSelectedFromSelection()
         if (m_model.tractor())
             selectMultitrack();
         else
-            emit selected(0);
+            emit selected(nullptr);
         return;
     }
 
-    int trackIndex = currentTrack();
-    int clipIndex = selection().isEmpty()? 0 : selection().first();
+    int trackIndex = selection().isEmpty()? currentTrack() : selection().first().y();
+    int clipIndex  = selection().isEmpty()? 0              : selection().first().x();
     QScopedPointer<Mlt::ClipInfo> info(getClipInfo(trackIndex, clipIndex));
     if (info && info->producer && info->producer->is_valid()) {
         m_updateCommand.reset(new Timeline::UpdateCommand(*this, trackIndex, clipIndex, info->start));
@@ -698,13 +742,13 @@ void TimelineDock::onRowsInserted(const QModelIndex& parent, int first, int last
     Q_UNUSED(parent)
     // Adjust selected clips for changed indices.
     if (-1 == m_selection.selectedTrack) {
-        QList<int> newSelection;
+        QList<QPoint> newSelection;
         int n = last - first + 1;
-        foreach (int i, m_selection.selectedClips) {
-            if (i < first)
-                newSelection << i;
+        foreach (auto i, m_selection.selectedClips) {
+            if (i.x() < first)
+                newSelection << QPoint(i.x(), parent.row());
             else
-                newSelection << (i + n);
+                newSelection << QPoint(i.x() + n, parent.row());
         }
         setSelection(newSelection);
     }
@@ -715,13 +759,13 @@ void TimelineDock::onRowsRemoved(const QModelIndex& parent, int first, int last)
     Q_UNUSED(parent)
     // Adjust selected clips for changed indices.
     if (-1 == m_selection.selectedTrack) {
-        QList<int> newSelection;
+        QList<QPoint> newSelection;
         int n = last - first + 1;
-        foreach (int i, m_selection.selectedClips) {
-            if (i < first)
-                newSelection << i;
-            else if (i > last)
-                newSelection << (i - n);
+        foreach (auto i, m_selection.selectedClips) {
+            if (i.x() < first)
+                newSelection << QPoint(i.x(), parent.row());
+            else if (i.x() > last)
+                newSelection << QPoint(i.x() - n, parent.row());
         }
         setSelection(newSelection);
     }
@@ -902,7 +946,7 @@ void TimelineDock::insert(int trackIndex, int position, const QString &xml)
     if (MAIN.isSourceClipMyProject()) return;
     if (MLT.isSeekableClip() || MLT.savedProducer() || !xml.isEmpty()) {
         QString xmlToUse = !xml.isEmpty()? xml
-            : MLT.XML(MLT.isClip()? 0 : MLT.savedProducer());
+            : MLT.XML(MLT.isClip()? nullptr : MLT.savedProducer());
         if (position < 0)
             position = m_position;
         if (m_model.trackList().size() == 0)
@@ -917,7 +961,7 @@ void TimelineDock::insert(int trackIndex, int position, const QString &xml)
 void TimelineDock::onInserted(int trackIndex, int clipIndex)
 {
     Q_UNUSED(trackIndex)
-    setSelection(QList<int>() << clipIndex);
+    setSelection(QList<QPoint>() << QPoint(clipIndex, trackIndex));
 }
 
 void TimelineDock::overwrite(int trackIndex, int position, const QString &xml)
@@ -931,7 +975,7 @@ void TimelineDock::overwrite(int trackIndex, int position, const QString &xml)
     if (MAIN.isSourceClipMyProject()) return;
     if (MLT.isSeekableClip() || MLT.savedProducer() || !xml.isEmpty()) {
         QString xmlToUse = !xml.isEmpty()? xml
-            : MLT.XML(MLT.isClip()? 0 : MLT.savedProducer());
+            : MLT.XML(MLT.isClip()? nullptr : MLT.savedProducer());
         if (position < 0)
             position = m_position;
         if (m_model.trackList().size() == 0)
@@ -946,7 +990,7 @@ void TimelineDock::overwrite(int trackIndex, int position, const QString &xml)
 void TimelineDock::onOverWritten(int trackIndex, int clipIndex)
 {
     Q_UNUSED(trackIndex)
-    setSelection(QList<int>() << clipIndex);
+    setSelection(QList<QPoint>() << QPoint(clipIndex, trackIndex));
 }
 
 void TimelineDock::appendFromPlaylist(Mlt::Playlist *playlist)
@@ -1153,5 +1197,5 @@ void TimelineDock::onTransitionAdded(int trackIndex, int clipIndex, int position
     Timeline::AddTransitionCommand* command = new Timeline::AddTransitionCommand(m_model, trackIndex, clipIndex, position, ripple);
     MAIN.undoStack()->push(command);
     // Select the transition.
-    setSelection(QList<int>() << command->getTransitionIndex());
+    setSelection(QList<QPoint>() << QPoint(command->getTransitionIndex(), trackIndex));
 }

src/docks/timelinedock.h
@@ -38,7 +38,7 @@ class TimelineDock : public QDockWidget
     Q_OBJECT
     Q_PROPERTY(int position READ position WRITE setPosition NOTIFY positionChanged)
     Q_PROPERTY(int currentTrack READ currentTrack WRITE setCurrentTrack NOTIFY currentTrackChanged)
-    Q_PROPERTY(QList<int> selection READ selection WRITE setSelection NOTIFY selectionChanged)
+    Q_PROPERTY(QVariantList selection READ selectionForJS WRITE setSelectionFromJS NOTIFY selectionChanged)
 
 public:
     explicit TimelineDock(QWidget *parent = 0);
@@ -65,8 +65,10 @@ public:
     void resetZoom();
     void makeTracksShorter();
     void makeTracksTaller();
-    void setSelection(QList<int> selection = QList<int>(), int trackIndex = -1, bool isMultitrack = false);
-    QList<int> selection() const;
+    void setSelectionFromJS(const QVariantList& list);
+    void setSelection(QList<QPoint> selection = QList<QPoint>(), int trackIndex = -1, bool isMultitrack = false);
+    QVariantList selectionForJS() const;
+    QList<QPoint> selection() const;
     void saveAndClearSelection();
     void restoreSelection();
     void selectClipUnderPlayhead();
@@ -164,7 +166,7 @@ private:
     QScopedPointer<Timeline::UpdateCommand> m_updateCommand;
     bool m_ignoreNextPositionChange;
     struct Selection {
-        QList<int> selectedClips;
+        QList<QPoint> selectedClips; // x is the clip index, y is the track index
         int selectedTrack;
         bool isMultitrackSelected;
     };

src/mainwindow.cpp
@@ -493,7 +493,7 @@ void MainWindow::onTimelineClipSelected()
     if (t->selection().isEmpty())
         return;
 
-    m_navigationPosition = t->centerOfClip(t->currentTrack(), t->selection().first());
+    m_navigationPosition = t->centerOfClip(t->selection().first().y(), t->selection().first().x());
 
     // Switch to Project player.
     if (m_player->tabIndex() != Player::ProjectTabIndex) {
@@ -1628,10 +1628,10 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
             if (m_timelineDock->selection().isEmpty()) {
                 m_timelineDock->selectClipUnderPlayhead();
             } else if (m_timelineDock->selection().size() == 1) {
-                int newIndex = m_timelineDock->selection().first() - 1;
+                int newIndex = m_timelineDock->selection().first().x() - 1;
                 if (newIndex < 0)
                     break;
-                m_timelineDock->setSelection(QList<int>() << newIndex);
+                m_timelineDock->setSelection(QList<QPoint>() << QPoint(newIndex, m_timelineDock->selection().first().y()));
                 m_navigationPosition = m_timelineDock->centerOfClip(m_timelineDock->currentTrack(), newIndex);
             }
         } else {
@@ -1643,10 +1643,10 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
             if (m_timelineDock->selection().isEmpty()) {
                 m_timelineDock->selectClipUnderPlayhead();
             } else if (m_timelineDock->selection().size() == 1) {
-                int newIndex = m_timelineDock->selection().first() + 1;
+                int newIndex = m_timelineDock->selection().first().x() + 1;
                 if (newIndex >= m_timelineDock->clipCount(-1))
                     break;
-                m_timelineDock->setSelection(QList<int>() << newIndex);
+                m_timelineDock->setSelection(QList<QPoint>() << QPoint(newIndex, m_timelineDock->selection().first().y()));
                 m_navigationPosition = m_timelineDock->centerOfClip(m_timelineDock->currentTrack(), newIndex);
             }
         } else {
@@ -1696,7 +1696,7 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
             m_timelineDock->show();
             m_timelineDock->raise();
             if (!m_timelineDock->selection().isEmpty())
-                m_timelineDock->copyClip(m_timelineDock->currentTrack(), m_timelineDock->selection().first());
+                m_timelineDock->copyClip(m_timelineDock->selection().first().y(), m_timelineDock->selection().first().x());
         }
         break;
     case Qt::Key_D:
@@ -1847,18 +1847,19 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
             m_playlistDock->on_actionOpen_triggered();
         } else if (isMultitrackValid()) {
             int newClipIndex = -1;
+            int trackIndex = m_timelineDock->currentTrack() - 1;
             if ((event->modifiers() & Qt::ControlModifier) &&
                     !m_timelineDock->selection().isEmpty() &&
-                    m_timelineDock->currentTrack() > 0) {
+                    trackIndex > -1) {
 
-                newClipIndex = m_timelineDock->clipIndexAtPosition(m_timelineDock->currentTrack() - 1, m_navigationPosition);
+                newClipIndex = m_timelineDock->clipIndexAtPosition(trackIndex, m_navigationPosition);
             }
 
             m_timelineDock->selectTrack(-1);
 
             if (newClipIndex >= 0) {
-                newClipIndex = qMin(newClipIndex, m_timelineDock->clipCount(m_timelineDock->currentTrack()) - 1);
-                m_timelineDock->setSelection(QList<int>() << newClipIndex);
+                newClipIndex = qMin(newClipIndex, m_timelineDock->clipCount(trackIndex) - 1);
+                m_timelineDock->setSelection(QList<QPoint>() << QPoint(newClipIndex, trackIndex));
             }
 
         } else if (m_playlistDock->isVisible() && m_playlistDock->model()->rowCount() > 0) {
@@ -1875,18 +1876,19 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
             m_playlistDock->on_actionOpen_triggered();
         } else if (isMultitrackValid()) {
             int newClipIndex = -1;
+            int trackIndex = m_timelineDock->currentTrack() + 1;
             if ((event->modifiers() & Qt::ControlModifier) &&
                     !m_timelineDock->selection().isEmpty() &&
-                    m_timelineDock->currentTrack() < m_timelineDock->model()->trackList().count() - 1) {
+                    trackIndex < m_timelineDock->model()->trackList().count()) {
 
-                newClipIndex = m_timelineDock->clipIndexAtPosition(m_timelineDock->currentTrack() + 1, m_navigationPosition);
+                newClipIndex = m_timelineDock->clipIndexAtPosition(trackIndex, m_navigationPosition);
             }
 
             m_timelineDock->selectTrack(1);
 
             if (newClipIndex >= 0) {
-                newClipIndex = qMin(newClipIndex, m_timelineDock->clipCount(m_timelineDock->currentTrack()) - 1);
-                m_timelineDock->setSelection(QList<int>() << newClipIndex);
+                newClipIndex = qMin(newClipIndex, m_timelineDock->clipCount(trackIndex) - 1);
+                m_timelineDock->setSelection(QList<QPoint>() << QPoint(newClipIndex, trackIndex));
             }
 
         } else if (m_playlistDock->isVisible() && m_playlistDock->model()->rowCount() > 0) {
@@ -2606,8 +2608,8 @@ void MainWindow::onMultitrackModified()
 
     // Reflect this playlist info onto the producer for keyframes dock.
     if (!m_timelineDock->selection().isEmpty()) {
-        int trackIndex = m_timelineDock->currentTrack();
-        int clipIndex = m_timelineDock->selection().first();
+        int trackIndex = m_timelineDock->selection().first().y();
+        int clipIndex = m_timelineDock->selection().first().x();
         QScopedPointer<Mlt::ClipInfo> info(m_timelineDock->getClipInfo(trackIndex, clipIndex));
         if (info && info->producer && info->producer->is_valid()) {
             int expected = info->frame_in;
@@ -3636,7 +3638,7 @@ void MainWindow::on_actionCopy_triggered()
     m_timelineDock->show();
     m_timelineDock->raise();
     if (!m_timelineDock->selection().isEmpty())
-        m_timelineDock->copyClip(m_timelineDock->currentTrack(), m_timelineDock->selection().first());
+        m_timelineDock->copyClip(m_timelineDock->selection().first().y(), m_timelineDock->selection().first().x());
 }
 
 void MainWindow::on_actionPaste_triggered()

src/qml/views/timeline/Timeline.js
@@ -141,3 +141,33 @@ function onMouseWheel(wheel) {
         }
     }
 }
+
+function toggleSelection(trackIndex, clipIndex) {
+    var result = []
+    var skip = false
+    timeline.selection.forEach(function(el) {
+        if (el.x !== clipIndex || el.y !== trackIndex)
+            result.push(el)
+        else
+            skip = true
+    })
+    if (!skip)
+        result.push(Qt.point(clipIndex, trackIndex))
+    return result
+}
+
+function selectRange(trackIndex, clipIndex) {
+    var result = [timeline.selection.length? timeline.selection[0] : Qt.point(clipIndex, trackIndex)]
+    // this only works on a single track for now
+    if (timeline.selection.length && trackIndex === result[0].y) {
+        var x
+        if (clipIndex > result[0].x) {
+            for (x = result[0].x + 1; x <= clipIndex; x++)
+                result.push(Qt.point(x, trackIndex))
+        } else {
+            for (x = clipIndex; x < result[0].x; x++)
+                result.push(Qt.point(x, trackIndex))
+        }
+    }
+    return result
+}

src/qml/views/timeline/TimelineToolbar.qml
@@ -178,7 +178,7 @@ ToolBar {
         iconName: 'edit-copy'
         iconSource: 'qrc:///icons/oxygen/32x32/actions/edit-copy.png'
         enabled: timeline.selection.length
-        onTriggered: timeline.copyClip(currentTrack, timeline.selection[0])
+        onTriggered: timeline.copyClip(timeline.selection[0].y, timeline.selection[0].x)
     }
 
     Action {
@@ -202,7 +202,7 @@ ToolBar {
         tooltip: qsTr('Ripple Delete - Remove current clip\nshifting following clips to the left (X)')
         iconName: 'list-remove'
         iconSource: 'qrc:///icons/oxygen/32x32/actions/list-remove.png'
-        onTriggered: timeline.remove(currentTrack, timeline.selection[0])
+        onTriggered: timeline.removeSelection()
    }
 
     Action {
@@ -210,7 +210,7 @@ ToolBar {
         tooltip: qsTr('Lift - Remove current clip without\naffecting position of other clips (Z)')
         iconName: 'lift'
         iconSource: 'qrc:///icons/oxygen/32x32/actions/lift.png'
-        onTriggered: timeline.lift(currentTrack, timeline.selection[0])
+        onTriggered: timeline.liftSelection()
     }
 
     Action {

commit cd2a0b039d657537746497a34166622852b775ea

fix clip context menu Properties broken in a2acc0d

src/qml/views/timeline/Clip.qml
@@ -696,7 +696,7 @@ Rectangle {
             text: qsTr('Properties')
             onTriggered: {
                 clipRoot.forceActiveFocus()
-                clipRoot.clicked(clipRoot, mouse)
+                clipRoot.clicked(clipRoot, null)
                 timeline.openProperties()
             }
         }

src/qml/views/timeline/timeline.qml
@@ -546,9 +546,9 @@ Rectangle {
                 var trackIndex = track.DelegateModel.itemsIndex
                 var clipIndex = clip.DelegateModel.itemsIndex
                 currentTrack = trackIndex
-                if (mouse.modifiers & Qt.ControlModifier)
+                if (mouse && mouse.modifiers & Qt.ControlModifier)
                     timeline.selection = Logic.toggleSelection(trackIndex, clipIndex)
-                else if (mouse.modifiers & Qt.ShiftModifier)
+                else if (mouse && mouse.modifiers & Qt.ShiftModifier)
                     timeline.selection = Logic.selectRange(trackIndex, clipIndex)
                 else if (!Logic.selectionContains(trackIndex, clipIndex))
                     // select one