mltframework/shotcut

commit 8bc7fbc44d6f0f17eec6818a2c3b440cdc4d9731

fix #818 checking Image Sequence may not update playlist duration

src/docks/playlistdock.cpp
@@ -658,6 +658,7 @@ void PlaylistDock::onDropped(const QMimeData *data, int row)
                     first = false;
                     setIndex(0);
                     on_actionOpen_triggered();
+                    return; // do not resetPlaylistIndex
                 }
             }
         }

commit 0830dcb16e5b2dbaf839e31b16040b541f2d1384

fix dropping multiple files to playlist on new session
regression in 8bc7fbc

src/docks/playlistdock.cpp
@@ -606,6 +606,7 @@ void PlaylistDock::onPlaylistClosed()
 
 void PlaylistDock::onDropped(const QMimeData *data, int row)
 {
+    bool resetIndex = true;
     if (data && data->hasUrls()) {
         int insertNextAt = row;
         bool first = true;
@@ -658,7 +659,7 @@ void PlaylistDock::onDropped(const QMimeData *data, int row)
                     first = false;
                     setIndex(0);
                     on_actionOpen_triggered();
-                    return; // do not resetPlaylistIndex
+                    resetIndex = false;
                 }
             }
         }
@@ -693,7 +694,8 @@ void PlaylistDock::onDropped(const QMimeData *data, int row)
             }
         }
     }
-    resetPlaylistIndex();
+    if (resetIndex)
+        resetPlaylistIndex();
 }
 
 void PlaylistDock::onMoveClip(int from, int to)