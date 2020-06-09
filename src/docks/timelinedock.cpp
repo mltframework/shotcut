5d1073862d851e799ced95b8ab97aa1a87bedbd6
Dan Dennedy <dan@dennedy.org>
2020-06-09 13:52:12 -0700

    fix Delete Proxy not replacing timeline clips

--- src/docks/timelinedock.cpp
+++ src/docks/timelinedock.cpp
@@ -1456,7 +1456,7 @@ void TimelineDock::replaceClipsWithHash(const QString& hash, Mlt::Producer& prod
         QScopedPointer<Mlt::ClipInfo> info(MAIN.timelineClipInfoByUuid(clip.get(kUuidProperty), trackIndex, clipIndex));
 
         if (info && info->producer->is_valid() && trackIndex >= 0 && clipIndex >= 0 && info->producer->type() != tractor_type) {
-            if (info->producer->get_int(kIsProxyProperty)) {
+            if (producer.get_int(kIsProxyProperty) && info->producer->get_int(kIsProxyProperty)) {
                 // Not much to do on a proxy clip but change its resource
                 info->producer->set(kOriginalResourceProperty, producer.get("resource"));
                 auto caption = Util::baseName(ProxyManager::resource(*info->producer));
