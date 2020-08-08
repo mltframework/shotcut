mltframework/shotcut

commit ec7ddec681d5c37ef79b15194dc04b7798357ee9

try to fix QML-based view respond to touch screen
https://forum.shotcut.org/t/touchscreen-question/16840
https://bugreports.qt.io/browse/QTBUG-66718

src/docks/filtersdock.cpp
@@ -45,6 +45,7 @@ FiltersDock::FiltersDock(MetadataModel* metadataModel, AttachedFiltersModel* att
     setMinimumWidth(300);
     m_qview.setFocusPolicy(Qt::StrongFocus);
     m_qview.quickWindow()->setPersistentSceneGraph(false);
+    m_qview.setAttribute(Qt::WA_AcceptTouchEvents);
     setWidget(&m_qview);
 #ifdef Q_OS_MAC
     setFeatures(DockWidgetClosable | DockWidgetMovable);

src/docks/keyframesdock.cpp
@@ -50,6 +50,7 @@ KeyframesDock::KeyframesDock(QmlProducer* qmlProducer, QWidget *parent)
     setMinimumWidth(300);
     m_qview.setFocusPolicy(Qt::StrongFocus);
     m_qview.quickWindow()->setPersistentSceneGraph(false);
+    m_qview.setAttribute(Qt::WA_AcceptTouchEvents);
     setWidget(&m_qview);
 
     QmlUtilities::setCommonProperties(m_qview.rootContext());

src/docks/timelinedock.cpp
@@ -65,6 +65,7 @@ TimelineDock::TimelineDock(QWidget *parent) :
     m_quickView.setResizeMode(QQuickWidget::SizeRootObjectToView);
     m_quickView.setClearColor(palette().window().color());
     m_quickView.quickWindow()->setPersistentSceneGraph(false);
+    m_quickView.setAttribute(Qt::WA_AcceptTouchEvents);
 
     connect(&m_model, SIGNAL(modified()), this, SLOT(clearSelectionIfInvalid()));
     connect(&m_model, &MultitrackModel::inserted, this, &TimelineDock::onInserted, Qt::QueuedConnection);

src/glwidget.cpp
@@ -75,6 +75,7 @@ GLWidget::GLWidget(QObject *parent)
     m_texture[0] = m_texture[1] = m_texture[2] = 0;
     quickWindow()->setPersistentOpenGLContext(true);
     quickWindow()->setPersistentSceneGraph(true);
+    setAttribute(Qt::WA_AcceptTouchEvents);
     quickWindow()->setClearBeforeRendering(false);
     setResizeMode(QQuickWidget::SizeRootObjectToView);
     QDir importPath = QmlUtilities::qmlDir();

commit dded35ed7a13e371b7388ef5c96cbc0f37079068

fix #899 playhead and clips moved unexpectedly on macOS
This was due to a regression in ec7ddec6 to fix support for touch
screens on other OSes. The touchpad on macOS is also a touch input
device and reacted to this change in an unforseeable way.

src/docks/filtersdock.cpp
@@ -44,11 +44,12 @@ FiltersDock::FiltersDock(MetadataModel* metadataModel, AttachedFiltersModel* att
     toggleViewAction()->setIcon(windowIcon());
     m_qview.setFocusPolicy(Qt::StrongFocus);
     m_qview.quickWindow()->setPersistentSceneGraph(false);
-    m_qview.setAttribute(Qt::WA_AcceptTouchEvents);
-    setWidget(&m_qview);
 #ifdef Q_OS_MAC
     setFeatures(DockWidgetClosable | DockWidgetMovable);
+#else
+    m_qview.setAttribute(Qt::WA_AcceptTouchEvents);
 #endif
+    setWidget(&m_qview);
 
     QmlUtilities::setCommonProperties(m_qview.rootContext());
     m_qview.rootContext()->setContextProperty("view", new QmlView(&m_qview));

src/docks/keyframesdock.cpp
@@ -50,7 +50,9 @@ KeyframesDock::KeyframesDock(QmlProducer* qmlProducer, QWidget *parent)
     setMinimumSize(200, 200);
     m_qview.setFocusPolicy(Qt::StrongFocus);
     m_qview.quickWindow()->setPersistentSceneGraph(false);
+#ifndef Q_OS_MAC
     m_qview.setAttribute(Qt::WA_AcceptTouchEvents);
+#endif
     setWidget(&m_qview);
 
     QmlUtilities::setCommonProperties(m_qview.rootContext());

src/docks/timelinedock.cpp
@@ -67,7 +67,9 @@ TimelineDock::TimelineDock(QWidget *parent) :
     m_quickView.setResizeMode(QQuickWidget::SizeRootObjectToView);
     m_quickView.setClearColor(palette().window().color());
     m_quickView.quickWindow()->setPersistentSceneGraph(false);
+#ifndef Q_OS_MAC
     m_quickView.setAttribute(Qt::WA_AcceptTouchEvents);
+#endif
 
     connect(&m_model, SIGNAL(modified()), this, SLOT(clearSelectionIfInvalid()));
     connect(&m_model, &MultitrackModel::inserted, this, &TimelineDock::onInserted, Qt::QueuedConnection);

src/glwidget.cpp
@@ -75,7 +75,9 @@ GLWidget::GLWidget(QObject *parent)
     m_texture[0] = m_texture[1] = m_texture[2] = 0;
     quickWindow()->setPersistentOpenGLContext(true);
     quickWindow()->setPersistentSceneGraph(true);
+#ifndef Q_OS_MAC
     setAttribute(Qt::WA_AcceptTouchEvents);
+#endif
     quickWindow()->setClearBeforeRendering(false);
     setResizeMode(QQuickWidget::SizeRootObjectToView);
     QDir importPath = QmlUtilities::qmlDir();