mltframework/shotcut

commit fc333d4d75ae9fa5f0ad0012fc02bbaf18b53034

refactor to QSaveFile in Mlt::Controller::saveXML
This fixes temp files not being deleted on Windows folders that may be
synced to cloud, automatic backup, or malware scanners. It also replaces
this complex code with something mature in Qt.

src/docks/encodedock.cpp
@@ -919,7 +919,7 @@ MeltJob* EncodeDock::createMeltJob(Mlt::Producer* service, const QString& target
     QString fileName = tmp->fileName();
     tmp->remove();
     auto isProxy = ui->previewScaleCheckBox->isChecked() && Settings.proxyEnabled();
-    MLT.saveXML(fileName, service, false /* without relative paths */, false /* without verify */, isProxy);
+    MLT.saveXML(fileName, service, false /* without relative paths */, isProxy);
 
     // parse xml
     QFile f1(fileName);

src/jobs/encodejob.cpp
@@ -86,7 +86,7 @@ void EncodeJob::onVideoQualityTriggered()
             tractor.set_track(encoded, 1);
             tractor.plant_transition(vqm);
             vqm.set("render", 0);
-            MLT.saveXML(tmp->fileName(), &tractor, false /* without relative paths */, false /* do not verify */ );
+            MLT.saveXML(tmp->fileName(), &tractor, false /* without relative paths */);
 
             // Add consumer element to XML.
             QFile f1(tmp->fileName());

src/mltcontroller.cpp
@@ -21,8 +21,7 @@
 #include <QMetaType>
 #include <QFileInfo>
 #include <QUuid>
-#include <QTemporaryFile>
-#include <QXmlStreamReader>
+#include <QSaveFile>
 #include <Logger.h>
 #include <Mlt.h>
 #include <cmath>
@@ -445,21 +444,12 @@ void Controller::refreshConsumer(bool scrubAudio)
     }
 }
 
-bool Controller::saveXML(const QString& filename, Service* service, bool withRelativePaths, bool verify, bool proxy)
+bool Controller::saveXML(const QString& filename, Service* service, bool withRelativePaths, bool proxy)
 {
     QMutexLocker locker(&m_saveXmlMutex);
     QFileInfo fi(filename);
-    QTemporaryFile tmp;
-    QString mltFileName = filename;
-    // First, write to a temp file.
-    if (verify) {
-        tmp.setFileTemplate(fi.absolutePath().append("/shotcut-XXXXXX.mlt"));
-        tmp.open();
-        tmp.close();
-        mltFileName = tmp.fileName();
-        LOG_DEBUG() << "writing temporary XML file" << mltFileName;
-    }
-    Consumer c(profile(), "xml", mltFileName.toUtf8().constData());
+    static const char* propertyName = "string";
+    Consumer c(profile(), "xml", proxy? filename.toUtf8().constData() : propertyName);
     Service s(service? service->get_service() : m_producer->get_service());
     if (s.is_valid()) {
         QString root = withRelativePaths? fi.absolutePath() : "";
@@ -478,74 +468,23 @@ bool Controller::saveXML(const QString& filename, Service* service, bool withRel
         c.start();
         if (ignore)
             s.set("ignore_points", ignore);
-
-        if (!proxy && ProxyManager::filterXML(mltFileName, root)) { // also verifies
-            if (verify) {
-                tmp.remove();
+        auto xml = QString::fromUtf8(c.get(propertyName));
+        if (!proxy && ProxyManager::filterXML(xml, root)) { // also verifies
+            QSaveFile file(filename);
+            if (!file.open(QIODevice::WriteOnly)) {
+                LOG_ERROR() << "failed to open MLT XML file for writing" << filename;
             }
-            tmp.setFileName(mltFileName);
-
-            // QFile::rename() can fail and remove the destination file. See its docs.
-            // So, save an existing target file as a backup.
-            QString backupName;
-            if (QFile::exists(filename)) {
-                QTemporaryFile backupTmp;
-                backupTmp.setFileTemplate(
-                    QString("%1/%2 - backup - XXXXXX.mlt").arg(fi.absolutePath()).arg(fi.completeBaseName()));
-                // Only remove the backup file if we successfully move the temp file to target.
-                backupTmp.setAutoRemove(false);
-                QFile existingFile(filename);
-                if (existingFile.open(QIODevice::ReadOnly)) {
-                    // Copy contents of existing file to temporary backup file.
-                    backupTmp.open();
-                    backupName = backupTmp.fileName();
-                    LOG_DEBUG() << "copy to backup" << filename << backupName;
-                    QByteArray buffer;
-                    while (!(buffer = existingFile.read(1048576LL /*1MiB*/)).isEmpty()) {
-                        backupTmp.write(buffer);
-                    }
-                    if (existingFile.error() != QFileDevice::NoError) {
-                        LOG_ERROR() << "backup error" << existingFile.errorString();
-                        return false;
-                    }
-                    if (backupTmp.size() != existingFile.size()) {
-                        LOG_ERROR() << "backup file size problem: existing file size" << existingFile.size() << ", backup file size" << backupTmp.size();
-                        return false;
-                    }
-                    backupTmp.close();
-                    existingFile.close();
-                    // Remove the existing file as its name becomes the target for rename.
-                    if (!existingFile.remove()) {
-                        LOG_ERROR() << "failed to remove existing file" << filename;
-                        return false;
-                    }
-                } else {
-                    // Do not overwrite the backup file.
-                    LOG_ERROR() << "failed to open existing file" << filename << "for backup:" << existingFile.errorString();
+            qint64 result = 0, written = 0;
+            auto data = xml.toUtf8();
+            LOG_DEBUG() << "writing to file" << filename;
+            while (written < data.size()) {
+                if ((result = file.write(data)) < 0) {
+                    LOG_ERROR() << "failed to save MLT XML to file" << filename;
                     return false;
                 }
+                written += result;
             }
-            // If the file is good, then move it into place.
-            LOG_DEBUG() << "rename" << mltFileName << filename;
-            tmp.setAutoRemove(false);
-            int attempts = 5;
-            for (int i = 0; i < attempts; i++) {
-                if (tmp.rename(filename)) {
-                    // Double-check the rename operation.
-                    if (QFile::exists(filename) && QFile::exists(backupName))
-                        QFile::remove(backupName);
-                    return true;
-                }
-                LOG_WARNING() << "rename failed, trying again";
-
-#ifdef Q_OS_WIN
-                ::Sleep(200);
-#else
-                ::usleep(200000);
-#endif
-            }
-            LOG_ERROR() << "rename failed" << mltFileName << filename;
-            return false;
+            return file.commit();
         }
     }
     return false;

src/mltcontroller.h
@@ -88,7 +88,7 @@ public:
     void onWindowResize();
     virtual void seek(int position);
     virtual void refreshConsumer(bool scrubAudio = false);
-    bool saveXML(const QString& filename, Service* service = nullptr, bool withRelativePaths = true, bool verify = true, bool proxy = false);
+    bool saveXML(const QString& filename, Service* service = nullptr, bool withRelativePaths = true, bool proxy = false);
     QString XML(Service* service = nullptr, bool withProfile = false, bool withMetadata = false);
     int consumerChanged();
     void setProfile(const QString& profile_name);

src/qmltypes/qmlfilter.cpp
@@ -345,7 +345,7 @@ void QmlFilter::analyze(bool isAudio)
         }
     }
 
-    MLT.saveXML(tmp->fileName(), &service, false /* without relative paths */, false /* without verify */);
+    MLT.saveXML(tmp->fileName(), &service, false /* without relative paths */);
     if (!isAudio) m_filter.set("analyze", 0);
     m_filter.set("disable", disable);
 

src/proxymanager.cpp
@@ -26,7 +26,6 @@
 
 #include <QObject>
 #include <QVector>
-#include <QTemporaryFile>
 #include <QXmlStreamReader>
 #include <QXmlStreamWriter>
 #include <QFile>
@@ -289,88 +288,80 @@ static void processProperties(QXmlStreamWriter& newXml, QVector<MltProperty>& pr
     properties.clear();
 }
 
-bool ProxyManager::filterXML(QString& fileName, const QString& root)
+bool ProxyManager::filterXML(QString& xmlString, const QString& root)
 {
-    QFile file(fileName);
-    QTemporaryFile tempFile(QFileInfo(fileName).dir().filePath("shotcut-XXXXXX.mlt"));
-    if (file.open(QIODevice::ReadOnly | QIODevice::Text) && tempFile.open()) {
-        tempFile.resize(0);
-        QXmlStreamReader xml(&file);
-        QXmlStreamWriter newXml(&tempFile);
-        bool isPropertyElement = false;
-        QVector<MltProperty> properties;
+    QString output;
+    QXmlStreamReader xml(xmlString);
+    QXmlStreamWriter newXml(&output);
+    bool isPropertyElement = false;
+    QVector<MltProperty> properties;
 
-        newXml.setAutoFormatting(true);
-        newXml.setAutoFormattingIndent(2);
+    newXml.setAutoFormatting(true);
+    newXml.setAutoFormattingIndent(2);
 
-        while (!xml.atEnd()) {
-            switch (xml.readNext()) {
-            case QXmlStreamReader::Characters:
-                if (!isPropertyElement)
-                    newXml.writeCharacters(xml.text().toString());
-                break;
-            case QXmlStreamReader::Comment:
-                newXml.writeComment(xml.text().toString());
-                break;
-            case QXmlStreamReader::DTD:
-                newXml.writeDTD(xml.text().toString());
-                break;
-            case QXmlStreamReader::EntityReference:
-                newXml.writeEntityReference(xml.name().toString());
-                break;
-            case QXmlStreamReader::ProcessingInstruction:
-                newXml.writeProcessingInstruction(xml.processingInstructionTarget().toString(), xml.processingInstructionData().toString());
-                break;
-            case QXmlStreamReader::StartDocument:
-                newXml.writeStartDocument(xml.documentVersion().toString(), xml.isStandaloneDocument());
-                break;
-            case QXmlStreamReader::EndDocument:
-                newXml.writeEndDocument();
-                break;
-            case QXmlStreamReader::StartElement: {
-                const QString element = xml.name().toString();
-                if (element == "property") {
-                    // Save each property element but do not output yet
-                    const QString name = xml.attributes().value("name").toString();
-                    properties << MltProperty(name, xml.readElementText());
-                    isPropertyElement = true;
-                } else {
-                    // At the start of a non-property element
-                    isPropertyElement = false;
-                    processProperties(newXml, properties, root);
-                    // Write the new start element
-                    newXml.writeStartElement(xml.namespaceUri().toString(), element);
-                    for (const auto& a : xml.attributes()) {
-                        newXml.writeAttribute(a);
-                    }
+    while (!xml.atEnd()) {
+        switch (xml.readNext()) {
+        case QXmlStreamReader::Characters:
+            if (!isPropertyElement)
+                newXml.writeCharacters(xml.text().toString());
+            break;
+        case QXmlStreamReader::Comment:
+            newXml.writeComment(xml.text().toString());
+            break;
+        case QXmlStreamReader::DTD:
+            newXml.writeDTD(xml.text().toString());
+            break;
+        case QXmlStreamReader::EntityReference:
+            newXml.writeEntityReference(xml.name().toString());
+            break;
+        case QXmlStreamReader::ProcessingInstruction:
+            newXml.writeProcessingInstruction(xml.processingInstructionTarget().toString(), xml.processingInstructionData().toString());
+            break;
+        case QXmlStreamReader::StartDocument:
+            newXml.writeStartDocument(xml.documentVersion().toString(), xml.isStandaloneDocument());
+            break;
+        case QXmlStreamReader::EndDocument:
+            newXml.writeEndDocument();
+            break;
+        case QXmlStreamReader::StartElement: {
+            const QString element = xml.name().toString();
+            if (element == "property") {
+                // Save each property element but do not output yet
+                const QString name = xml.attributes().value("name").toString();
+                properties << MltProperty(name, xml.readElementText());
+                isPropertyElement = true;
+            } else {
+                // At the start of a non-property element
+                isPropertyElement = false;
+                processProperties(newXml, properties, root);
+                // Write the new start element
+                newXml.writeStartElement(xml.namespaceUri().toString(), element);
+                for (const auto& a : xml.attributes()) {
+                    newXml.writeAttribute(a);
                 }
-                break;
             }
-            case QXmlStreamReader::EndElement:
-                // At the end of a non-property element
-                if (xml.name() != "property") {
-                    processProperties(newXml, properties, root);
-                    newXml.writeEndElement();
-                }
-                break;
-            default:
-                break;
-            }
+            break;
         }
-        if (tempFile.isOpen())
-            tempFile.close();
+        case QXmlStreamReader::EndElement:
+            // At the end of a non-property element
+            if (xml.name() != "property") {
+                processProperties(newXml, properties, root);
+                newXml.writeEndElement();
+            }
+            break;
+        default:
+            break;
+        }
+    }
 
-        // Useful for debugging
+    // Useful for debugging
 //        tempFile.open();
 //        LOG_DEBUG() << tempFile.readAll().constData();
 //        tempFile.close();
 
-        if (!xml.hasError()) {
-            fileName = tempFile.fileName();
-            LOG_DEBUG() << fileName;
-            tempFile.setAutoRemove(false);
-            return true;
-        }
+    if (!xml.hasError()) {
+        xmlString = output;
+        return true;
     }
     return false;
 }

src/proxymanager.h
@@ -45,7 +45,7 @@ public:
     static void generateVideoProxy(Mlt::Producer& producer, bool fullRange,
         ScanMode scanMode = Automatic, const QPoint& aspectRatio = QPoint(), bool replace = true);
     static void generateImageProxy(Mlt::Producer& producer, bool replace = true);
-    static bool filterXML(QString& fileName, const QString& root);
+    static bool filterXML(QString& xml, const QString& root);
     static bool fileExists(Mlt::Producer& producer);
     static bool filePending(Mlt::Producer& producer);
     static bool isValidImage(Mlt::Producer& producer);

commit e1953cea57c8f298d5a27f97a0ee108ef2b62771

fix saving temporary MLT XML on Windows
Also potentially fixes writing the complete XML string. Regressions in
fc333d4

src/docks/encodedock.cpp
@@ -919,7 +919,7 @@ MeltJob* EncodeDock::createMeltJob(Mlt::Producer* service, const QString& target
     QString fileName = tmp->fileName();
     tmp->remove();
     auto isProxy = ui->previewScaleCheckBox->isChecked() && Settings.proxyEnabled();
-    MLT.saveXML(fileName, service, false /* without relative paths */, isProxy);
+    MLT.saveXML(fileName, service, false /* without relative paths */, nullptr, isProxy);
 
     // parse xml
     QFile f1(fileName);

src/jobs/encodejob.cpp
@@ -71,10 +71,9 @@ void EncodeJob::onVideoQualityTriggered()
         if (Util::warnIfNotWritable(reportPath, &MAIN, caption))
             return;
 
-        // Get temp filename for the new XML.
+        // Get temp file for the new XML.
         QScopedPointer<QTemporaryFile> tmp(Util::writableTemporaryFile(reportPath));
         tmp->open();
-        tmp->close();
 
         // Generate the XML for the comparison.
         Mlt::Tractor tractor(MLT.profile());
@@ -86,7 +85,8 @@ void EncodeJob::onVideoQualityTriggered()
             tractor.set_track(encoded, 1);
             tractor.plant_transition(vqm);
             vqm.set("render", 0);
-            MLT.saveXML(tmp->fileName(), &tractor, false /* without relative paths */);
+            MLT.saveXML(tmp->fileName(), &tractor, false /* without relative paths */, tmp.data());
+            tmp->close();
 
             // Add consumer element to XML.
             QFile f1(tmp->fileName());

src/mltcontroller.cpp
@@ -22,6 +22,7 @@
 #include <QFileInfo>
 #include <QUuid>
 #include <QSaveFile>
+#include <QTextStream>
 #include <Logger.h>
 #include <Mlt.h>
 #include <cmath>
@@ -42,6 +43,7 @@ namespace Mlt {
 static const int kThumbnailOutSeekFactor = 5;
 static Controller* instance = nullptr;
 const QString XmlMimeType("application/vnd.mlt+xml");
+static const char* kMltXmlPropertyName = "string";
 
 Controller::Controller()
     : m_profile(kDefaultMltProfile)
@@ -444,12 +446,12 @@ void Controller::refreshConsumer(bool scrubAudio)
     }
 }
 
-bool Controller::saveXML(const QString& filename, Service* service, bool withRelativePaths, bool proxy)
+bool Controller::saveXML(const QString& filename, Service* service, bool withRelativePaths,
+                         QTemporaryFile* tempFile, bool proxy)
 {
     QMutexLocker locker(&m_saveXmlMutex);
     QFileInfo fi(filename);
-    static const char* propertyName = "string";
-    Consumer c(profile(), "xml", proxy? filename.toUtf8().constData() : propertyName);
+    Consumer c(profile(), "xml", proxy? filename.toUtf8().constData() : kMltXmlPropertyName);
     Service s(service? service->get_service() : m_producer->get_service());
     if (s.is_valid()) {
         QString root = withRelativePaths? fi.absolutePath() : "";
@@ -468,23 +470,32 @@ bool Controller::saveXML(const QString& filename, Service* service, bool withRel
         c.start();
         if (ignore)
             s.set("ignore_points", ignore);
-        auto xml = QString::fromUtf8(c.get(propertyName));
+        auto xml = QString::fromUtf8(c.get(kMltXmlPropertyName));
         if (!proxy && ProxyManager::filterXML(xml, root)) { // also verifies
-            QSaveFile file(filename);
-            if (!file.open(QIODevice::WriteOnly)) {
-                LOG_ERROR() << "failed to open MLT XML file for writing" << filename;
-            }
-            qint64 result = 0, written = 0;
-            auto data = xml.toUtf8();
-            LOG_DEBUG() << "writing to file" << filename;
-            while (written < data.size()) {
-                if ((result = file.write(data)) < 0) {
-                    LOG_ERROR() << "failed to save MLT XML to file" << filename;
+            if (tempFile) {
+                QTextStream stream(tempFile);
+                stream.setCodec("UTF-8");
+                stream << xml;
+                if (tempFile->error() != QFileDevice::NoError) {
+                    LOG_ERROR() << "error while writing MLT XML file" << tempFile->fileName() << ":" << tempFile->errorString();
                     return false;
                 }
-                written += result;
+            } else {
+                QSaveFile file(filename);
+                file.setDirectWriteFallback(true);
+                if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
+                    LOG_ERROR() << "failed to open MLT XML file for writing" << filename;
+                    return false;
+                }
+                QTextStream stream(&file);
+                stream.setCodec("UTF-8");
+                stream << xml;
+                if (file.error() != QFileDevice::NoError) {
+                    LOG_ERROR() << "error while writing MLT XML file" << filename << ":" << file.errorString();
+                    return false;
+                }
+                return file.commit();
             }
-            return file.commit();
         }
     }
     return false;
@@ -492,8 +503,7 @@ bool Controller::saveXML(const QString& filename, Service* service, bool withRel
 
 QString Controller::XML(Service* service, bool withProfile, bool withMetadata)
 {
-    static const char* propertyName = "string";
-    Consumer c(profile(), "xml", propertyName);
+    Consumer c(profile(), "xml", kMltXmlPropertyName);
     Service s(service? service->get_service() : (m_producer && m_producer->is_valid())? m_producer->get_service() : nullptr);
     if (!s.is_valid())
         return "";
@@ -510,7 +520,7 @@ QString Controller::XML(Service* service, bool withProfile, bool withMetadata)
     c.start();
     if (ignore)
         s.set("ignore_points", ignore);
-    return QString::fromUtf8(c.get(propertyName));
+    return QString::fromUtf8(c.get(kMltXmlPropertyName));
 }
 
 int Controller::consumerChanged()

src/mltcontroller.h
@@ -22,6 +22,7 @@
 #include <QString>
 #include <QUuid>
 #include <QScopedPointer>
+#include <QTemporaryFile>
 #include <QMutex>
 #include <Mlt.h>
 #include "transportcontrol.h"
@@ -88,7 +89,8 @@ public:
     void onWindowResize();
     virtual void seek(int position);
     virtual void refreshConsumer(bool scrubAudio = false);
-    bool saveXML(const QString& filename, Service* service = nullptr, bool withRelativePaths = true, bool proxy = false);
+    bool saveXML(const QString& filename, Service* service = nullptr, bool withRelativePaths = true,
+                 QTemporaryFile* tempFile = nullptr, bool proxy = false);
     QString XML(Service* service = nullptr, bool withProfile = false, bool withMetadata = false);
     int consumerChanged();
     void setProfile(const QString& profile_name);

src/qmltypes/qmlfilter.cpp
@@ -320,11 +320,11 @@ void QmlFilter::analyze(bool isAudio)
 {
     Mlt::Service service(mlt_service(m_filter.get_data("service")));
 
-    // get temp filename for input xml
+    // get temp file for input xml
     QString filename(m_filter.get("filename"));
     QScopedPointer<QTemporaryFile> tmp(Util::writableTemporaryFile(filename));
     tmp->open();
-    tmp->close();
+
     m_filter.set("results", nullptr, 0);
     int disable = m_filter.get_int("disable");
     m_filter.set("disable", 0);
@@ -345,7 +345,9 @@ void QmlFilter::analyze(bool isAudio)
         }
     }
 
-    MLT.saveXML(tmp->fileName(), &service, false /* without relative paths */);
+    MLT.saveXML(tmp->fileName(), &service, false /* without relative paths */, tmp.data());
+    tmp->close();
+
     if (!isAudio) m_filter.set("analyze", 0);
     m_filter.set("disable", disable);
 