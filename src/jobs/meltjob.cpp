mltframework/shotcut

commit 9016f494387f663accf9dd89765ba0f5f64050e0

fix crash running a melt job with empty XML

src/jobs/meltjob.cpp
@@ -24,6 +24,7 @@
 #include <QAction>
 #include <QDialog>
 #include <QDir>
+#include <QTimer>
 #include <Logger.h>
 #include "mainwindow.h"
 #include "dialogs/textviewerdialog.h"
@@ -84,6 +85,15 @@ MeltJob::~MeltJob()
 
 void MeltJob::start()
 {
+    if (!m_xml) {
+        AbstractJob::start();
+        LOG_ERROR() << "the job XML is empty!";
+        appendToLog("Error: the job XML is empty!\n");
+        QTimer::singleShot(0, [=]() {
+            emit finished(this, false);
+        });
+        return;
+    }
     QString shotcutPath = qApp->applicationDirPath();
     QFileInfo meltPath(shotcutPath, "melt");
     setReadChannel(QProcess::StandardError);

commit 24a02d6ee8330f894c28856a64cb0087e45c8e0f

fix Properties > Reverse broken by 9016f49

src/jobs/meltjob.cpp
@@ -85,7 +85,7 @@ MeltJob::~MeltJob()
 
 void MeltJob::start()
 {
-    if (!m_xml) {
+    if (m_args.isEmpty() && !m_xml) {
         AbstractJob::start();
         LOG_ERROR() << "the job XML is empty!";
         appendToLog("Error: the job XML is empty!\n");