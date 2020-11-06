mltframework/shotcut

commit e34d43f1355ca0ee0d699b18212acca383824adc

fix #489 Timeline waveforms drawn too much
This optimization by checking wether items are in the current viewport
is applied to thumbnails as well.

src/qml/views/timeline/Clip.qml
@@ -109,7 +109,11 @@ Rectangle {
 
     Image {
         id: outThumbnail
-        visible: settings.timelineShowThumbnails && parent.height > 20
+        visible: !isBlank && settings.timelineShowThumbnails && parent.height > 20 &&
+                 ((clipRoot.x + x + width)   > scrollView.flickableItem.contentX) && // right edge
+                 ((clipRoot.x + x)           < scrollView.flickableItem.contentX + scrollView.width) && // left edge
+                 ((trackRoot.y + y + height) > scrollView.flickableItem.contentY) && // bottom edge
+                 ((trackRoot.y + y)          < scrollView.flickableItem.contentY + scrollView.height) // top edge
         anchors.right: parent.right
         anchors.top: parent.top
         anchors.topMargin: parent.border.width
@@ -123,7 +127,11 @@ Rectangle {
 
     Image {
         id: inThumbnail
-        visible: settings.timelineShowThumbnails && parent.height > 20
+        visible: !isBlank && settings.timelineShowThumbnails && parent.height > 20 &&
+                 ((clipRoot.x + x + width)   > scrollView.flickableItem.contentX) && // right edge
+                 ((clipRoot.x + x)           < scrollView.flickableItem.contentX + scrollView.width) && // left edge
+                 ((trackRoot.y + y + height) > scrollView.flickableItem.contentY) && // bottom edge
+                 ((trackRoot.y + y)          < scrollView.flickableItem.contentY + scrollView.height) // top edge
         anchors.left: parent.left
         anchors.top: parent.top
         anchors.topMargin: parent.border.width
@@ -155,6 +163,7 @@ Rectangle {
 
         Repeater {
             id: waveformRepeater
+            model: (clipRoot.width + waveform.maxWidth - 1) / waveform.maxWidth
             TimelineWaveform {
                 width: Math.min(waveform.innerWidth, waveform.maxWidth)
                 height: waveform.height
@@ -163,6 +172,10 @@ Rectangle {
                 inPoint: Math.round((clipRoot.inPoint + index * waveform.maxWidth / timeScale) * speed) * channels
                 outPoint: inPoint + Math.round(width / timeScale * speed) * channels
                 levels: audioLevels
+                active: ((clipRoot.x + x + width)   > scrollView.flickableItem.contentX) && // right edge
+                        ((clipRoot.x + x)           < scrollView.flickableItem.contentX + scrollView.width) && // left edge
+                        ((trackRoot.y + y + height) > scrollView.flickableItem.contentY) && // bottom edge
+                        ((trackRoot.y + y)          < scrollView.flickableItem.contentY + scrollView.height) // top edge
             }
         }
     }

src/qmltypes/timelineitems.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016 Meltytech, LLC
+ * Copyright (c) 2015-2020 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
 
 #include "timelineitems.h"
 #include "mltcontroller.h"
+#include <Logger.h>
 
 #include <QQuickPaintedItem>
 #include <QPainter>
@@ -96,6 +97,7 @@ class TimelineWaveform : public QQuickPaintedItem
     Q_PROPERTY(QColor fillColor MEMBER m_color NOTIFY propertyChanged)
     Q_PROPERTY(int inPoint MEMBER m_inPoint NOTIFY inPointChanged)
     Q_PROPERTY(int outPoint MEMBER m_outPoint NOTIFY outPointChanged)
+    Q_PROPERTY(bool active MEMBER m_isActive NOTIFY propertyChanged)
 
 public:
     TimelineWaveform()
@@ -106,6 +108,8 @@ public:
 
     void paint(QPainter *painter)
     {
+        if (!m_isActive)
+            return;
         QVariantList data = m_audioLevels.toList();
         if (data.isEmpty())
             return;
@@ -116,6 +120,8 @@ public:
         const int inPoint = qRound(m_inPoint / MLT.profile().fps() * 25.0);
         const int outPoint = qRound(m_outPoint / MLT.profile().fps() * 25.0);
         const qreal indicesPrPixel = qreal(outPoint - inPoint) / width();
+
+//        LOG_DEBUG() << "In/out points" << inPoint << "/" << outPoint;
 
         QPainterPath path;
         path.moveTo(-1, height());
@@ -146,6 +152,7 @@ private:
     int m_inPoint;
     int m_outPoint;
     QColor m_color;
+    bool m_isActive {true};
 };
 
 void registerTimelineItems()

commit ac24e1b11b199f9891f556ea1e700036442314a7

fix #925 performance regression in e34d43f

src/qml/views/keyframes/Clip.qml
@@ -92,11 +92,7 @@ Rectangle {
     Image {
         id: outThumbnail
         visible: settings.timelineShowThumbnails && outThumbnailVisible && metadata !== null &&
-                 parent.height > 20 && x > inThumbnail.width &&
-                 ((clipRoot.x + x + width)   > scrollView.flickableItem.contentX) && // right edge
-                 ((clipRoot.x + x)           < scrollView.flickableItem.contentX + scrollView.width) && // left edge
-                 ((trackRoot.y + y + height) > scrollView.flickableItem.contentY) && // bottom edge
-                 ((trackRoot.y + y)          < scrollView.flickableItem.contentY + scrollView.height) // top edge
+                 parent.height > 20 && x > inThumbnail.width
         anchors.right: parent.right
         anchors.top: parent.top
         anchors.topMargin: parent.border.width
@@ -110,11 +106,7 @@ Rectangle {
 
     Image {
         id: inThumbnail
-        visible: settings.timelineShowThumbnails && inThumbnailVisible && metadata !== null && parent.height > 20 &&
-                 ((clipRoot.x + x + width)   > scrollView.flickableItem.contentX) && // right edge
-                 ((clipRoot.x + x)           < scrollView.flickableItem.contentX + scrollView.width) && // left edge
-                 ((trackRoot.y + y + height) > scrollView.flickableItem.contentY) && // bottom edge
-                 ((trackRoot.y + y)          < scrollView.flickableItem.contentY + scrollView.height) // top edge
+        visible: settings.timelineShowThumbnails && inThumbnailVisible && metadata !== null && parent.height > 20
         anchors.left: parent.left
         anchors.top: parent.top
         anchors.topMargin: parent.border.width

src/qml/views/timeline/Clip.qml
@@ -109,12 +109,7 @@ Rectangle {
 
     Image {
         id: outThumbnail
-        visible: !isBlank && settings.timelineShowThumbnails &&
-                 parent.height > 20 && x > inThumbnail.width &&
-                 ((clipRoot.x + x + width)   > scrollView.flickableItem.contentX) && // right edge
-                 ((clipRoot.x + x)           < scrollView.flickableItem.contentX + scrollView.width) && // left edge
-                 ((trackRoot.y + y + height) > scrollView.flickableItem.contentY) && // bottom edge
-                 ((trackRoot.y + y)          < scrollView.flickableItem.contentY + scrollView.height) // top edge
+        visible: !isBlank && settings.timelineShowThumbnails && parent.height > 20 && x > inThumbnail.width
         anchors.right: parent.right
         anchors.top: parent.top
         anchors.topMargin: parent.border.width
@@ -128,11 +123,7 @@ Rectangle {
 
     Image {
         id: inThumbnail
-        visible: !isBlank && settings.timelineShowThumbnails && parent.height > 20 &&
-                 ((clipRoot.x + x + width)   > scrollView.flickableItem.contentX) && // right edge
-                 ((clipRoot.x + x)           < scrollView.flickableItem.contentX + scrollView.width) && // left edge
-                 ((trackRoot.y + y + height) > scrollView.flickableItem.contentY) && // bottom edge
-                 ((trackRoot.y + y)          < scrollView.flickableItem.contentY + scrollView.height) // top edge
+        visible: !isBlank && settings.timelineShowThumbnails && parent.height > 20
         anchors.left: parent.left
         anchors.top: parent.top
         anchors.topMargin: parent.border.width