mltframework/shotcut

commit fe991ad02452f5b8340b337bbc16b7ad4cccb37c

make clicking black from transparent behave like the other colors

src/qml/modules/Shotcut/Controls/GradientControl.qml
@@ -72,8 +72,8 @@ RowLayout {
                     // If the user changed color but left alpha at 0,
                     // they probably want to reset alpha to opaque.
                     console.log('currentColor.a=' + currentColor.a + ' currentColor=' + currentColor + ' myColor=' + myColor)
-                    if (currentColor.a === 0 && !Qt.colorEqual(currentColor, myColor))
-                        currentColor.a = 1.0
+                    if (currentColor.a === 0 && (!Qt.colorEqual(currentColor, myColor) ||
+                                                 (Qt.colorEqual(currentColor, 'transparent') && Qt.colorEqual(myColor, 'transparent'))))
                     parent.parent._setStopColor(handelRect.stopIndex, String(currentColor))
                 }
                 modality: Qt.ApplicationModal

src/qml/modules/Shotcut/Controls/ColorPicker.qml
@@ -70,7 +70,8 @@ RowLayout {
             // If the user changed color but left alpha at 0,
             // they probably want to reset alpha to opaque.
             console.log('currentColor.a=' + currentColor.a + ' currentColor=' + currentColor + ' myColor=' + myColor)
-            if (currentColor.a === 0 && !Qt.colorEqual(currentColor, myColor))
+            if (currentColor.a === 0 && (!Qt.colorEqual(currentColor, myColor) ||
+                                         (Qt.colorEqual(currentColor, 'transparent') && Qt.colorEqual(myColor, 'transparent'))))
                 currentColor.a = 1.0
             // Assign the new color value. Unlike docs say, using currentColor
             // is actually more cross-platform compatible.

src/widgets/colorproducerwidget.cpp
@@ -75,7 +75,8 @@ void ColorProducerWidget::on_colorButton_clicked()
         auto newColor = dialog.currentColor();
         auto rgb = newColor;
         rgb.setAlpha(color.alpha());
-        if (newColor.alpha() == 0 && rgb != color) {
+        if (newColor.alpha() == 0 && (rgb != color ||
+            (newColor == QColorConstants::Transparent && color == QColorConstants::Transparent))) {
             newColor.setAlpha(255);
         }
         ui->colorLabel->setText(colorToString(newColor));

src/widgets/textproducerwidget.cpp
@@ -76,7 +76,8 @@ void TextProducerWidget::on_colorButton_clicked()
         auto newColor = dialog.currentColor();
         auto rgb = newColor;
         rgb.setAlpha(color.alpha());
-        if (newColor.alpha() == 0 && rgb != color) {
+        if (newColor.alpha() == 0 && (rgb != color ||
+            (newColor == QColorConstants::Transparent && color == QColorConstants::Transparent))) {
             newColor.setAlpha(255);
         }
         ui->colorLabel->setText(colorToString(newColor));

commit dcbac18b73fbd91717f5e2ec7824131e0789f94a

fix gradient color selection broken in fe991ad0

src/qml/modules/Shotcut/Controls/GradientControl.qml
@@ -73,7 +73,9 @@ RowLayout {
                     // they probably want to reset alpha to opaque.
                     console.log('currentColor.a=' + currentColor.a + ' currentColor=' + currentColor + ' myColor=' + myColor)
                     if (currentColor.a === 0 && (!Qt.colorEqual(currentColor, myColor) ||
-                                                 (Qt.colorEqual(currentColor, 'transparent') && Qt.colorEqual(myColor, 'transparent'))))
+                                                 (Qt.colorEqual(currentColor, 'transparent') && Qt.colorEqual(myColor, 'transparent')))) {
+                        currentColor.a = 1.0
+                    }
                     parent.parent._setStopColor(handelRect.stopIndex, String(currentColor))
                 }
                 modality: application.dialogModality