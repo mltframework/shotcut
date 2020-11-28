mltframework/shotcut

commit 927e3d73892b80c7f78cc8b1bd7833e07ab696a6

fix combo boxes in filters may not open
This happens with Qt 5.15 on systems with multiple screens at different
display scales just like the menus. All popup style windows in Quick are
broken. This converts them all to Quick Controls 2 API.

src/qml/filters/alpha_adjust/ui.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2019 Meltytech, LLC
+ * Copyright (c) 2015-2020 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@ import QtQuick 2.5
 import QtQuick.Controls 1.4
 import QtQuick.Layouts 1.1
 import Shotcut.Controls 1.0
+import QtQuick.Controls 2.12 as Controls2
 import QtQml.Models 2.2
 
 Item {
@@ -54,7 +55,7 @@ Item {
             text: qsTr('Mode')
             Layout.alignment: Qt.AlignRight
         }
-        ComboBox {
+        Controls2.ComboBox {
             id: modeCombo
             implicitWidth: 180
             model: ListModel {
@@ -68,6 +69,8 @@ Item {
                 ListElement { text: qsTr('Threshold'); value: 0.8 }
                 ListElement { text: qsTr('Blur'); value: 1.0 }
             }
+            textRole: 'text'
+            valueRole: 'value'
             onCurrentIndexChanged: {
                 filter.set(paramOperation, operationModel.get(currentIndex).value)
             }

src/qml/filters/alpha_view/ui.qml
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2015 Meltytech, LLC
- * Author: Dan Dennedy <dan@dennedy.org>
+ * Copyright (c) 2020 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,9 +16,8 @@
  */
 
 import QtQuick 2.5
-import QtQuick.Controls 1.4
+import QtQuick.Controls 2.12
 import QtQuick.Layouts 1.1
-import Shotcut.Controls 1.0
 import QtQml.Models 2.2
 
 Item {
@@ -58,6 +56,8 @@ Item {
                     ListElement { text: qsTr('Gray Background'); value: 0.64 }
                     ListElement { text: qsTr('White Background'); value: 0.79 }
                 }
+                textRole: 'text'
+                valueRole: 'value'
                 onCurrentIndexChanged: {
                     filter.set(paramDisplay, displayModel.get(currentIndex).value)
                 }

src/qml/filters/blend_mode/ui.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Meltytech, LLC
+ * Copyright (c) 2019-2020 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
 
 import QtQuick 2.1
 import QtQuick.Controls 1.0
+import QtQuick.Controls 2.12 as Controls2
 import QtQuick.Layouts 1.0
 import Shotcut.Controls 1.0
 
@@ -46,7 +47,7 @@ Item {
         columns: 4
 
         Label { text: qsTr('Blend mode') }
-        ComboBox {
+        Controls2.ComboBox {
             id: combo
             model: ListModel {
                 id: comboItems
@@ -69,6 +70,8 @@ Item {
                 ListElement { text: qsTr('HSL Color'); value: 'hslcolor' }
                 ListElement { text: qsTr('HSL Luminosity'); value: 'hslluminocity' }
             }
+            textRole: 'text'
+            valueRole: 'value'
             onCurrentIndexChanged: {
                 filter.set(propertyName, comboItems.get(currentIndex).value)
             }

src/qml/filters/gradient/ui.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Meltytech, LLC
+ * Copyright (c) 2019-2020 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
 
 import QtQuick 2.0
 import QtQuick.Controls 1.1
+import QtQuick.Controls 2.12 as Controls2
 import QtQuick.Layouts 1.1
 import Shotcut.Controls 1.0
 
@@ -368,7 +369,7 @@ Item {
         }
 
         Label { text: qsTr('Blend mode') }
-        ComboBox {
+        Controls2.ComboBox {
             id: blendCombo
             model: ListModel {
                 id: comboItems
@@ -391,6 +392,8 @@ Item {
                 ListElement { text: qsTr('HSL Color'); value: 'hslcolor' }
                 ListElement { text: qsTr('HSL Luminosity'); value: 'hslluminocity' }
             }
+            textRole: 'text'
+            valueRole: 'value'
             onCurrentIndexChanged: {
                 filter.set(blendProperty, comboItems.get(currentIndex).value)
             }

src/qml/filters/select0r/ui.qml
@@ -17,6 +17,7 @@
 
 import QtQuick 2.0
 import QtQuick.Controls 1.1
+import QtQuick.Controls 2.12 as Controls2
 import QtQuick.Layouts 1.1
 import Shotcut.Controls 1.0
 
@@ -217,7 +218,7 @@ Item {
             text: qsTr('Shape')
             Layout.alignment: Qt.AlignRight
         }
-        ComboBox {
+        Controls2.ComboBox {
             id: shapeCombo
             implicitWidth: 180
             model: ListModel {
@@ -226,6 +227,8 @@ Item {
                 ListElement { text: qsTr('Ellipsoid'); value: 0.5 }
                 ListElement { text: qsTr('Diamond');   value: 1.0 }
             }
+            textRole: 'text'
+            valueRole: 'value'
             onCurrentIndexChanged: filter.set(shapeParam, shapeModel.get(currentIndex).value)
         }
         UndoButton {
@@ -236,7 +239,7 @@ Item {
             text: qsTr('Edge')
             Layout.alignment: Qt.AlignRight
         }
-        ComboBox {
+        Controls2.ComboBox {
             id: edgeCombo
             implicitWidth: 180
             model: ListModel {
@@ -247,6 +250,8 @@ Item {
                 ListElement { text: qsTr('Thin');   value: 0.7 }
                 ListElement { text: qsTr('Slope');  value: 0.9 }
             }
+            textRole: 'text'
+            valueRole: 'value'
             onCurrentIndexChanged: filter.set(edgeParam, edgeModel.get(currentIndex).value)
         }
         UndoButton {
@@ -274,7 +279,7 @@ Item {
             text: qsTr('Operation')
             Layout.alignment: Qt.AlignRight
         }
-        ComboBox {
+        Controls2.ComboBox {
             id: operationCombo
             implicitWidth: 180
             model: ListModel {
@@ -285,6 +290,8 @@ Item {
                 ListElement { text: qsTr('Add');       value: 0.7 }
                 ListElement { text: qsTr('Subtract');  value: 1.0 }
             }
+            textRole: 'text'
+            valueRole: 'value'
             onCurrentIndexChanged: filter.set(operationParam, operationModel.get(currentIndex).value )
         }
         UndoButton {

src/qml/filters/timer/ui.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019 Meltytech, LLC
+ * Copyright (c) 2018-2020 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
  */
 
 import QtQuick 2.0
+import QtQuick.Controls 2.12 as Controls2
 import QtQuick.Controls 1.1
 import QtQuick.Layouts 1.1
 import Shotcut.Controls 1.0
@@ -135,7 +136,7 @@ Item {
             text: qsTr('Format')
             Layout.alignment: Qt.AlignRight
         }
-        ComboBox {
+        Controls2.ComboBox {
             id: formatCombo
             model: ListModel {
                 ListElement { text: QT_TR_NOOP('HH:MM:SS'); format: "HH:MM:SS" }
@@ -148,6 +149,8 @@ Item {
                 ListElement { text: QT_TR_NOOP('SS.SS'); format: "SS.SS" }
                 ListElement { text: QT_TR_NOOP('SS.SSS'); format: "SS.SSS" }
             }
+            textRole: 'text'
+            valueRole: 'format'
             onCurrentIndexChanged: {
                 filter.set('format', model.get(currentIndex).format)
             }
@@ -157,12 +160,14 @@ Item {
             text: qsTr('Direction')
             Layout.alignment: Qt.AlignRight
         }
-        ComboBox {
+        Controls2.ComboBox {
             id: directionCombo
             model: ListModel {
                 ListElement { text: QT_TR_NOOP('Up'); direction: "up" }
                 ListElement { text: QT_TR_NOOP('Down'); direction: "down" }
             }
+            textRole: 'text'
+            valueRole: 'direction'
             onCurrentIndexChanged: {
                 filter.set('direction', model.get(currentIndex).direction)
             }

src/qml/filters/audio_channelcopy/ui.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2018 Meltytech, LLC
+ * Copyright (c) 2013-2020 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
 
 import QtQuick 2.1
 import QtQuick.Controls 1.0
+import QtQuick.Controls 2.12 as Controls2
 import QtQuick.Layouts 1.0
 
 Item {
@@ -53,13 +54,13 @@ Item {
 
         RowLayout {
             Label { text: qsTr('Copy from') }
-            ComboBox {
+            Controls2.ComboBox {
                 id: fromCombo
                 model: [qsTr('Left'), qsTr('Right')]
                 onCurrentIndexChanged: filter.set(fromParameter, currentIndex)
             }
             Label { text: qsTr('to') }
-            ComboBox {
+            Controls2.ComboBox {
                 id: toCombo
                 model: fromCombo.model
                 onCurrentIndexChanged: filter.set(toParameter, currentIndex)

src/qml/filters/audio_pan/ui.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019 Meltytech, LLC
+ * Copyright (c) 2013-2020 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
 
 import QtQuick 2.1
 import QtQuick.Controls 1.0
+import QtQuick.Controls 2.12 as Controls2
 import QtQuick.Layouts 1.0
 import Shotcut.Controls 1.0
 
@@ -126,7 +127,7 @@ Item {
             text: qsTr('Channel')
             Layout.alignment: Qt.AlignRight
         }
-        ComboBox {
+        Controls2.ComboBox {
             id: combo
             Layout.columnSpan: 3
             model: [qsTr('Left'), qsTr('Right')]

commit a219506576ae13474ba1233ce40c3de77ea94f9c

fix some filter UIs are blank on macOS
regression in 927e3d73

src/qml/filters/alpha_adjust/ui.qml
@@ -70,7 +70,6 @@ Item {
                 ListElement { text: qsTr('Blur'); value: 1.0 }
             }
             textRole: 'text'
-            valueRole: 'value'
             onActivated: {
                 filter.set(paramOperation, operationModel.get(currentIndex).value)
             }

src/qml/filters/alpha_view/ui.qml
@@ -57,7 +57,6 @@ Item {
                     ListElement { text: qsTr('White Background'); value: 0.79 }
                 }
                 textRole: 'text'
-                valueRole: 'value'
                 onActivated: {
                     filter.set(paramDisplay, displayModel.get(currentIndex).value)
                 }

src/qml/filters/blend_mode/ui.qml
@@ -72,7 +72,6 @@ Item {
                 ListElement { text: qsTr('HSL Luminosity'); value: 'hslluminocity' }
             }
             textRole: 'text'
-            valueRole: 'value'
             onActivated: {
                 filter.set(propertyName, comboItems.get(currentIndex).value)
             }

src/qml/filters/gradient/ui.qml
@@ -393,7 +393,6 @@ Item {
                 ListElement { text: qsTr('HSL Luminosity'); value: 'hslluminocity' }
             }
             textRole: 'text'
-            valueRole: 'value'
             onActivated: {
                 filter.set(blendProperty, comboItems.get(currentIndex).value)
             }

src/qml/filters/select0r/ui.qml
@@ -228,7 +228,6 @@ Item {
                 ListElement { text: qsTr('Diamond');   value: 1.0 }
             }
             textRole: 'text'
-            valueRole: 'value'
             onActivated: filter.set(shapeParam, shapeModel.get(currentIndex).value)
         }
         UndoButton {
@@ -254,7 +253,6 @@ Item {
                 ListElement { text: qsTr('Slope');  value: 0.9 }
             }
             textRole: 'text'
-            valueRole: 'value'
             onActivated: filter.set(edgeParam, edgeModel.get(currentIndex).value)
         }
         UndoButton {
@@ -297,7 +295,6 @@ Item {
                 ListElement { text: qsTr('Subtract');  value: 1.0 }
             }
             textRole: 'text'
-            valueRole: 'value'
             onActivated: filter.set(operationParam, operationModel.get(currentIndex).value )
         }
         UndoButton {

src/qml/filters/timer/ui.qml
@@ -150,7 +150,6 @@ Item {
                 ListElement { text: QT_TR_NOOP('SS.SSS'); format: "SS.SSS" }
             }
             textRole: 'text'
-            valueRole: 'format'
             onActivated: {
                 filter.set('format', model.get(currentIndex).format)
             }
@@ -167,7 +166,6 @@ Item {
                 ListElement { text: QT_TR_NOOP('Down'); direction: "down" }
             }
             textRole: 'text'
-            valueRole: 'direction'
             onActivated: {
                 filter.set('direction', model.get(currentIndex).direction)
             }