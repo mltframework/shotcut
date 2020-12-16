5b8f21b716ab85e38c887d3b415a906cf7190315
Brian Matherly <code@brianmatherly.com>
2020-12-15 22:06:50 -0600

    Support timeremap on xml clips

--- src/mltcontroller.cpp
+++ src/mltcontroller.cpp
@@ -177,7 +177,7 @@ bool Controller::openXML(const QString &filename)
 {
     bool error = true;
     close();
-    Producer* producer = new Mlt::Producer(profile(), "xml", filename.toUtf8().constData());
+    Producer* producer = new Mlt::Chain(profile(), "xml", filename.toUtf8().constData());
     if (producer->is_valid()) {
         double fps = profile().fps();
         if (!profile().is_explicit()) {
@@ -190,7 +190,7 @@ bool Controller::openXML(const QString &filename)
         if (isFpsDifferent(profile().fps(), fps)) {
             // reopen with the correct fps
             delete producer;
-            producer = new Mlt::Producer(profile(), "xml", filename.toUtf8().constData());
+            producer = new Mlt::Chain(profile(), "xml", filename.toUtf8().constData());
         }
         producer->set(kShotcutVirtualClip, 1);
         producer->set("resource", filename.toUtf8().constData());
