mltframework/shotcut

commit e07950d1b9856a6e6dd913144962fc5d93b6422b

fix some clazy warnings
https://github.com/KDE/clazy

src/docks/timelinedock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,9 +35,8 @@
 #include <QtQuick>
 #include <Logger.h>
 
-static QString kNonSeekableWarning = QObject::tr("You cannot add a non-seekable source.");
-static auto kFileUrlProtocol = QStringLiteral("file://");
-static auto kFilesUrlDelimiter = QStringLiteral(",file://");
+static const char* kFileUrlProtocol = "file://";
+static const char* kFilesUrlDelimiter = ",file://";
 
 TimelineDock::TimelineDock(QWidget *parent) :
     QDockWidget(parent),
@@ -167,6 +166,11 @@ void TimelineDock::pulseLockButtonOnTrack(int trackIndex)
     emit showStatusMessage(tr("This track is locked"));
 }
 
+void TimelineDock::emitNonSeekableWarning()
+{
+    emit showStatusMessage(tr("You cannot add a non-seekable source."));
+}
+
 void TimelineDock::chooseClipAtPosition(int position, int& trackIndex, int& clipIndex)
 {
     QScopedPointer<Mlt::Producer> clip;
@@ -276,14 +280,14 @@ QVariantList TimelineDock::selectionForJS() const
     return result;
 }
 
-QList<QPoint> TimelineDock::selection() const
+const QList<QPoint> TimelineDock::selection() const
 {
     if (!m_quickView.rootObject())
         return QList<QPoint>();
     return m_selection.selectedClips;
 }
 
-QVector<QUuid> TimelineDock::selectionUuids()
+const QVector<QUuid> TimelineDock::selectionUuids()
 {
     QVector<QUuid> result;
     for (const auto& clip : selection()) {
@@ -554,7 +558,7 @@ void TimelineDock::append(int trackIndex)
             new Timeline::AppendCommand(m_model, trackIndex, MLT.XML(&producer)));
         selectClipUnderPlayhead();
     } else if (!MLT.isSeekableClip()) {
-        emit showStatusMessage(kNonSeekableWarning);
+        emitNonSeekableWarning();
     }
 }
 
@@ -636,7 +640,6 @@ void TimelineDock::liftSelection()
     if (selection().isEmpty())
         return;
     int n = selection().size();
-    QVector<QUuid> uuids;
     if (n > 1)
         MAIN.undoStack()->beginMacro(tr("Lift %1 from timeline").arg(n));
     int trackIndex, clipIndex;
@@ -857,7 +860,7 @@ void TimelineDock::onProducerModified()
 void TimelineDock::replace(int trackIndex, int clipIndex, const QString& xml)
 {
     if (xml.isEmpty() && !MLT.isClip() && !MLT.savedProducer()) {
-        showStatusMessage(tr("There is nothing in the Source player."));
+        emit showStatusMessage(tr("There is nothing in the Source player."));
         return;
     }
     if (!m_model.trackList().count() || MAIN.isSourceClipMyProject())
@@ -872,7 +875,7 @@ void TimelineDock::replace(int trackIndex, int clipIndex, const QString& xml)
         clipIndex = clipIndexAtPlayhead(trackIndex);
     Mlt::Producer producer(producerForClip(trackIndex, clipIndex));
     if (producer.is_valid() && producer.type() == tractor_type) {
-        showStatusMessage(tr("You cannot replace a transition."));
+        emit showStatusMessage(tr("You cannot replace a transition."));
         return;
     }
     if (MLT.isSeekableClip() || MLT.savedProducer() || !xml.isEmpty()) {
@@ -882,7 +885,7 @@ void TimelineDock::replace(int trackIndex, int clipIndex, const QString& xml)
         MAIN.undoStack()->push(
             new Timeline::ReplaceCommand(m_model, trackIndex, clipIndex, xmlToUse));
     } else if (!MLT.isSeekableClip()) {
-        emit showStatusMessage(kNonSeekableWarning);
+        emitNonSeekableWarning();
     }
 }
 
@@ -1097,7 +1100,8 @@ static QString convertUrlsToXML(const QString& xml)
         LongUiTask longTask(QObject::tr("Drop Files"));
         Mlt::Playlist playlist(MLT.profile());
         QList<QUrl> urls;
-        for (auto s : xml.split(kFilesUrlDelimiter)) {
+        const auto& strings = xml.split(kFilesUrlDelimiter);
+        for (const auto& s : strings) {
 #ifdef Q_OS_WIN
             if (!s.startsWith(kFileUrlProtocol)) {
                 s.prepend(kFileUrlProtocol);
@@ -1181,7 +1185,7 @@ void TimelineDock::insert(int trackIndex, int position, const QString &xml, bool
         MAIN.undoStack()->push(
             new Timeline::InsertCommand(m_model, trackIndex, position, xmlToUse, seek));
     } else if (!MLT.isSeekableClip()) {
-        emit showStatusMessage(kNonSeekableWarning);
+        emitNonSeekableWarning();
     }
 }
 
@@ -1230,7 +1234,7 @@ void TimelineDock::overwrite(int trackIndex, int position, const QString &xml, b
         MAIN.undoStack()->push(
             new Timeline::OverwriteCommand(m_model, trackIndex, position, xmlToUse, seek));
     } else if (!MLT.isSeekableClip()) {
-        emit showStatusMessage(kNonSeekableWarning);
+        emitNonSeekableWarning();
     }
 }
 
@@ -1509,7 +1513,7 @@ void TimelineDock::replaceClipsWithHash(const QString& hash, Mlt::Producer& prod
                 info->producer->set(kOriginalResourceProperty, producer.get("resource"));
                 auto caption = Util::baseName(ProxyManager::resource(*info->producer));
                 if (!::qstrcmp(info->producer->get("mlt_service"), "timewarp")) {
-                    caption = QString("%1 (%2x)").arg(caption).arg(info->producer->get("warp_speed"));
+                    caption = QString("%1 (%2x)").arg(caption, info->producer->get("warp_speed"));
                 }
                 info->producer->set(kShotcutCaptionProperty, caption.toUtf8().constData());
             } else {

src/docks/timelinedock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -63,8 +63,8 @@ public:
     void setSelectionFromJS(const QVariantList& list);
     void setSelection(QList<QPoint> selection = QList<QPoint>(), int trackIndex = -1, bool isMultitrack = false);
     QVariantList selectionForJS() const;
-    QList<QPoint> selection() const;
-    QVector<QUuid> selectionUuids();
+    const QList<QPoint> selection() const;
+    const QVector<QUuid> selectionUuids();
     void saveAndClearSelection();
     void restoreSelection();
     void selectClipUnderPlayhead();
@@ -166,6 +166,7 @@ protected:
 private:
     bool isBlank(int trackIndex, int clipIndex);
     void pulseLockButtonOnTrack(int trackIndex);
+    void emitNonSeekableWarning();
 
     Ui::TimelineDock *ui;
     QQuickWidget m_quickView;

src/qml/views/filter/FilterMenu.js
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2014-2015 Meltytech, LLC
- * Author: Brian Matherly <code@brianmatherly.com>
+ * Copyright (c) 2014-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,7 +31,7 @@ function maxMenuHeight(pad) {
 function calcMenuRect(triggerItem, pad) {
     var result = Qt.rect(0, 0, 0, 0)
     var itemPos = triggerItem.mapToItem(null,0,0)
-    var triggerPos = Qt.point(itemPos.x + view.pos.x, itemPos.y + view.pos.y)
+    var triggerPos = Qt.point(itemPos.x + view.pos().x, itemPos.y + view.pos().y)
     var mainWinRect = application.mainWinRect
     
     result.height = Math.min(maxMenuHeight(pad), mainWinRect.height)

src/qmltypes/qmlview.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2014-2016 Meltytech, LLC
- * Author: Brian Matherly <code@brianmatherly.com>
+ * Copyright (c) 2014-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,11 +26,10 @@ class QWidget;
 class QmlView : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QPoint pos READ pos);
 
 public:
     explicit QmlView(QWidget* qview);
-    QPoint pos();
+    Q_INVOKABLE QPoint pos();
 
 private:
     QWidget* m_qview;

src/util.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2020 Meltytech, LLC
+ * Copyright (c) 2014-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -176,7 +176,7 @@ static inline bool isValidGoProSuffix(const QFileInfo& info)
     return list.contains(info.suffix().toUpper());
 }
 
-QStringList Util::sortedFileList(const QList<QUrl>& urls)
+const QStringList Util::sortedFileList(const QList<QUrl>& urls)
 {
     QStringList result;
     QMap<QString, QStringList> goproFiles;
@@ -375,7 +375,7 @@ void Util::applyCustomProperties(Mlt::Producer& destination, Mlt::Producer& sour
 
         resource = destination.get("_shotcut:resource");
         destination.set("warp_resource", resource.toUtf8().constData());
-        resource = QString("%1:%2:%3").arg("timewarp").arg(source.get("warp_speed")).arg(resource);
+        resource = QString("%1:%2:%3").arg("timewarp", source.get("warp_speed"), resource);
         destination.set("resource", resource.toUtf8().constData());
         double speedRatio = 1.0 / speed;
         int length = qRound(destination.get_length() * speedRatio);
@@ -486,7 +486,7 @@ bool Util::isMemoryLow()
     if (meminfo.open(QIODevice::ReadOnly)) {
         for (auto line = meminfo.readLine(1024); availableKB == UINT_MAX && !line.isEmpty(); line = meminfo.readLine(1024)) {
             if (line.startsWith("MemAvailable")) {
-                auto fields = line.split(' ');
+                const auto& fields = line.split(' ');
                 for (const auto& s : fields) {
                     bool ok = false;
                     auto kB = s.toUInt(&ok);

src/util.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2020 Meltytech, LLC
+ * Copyright (c) 2014-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -43,7 +43,7 @@ public:
     static bool warnIfNotWritable(const QString& filePath, QWidget* parent, const QString& caption);
     static QString producerTitle(const Mlt::Producer& producer);
     static QString removeFileScheme(QUrl &url);
-    static QStringList sortedFileList(const QList<QUrl>& urls);
+    static const QStringList sortedFileList(const QList<QUrl>& urls);
     static int coerceMultiple(int value, int multiple = 2);
     static QList<QUrl> expandDirectories(const QList<QUrl>& urls);
     static bool isDecimalPoint(QChar ch);

commit ff6c2f0459dea342cd223e1e45cee8dd340c6c1e

fix compile error on const on Windows
regression in e07950d1

src/docks/timelinedock.cpp
@@ -1101,7 +1101,7 @@ static QString convertUrlsToXML(const QString& xml)
         Mlt::Playlist playlist(MLT.profile());
         QList<QUrl> urls;
         const auto& strings = xml.split(kFilesUrlDelimiter);
-        for (const auto& s : strings) {
+        for (auto s : strings) {
 #ifdef Q_OS_WIN
             if (!s.startsWith(kFileUrlProtocol)) {
                 s.prepend(kFileUrlProtocol);