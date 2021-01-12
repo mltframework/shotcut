mltframework/shotcut

commit 031d3a9225b8bead669958c6338406ba6d0eb42f

add undo/redo for changing a filter

src/qmltypes/qmlfilter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,6 +25,7 @@
 #include "settings.h"
 #include "util.h"
 #include "proxymanager.h"
+#include "commands/filtercommands.h"
 #include <Logger.h>
 
 #include <QDir>
@@ -35,11 +36,13 @@
 #include <MltProducer.h>
 
 QmlFilter::QmlFilter()
-    : QObject(0)
-    , m_metadata(0)
-    , m_filter(mlt_filter(0))
-    , m_producer(mlt_producer(0))
+    : QObject(nullptr)
+    , m_metadata(nullptr)
+    , m_filter(mlt_filter(nullptr))
+    , m_producer(mlt_producer(nullptr))
     , m_isNew(false)
+    , m_changeCommand(nullptr)
+    , m_changeCommandPushed(false)
 {
     connect(this, SIGNAL(inChanged(int)), this, SIGNAL(durationChanged()));
     connect(this, SIGNAL(outChanged(int)), this, SIGNAL(durationChanged()));
@@ -54,12 +57,18 @@ QmlFilter::QmlFilter(Mlt::Filter& mltFilter, const QmlMetadata* metadata, QObjec
     , m_producer(mlt_producer(m_filter.is_valid()? m_filter.get_data("service") : 0))
     , m_path(m_metadata->path().absolutePath().append('/'))
     , m_isNew(false)
+    , m_changeCommand(nullptr)
+    , m_changeCommandPushed(false)
 {
     connect(this, SIGNAL(changed(QString)), SIGNAL(changed()));
 }
 
 QmlFilter::~QmlFilter()
 {
+    if (m_changeCommand && !m_changeCommandPushed) {
+        delete m_changeCommand;
+        m_changeCommand = nullptr;
+    }
 }
 
 QString QmlFilter::get(QString name, int position)
@@ -132,6 +141,7 @@ void QmlFilter::set(QString name, QString value, int position)
         if (qstrcmp(m_filter.get(qUtf8Printable(name)), qUtf8Printable(value)))  {
             m_filter.set_string(qUtf8Printable(name), qUtf8Printable(value)) ;
             emit changed(name);
+            updateChangeCommand(name);
         }
     } else {
         // Only set an animation keyframe if it does not already exist with the same value.
@@ -140,6 +150,7 @@ void QmlFilter::set(QString name, QString value, int position)
                 || value != m_filter.anim_get(qUtf8Printable(name), position, duration())) {
             m_filter.anim_set(qUtf8Printable(name), qUtf8Printable(value), position, duration());
             emit changed(name);
+            updateChangeCommand(name);
         }
     }
 }
@@ -152,7 +163,7 @@ void QmlFilter::set(QString name, double value, int position, mlt_keyframe_type 
             || m_filter.get_double(qUtf8Printable(name)) != value) {
             double delta = value - m_filter.get_double(qUtf8Printable(name));
             m_filter.set(qUtf8Printable(name), value);
-            emit changed(name);
+            updateChangeCommand(name);
             if (name == "in") {
                 emit inChanged(delta);
             } else if (name == "out") {
@@ -167,6 +178,7 @@ void QmlFilter::set(QString name, double value, int position, mlt_keyframe_type 
             mlt_keyframe_type type = getKeyframeType(animation, position, keyframeType);
             m_filter.anim_set(qUtf8Printable(name), value, position, duration(), type);
             emit changed(name);
+            updateChangeCommand(name);
         }
     }
 }
@@ -180,6 +192,7 @@ void QmlFilter::set(QString name, int value, int position, mlt_keyframe_type key
             int delta = value - m_filter.get_int(qUtf8Printable(name));
             m_filter.set(qUtf8Printable(name), value);
             emit changed(name);
+            updateChangeCommand(name);
             if (name == "in") {
                 emit inChanged(delta);
             } else if (name == "out") {
@@ -194,6 +207,7 @@ void QmlFilter::set(QString name, int value, int position, mlt_keyframe_type key
             mlt_keyframe_type type = getKeyframeType(animation, position, keyframeType);
             m_filter.anim_set(qUtf8Printable(name), value, position, duration(), type);
             emit changed(name);
+            updateChangeCommand(name);
         }
     }
 }
@@ -213,6 +227,7 @@ void QmlFilter::set(QString name, double x, double y, double width, double heigh
             || width != rect.w || height != rect.h || opacity != rect.o) {
             m_filter.set(qUtf8Printable(name), x, y, width, height, opacity);
             emit changed(name);
+            updateChangeCommand(name);
         }
     } else {
         mlt_rect rect = m_filter.anim_get_rect(qUtf8Printable(name), position, duration());
@@ -228,6 +243,7 @@ void QmlFilter::set(QString name, double x, double y, double width, double heigh
             mlt_keyframe_type type = getKeyframeType(animation, position, keyframeType);
             m_filter.anim_set(qUtf8Printable(name), rect, position, duration(), type);
             emit changed(name);
+            updateChangeCommand(name);
         }
     }
 }
@@ -243,6 +259,7 @@ void QmlFilter::setGradient(QString name, const QStringList& gradient)
         }
     }
     emit changed();
+    updateChangeCommand(name);
 }
 
 void QmlFilter::set(QString name, const QRectF& rect, double opacity, int position, mlt_keyframe_type keyframeType)
@@ -408,7 +425,7 @@ int QmlFilter::framesFromTime(const QString &time)
     return 0;
 }
 
-QString QmlFilter::timeFromFrames(int frames, TimeFormat format)
+QString QmlFilter::timeFromFrames(int frames, QmlFilter::TimeFormat format)
 {
     if (MLT.producer()) {
         mlt_time_format mltFormat = mlt_time_smpte_df;
@@ -624,6 +641,17 @@ int QmlFilter::keyframeIndex(Mlt::Animation& animation, int position)
     return result;
 }
 
+void QmlFilter::updateChangeCommand(const QString& name)
+{
+    if (m_changeCommand) {
+        if (!m_changeCommandPushed) {
+            MAIN.undoStack()->push(m_changeCommand);
+            m_changeCommandPushed = true;
+        }
+        m_changeCommand->update(name);
+    }
+}
+
 mlt_keyframe_type QmlFilter::getKeyframeType(Mlt::Animation& animation, int position, mlt_keyframe_type defaultType)
 {
     mlt_keyframe_type result = mlt_keyframe_linear;
@@ -654,12 +682,21 @@ void QmlFilter::deselect()
     MAIN.filterController()->setCurrentFilter(-2);
 }
 
+void QmlFilter::startUndoTracking(FilterController* dock)
+{
+    if (m_changeCommand) {
+        delete m_changeCommand;
+    }
+    m_changeCommand = new Filter::ChangeParameterCommand(m_metadata->name(), m_filter, dock);
+}
+
 AnalyzeDelegate::AnalyzeDelegate(Mlt::Filter& filter)
     : QObject(0)
     , m_uuid(QUuid::createUuid())
     , m_serviceName(filter.get("mlt_service"))
 {
-    filter.set(kShotcutHashProperty, m_uuid.toByteArray().data());
+    auto ba = m_uuid.toByteArray();
+    filter.set(kShotcutHashProperty, ba.data());
 }
 
 class FindFilterParser : public Mlt::Parser

src/qmltypes/qmlfilter.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,12 +32,17 @@
 
 class AbstractJob;
 class EncodeJob;
+class QUndoCommand;
+class FilterController;
+namespace Filter {
+    class ChangeParameterCommand;
+}
 
 class QmlFilter : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(bool isNew READ isNew)
-    Q_PROPERTY(QString path READ path)
+    Q_PROPERTY(bool isNew READ isNew CONSTANT)
+    Q_PROPERTY(QString path READ path CONSTANT)
     Q_PROPERTY(QStringList presets READ presets NOTIFY presetsChanged)
     Q_PROPERTY(int in READ in WRITE setIn NOTIFY inChanged)
     Q_PROPERTY(int out READ out WRITE setOut NOTIFY outChanged)
@@ -54,7 +59,7 @@ public:
         TIME_TIMECODE_DF,
         TIME_TIMECODE_NDF,
     };
-    Q_ENUMS(TimeFormat)
+    Q_ENUM(TimeFormat)
 
     explicit QmlFilter();
     explicit QmlFilter(Mlt::Filter& mltFilter, const QmlMetadata* metadata, QObject *parent = nullptr);
@@ -87,7 +92,7 @@ public:
     Q_INVOKABLE void deletePreset(const QString& name);
     Q_INVOKABLE void analyze(bool isAudio = false);
     Q_INVOKABLE static int framesFromTime(const QString& time);
-    Q_INVOKABLE static QString timeFromFrames(int frames, TimeFormat format = TIME_TIMECODE_DF);
+    Q_INVOKABLE static QString timeFromFrames(int frames, QmlFilter::TimeFormat format = TIME_TIMECODE_DF);
     Q_INVOKABLE void getHash();
     Mlt::Producer& producer() { return m_producer; }
     int in();
@@ -107,6 +112,7 @@ public:
     mlt_keyframe_type getKeyframeType(Mlt::Animation& animation, int position, mlt_keyframe_type defaultType);
     Q_INVOKABLE bool isAtLeastVersion(const QString& version);
     Q_INVOKABLE static void deselect();
+    void startUndoTracking(FilterController* dock);
 
 public slots:
     void preset(const QString& name);
@@ -129,9 +135,14 @@ private:
     QString m_path;
     bool m_isNew;
     QStringList m_presets;
+    Filter::ChangeParameterCommand* m_changeCommand;
+    bool m_changeCommandPushed;
     
     QString objectNameOrService();
     int keyframeIndex(Mlt::Animation& animation, int position);
+
+private slots:
+    void updateChangeCommand(const QString& name);
 };
 
 class AnalyzeDelegate : public QObject

src/commands/filtercommands.cpp
@@ -17,6 +17,7 @@
 
 #include "filtercommands.h"
 #include "qmltypes/qmlmetadata.h"
+#include "controllers/filtercontroller.h"
 #include <Logger.h>
 
 namespace Filter {
@@ -118,6 +119,43 @@ void DisableCommand::undo()
     Q_ASSERT(m_producer.is_valid());
     LOG_DEBUG() << text() << m_index;
     m_model.toggleDisable(m_producer, m_index);
+}
+
+ChangeParameterCommand::ChangeParameterCommand(const QString& filterName, Mlt::Filter& filter, FilterController* controller, QUndoCommand* parent)
+    : QUndoCommand(parent)
+    , m_filterName(filterName)
+    , m_filter(filter)
+    , m_filterController(controller)
+    , m_firstRedo(true)
+{
+    setText(QObject::tr("Change %1 filter").arg(m_filterName));
+    m_before.inherit(m_filter);
+    m_after.inherit(m_filter);
+}
+
+void ChangeParameterCommand::update(const QString& parameter)
+{
+    m_after.pass_property(m_filter, parameter.toUtf8().constData());
+}
+
+void ChangeParameterCommand::redo()
+{
+    LOG_DEBUG() << m_filterName;
+    if (m_firstRedo) {
+        m_firstRedo = false;
+    } else {
+        m_filter.inherit(m_after);
+        if (m_filterController) {
+            m_filterController->onUndoOrRedo(m_filter);
+        }
+    }
+}
+
+void ChangeParameterCommand::undo()
+{
+    LOG_DEBUG() << m_filterName;
+    m_filter.inherit(m_before);
+    m_filterController->onUndoOrRedo(m_filter);
 }
 
 } // namespace Filter

src/commands/filtercommands.h
@@ -19,10 +19,13 @@
 #define FILTERCOMMANDS_H
 
 #include "models/attachedfiltersmodel.h"
+#include "qmltypes/qmlfilter.h"
+#include <MltFilter.h>
 #include <QUndoCommand>
 #include <QString>
 
 class QmlMetadata;
+class FilterController;
 
 namespace Filter
 {
@@ -77,6 +80,23 @@ private:
     AttachedFiltersModel& m_model;
     int m_index;
     Mlt::Producer m_producer;
+};
+
+class ChangeParameterCommand : public QUndoCommand
+{
+public:
+    ChangeParameterCommand(const QString& filterName, Mlt::Filter& filter, FilterController* controller, QUndoCommand * parent = 0);
+    void update(const QString& parameter);
+    void redo();
+    void undo();
+
+private:
+    QString m_filterName;
+    Mlt::Filter m_filter;
+    Mlt::Properties m_before;
+    Mlt::Properties m_after;
+    FilterController* m_filterController;
+    bool m_firstRedo;
 };
 
 } // namespace Filter

src/controllers/filtercontroller.cpp
@@ -111,6 +111,14 @@ QmlMetadata *FilterController::metadataForService(Mlt::Service *service)
     return meta;
 }
 
+void FilterController::onUndoOrRedo(Mlt::Filter& filter)
+{
+    if (m_mltFilter && filter.get_service() == m_mltFilter->get_service()) {
+        MLT.refreshConsumer();
+        emit undoOrRedo();
+    }
+}
+
 void FilterController::timerEvent(QTimerEvent* event)
 {
     loadFilterMetadata();
@@ -166,6 +174,9 @@ void FilterController::setCurrentFilter(int attachedIndex, bool isNew)
 
     emit currentFilterChanged(filter, meta, m_currentFilterIndex);
     m_currentFilter.reset(filter);
+    if (filter) {
+        filter->startUndoTracking(this);
+    }
 }
 
 void FilterController::onFadeInChanged()

src/controllers/filtercontroller.h
@@ -40,6 +40,7 @@ public:
 
     QmlMetadata* metadataForService(Mlt::Service *service);
     QmlFilter* currentFilter() const { return m_currentFilter.data(); }
+    void onUndoOrRedo(Mlt::Filter& filter);
 
 protected:
     void timerEvent(QTimerEvent*);
@@ -48,6 +49,7 @@ signals:
     void currentFilterChanged(QmlFilter* filter, QmlMetadata* meta, int index);
     void statusChanged(QString);
     void filterChanged(Mlt::Filter*);
+    void undoOrRedo();
 
 public slots:
     void setProducer(Mlt::Producer *producer = 0);

src/docks/filtersdock.h
@@ -52,13 +52,11 @@ public slots:
     void onSeeked(int position);
     void onShowFrame(const SharedFrame& frame);
     void openFilterMenu() const;
+    void resetQview();
 
 protected:
     bool event(QEvent *event);
     void keyPressEvent(QKeyEvent* event);
-
-private slots:
-    void resetQview();
 
 private:
     QQuickWidget m_qview;

src/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2020 Meltytech, LLC
+ * Copyright (c) 2011-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -369,6 +369,7 @@ MainWindow::MainWindow()
     connect(m_filtersDock->toggleViewAction(), SIGNAL(triggered(bool)), this, SLOT(onFiltersDockTriggered(bool)));
     connect(ui->actionFilters, SIGNAL(triggered()), this, SLOT(onFiltersDockTriggered()));
     connect(m_filterController, SIGNAL(currentFilterChanged(QmlFilter*, QmlMetadata*, int)), m_filtersDock, SLOT(setCurrentFilter(QmlFilter*, QmlMetadata*, int)));
+    connect(m_filterController, &FilterController::undoOrRedo, m_filtersDock, &FiltersDock::resetQview);
     connect(this, SIGNAL(producerOpened()), m_filterController, SLOT(setProducer()));
     connect(m_filterController->attachedModel(), SIGNAL(changed()), SLOT(onFilterModelChanged()));
     connect(m_filtersDock, SIGNAL(changed()), SLOT(onFilterModelChanged()));

commit 9002c365b6f4674c07a12113c27f950e4658666f

fix MLT refresh regression on change filter double param
regression in 031d3a9225b8bead669958c6338406ba6d0eb42f

src/qmltypes/qmlfilter.cpp
@@ -163,6 +163,7 @@ void QmlFilter::set(QString name, double value, int position, mlt_keyframe_type 
             || m_filter.get_double(qUtf8Printable(name)) != value) {
             double delta = value - m_filter.get_double(qUtf8Printable(name));
             m_filter.set(qUtf8Printable(name), value);
+            emit changed(name);
             updateChangeCommand(name);
             if (name == "in") {
                 emit inChanged(delta);
@@ -684,12 +685,12 @@ void QmlFilter::deselect()
     MAIN.filterController()->setCurrentFilter(-2);
 }
 
-void QmlFilter::startUndoTracking(FilterController* dock)
+void QmlFilter::startUndoTracking(FilterController* controller)
 {
     if (m_changeCommand) {
         delete m_changeCommand;
     }
-    m_changeCommand = new Filter::ChangeParameterCommand(m_metadata->name(), m_filter, dock);
+    m_changeCommand = new Filter::ChangeParameterCommand(m_metadata->name(), m_filter, controller);
 }
 
 AnalyzeDelegate::AnalyzeDelegate(Mlt::Filter& filter)

src/qmltypes/qmlfilter.h
@@ -112,7 +112,7 @@ public:
     mlt_keyframe_type getKeyframeType(Mlt::Animation& animation, int position, mlt_keyframe_type defaultType);
     Q_INVOKABLE bool isAtLeastVersion(const QString& version);
     Q_INVOKABLE static void deselect();
-    void startUndoTracking(FilterController* dock);
+    void startUndoTracking(FilterController* controller);
 
 public slots:
     void preset(const QString& name);