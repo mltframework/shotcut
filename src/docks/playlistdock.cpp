mltframework/shotcut

commit c6d87ae1b10b2a4e0fc3bc3b78d44244e06673a2

playlist copy & file open not clearing playlist current
fixes #1010

src/docks/playlistdock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2020 Meltytech, LLC
+ * Copyright (c) 2012-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -515,6 +515,15 @@ void PlaylistDock::onProducerOpened()
 {
     if (!MLT.isMultitrack())
         ui->addButton->setEnabled(true);
+    if (MLT.producer() && MLT.producer()->is_valid()) {
+        auto row = MLT.producer()->get_int(kPlaylistIndexProperty) - 1;
+        if (row < 0) {
+            resetPlaylistIndex();
+            emit m_model.dataChanged(m_model.createIndex(0, PlaylistModel::COLUMN_THUMBNAIL),
+                m_model.createIndex(m_model.playlist()->count() - 1, PlaylistModel::COLUMN_THUMBNAIL),
+                QVector<int>() << PlaylistModel::COLUMN_THUMBNAIL);
+        }
+    }
 }
 
 void PlaylistDock::onInChanged()
@@ -960,6 +969,7 @@ void PlaylistDock::resetPlaylistIndex()
         Mlt::Producer clip(m_model.playlist()->get_clip(j));
         clip.parent().Mlt::Properties::clear(kPlaylistIndexProperty);
     }
+    setUpdateButtonEnabled(false);
 }
 
 void PlaylistDock::emitDataChanged(const QVector<int> &roles)
@@ -1061,12 +1071,12 @@ void PlaylistDock::on_actionCopy_triggered()
     if (!index.isValid() || !m_model.playlist()) return;
     Mlt::ClipInfo* i = m_model.playlist()->clip_info(index.row());
     if (i) {
+        resetPlaylistIndex();
         QString xml = MLT.XML(i->producer);
         Mlt::Producer* p = new Mlt::Producer(MLT.profile(), "xml-string", xml.toUtf8().constData());
         p->set_in_and_out(i->frame_in, i->frame_out);
         emit clipOpened(p);
         delete i;
-        resetPlaylistIndex();
     }
 }
 

src/models/playlistmodel.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2020 Meltytech, LLC
+ * Copyright (c) 2012-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -342,7 +342,7 @@ QVariant PlaylistModel::data(const QModelIndex &index, int role) const
                 }
                 if (parent.is_valid() && parent.get_int(kPlaylistIndexProperty) == index.row() + 1) {
                     QPen pen(Qt::red);
-                    pen.setWidthF(MAIN.devicePixelRatioF());
+                    pen.setWidthF(1.5 * MAIN.devicePixelRatioF());
                     painter.setPen(pen);
                     rect.setX(0);
                     rect.setY(0);

commit 34f79169b5e46d03c637524c90c496d462b2bc87

fix crash regression in c6d87ae1 when playlist was never used

src/docks/playlistdock.cpp
@@ -517,7 +517,7 @@ void PlaylistDock::onProducerOpened()
         ui->addButton->setEnabled(true);
     if (MLT.producer() && MLT.producer()->is_valid()) {
         auto row = MLT.producer()->get_int(kPlaylistIndexProperty) - 1;
-        if (row < 0) {
+        if (row < 0 && m_model.rowCount() > 0) {
             resetPlaylistIndex();
             emit m_model.dataChanged(m_model.createIndex(0, PlaylistModel::COLUMN_THUMBNAIL),
                 m_model.createIndex(m_model.playlist()->count() - 1, PlaylistModel::COLUMN_THUMBNAIL),