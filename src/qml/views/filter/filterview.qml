mltframework/shotcut

commit 1b668b05db2a4d6678611d2bb7448bec374e2a98

refactor to enum QmlFilter::CurrentFilterIndex

src/qml/views/filter/filterview.qml
@@ -22,7 +22,7 @@ import Shotcut.Controls 1.0 as Shotcut
 
 Rectangle {
     id: root
-    property int selectedIndex: -1
+    property int selectedIndex: filter.NoCurrentFilter
     signal currentFilterRequested(int attachedIndex)
     
     function clearCurrentFilter() {
@@ -142,7 +142,7 @@ Rectangle {
             implicitWidth: height
             icon.name: 'list-remove'
             icon.source: 'qrc:///icons/oxygen/32x32/actions/list-remove.png'
-            enabled: selectedIndex > -1
+            enabled: selectedIndex > filter.NoCurrentFilter
             opacity: enabled ? 1.0 : 0.5
             Shotcut.HoverTip { text: qsTr('Remove selected filter') }
             onClicked: {
@@ -159,7 +159,7 @@ Rectangle {
             implicitWidth: height
             icon.name: 'edit-copy'
             icon.source: 'qrc:///icons/oxygen/32x32/actions/edit-copy.png'
-            enabled: selectedIndex > -1
+            enabled: selectedIndex > filter.NoCurrentFilter
             opacity: enabled ? 1.0 : 0.5
             Shotcut.HoverTip { text: qsTr('Copy the filters') }
             onClicked: application.copyFilters()
@@ -192,7 +192,7 @@ Rectangle {
         Shotcut.Button {
             id: moveDownButton
             implicitWidth: height
-            enabled: selectedIndex > -1 && selectedIndex + 1 < attachedfiltersmodel.rowCount()
+            enabled: selectedIndex > filter.NoCurrentFilter && selectedIndex + 1 < attachedfiltersmodel.rowCount()
             opacity: enabled ? 1.0 : 0.5
             icon.name: 'overwrite'
             icon.source: 'qrc:///icons/oxygen/32x32/actions/overwrite.png'
@@ -209,13 +209,13 @@ Rectangle {
             implicitWidth: height
             icon.name: 'window-close'
             icon.source: 'qrc:///icons/oxygen/32x32/actions/window-close.png'
-            enabled: selectedIndex > -1
+            enabled: selectedIndex > filter.NoCurrentFilter
             opacity: enabled ? 1.0 : 0.5
             Shotcut.HoverTip { text: qsTr('Deselect the filter') }
             onClicked: {
                 clearCurrentFilter()
-                attachedFilters.setCurrentFilter(-2)
-                selectedIndex = -1
+                attachedFilters.setCurrentFilter(filter.DeselectCurrentFilter)
+                selectedIndex = filter.NoCurrentFilter
                 filter.deselect()
             }
         }

src/controllers/filtercontroller.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2020 Meltytech, LLC
+ * Copyright (c) 2014-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,7 +32,7 @@ FilterController::FilterController(QObject* parent) : QObject(parent),
  m_mltFilter(0),
  m_metadataModel(this),
  m_attachedModel(this),
- m_currentFilterIndex(-1)
+ m_currentFilterIndex(QmlFilter::NoCurrentFilter)
 {
     startTimer(0);
     connect(&m_attachedModel, SIGNAL(changed()), this, SLOT(handleAttachedModelChange()));
@@ -156,7 +156,7 @@ void FilterController::setCurrentFilter(int attachedIndex, bool isNew)
     QmlMetadata* meta = m_attachedModel.getMetadata(m_currentFilterIndex);
     QmlFilter* filter = 0;
     if (meta) {
-        emit currentFilterChanged(nullptr, nullptr, -1);
+        emit currentFilterChanged(nullptr, nullptr, QmlFilter::NoCurrentFilter);
         m_mltFilter = m_attachedModel.getFilter(m_currentFilterIndex);
         filter = new QmlFilter(*m_mltFilter, meta);
         filter->setIsNew(isNew);
@@ -207,18 +207,18 @@ void FilterController::handleAttachedModelChange()
 
 void FilterController::handleAttachedModelAboutToReset()
 {
-    setCurrentFilter(-1);
+    setCurrentFilter(QmlFilter::NoCurrentFilter);
 }
 
 void FilterController::handleAttachedRowsRemoved(const QModelIndex&, int first, int)
 {
-    m_currentFilterIndex = -2; // Force update
+    m_currentFilterIndex = QmlFilter::DeselectCurrentFilter; // Force update
     setCurrentFilter(qBound(0, first, m_attachedModel.rowCount() - 1));
 }
 
 void FilterController::handleAttachedRowsInserted(const QModelIndex&, int first, int)
 {
-    m_currentFilterIndex = -2; // Force update
+    m_currentFilterIndex = QmlFilter::DeselectCurrentFilter; // Force update
     setCurrentFilter(qBound(0, first, m_attachedModel.rowCount() - 1), true);
 }
 
@@ -244,7 +244,7 @@ void FilterController::onQmlFilterChanged(const QString &name)
 
 void FilterController::removeCurrent()
 {
-    if (m_currentFilterIndex > -1)
+    if (m_currentFilterIndex > QmlFilter::NoCurrentFilter)
         m_attachedModel.remove(m_currentFilterIndex);
 }
 

src/docks/filtersdock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -59,7 +59,7 @@ FiltersDock::FiltersDock(MetadataModel* metadataModel, AttachedFiltersModel* att
     connect(&m_producer, SIGNAL(seeked(int)), SIGNAL(seeked(int)));
     connect(this, SIGNAL(producerInChanged(int)), &m_producer, SIGNAL(inChanged(int)));
     connect(this, SIGNAL(producerOutChanged(int)), &m_producer, SIGNAL(outChanged(int)));
-    setCurrentFilter(0, 0, -1);
+    setCurrentFilter(0, 0, QmlFilter::NoCurrentFilter);
     connect(m_qview.quickWindow(), SIGNAL(sceneGraphInitialized()), SLOT(resetQview()));
 
     LOG_DEBUG() << "end";
@@ -163,5 +163,5 @@ void FiltersDock::resetQview()
 
     QObject::connect(m_qview.rootObject(), SIGNAL(currentFilterRequested(int)),
         SIGNAL(currentFilterRequested(int)));
-    emit currentFilterRequested(-1);
+    emit currentFilterRequested(QmlFilter::NoCurrentFilter);
 }

src/qmltypes/qmlfilter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -651,7 +651,7 @@ bool QmlFilter::isAtLeastVersion(const QString& version)
 
 void QmlFilter::deselect()
 {
-    MAIN.filterController()->setCurrentFilter(-2);
+    MAIN.filterController()->setCurrentFilter(DeselectCurrentFilter);
 }
 
 AnalyzeDelegate::AnalyzeDelegate(Mlt::Filter& filter)

src/qmltypes/qmlfilter.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -54,7 +54,13 @@ public:
         TIME_TIMECODE_DF,
         TIME_TIMECODE_NDF,
     };
-    Q_ENUMS(TimeFormat)
+    Q_ENUM(TimeFormat)
+
+    enum CurrentFilterIndex {
+        NoCurrentFilter = -1,
+        DeselectCurrentFilter = -2
+    };
+    Q_ENUM(CurrentFilterIndex)
 
     explicit QmlFilter();
     explicit QmlFilter(Mlt::Filter& mltFilter, const QmlMetadata* metadata, QObject *parent = nullptr);

commit 8c7293660a33c6adea1b51b0b0d10b432763f5e4

fix removing filter regression in 1b668b05

src/qml/views/filter/filterview.qml
@@ -19,10 +19,11 @@ import QtQuick 2.1
 import QtQuick.Controls 2.12
 import QtQuick.Layouts 1.0
 import Shotcut.Controls 1.0 as Shotcut
+import org.shotcut.qml 1.0 as Shotcut
 
 Rectangle {
     id: root
-    property int selectedIndex: filter.NoCurrentFilter
+    property int selectedIndex: Shotcut.Filter.NoCurrentFilter
     signal currentFilterRequested(int attachedIndex)
     
     function clearCurrentFilter() {
@@ -142,7 +143,7 @@ Rectangle {
             implicitWidth: height
             icon.name: 'list-remove'
             icon.source: 'qrc:///icons/oxygen/32x32/actions/list-remove.png'
-            enabled: selectedIndex > filter.NoCurrentFilter
+            enabled: selectedIndex > Shotcut.Filter.NoCurrentFilter
             opacity: enabled ? 1.0 : 0.5
             Shotcut.HoverTip { text: qsTr('Remove selected filter') }
             onClicked: {
@@ -159,7 +160,7 @@ Rectangle {
             implicitWidth: height
             icon.name: 'edit-copy'
             icon.source: 'qrc:///icons/oxygen/32x32/actions/edit-copy.png'
-            enabled: selectedIndex > filter.NoCurrentFilter
+            enabled: selectedIndex > Shotcut.Filter.NoCurrentFilter
             opacity: enabled ? 1.0 : 0.5
             Shotcut.HoverTip { text: qsTr('Copy the filters') }
             onClicked: application.copyFilters()
@@ -192,7 +193,7 @@ Rectangle {
         Shotcut.Button {
             id: moveDownButton
             implicitWidth: height
-            enabled: selectedIndex > filter.NoCurrentFilter && selectedIndex + 1 < attachedfiltersmodel.rowCount()
+            enabled: selectedIndex > Shotcut.Filter.NoCurrentFilter && selectedIndex + 1 < attachedfiltersmodel.rowCount()
             opacity: enabled ? 1.0 : 0.5
             icon.name: 'overwrite'
             icon.source: 'qrc:///icons/oxygen/32x32/actions/overwrite.png'
@@ -209,13 +210,13 @@ Rectangle {
             implicitWidth: height
             icon.name: 'window-close'
             icon.source: 'qrc:///icons/oxygen/32x32/actions/window-close.png'
-            enabled: selectedIndex > filter.NoCurrentFilter
+            enabled: selectedIndex > Shotcut.Filter.NoCurrentFilter
             opacity: enabled ? 1.0 : 0.5
             Shotcut.HoverTip { text: qsTr('Deselect the filter') }
             onClicked: {
                 clearCurrentFilter()
-                attachedFilters.setCurrentFilter(filter.DeselectCurrentFilter)
-                selectedIndex = filter.NoCurrentFilter
+                attachedFilters.setCurrentFilter(Shotcut.Filter.DeselectCurrentFilter)
+                selectedIndex = Shotcut.Filter.NoCurrentFilter
                 filter.deselect()
             }
         }