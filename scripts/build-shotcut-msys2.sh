mltframework/shotcut

commit e7aeed2ae0e0c5d28272d58817b9a5c9fb5662e6

fix libaom_av1 encoding broken on Windows

scripts/build-shotcut-msys2.sh
@@ -50,7 +50,7 @@ ZIMG_REVISION=
 DAV1D_HEAD=1
 DAV1D_REVISION=
 AOM_HEAD=1
-AOM_REVISION=1
+AOM_REVISION=
 
 
 # QT_INCLUDE_DIR="$(pkg-config --variable=prefix QtCore)/include"
@@ -1020,6 +1020,7 @@ function deploy
 
   log Reorganizing installed files
   cmd mv bin/*.dll .
+  cmd mv lib/*.dll .
   if [ "$SDK" = "1" ]; then
     cmd mv bin/*.exe .
   else

.github/workflows/build-sdk-windows.yml
@@ -50,6 +50,7 @@ jobs:
       - name: Build
         shell: msys2 {0}
         run: |
+          pacman --noconfirm -Rdd mingw-w64-x86_64-aom
           echo Preparing version
           echo $(date +"%y.%m.%d") > VERSION
           VERSION=$(cat VERSION)

.github/workflows/build-windows.yml
@@ -55,6 +55,7 @@ jobs:
       - name: Build
         shell: msys2 {0}
         run: |
+          pacman --noconfirm -Rdd mingw-w64-x86_64-aom
           echo Preparing version
           echo $(date +"%y.%m.%d") > VERSION
           VERSION=$(cat VERSION)

commit afca382806a56f1e156b9db8db24df824e22efba

fix non-versioned MLT DLLs packaged and misplaced in SDK
regression in e7aeed2

scripts/build-shotcut-msys2.sh
@@ -1020,7 +1020,7 @@ function deploy
 
   log Reorganizing installed files
   cmd mv bin/*.dll .
-  cmd mv lib/*.dll .
+  cmd mv lib/libaom.dll .
   if [ "$SDK" = "1" ]; then
     cmd mv bin/*.exe .
   else