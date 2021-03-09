mltframework/shotcut

commit 35a7226d6c3f21dd68fefd85a1d4fd72cd8c4a04

fix timeline append changes current track

src/docks/timelinedock.cpp
@@ -73,8 +73,9 @@ TimelineDock::TimelineDock(QWidget *parent) :
 #endif
 
     connect(&m_model, SIGNAL(modified()), this, SLOT(clearSelectionIfInvalid()));
-    connect(&m_model, &MultitrackModel::inserted, this, &TimelineDock::onInserted, Qt::QueuedConnection);
-    connect(&m_model, &MultitrackModel::overWritten, this, &TimelineDock::onOverWritten, Qt::QueuedConnection);
+    connect(&m_model, &MultitrackModel::appended, this, &TimelineDock::selectClip, Qt::QueuedConnection);
+    connect(&m_model, &MultitrackModel::inserted, this, &TimelineDock::selectClip, Qt::QueuedConnection);
+    connect(&m_model, &MultitrackModel::overWritten, this, &TimelineDock::selectClip, Qt::QueuedConnection);
     connect(&m_model, SIGNAL(rowsInserted(QModelIndex,int,int)), SLOT(onRowsInserted(QModelIndex,int,int)));
     connect(&m_model, SIGNAL(rowsRemoved(QModelIndex,int,int)), SLOT(onRowsRemoved(QModelIndex,int,int)));
 
@@ -556,7 +557,6 @@ void TimelineDock::append(int trackIndex)
         ProxyManager::generateIfNotExists(producer);
         MAIN.undoStack()->push(
             new Timeline::AppendCommand(m_model, trackIndex, MLT.XML(&producer)));
-        selectClipUnderPlayhead();
     } else if (!MLT.isSeekableClip()) {
         emitNonSeekableWarning();
     }
@@ -1189,9 +1189,8 @@ void TimelineDock::insert(int trackIndex, int position, const QString &xml, bool
     }
 }
 
-void TimelineDock::onInserted(int trackIndex, int clipIndex)
+void TimelineDock::selectClip(int trackIndex, int clipIndex)
 {
-    Q_UNUSED(trackIndex)
     setSelection(QList<QPoint>() << QPoint(clipIndex, trackIndex));
 }
 
@@ -1238,12 +1237,6 @@ void TimelineDock::overwrite(int trackIndex, int position, const QString &xml, b
     }
 }
 
-void TimelineDock::onOverWritten(int trackIndex, int clipIndex)
-{
-    Q_UNUSED(trackIndex)
-    setSelection(QList<QPoint>() << QPoint(clipIndex, trackIndex));
-}
-
 void TimelineDock::appendFromPlaylist(Mlt::Playlist *playlist, bool skipProxy)
 {
     int trackIndex = currentTrack();
@@ -1261,7 +1254,6 @@ void TimelineDock::appendFromPlaylist(Mlt::Playlist *playlist, bool skipProxy)
     }
     MAIN.undoStack()->push(
         new Timeline::AppendCommand(m_model, trackIndex, MLT.XML(playlist), skipProxy));
-    selectClipUnderPlayhead();
 }
 
 void TimelineDock::splitClip(int trackIndex, int clipIndex)

src/docks/timelinedock.h
@@ -191,8 +191,7 @@ private slots:
     void load(bool force = false);
     void onTopLevelChanged(bool floating);
     void onTransitionAdded(int trackIndex, int clipIndex, int position, bool ripple);
-    void onInserted(int trackIndex, int clipIndex);
-    void onOverWritten(int trackIndex, int clipIndex);
+    void selectClip(int trackIndex, int clipIndex);
 };
 
 class TimelineSelectionBlocker

src/models/multitrackmodel.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -1105,6 +1105,7 @@ int MultitrackModel::appendClip(int trackIndex, Mlt::Producer &clip)
         endInsertRows();
         QModelIndex index = createIndex(i, 0, trackIndex);
         AudioLevelsTask::start(clip.parent(), this, index);
+        emit appended(trackIndex, i);
         emit modified();
         emit seeked(playlist.clip_start(i) + playlist.clip_length(i));
         return i;

src/models/multitrackmodel.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -125,6 +125,7 @@ signals:
     void filterInChanged(int delta, Mlt::Filter*);
     void filterOutChanged(int delta, Mlt::Filter*);
     void reloadRequested();
+    void appended(int trackIndex, int clipIndex);
     void inserted(int trackIndex, int clipIndex);
     void overWritten(int trackIndex, int clipIndex);
 

commit 750841f4a77cf59104c84b4ae312c59e77db5cd5

fix black frames in slideshow regression in 35a7226
https://forum.shotcut.org/t/black-frames-in-slideshow/25860

src/docks/timelinedock.cpp
@@ -1259,8 +1259,10 @@ void TimelineDock::appendFromPlaylist(Mlt::Playlist *playlist, bool skipProxy)
         if (clipIndex >= 0)
             m_model.removeClip(trackIndex, clipIndex, Settings.timelineRippleAllTracks());
     }
+    disconnect(&m_model, &MultitrackModel::appended, this, &TimelineDock::selectClip);
     MAIN.undoStack()->push(
         new Timeline::AppendCommand(m_model, trackIndex, MLT.XML(playlist), skipProxy));
+    connect(&m_model, &MultitrackModel::appended, this, &TimelineDock::selectClip, Qt::QueuedConnection);
 }
 
 void TimelineDock::splitClip(int trackIndex, int clipIndex)