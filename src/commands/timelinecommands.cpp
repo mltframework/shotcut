mltframework/shotcut

commit 4d9a3ec4ad330cf660abc172f652307cc2a69005

fix #989 undo trim clip in may shift clips
This occurs when the trim action closes any gap/blank before it
thereby changing the index of the clip being trimmed.

src/commands/timelinecommands.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 Meltytech, LLC
+ * Copyright (c) 2013-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -481,7 +481,11 @@ void TrimClipInCommand::redo()
     if (m_redo) {
         LOG_DEBUG() << "trackIndex" << m_trackIndex << "clipIndex" << m_clipIndex << "delta" << m_delta;
         m_undoHelper.reset(new UndoHelper(m_model));
-        if (!m_ripple) m_undoHelper->setHints(UndoHelper::SkipXML);
+        if (m_ripple) {
+            m_undoHelper->setHints(UndoHelper::SkipXML);
+        } else {
+            m_undoHelper->setHints(UndoHelper::RestoreTracks);
+        }
         m_undoHelper->recordBeforeState();
         m_model.trimClipIn(m_trackIndex, m_clipIndex, m_delta, m_ripple, m_rippleAllTracks);
         m_undoHelper->recordAfterState();

src/docks/timelinedock.cpp
@@ -1026,7 +1026,11 @@ bool TimelineDock::trimClipIn(int trackIndex, int clipIndex, int oldClipIndex, i
     else if (m_model.trimClipInValid(trackIndex, clipIndex, delta, ripple)) {
         if (!m_undoHelper) {
             m_undoHelper.reset(new UndoHelper(m_model));
-            if (ripple) m_undoHelper->setHints(UndoHelper::SkipXML);
+            if (ripple) {
+                m_undoHelper->setHints(UndoHelper::SkipXML);
+            } else {
+                m_undoHelper->setHints(UndoHelper::RestoreTracks);
+            }
             m_undoHelper->recordBeforeState();
         }
         clipIndex = m_model.trimClipIn(trackIndex, clipIndex, delta, ripple, Settings.timelineRippleAllTracks());

commit 7fa329ebd18849f998221935ee5a0a15a4098e56

fix undo after ripple trim in shortcut
regression in 4d9a3ec

https://forum.shotcut.org/t/deleting-transition-with-ripple-all-delete-
breaks-undo/25672

src/commands/timelinecommands.cpp
@@ -481,11 +481,8 @@ void TrimClipInCommand::redo()
     if (m_redo) {
         LOG_DEBUG() << "trackIndex" << m_trackIndex << "clipIndex" << m_clipIndex << "delta" << m_delta;
         m_undoHelper.reset(new UndoHelper(m_model));
-        if (m_ripple) {
+        if (!m_ripple)
             m_undoHelper->setHints(UndoHelper::SkipXML);
-        } else {
-            m_undoHelper->setHints(UndoHelper::RestoreTracks);
-        }
         m_undoHelper->recordBeforeState();
         m_model.trimClipIn(m_trackIndex, m_clipIndex, m_delta, m_ripple, m_rippleAllTracks);
         m_undoHelper->recordAfterState();