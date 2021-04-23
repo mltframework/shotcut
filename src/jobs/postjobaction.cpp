mltframework/shotcut

commit 00d36cc9db48385eeddeb061c7fcf7f48f026ed0

Only put avformat producers in a chain.
Producers are created in many different places. Create a common
function to call from all locations to set common properties and
to decide when to encapsulate in a chain.

src/jobs/postjobaction.cpp
@@ -63,33 +63,22 @@ void ReplaceOnePostJobAction::doAction()
     if (!m_fileNameToRemove.isEmpty()) {
         QFile::remove(m_fileNameToRemove);
     }
-    Mlt::Producer producer(MLT.profile(), m_dstFile.toUtf8().constData());
-    if (producer.is_valid()) {
-        if (!qstrcmp(producer.get("mlt_service"), "avformat")) {
-            producer.set("mlt_service", "avformat-novalidate");
-            producer.set("mute_on_pause", 0);
-        }
-        MLT.lockCreationTime(&producer);
-        producer.set_in_and_out(m_in, -1);
-        Mlt::Chain chain(MLT.profile());
-        chain.set_source(producer);
-        MAIN.replaceInTimeline(m_uuid, chain);
+    Mlt::Producer newProducer(MLT.profile(), m_dstFile.toUtf8().constData());
+    if (newProducer.is_valid()) {
+        Mlt::Producer* producer = MLT.setupNewProducer(&newProducer);
+        MAIN.replaceInTimeline(m_uuid, *producer);
+        delete producer;
     }
 }
 
 void ReplaceAllPostJobAction::doAction()
 {
     FilePropertiesPostJobAction::doAction();
-    Mlt::Producer producer(MLT.profile(), m_dstFile.toUtf8().constData());
-    if (producer.is_valid()) {
-        if (!qstrcmp(producer.get("mlt_service"), "avformat")) {
-            producer.set("mlt_service", "avformat-novalidate");
-            producer.set("mute_on_pause", 0);
-        }
-        MLT.lockCreationTime(&producer);
-        Mlt::Chain chain(MLT.profile());
-        chain.set_source(producer);
-        MAIN.replaceAllByHash(m_hash, chain);
+    Mlt::Producer newProducer(MLT.profile(), m_dstFile.toUtf8().constData());
+    if (newProducer.is_valid()) {
+        Mlt::Producer* producer = MLT.setupNewProducer(&newProducer);
+        MAIN.replaceAllByHash(m_hash, *producer);
+        delete producer;
     }
 }
 
@@ -98,17 +87,13 @@ void ProxyReplacePostJobAction::doAction()
     QFileInfo info(m_dstFile);
     QString newFileName = info.path() + "/" + info.baseName() + "." + info.suffix();
     if (QFile::rename(m_dstFile, newFileName)) {
-        Mlt::Producer producer(MLT.profile(), newFileName.toUtf8().constData());
-        if (producer.is_valid()) {
-            producer.set(kIsProxyProperty, 1);
-            producer.set(kOriginalResourceProperty, m_srcFile.toUtf8().constData());
-            if (!qstrcmp(producer.get("mlt_service"), "avformat")) {
-                producer.set("mlt_service", "avformat-novalidate");
-                producer.set("mute_on_pause", 0);
-            }
-            Mlt::Chain chain(MLT.profile());
-            chain.set_source(producer);
-            MAIN.replaceAllByHash(m_hash, chain, true);
+        Mlt::Producer newProducer(MLT.profile(), newFileName.toUtf8().constData());
+        if (newProducer.is_valid()) {
+            Mlt::Producer* producer = MLT.setupNewProducer(&newProducer);
+            producer->set(kIsProxyProperty, 1);
+            producer->set(kOriginalResourceProperty, m_srcFile.toUtf8().constData());
+            MAIN.replaceAllByHash(m_hash, *producer, true);
+            delete producer;
         } else {
             LOG_WARNING() << "proxy file is invalid" << newFileName;
             QFile::remove(m_dstFile);

src/docks/playlistdock.cpp
@@ -692,19 +692,7 @@ void PlaylistDock::onDropped(const QMimeData *data, int row)
                         }
                     }
                 }
-                // Convert avformat to avformat-novalidate so that XML loads faster.
-                if (!qstrcmp(producer->get("mlt_service"), "avformat")) {
-                    producer->set("mlt_service", "avformat-novalidate");
-                    producer->set("mute_on_pause", 0);
-                }
-                MLT.setImageDurationFromDefault(producer);
-                MLT.lockCreationTime(producer);
-                producer->get_length_time(mlt_time_clock);
-                Mlt::Chain chain(MLT.profile());
-                if (producer->type() != mlt_service_chain_type) {
-                    chain.set_source(*producer);
-                    producer = &chain;
-                }
+                producer = MLT.setupNewProducer(producer);
                 if (MLT.isSeekable(producer)) {
                     ProxyManager::generateIfNotExists(*producer);
                     if (row == -1)
@@ -727,6 +715,7 @@ void PlaylistDock::onDropped(const QMimeData *data, int row)
                     setIndex(0);
                     resetIndex = false;
                 }
+                delete producer;
             }
         }
     }

src/docks/timelinedock.cpp
@@ -1132,22 +1132,14 @@ static QString convertUrlsToXML(const QString& xml)
                 p = Mlt::Producer(MLT.profile(), path.toUtf8().constData());
             }
             if (p.is_valid()) {
-                // Convert avformat to avformat-novalidate so that XML loads faster.
-                if (!qstrcmp(p.get("mlt_service"), "avformat")) {
-                    if (!p.get_int("seekable")) {
-                        MAIN.showStatusMessage(QObject::tr("Not adding non-seekable file: ") + Util::baseName(path));
-                        continue;
-                    }
-                    p.set("mlt_service", "avformat-novalidate");
-                    p.set("mute_on_pause", 0);
+                if (!qstrcmp(p.get("mlt_service"), "avformat") && !p.get_int("seekable")) {
+                    MAIN.showStatusMessage(QObject::tr("Not adding non-seekable file: ") + Util::baseName(path));
+                    continue;
                 }
                 ProxyManager::generateIfNotExists(p);
-                MLT.setImageDurationFromDefault(&p);
-                MLT.lockCreationTime(&p);
-                p.get_length_time(mlt_time_clock);
-                Mlt::Chain chain(MLT.profile());
-                chain.set_source(p);
-                playlist.append(chain);
+                Mlt::Producer* producer = MLT.setupNewProducer(&p);
+                playlist.append(*producer);
+                delete producer;
             }
         }
         return MLT.XML(&playlist);

src/mainwindow.cpp
@@ -3422,28 +3422,16 @@ void MainWindow::processMultipleFiles()
             longTask.reportProgress(QFileInfo(filename).fileName(), i, count);
             Mlt::Producer p(MLT.profile(), filename.toUtf8().constData());
             if (p.is_valid()) {
-                // Convert avformat to avformat-novalidate so that XML loads faster.
-                if (!qstrcmp(p.get("mlt_service"), "avformat")) {
-                    p.set("mlt_service", "avformat-novalidate");
-                    p.set("mute_on_pause", 0);
-                }
                 if (QDir::toNativeSeparators(filename) == QDir::toNativeSeparators(MAIN.fileName())) {
                     MAIN.showStatusMessage(QObject::tr("You cannot add a project to itself!"));
                     continue;
                 }
-                MLT.setImageDurationFromDefault(&p);
-                MLT.lockCreationTime(&p);
-                p.get_length_time(mlt_time_clock);
                 Util::getHash(p);
                 ProxyManager::generateIfNotExists(p);
-                if (p.type() != mlt_service_chain_type) {
-                    Mlt::Chain chain(MLT.profile());
-                    chain.set_source(p);
-                    undoStack()->push(new Playlist::AppendCommand(*m_playlistDock->model(), MLT.XML(&chain), false));
-                } else {
-                    undoStack()->push(new Playlist::AppendCommand(*m_playlistDock->model(), MLT.XML(&p), false));
-                }
+                Mlt::Producer* producer = MLT.setupNewProducer(&p);
+                undoStack()->push(new Playlist::AppendCommand(*m_playlistDock->model(), MLT.XML(producer), false));
                 m_recentDock->add(filename.toUtf8().constData());
+                delete producer;
             }
         }
         emit m_playlistDock->model()->modified();

src/mltcontroller.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2020 Meltytech, LLC
+ * Copyright (c) 2011-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -144,26 +144,14 @@ int Controller::open(const QString &url, const QString& urlToSave)
             delete newProducer;
             newProducer = new Mlt::Producer(profile(), url.toUtf8().constData());
         }
-        // Convert avformat to avformat-novalidate so that XML loads faster.
-        if (!qstrcmp(newProducer->get("mlt_service"), "avformat")) {
-            newProducer->set("mlt_service", "avformat-novalidate");
-            newProducer->set("mute_on_pause", 0);
-        }
         if (m_url.isEmpty() && QString(newProducer->get("xml")) == "was here") {
             if (newProducer->get_int("_original_type") != mlt_service_tractor_type ||
                (newProducer->get_int("_original_type") == mlt_service_tractor_type && newProducer->get(kShotcutXmlProperty)))
                 m_url = urlToSave;
         }
-        setImageDurationFromDefault(newProducer);
-        lockCreationTime(newProducer);
-        // Encapsulate avformat producers in a chain to enable timing effects
-        if (!qstrcmp(newProducer->get("mlt_service"), "avformat-novalidate") &&
-            newProducer->type() != mlt_service_chain_type) {
-            Mlt::Chain* chain = new Mlt::Chain(MLT.profile());
-            chain->set_source(*newProducer);
-            delete newProducer;
-            newProducer = chain;
-        }
+        Producer* producer = setupNewProducer(newProducer);
+        delete newProducer;
+        newProducer = producer;
     }
     else {
         delete newProducer;
@@ -985,6 +973,34 @@ void Controller::lockCreationTime(Producer* producer) const
             producer->set_creation_time(creation_time);
         }
     }
+}
+
+Producer* Controller::setupNewProducer(Producer* newProducer) const
+{
+    // Call this function before adding a new producer to Shotcut so that
+    // It will be configured correctly. The returned producer must be deleted.
+    QString serviceName = newProducer->get("mlt_service");
+    if (serviceName == "avformat")
+    {
+        // Convert avformat to avformat-novalidate so that XML loads faster.
+        newProducer->set("mlt_service", "avformat-novalidate");
+    }
+    setImageDurationFromDefault(newProducer);
+    lockCreationTime(newProducer);
+    newProducer->get_length_time(mlt_time_clock);
+
+    if (serviceName.startsWith("avformat"))
+    {
+        newProducer->set("mute_on_pause", 0);
+        // Encapsulate in a chain to enable timing effects
+        if (newProducer->type() != mlt_service_chain_type)
+        {
+            Mlt::Chain* chain = new Mlt::Chain(MLT.profile());
+            chain->set_source(*newProducer);
+            return chain;
+        }
+    }
+    return new Mlt::Producer(newProducer);
 }
 
 QUuid Controller::uuid(Mlt::Properties &properties) const

src/mltcontroller.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2020 Meltytech, LLC
+ * Copyright (c) 2011-2021 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -119,6 +119,7 @@ public:
     void setImageDurationFromDefault(Service* service) const;
     void setDurationFromDefault(Producer* service) const;
     void lockCreationTime(Producer* producer) const;
+    Producer* setupNewProducer(Producer* newProducer) const;
     QUuid uuid(Mlt::Properties &properties) const;
     void setUuid(Mlt::Properties &properties, QUuid uid) const;
     QUuid ensureHasUuid(Mlt::Properties& properties) const;

src/widgets/avformatproducerwidget.cpp
@@ -974,17 +974,14 @@ bool AvformatProducerWidget::revertToOriginalResource()
                 int trackIndex = parts[1].toInt();
                 QUuid uuid = MAIN.timelineClipUuid(trackIndex, clipIndex);
                 if (!uuid.isNull()) {
-                    Mlt::Producer producer(MLT.profile(), resource.toUtf8().constData());
-                    if (producer.is_valid()) {
-                        if (!qstrcmp(producer.get("mlt_service"), "avformat")) {
-                            producer.set("mlt_service", "avformat-novalidate");
-                            producer.set("mute_on_pause", 0);
-                        }
-                        MLT.lockCreationTime(&producer);
-                        Mlt::Chain chain(MLT.profile());
-                        chain.set_source(producer);
-                        chain.set_in_and_out(m_producer->get_int(kOriginalInProperty), m_producer->get_int(kOriginalOutProperty));
-                        MAIN.replaceInTimeline(uuid, chain);
+                    Mlt::Producer newProducer(MLT.profile(), resource.toUtf8().constData());
+                    if (newProducer.is_valid()) {
+                        Mlt::Producer* producer = MLT.setupNewProducer(&newProducer);
+                        producer->set(kIsProxyProperty, 1);
+                        producer->set(kOriginalResourceProperty, resource.toUtf8().constData());
+                        producer->set_in_and_out(m_producer->get_int(kOriginalInProperty), m_producer->get_int(kOriginalOutProperty));
+                        MAIN.replaceInTimeline(uuid, *producer);
+                        delete producer;
                         return true;
                     }
                 }
@@ -1284,14 +1281,10 @@ void AvformatProducerWidget::on_actionDisableProxy_triggered(bool checked)
         if (producer()->get_int(kIsProxyProperty) && producer()->get(kOriginalResourceProperty)) {
             Mlt::Producer original(MLT.profile(), producer()->get(kOriginalResourceProperty));
             if (original.is_valid()) {
-                if (!qstrcmp(original.get("mlt_service"), "avformat")) {
-                    original.set("mlt_service", "avformat-novalidate");
-                    original.set("mute_on_pause", 0);
-                }
-                original.set(kDisableProxyProperty, 1);
-                Mlt::Chain chain(MLT.profile());
-                chain.set_source(original);
-                MAIN.replaceAllByHash(Util::getHash(original), chain, true);
+                Mlt::Producer* producer = MLT.setupNewProducer(&original);
+                producer->set(kDisableProxyProperty, 1);
+                MAIN.replaceAllByHash(Util::getHash(original), *producer, true);
+                delete producer;
             }
         }
     } else {
@@ -1329,13 +1322,9 @@ void AvformatProducerWidget::on_actionDeleteProxy_triggered()
     if (producer()->get_int(kIsProxyProperty) && producer()->get(kOriginalResourceProperty)) {
         Mlt::Producer original(MLT.profile(), producer()->get(kOriginalResourceProperty));
         if (original.is_valid()) {
-            if (!qstrcmp(original.get("mlt_service"), "avformat")) {
-                original.set("mlt_service", "avformat-novalidate");
-                original.set("mute_on_pause", 0);
-            }
-            Mlt::Chain chain(MLT.profile());
-            chain.set_source(original);
-            MAIN.replaceAllByHash(hash, chain, true);
+            Mlt::Producer* producer = MLT.setupNewProducer(&original);
+            MAIN.replaceAllByHash(hash, *producer, true);
+            delete producer;
         }
     }
 }

commit 4a42d842b3e1363db70ac4094b285dae6ea83db0

fix convert sub-clip replacement broken in 00d36cc

src/jobs/postjobaction.cpp
@@ -66,6 +66,7 @@ void ReplaceOnePostJobAction::doAction()
     Mlt::Producer newProducer(MLT.profile(), m_dstFile.toUtf8().constData());
     if (newProducer.is_valid()) {
         Mlt::Producer* producer = MLT.setupNewProducer(&newProducer);
+        producer->set_in_and_out(m_in, -1);
         MAIN.replaceInTimeline(m_uuid, *producer);
         delete producer;
     }