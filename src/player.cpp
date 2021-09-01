mltframework/shotcut

commit f65cd7628aedaa33fca3a919accfb4bbb96f3b44

fix #952 timeline playhead can go past end

src/player.cpp
@@ -536,7 +536,7 @@ void Player::seek(int position)
 {
     if (m_isSeekable) {
         if (position >= 0) {
-            emit seeked(qMin(position, m_duration - 1));
+            emit seeked(qMin(position, MLT.isMultitrack()? m_duration : m_duration - 1));
         }
     }
     // Seek implies pause.

src/docks/timelinedock.cpp
@@ -548,7 +548,7 @@ void TimelineDock::onShowFrame(const SharedFrame& frame)
     if (m_ignoreNextPositionChange) {
         m_ignoreNextPositionChange = false;
     } else if (MLT.isMultitrack() && m_position != frame.get_position()) {
-        m_position = frame.get_position();
+        m_position = qMin(frame.get_position(), m_model.tractor()->get_length());
         emit positionChanged();
     }
 }
@@ -556,7 +556,7 @@ void TimelineDock::onShowFrame(const SharedFrame& frame)
 void TimelineDock::onSeeked(int position)
 {
     if (MLT.isMultitrack() && m_position != position) {
-        m_position = position;
+        m_position = qMin(position, m_model.tractor()->get_length());
         emit positionChanged();
     }
 }

src/mainwindow.cpp
@@ -1815,7 +1815,7 @@ void MainWindow::keyPressEvent(QKeyEvent* event)
         break;
     case Qt::Key_End:
         if (MLT.producer())
-            m_player->seek(MLT.producer()->get_length() - 1);
+            m_player->seek(MLT.producer()->get_length());
         break;
     case Qt::Key_Left:
         if ((event->modifiers() & Qt::ControlModifier) && m_timelineDock->isVisible()) {

commit f17afa3f788f4b7f8232c9b077b3ee880eacbca5

fix step one frame left after going to end
regression in f65cd762

src/player.cpp
@@ -690,7 +690,7 @@ void Player::onFrameDisplayed(const SharedFrame& frame)
         onProducerOpened(false);
     }
     int position = frame.get_position();
-    if (position < m_duration) {
+    if (position <= m_duration) {
         m_position = position;
         m_positionSpinner->blockSignals(true);
         m_positionSpinner->setValue(position);