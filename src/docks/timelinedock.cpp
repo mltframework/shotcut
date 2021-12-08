13f9313e87cec075cf29388779cfc9f3398470e7
Dan Dennedy <dan@dennedy.org>
2021-12-08 14:57:34 -0800

    fix crash pasting from clipboard to empty timeline

--- src/docks/timelinedock.cpp
+++ src/docks/timelinedock.cpp
@@ -189,7 +189,7 @@ void TimelineDock::addTrackIfNeeded(int trackIndex, Mlt::Producer* srcTrack)
     if (trackIndex >= n) {
         if (m_selection.selectedTrack != -1)
             setSelection();
-        if (srcTrack->get_int(kAudioTrackProperty) || m_model.trackList()[n - 1].type == AudioTrackType) {
+        if (srcTrack->get_int(kAudioTrackProperty) || (n > 0 && m_model.trackList()[n - 1].type == AudioTrackType)) {
             MAIN.undoStack()->push(
                 new Timeline::InsertTrackCommand(m_model, trackIndex, AudioTrackType));
         } else {
