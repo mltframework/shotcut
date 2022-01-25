954db37f5ee1af27313b7a3f3668bd1a37bd4f59
Dan Dennedy <dan@dennedy.org>
2022-01-24 17:15:34 -0800

    update copyright year in ffmpegjob.cpp

--- src/jobs/ffmpegjob.cpp
+++ src/jobs/ffmpegjob.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 Meltytech, LLC
+ * Copyright (c) 2016-2022 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
