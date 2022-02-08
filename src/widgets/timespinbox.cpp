16cb55a5e86d5e973ece812cf66bf502116723e1
Dan Dennedy <dan@dennedy.org>
2022-02-08 13:35:55 -0800

    Revert "Handle delete and backspace in timespinbox"
    This reverts commit 7867a64950197a05af90145b3e8020ab051eb779.

--- src/widgets/timespinbox.cpp
+++ src/widgets/timespinbox.cpp
@@ -76,8 +76,6 @@ void TimeSpinBox::keyPressEvent(QKeyEvent* event)
         event->key() == Qt::Key_Right ||
         event->key() == Qt::Key_Enter ||
         event->key() == Qt::Key_Return ||
-        event->key() == Qt::Key_Delete ||
-        event->key() == Qt::Key_Backspace ||
         event->key() == Qt::Key_Tab)
     {
         QSpinBox::keyPressEvent(event);
