mltframework/shotcut

commit 4f3b29810b850bb3e9a2c808b9ec859fab260466

Convert simple keyframes to advanced keyframes
If simple keyframes are in use, and the user clicks a keyframe button
to use advanced keyframes, prompt the user and give an option to
cancel or convert all simple keyframes to advanced.

src/qml/filters/crop_rectangle/ui.qml
@@ -98,7 +98,7 @@ Item {
     function updateFilterRatio(position) {
         if (blockUpdate) return
         var value = slider.value / 100.0
-
+        setRatioControls()
         if (position !== null) {
             if (position <= 0 && filter.animateIn > 0) {
                 startValueRadius = value
@@ -130,7 +130,7 @@ Item {
 
     function updateFilterRect(position) {
         var rect
-
+        setRectControls()
         if (position !== null) {
             filter.blockSignals = true
             if (position <= 0 && filter.animateIn > 0) {
@@ -372,29 +372,24 @@ Item {
     }
 
     function updateFilter() {
-        if (filter.animateIn <= 0 && filter.animateOut <= 0) {
-            // When disabling simple keyframes. Clear out the keyframes before proceeding.
-            filter.blockSignals = true
-            if (!positionKeyframesButton.checked && filter.keyframeCount(rectProperty) > 0) {
-                filter.resetProperty(rectProperty)
-            }
-            if (!radiusKeyframesButton.checked && filter.keyframeCount('radius') > 0) {
-                filter.resetProperty('radius')
-            }
-            filter.blockSignals = false
-        }
         updateFilterRatio(null)
         updateFilterRect(null)
     }
     
     Connections {
         target: filter
-        onChanged: setRectControls()
+        onChanged: {
+            setRectControls()
+            setRatioControls()
+        }
         onInChanged: updateFilter()
         onOutChanged: updateFilter()
         onAnimateInChanged: updateFilter()
         onAnimateOutChanged: updateFilter()
-        onPropertyChanged: setRatioControls()
+        onPropertyChanged: {
+            setRectControls()
+            setRatioControls()
+        }
     }
 
     Connections {

src/qml/filters/audio_balance/ui.qml
@@ -155,6 +155,7 @@ Item {
 
     Connections {
         target: filter
+        onChanged: setControls()
         onInChanged: updateFilter(null)
         onOutChanged: updateFilter(null)
         onAnimateInChanged: updateFilter(null)

src/qml/filters/audio_gain/ui.qml
@@ -51,6 +51,7 @@ Item {
 
     Connections {
         target: filter
+        onChanged: setControls()
         onInChanged: updateFilter(null)
         onOutChanged: updateFilter(null)
         onAnimateInChanged: updateFilter(null)

src/qml/filters/audio_noisegate/ui.qml
@@ -88,6 +88,7 @@ Shotcut.KeyframableFilter {
     }
 
     function updateSimpleKeyframes() {
+        setControls()
         updateFilter(lfkey, lfkeySlider.value, lfkeyKeyframesButton, null)
         updateFilter(hfkey, hfkeySlider.value, hfkeyKeyframesButton, null)
         updateFilter(threshold, thresholdSlider.value, thresholdKeyframesButton, null)
@@ -299,6 +300,7 @@ Shotcut.KeyframableFilter {
 
     Connections {
         target: filter
+        onChanged: setControls()
         onInChanged: updateSimpleKeyframes()
         onOutChanged: updateSimpleKeyframes()
         onAnimateInChanged: updateSimpleKeyframes()

src/qml/filters/audio_pan/ui.qml
@@ -173,6 +173,7 @@ Item {
 
     Connections {
         target: filter
+        onChanged: setControls()
         onInChanged: updateFilter(null)
         onOutChanged: updateFilter(null)
         onAnimateInChanged: updateFilter(null)

src/qml/filters/audio_pitch/ui.qml
@@ -45,6 +45,7 @@ Item {
 
     Connections {
         target: filter
+        onChanged: setControls()
         onInChanged: updateFilter(null)
         onOutChanged: updateFilter(null)
         onAnimateInChanged: updateFilter(null)

src/qml/filters/bigsh0t_eq_mask/ui.qml
@@ -17,23 +17,7 @@ Item {
     property double vfov1Start : 0.0; property double vfov1Middle : 0.0; property double vfov1End : 0.0;
 
     function updateSimpleKeyframes() {
-        if (filter.animateIn <= 0 && filter.animateOut <= 0) {
-            // When disabling simple keyframes. Clear out the keyframes before proceeding.
-            filter.blockSignals = true
-            if (!hfov0KeyframesButton.checked && filter.keyframeCount("hfov0") > 0) {
-                filter.resetProperty("hfov0")
-            }
-            if (!hfov1KeyframesButton.checked && filter.keyframeCount("hfov1") > 0) {
-                filter.resetProperty("hfov1")
-            }
-            if (!vfov0KeyframesButton.checked && filter.keyframeCount("vfov0") > 0) {
-                filter.resetProperty("vfov0")
-            }
-            if (!vfov1KeyframesButton.checked && filter.keyframeCount("vfov1") > 0) {
-                filter.resetProperty("vfov1")
-            }
-            filter.blockSignals = false
-        } else if (filter.animateIn > 0 || filter.animateOut > 0) {
+        if (filter.animateIn > 0 || filter.animateOut > 0) {
             // When enabling simple keyframes, initialize the keyframes with the current value
             if (filter.keyframeCount("hfov0") <= 0) {
                 hfov0Start = hfov0Middle = hfov0End = filter.getDouble("hfov0")
@@ -48,6 +32,7 @@ Item {
                 vfov1Start = vfov1Middle = vfov1End = filter.getDouble("vfov1")
             }
         }
+        setControls()
         updateProperty_hfov0(null)
         updateProperty_hfov1(null)
         updateProperty_vfov0(null)
@@ -212,6 +197,7 @@ Item {
 
     Connections {
         target: filter
+        onChanged: setControls()
         onPropertyChanged: setControls()
     }
 

src/qml/filters/bigsh0t_eq_to_rect/ui.qml
@@ -19,26 +19,7 @@ Item {
     property int interpolationValue : 0;
 
     function updateSimpleKeyframes() {
-        if (filter.animateIn <= 0 && filter.animateOut <= 0) {
-            // When disabling simple keyframes. Clear out the keyframes before proceeding.
-            filter.blockSignals = true
-            if (!yawKeyframesButton.checked && filter.keyframeCount("yaw") > 0) {
-                filter.resetProperty("yaw")
-            }
-            if (!pitchKeyframesButton.checked && filter.keyframeCount("pitch") > 0) {
-                filter.resetProperty("pitch")
-            }
-            if (!rollKeyframesButton.checked && filter.keyframeCount("roll") > 0) {
-                filter.resetProperty("roll")
-            }
-            if (!fovKeyframesButton.checked && filter.keyframeCount("fov") > 0) {
-                filter.resetProperty("fov")
-            }
-            if (!fisheyeKeyframesButton.checked && filter.keyframeCount("fisheye") > 0) {
-                filter.resetProperty("fisheye")
-            }
-            filter.blockSignals = false
-        } else if (filter.animateIn > 0 || filter.animateOut > 0) {
+        if (filter.animateIn > 0 || filter.animateOut > 0) {
             // When enabling simple keyframes, initialize the keyframes with the current value
             if (filter.keyframeCount("yaw") <= 0) {
                 yawStart = yawMiddle = yawEnd = filter.getDouble("yaw")
@@ -56,6 +37,7 @@ Item {
                 fisheyeStart = fisheyeMiddle = fisheyeEnd = filter.getDouble("fisheye")
             }
         }
+        setControls()
         updateProperty_yaw (null)
         updateProperty_pitch (null)
         updateProperty_roll (null)
@@ -99,6 +81,7 @@ Item {
         fovSlider.value = filter.getDouble("fov", position)
         fovKeyframesButton.checked = filter.animateIn <= 0 && filter.animateOut <= 0 && filter.keyframeCount("fov") > 0
         fisheyeSlider.value = filter.getDouble("fisheye", position)
+        fisheyeKeyframesButton.checked = filter.animateIn <= 0 && filter.animateOut <= 0 && filter.keyframeCount("fisheye") > 0
         interpolationComboBox.currentIndex = filter.get("interpolation")
         blockUpdate = false
     }

commit 9e98df0af7e62611891cc88e417006d50989a6ca

fix position and size fields in crop: rectangle
regression in 4f3b2981

src/qml/filters/crop_rectangle/ui.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Meltytech, LLC
+ * Copyright (c) 2020-2022 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -130,7 +130,6 @@ Item {
 
     function updateFilterRect(position) {
         var rect
-        setRectControls()
         if (position !== null) {
             filter.blockSignals = true
             if (position <= 0 && filter.animateIn > 0) {