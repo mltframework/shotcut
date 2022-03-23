mltframework/shotcut

commit 8c0ea769d11faeb37514fd59ee700d5c07cd41f6

hide Properties > Sync when not applicable

src/widgets/avformatproducerwidget.cpp
@@ -605,7 +605,7 @@ void AvformatProducerWidget::onFrameDecoded()
             }
         }
     }
-    ui->syncSlider->setValue(qRound(m_producer->get_double("video_delay") * 1000.0));
+    setSyncVisibility();
 
     if (Settings.showConvertClipDialog() && !m_producer->get_int(kShotcutSkipConvertProperty)) {
         auto transferItem = ui->videoTableWidget->item(5, 1);
@@ -1025,6 +1025,15 @@ bool AvformatProducerWidget::revertToOriginalResource()
         }
     }
     return false;
+}
+
+void AvformatProducerWidget::setSyncVisibility()
+{
+    ui->syncSlider->setVisible(ui->tabWidget->isTabEnabled(0) && ui->tabWidget->isTabEnabled(1) &&
+                               m_producer->get_int("video_index") != -1 &&
+                               m_producer->get_int("audio_index") != -1);
+    ui->syncLabel->setVisible(ui->syncSlider->isVisible());
+    ui->syncSpinBox->setVisible(ui->syncSlider->isVisible());
 }
 
 void AvformatProducerWidget::on_reverseButton_clicked()

src/widgets/avformatproducerwidget.h
@@ -134,6 +134,7 @@ private:
     void recreateProducer();
     void convert(TranscodeDialog& dialog);
     bool revertToOriginalResource();
+    void setSyncVisibility();
 };
 
 

src/widgets/avformatproducerwidget.ui
@@ -792,7 +792,7 @@
             </widget>
            </item>
            <item row="2" column="0">
-            <widget class="QLabel" name="label_3">
+            <widget class="QLabel" name="syncLabel">
              <property name="text">
               <string>Sync</string>
              </property>
@@ -817,7 +817,7 @@
               </spacer>
              </item>
              <item>
-              <widget class="QSpinBox" name="spinBox">
+              <widget class="QSpinBox" name="syncSpinBox">
                <property name="suffix">
                 <string> ms</string>
                </property>
@@ -1067,7 +1067,7 @@
   <tabstop>menuButton</tabstop>
   <tabstop>audioTrackComboBox</tabstop>
   <tabstop>syncSlider</tabstop>
-  <tabstop>spinBox</tabstop>
+  <tabstop>syncSpinBox</tabstop>
   <tabstop>metadataTable</tabstop>
   <tabstop>scrollArea</tabstop>
   <tabstop>reverseButton</tabstop>
@@ -1082,7 +1082,7 @@
   <connection>
    <sender>syncSlider</sender>
    <signal>valueChanged(int)</signal>
-   <receiver>spinBox</receiver>
+   <receiver>syncSpinBox</receiver>
    <slot>setValue(int)</slot>
    <hints>
     <hint type="sourcelabel">
@@ -1096,7 +1096,7 @@
    </hints>
   </connection>
   <connection>
-   <sender>spinBox</sender>
+   <sender>syncSpinBox</sender>
    <signal>valueChanged(int)</signal>
    <receiver>syncSlider</receiver>
    <slot>setValue(int)</slot>

commit bdc459df885343654dc44b22ef1ce5b3f86207c7

fix Sync regression in 8c0ea76

src/widgets/avformatproducerwidget.cpp
@@ -606,6 +606,7 @@ void AvformatProducerWidget::onFrameDecoded()
             }
         }
     }
+    ui->syncSlider->setValue(qRound(m_producer->get_double("video_delay") * 1000.0));
     setSyncVisibility();
 
     if (Settings.showConvertClipDialog() && !m_producer->get_int(kShotcutSkipConvertProperty)) {