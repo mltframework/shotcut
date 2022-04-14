851fe2627497d69103dc7e590bea8f26df685dd7
Dan Dennedy <dan@dennedy.org>
2022-04-14 11:16:17 -0700

    use mktemp for macOS codesign

--- scripts/codesign_and_notarize.sh
+++ scripts/codesign_and_notarize.sh
@@ -8,21 +8,22 @@ find ~/Desktop/Shotcut.app/Contents/PlugIns -type f -exec codesign --options=run
 find ~/Desktop/Shotcut.app/Contents/Resources -type f -exec codesign --options=runtime -v -s "$SIGNER" {} \;
 xattr -cr ~/Desktop/Shotcut.app
 codesign --options=runtime -v -s "$SIGNER" \
-  --entitlements ~/Projects/Shotcut/src/shotcut/scripts/notarization.entitlements \
+  --entitlements ./notarization.entitlements \
   ~/Desktop/Shotcut.app/Contents/MacOS/{melt,ffmpeg,ffplay,ffprobe}
 codesign --options=runtime -v -s "$SIGNER" \
-  --entitlements ~/Projects/Shotcut/src/shotcut/scripts/notarization.entitlements \
+  --entitlements ./notarization.entitlements \
   ~/Desktop/Shotcut.app
 codesign --verify --deep --strict --verbose=2 ~/Desktop/Shotcut.app
 spctl -a -t exec -vv ~/Desktop/Shotcut.app
 
-rm -rf ~/tmp/*
-mv ~/Desktop/Shotcut.app ~/tmp
-ln -s /Applications ~/tmp
-cp ~/Projects/Shotcut/src/shotcut/COPYING ~/tmp
+TMP=$(mktemp -d)
+mv ~/Desktop/Shotcut.app $TMP
+ln -s /Applications $TMP
+cp ../COPYING $TMP
 rm ~/Desktop/shotcut-macos-${VERSION}.dmg
-hdiutil create -srcfolder ~/tmp -volname Shotcut -format UDBZ -size 300m \
+hdiutil create -srcfolder $TMP -volname Shotcut -format UDBZ -size 300m \
   ~/Desktop/shotcut-macos-${VERSION}.dmg
+rm -rf $TMP
 
 ./notarize.sh ~/Desktop/shotcut-macos-${VERSION}.dmg
 
