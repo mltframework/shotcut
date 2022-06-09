mltframework/shotcut

commit 0bd5b438cb1dbf7488f576a39ac7c810214bb947

glaxnimate: fix preview if anim first frame not 0

src/widgets/glaxnimateproducerwidget.cpp
@@ -331,6 +331,7 @@ void GlaxnimateProducerWidget::onFileChanged(const QString &path)
         m_title = caption;
         m_producer->set("refresh", 1);
         MLT.refreshConsumer();
+        ui->durationSpinBox->setValue(m_producer->get_length());
     }
 }
 
@@ -339,6 +340,7 @@ void GlaxnimateProducerWidget::on_reloadButton_clicked()
     if (m_producer && m_producer->is_valid()) {
         m_producer->set("refresh", 1);
         MLT.refreshConsumer();
+        ui->durationSpinBox->setValue(m_producer->get_length());
     }
 }
 
@@ -400,6 +402,11 @@ void GlaxnimateIpcServer::onConnect()
     m_isProtocolValid = false;
 }
 
+int GlaxnimateIpcServer::toMltFps(float frame) const
+{
+    return qRound(frame / parent->m_producer.get_double("meta.media.frame_rate") * MLT.profile().fps());
+}
+
 void GlaxnimateIpcServer::onReadyRead()
 {
     if (!m_isProtocolValid) {
@@ -424,7 +431,8 @@ void GlaxnimateIpcServer::onReadyRead()
             return;
 
         // Only if the frame number is different
-        int frameNum = parent->m_producer.get_int(kPlaylistStartProperty) + qRound(time);
+        int frameNum = parent->m_producer.get_int(kPlaylistStartProperty) + toMltFps(
+                           time) - parent->m_producer.get_int("first_frame");
         if (frameNum != parent->m_frameNum) {
             LOG_DEBUG() << "glaxnimate time =" << time;
 

src/widgets/glaxnimateproducerwidget.h
@@ -62,6 +62,7 @@ private slots:
     void onSocketError(QLocalSocket::LocalSocketError socketError);
 
 private:
+    int toMltFps(float frame) const;
     bool copyToShared(const QImage &image);
 };
 

commit 3419880d9c12d07c14844ca3d588d6e91fb6889a

fix glaxnimate preview with Mask: Draw filter
regression in 0bd5b43

src/widgets/glaxnimateproducerwidget.cpp
@@ -403,7 +403,10 @@ void GlaxnimateIpcServer::onConnect()
 
 int GlaxnimateIpcServer::toMltFps(float frame) const
 {
-    return qRound(frame / parent->m_producer.get_double("meta.media.frame_rate") * MLT.profile().fps());
+    if (parent->m_producer.get_double("meta.media.frame_rate") > 0) {
+        return qRound(frame / parent->m_producer.get_double("meta.media.frame_rate") * MLT.profile().fps());
+    }
+    return frame;
 }
 
 void GlaxnimateIpcServer::onReadyRead()
@@ -433,7 +436,7 @@ void GlaxnimateIpcServer::onReadyRead()
         int frameNum = parent->m_producer.get_int(kPlaylistStartProperty) + toMltFps(
                            time) - parent->m_producer.get_int("first_frame");
         if (frameNum != parent->m_frameNum) {
-            LOG_DEBUG() << "glaxnimate time =" << time;
+            LOG_DEBUG() << "glaxnimate time =" << time << "=> Shotcut frameNum =" << frameNum;
 
             // Get the image from MLT
             parent->m_glaxnimateProducer->seek(frameNum);