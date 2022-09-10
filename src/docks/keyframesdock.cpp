mltframework/shotcut

commit 783b81120cd872186217ec1176831bb9f8bec0d5

Convert QML Menus to QWidgets and QActions (#1299)
* Remove timelinedock.ui

The file was boilerplate and the scrollview was being overwritten
by the QQuickWidget in the TimelineDock constructor.

* Reimplement QML timeline toolbar and menus using QWidgets

* Reimplement QML keyframes toolbar and menus using QWidgets

* Reimplement playlist toolbar and menus using QActions

* Resolve review comments for timeline and playlist actions

src/docks/keyframesdock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 Meltytech, LLC
+ * Copyright (c) 2016-2022 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,25 +17,32 @@
 
 #include "keyframesdock.h"
 
-#include <QQuickView>
-#include <QQuickItem>
-#include <QQmlEngine>
-#include <QDir>
-#include <QUrl>
-#include <QQmlContext>
-#include <QIcon>
-#include <QAction>
-
-#include <Logger.h>
-
+#include "models/attachedfiltersmodel.h"
+#include "qmltypes/qmlproducer.h"
 #include "qmltypes/qmlutilities.h"
 #include "qmltypes/qmlview.h"
-#include "models/attachedfiltersmodel.h"
-#include "mltcontroller.h"
-#include "settings.h"
+#include "widgets/docktoolbar.h"
 #include "mainwindow.h"
-#include "controllers/filtercontroller.h"
-#include "qmltypes/qmlproducer.h"
+#include "settings.h"
+#include <Logger.h>
+
+#include <QAction>
+#include <QDir>
+#include <QIcon>
+#include <QMenu>
+#include <QUrl>
+#include <QVBoxLayout>
+#include <QQmlContext>
+#include <QQmlEngine>
+#include <QQuickItem>
+#include <QQuickView>
+#include <QSlider>
+#include <QToolButton>
+
+#include <cmath>
+
+static QmlMetadata m_emptyQmlMetadata;
+static QmlFilter m_emptyQmlFilter;
 
 KeyframesDock::KeyframesDock(QmlProducer *qmlProducer, QWidget *parent)
     : QDockWidget(tr("Keyframes"), parent)
@@ -48,6 +55,78 @@ KeyframesDock::KeyframesDock(QmlProducer *qmlProducer, QWidget *parent)
     setWindowIcon(icon);
     toggleViewAction()->setIcon(windowIcon());
     setMinimumSize(200, 50);
+
+    setupActions();
+
+    m_mainMenu = new QMenu(tr("Keyframes Main Menu"), this);
+    m_mainMenu->addAction(m_actions["keyframesTrimInAction"]);
+    m_mainMenu->addAction(m_actions["keyframesTrimOutAction"]);
+    m_mainMenu->addAction(m_actions["keyframesAnimateInAction"]);
+    m_mainMenu->addAction(m_actions["keyframesAnimateOutAction"]);
+    m_mainMenu->addAction(m_actions["keyframesToggleKeyframeAction"]);
+    m_mainMenu->addAction(m_actions["keyframesSeekPreviousSimpleAction"]);
+    m_mainMenu->addAction(m_actions["keyframesSeekNextSimpleAction"]);
+    QMenu *viewMenu = new QMenu(tr("View"), this);
+    viewMenu->addAction(m_actions["keyframesZoomOutAction"]);
+    viewMenu->addAction(m_actions["keyframesZoomInAction"]);
+    viewMenu->addAction(m_actions["keyframesZoomFitAction"]);
+    m_mainMenu->addMenu(viewMenu);
+
+    m_keyMenu = new QMenu(tr("Keyframes Context Menu"), this);
+    QMenu *keyTypeMenu = new QMenu(tr("Keyframe Type"), this);
+    keyTypeMenu->addAction(m_actions["keyframesTypeHoldAction"]);
+    keyTypeMenu->addAction(m_actions["keyframesTypeLinearAction"]);
+    keyTypeMenu->addAction(m_actions["keyframesTypeSmoothAction"]);
+    m_keyMenu->addMenu(keyTypeMenu);
+    m_keyMenu->addAction(m_actions["keyframesRemoveAction"]);
+
+    m_clipMenu = new QMenu(tr("Keyframes Clip Menu"), this);
+    m_clipMenu->addAction(m_actions["keyframesRebuildAudioWaveformAction"]);
+
+    QVBoxLayout *vboxLayout = new QVBoxLayout();
+    vboxLayout->setSpacing(0);
+    vboxLayout->setContentsMargins(0, 0, 0, 0);
+
+    DockToolBar *toolbar = new DockToolBar(tr("Timeline Controls"));
+    QToolButton *menuButton = new QToolButton();
+    menuButton->setIcon(QIcon::fromTheme("show-menu",
+                                         QIcon(":/icons/oxygen/32x32/actions/show-menu.png")));
+    menuButton->setToolTip(tr("Timeline Menu"));
+    menuButton->setAutoRaise(true);
+    menuButton->setPopupMode(QToolButton::QToolButton::InstantPopup);
+    menuButton->setMenu(m_mainMenu);
+    toolbar->addWidget(menuButton);
+    toolbar->addSeparator();
+    toolbar->addAction(m_actions["keyframesTrimInAction"]);
+    toolbar->addAction(m_actions["keyframesTrimOutAction"]);
+    toolbar->addAction(m_actions["keyframesAnimateInAction"]);
+    toolbar->addAction(m_actions["keyframesAnimateOutAction"]);
+    toolbar->addSeparator();
+    toolbar->addAction(m_actions["keyframesZoomOutAction"]);
+    QSlider *zoomSlider = new QSlider();
+    zoomSlider->setOrientation(Qt::Horizontal);
+    zoomSlider->setMaximumWidth(200);
+    zoomSlider->setMinimum(0);
+    zoomSlider->setMaximum(300);
+    zoomSlider->setValue(100);
+    connect(zoomSlider, &QSlider::valueChanged, this, [&](int value) {
+        if (!isVisible() || !m_qview.rootObject()) return;
+        setZoom(value / 100.0);
+    });
+    connect(this, &KeyframesDock::timeScaleChanged, zoomSlider, [ = ]() {
+        double value = 1.0;
+        if (m_qview.rootObject()) {
+            double scaleFactor = m_qview.rootObject()->property("timeScale").toDouble();
+            double value = round(pow(scaleFactor - 0.01, 1.0 / 3.0) * 100.0);
+            zoomSlider->setValue(value);
+        }
+    });
+    toolbar->addWidget(zoomSlider);
+    toolbar->addAction(m_actions["keyframesZoomInAction"]);
+    toolbar->addAction(m_actions["keyframesZoomFitAction"]);
+
+    vboxLayout->setMenuBar(toolbar);
+
     m_qview.setFocusPolicy(Qt::StrongFocus);
     m_qview.quickWindow()->setPersistentSceneGraph(false);
 #ifndef Q_OS_MAC
@@ -62,7 +141,247 @@ KeyframesDock::KeyframesDock(QmlProducer *qmlProducer, QWidget *parent)
     setCurrentFilter(0, 0);
     connect(m_qview.quickWindow(), SIGNAL(sceneGraphInitialized()), SLOT(load()));
 
+    vboxLayout->addWidget(&m_qview);
+    QWidget *dockContentsWidget = new QWidget();
+    dockContentsWidget->setLayout(vboxLayout);
+    QDockWidget::setWidget(dockContentsWidget);
+
     LOG_DEBUG() << "end";
+}
+
+void KeyframesDock::setupActions()
+{
+    QIcon icon;
+    QAction *action;
+
+    action = new QAction(tr("Set Filter Start"), this);
+    action->setObjectName("keyframesTrimInAction");
+    action->setShortcut(QKeySequence(Qt::Key_BracketLeft));
+    icon = QIcon::fromTheme("keyframes-filter-in",
+                            QIcon(":/icons/oxygen/32x32/actions/keyframes-filter-in.png"));
+    action->setIcon(icon);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (m_qmlProducer && m_filter && m_filter->allowTrim()) {
+            int i = m_qmlProducer->position() + m_qmlProducer->in();
+            m_model.trimFilterIn(i);
+        }
+    });
+    connect(this, &KeyframesDock::newFilter, action, [ = ]() {
+        bool enabled = false;
+        if (m_filter && m_filter->allowTrim())
+            enabled = true;
+        action->setEnabled(enabled);
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Set Filter End"), this);
+    action->setObjectName("keyframesTrimOutAction");
+    action->setShortcut(QKeySequence(Qt::Key_BracketRight));
+    icon = QIcon::fromTheme("keyframes-filter-out",
+                            QIcon(":/icons/oxygen/32x32/actions/keyframes-filter-out.png"));
+    action->setIcon(icon);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (m_qmlProducer && m_filter && m_filter->allowTrim()) {
+            int i = m_qmlProducer->position() + m_qmlProducer->in();
+            m_model.trimFilterOut(i);
+        }
+    });
+    connect(this, &KeyframesDock::newFilter, action, [ = ]() {
+        bool enabled = false;
+        if (m_filter && m_filter->allowTrim())
+            enabled = true;
+        action->setEnabled(enabled);
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Set First Simple Keyframe"), this);
+    action->setObjectName("keyframesAnimateInAction");
+    action->setShortcut(QKeySequence(Qt::Key_BraceLeft));
+    icon = QIcon::fromTheme("keyframes-simple-in",
+                            QIcon(":/icons/oxygen/32x32/actions/keyframes-simple-in.png"));
+    action->setIcon(icon);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (m_qmlProducer && m_filter && m_filter->allowAnimateIn()) {
+            int i = m_qmlProducer->position() + m_qmlProducer->in() - m_filter->in();
+            m_filter->setAnimateIn(i);
+        }
+    });
+    connect(this, &KeyframesDock::newFilter, action, [ = ]() {
+        bool enabled = false;
+        if (m_filter && m_filter->allowAnimateIn())
+            enabled = true;
+        action->setEnabled(enabled);
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Set Second Simple Keyframe"), this);
+    action->setObjectName("keyframesAnimateOutAction");
+    action->setShortcut(QKeySequence(Qt::Key_BraceRight));
+    icon = QIcon::fromTheme("keyframes-simple-out",
+                            QIcon(":/icons/oxygen/32x32/actions/keyframes-simple-out.png"));
+    action->setIcon(icon);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (m_qmlProducer && m_filter && m_filter->allowAnimateOut()) {
+            int i = m_qmlProducer->position() + m_qmlProducer->in() - m_filter->in();
+            m_filter->setAnimateOut(i);
+        }
+    });
+    connect(this, &KeyframesDock::newFilter, action, [ = ]() {
+        bool enabled = false;
+        if (m_filter && m_filter->allowAnimateOut())
+            enabled = true;
+        action->setEnabled(enabled);
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Zoom Keyframes Out"), this);
+    action->setObjectName("keyframesZoomOutAction");
+    action->setShortcut(QKeySequence(Qt::ALT + Qt::Key_Minus));
+    icon = QIcon::fromTheme("zoom-out",
+                            QIcon(":/icons/oxygen/32x32/actions/zoom-out.png"));
+    action->setIcon(icon);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (!isVisible() || !m_qview.rootObject()) return;
+        zoomOut();
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Zoom Keyframes In"), this);
+    action->setObjectName("keyframesZoomInAction");
+    action->setShortcut(QKeySequence(Qt::ALT + Qt::Key_Plus));
+    icon = QIcon::fromTheme("zoom-in",
+                            QIcon(":/icons/oxygen/32x32/actions/zoom-in.png"));
+    action->setIcon(icon);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (!isVisible() || !m_qview.rootObject()) return;
+        zoomIn();
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Zoom Keyframes To Fit"), this);
+    action->setObjectName("keyframesZoomFitAction");
+    action->setShortcut(QKeySequence(Qt::ALT + Qt::Key_0));
+    icon = QIcon::fromTheme("zoom-fit-best",
+                            QIcon(":/icons/oxygen/32x32/actions/zoom-fit-best.png"));
+    action->setIcon(icon);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (!isVisible() || !m_qview.rootObject()) return;
+        zoomToFit();
+    });
+    m_actions[action->objectName()] = action;
+
+    QActionGroup *keyframeTypeActionGroup = new QActionGroup(this);
+    keyframeTypeActionGroup->setExclusive(true);
+
+    action = new QAction(tr("Hold"), this);
+    action->setObjectName("keyframesTypeHoldAction");
+    connect(action, &QAction::triggered, this, [&]() {
+        if (!isVisible() || !m_qview.rootObject()) return;
+        int currentTrack = m_qview.rootObject()->property("currentTrack").toInt();
+        for (auto keyframeIndex : m_qview.rootObject()->property("selection").toList()) {
+            m_model.setInterpolation(currentTrack, keyframeIndex.toInt(),
+                                     KeyframesModel::DiscreteInterpolation);
+        }
+    });
+    keyframeTypeActionGroup->addAction(action);
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Linear"), this);
+    action->setObjectName("keyframesTypeLinearAction");
+    connect(action, &QAction::triggered, this, [&]() {
+        if (!isVisible() || !m_qview.rootObject()) return;
+        int currentTrack = m_qview.rootObject()->property("currentTrack").toInt();
+        for (auto keyframeIndex : m_qview.rootObject()->property("selection").toList()) {
+            m_model.setInterpolation(currentTrack, keyframeIndex.toInt(), KeyframesModel::LinearInterpolation);
+        }
+    });
+    keyframeTypeActionGroup->addAction(action);
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Smooth"), this);
+    action->setObjectName("keyframesTypeSmoothAction");
+    connect(action, &QAction::triggered, this, [&]() {
+        if (!isVisible() || !m_qview.rootObject()) return;
+        int currentTrack = m_qview.rootObject()->property("currentTrack").toInt();
+        for (auto keyframeIndex : m_qview.rootObject()->property("selection").toList()) {
+            m_model.setInterpolation(currentTrack, keyframeIndex.toInt(), KeyframesModel::SmoothInterpolation);
+        }
+    });
+    keyframeTypeActionGroup->addAction(action);
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Remove"), this);
+    action->setObjectName("keyframesRemoveAction");
+    connect(action, &QAction::triggered, this, [&]() {
+        if (!isVisible() || !m_qview.rootObject()) return;
+        int currentTrack = m_qview.rootObject()->property("currentTrack").toInt();
+        for (auto keyframeIndex : m_qview.rootObject()->property("selection").toList()) {
+            m_model.remove(currentTrack, keyframeIndex.toInt());
+        }
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Rebuild Audio Waveform"), this);
+    action->setObjectName("keyframesRebuildAudioWaveformAction");
+    action->setEnabled(Settings.timelineShowWaveforms());
+    connect(action, &QAction::triggered, this, [&](bool checked) {
+        if (m_qmlProducer && Settings.timelineShowWaveforms()) {
+            m_qmlProducer->remakeAudioLevels();
+        }
+    });
+    connect(&Settings, &ShotcutSettings::timelineShowWaveformsChanged, action, [ = ]() {
+        action->setEnabled(Settings.timelineShowWaveforms());
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Seek Previous Simple Keyframe"), this);
+    action->setObjectName("keyframesSeekPreviousSimpleAction");
+    action->setShortcut(QKeySequence(Qt::ALT + Qt::Key_BracketLeft));
+    action->setEnabled(m_qmlProducer && m_filter);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (m_qmlProducer && m_filter) {
+            seekPreviousSimple();
+        }
+    });
+    connect(this, &KeyframesDock::newFilter, action, [ = ]() {
+        action->setEnabled(m_qmlProducer && m_filter);
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Seek Next Simple Keyframe"), this);
+    action->setObjectName("keyframesSeekNextSimpleAction");
+    action->setShortcut(QKeySequence(Qt::ALT + Qt::Key_BracketRight));
+    action->setEnabled(m_qmlProducer && m_filter);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (m_qmlProducer && m_filter) {
+            seekNextSimple();
+        }
+    });
+    connect(this, &KeyframesDock::newFilter, action, [ = ]() {
+        action->setEnabled(m_qmlProducer && m_filter);
+    });
+    m_actions[action->objectName()] = action;
+
+    action = new QAction(tr("Toggle Keyframe At Playhead"), this);
+    action->setObjectName("keyframesToggleKeyframeAction");
+    action->setShortcut(QKeySequence(Qt::Key_Semicolon));
+    action->setEnabled(m_qmlProducer && m_filter);
+    connect(action, &QAction::triggered, this, [&]() {
+        if (m_qmlProducer && m_filter && currentParameter() >= 0) {
+            auto position = m_qmlProducer->position() - m_filter->in() - m_qmlProducer->in();
+            auto parameterIndex = currentParameter();
+            if (m_model.isKeyframe(parameterIndex, position)) {
+                auto keyframeIndex = m_model.keyframeIndex(parameterIndex, position);
+                m_model.remove(parameterIndex, keyframeIndex);
+            } else {
+                m_model.addKeyframe(parameterIndex, position);
+            }
+        }
+    });
+    connect(this, &KeyframesDock::newFilter, action, [ = ]() {
+        action->setEnabled(m_qmlProducer && m_filter);
+    });
+    m_actions[action->objectName()] = action;
 }
 
 int KeyframesDock::seekPrevious()
@@ -73,7 +392,7 @@ int KeyframesDock::seekPrevious()
         position -= m_qmlProducer->in();
         m_qmlProducer->setPosition(position);
         return m_model.keyframeIndex(currentParameter(),
-                                     position + m_qmlProducer->in() - MAIN.filterController()->currentFilter()->in());
+                                     position + m_qmlProducer->in() - m_filter->in());
     }
     return 0;
 }
@@ -89,24 +408,27 @@ int KeyframesDock::seekNext()
         else
             position = m_qmlProducer->position();
         return m_model.keyframeIndex(currentParameter(),
-                                     position + m_qmlProducer->in() - MAIN.filterController()->currentFilter()->in());
+                                     position + m_qmlProducer->in() - m_filter->in());
     }
     return 0;
 }
 
 void KeyframesDock::setCurrentFilter(QmlFilter *filter, QmlMetadata *meta)
 {
-    if (!filter || !filter->producer().is_valid()) {
-        filter = &m_emptyQmlFilter;
-        meta = &m_emptyQmlMetadata;
+    m_filter = filter;
+    m_metadata = meta;
+    if (!m_filter || !m_filter->producer().is_valid()) {
+        m_filter = &m_emptyQmlFilter;
+        m_metadata = &m_emptyQmlMetadata;
     }
-    m_model.load(filter, meta);
+    m_model.load(m_filter, m_metadata);
     disconnect(this, SIGNAL(changed()));
-    connect(filter, SIGNAL(changed()), SIGNAL(changed()));
-    connect(filter, SIGNAL(changed(QString)), &m_model, SLOT(onFilterChanged(QString)));
-    connect(filter, SIGNAL(animateInChanged()), &m_model, SLOT(reload()));
-    connect(filter, SIGNAL(animateOutChanged()), &m_model, SLOT(reload()));
-    connect(filter, SIGNAL(inChanged(int)), &m_model, SLOT(onFilterInChanged(int)));
+    connect(m_filter, SIGNAL(changed()), SIGNAL(changed()));
+    connect(m_filter, SIGNAL(changed(QString)), &m_model, SLOT(onFilterChanged(QString)));
+    connect(m_filter, SIGNAL(animateInChanged()), &m_model, SLOT(reload()));
+    connect(m_filter, SIGNAL(animateOutChanged()), &m_model, SLOT(reload()));
+    connect(m_filter, SIGNAL(inChanged(int)), &m_model, SLOT(onFilterInChanged(int)));
+    emit newFilter();
 }
 
 bool KeyframesDock::event(QEvent *event)
@@ -163,6 +485,11 @@ void KeyframesDock::load(bool force)
         m_qview.quickWindow()->setColor(palette().window().color());
         QUrl source = QUrl::fromLocalFile(viewPath.absoluteFilePath("keyframes.qml"));
         m_qview.setSource(source);
+        connect(m_qview.rootObject(), SIGNAL(timeScaleChanged()), this, SIGNAL(timeScaleChanged()));
+        connect(m_qview.rootObject(), SIGNAL(rightClicked()),  this, SLOT(onDockRightClicked()));
+        connect(m_qview.rootObject(), SIGNAL(keyframeRightClicked()),  this,
+                SLOT(onKeyframeRightClicked()));
+        connect(m_qview.rootObject(), SIGNAL(clipRightClicked()),  this, SLOT(onClipRightClicked()));
     }
 }
 
@@ -171,4 +498,19 @@ void KeyframesDock::onProducerModified()
     // The clip name may have changed.
     if (m_qmlProducer)
         emit m_qmlProducer->producerChanged();
+}
+
+void KeyframesDock::onDockRightClicked()
+{
+    m_mainMenu->popup(QCursor::pos());
+}
+
+void KeyframesDock::onKeyframeRightClicked()
+{
+    m_keyMenu->popup(QCursor::pos());
+}
+
+void KeyframesDock::onClipRightClicked()
+{
+    m_clipMenu->popup(QCursor::pos());
 }

src/CMakeLists.txt
@@ -46,7 +46,6 @@ add_executable(shotcut WIN32 MACOSX_BUNDLE
   docks/recentdock.ui
   docks/scopedock.cpp docks/scopedock.h
   docks/timelinedock.cpp docks/timelinedock.h
-  docks/timelinedock.ui
   glwidget.cpp glwidget.h
   jobqueue.cpp jobqueue.h
   jobs/abstractjob.cpp jobs/abstractjob.h
@@ -121,6 +120,7 @@ add_executable(shotcut WIN32 MACOSX_BUNDLE
   widgets/decklinkproducerwidget.ui
   widgets/directshowvideowidget.cpp widgets/directshowvideowidget.h
   widgets/directshowvideowidget.ui
+  widgets/docktoolbar.cpp widgets/docktoolbar.h
   widgets/editmarkerwidget.cpp widgets/editmarkerwidget.h
   widgets/exportpresetstreeview.cpp widgets/exportpresetstreeview.h
   widgets/frameratewidget.cpp widgets/frameratewidget.h

src/docks/jobsdock.ui
@@ -99,6 +99,17 @@
        <number>6</number>
       </property>
       <item>
+       <widget class="QPushButton" name="menuButton">
+        <property name="toolTip">
+         <string>Jobs Menu</string>
+        </property>
+        <property name="icon">
+         <iconset theme="show-menu" resource="../../icons/resources.qrc">
+          <normaloff>:/icons/light/32x32/format-justify-fill.png</normaloff>:/icons/light/32x32/show-menu.png</iconset>
+        </property>
+       </widget>
+      </item>
+      <item>
        <widget class="QPushButton" name="pauseButton">
         <property name="toolTip">
          <string>Stop automatically processing the next pending job in
@@ -120,17 +131,6 @@ the list. This does not stop a currently running job. Right-
         </property>
         <property name="text">
          <string>Clean</string>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <widget class="QPushButton" name="menuButton">
-        <property name="toolTip">
-         <string>Jobs Menu</string>
-        </property>
-        <property name="icon">
-         <iconset theme="show-menu" resource="../../icons/resources.qrc">
-          <normaloff>:/icons/light/32x32/format-justify-fill.png</normaloff>:/icons/light/32x32/show-menu.png</iconset>
         </property>
        </widget>
       </item>

src/docks/keyframesdock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 Meltytech, LLC
+ * Copyright (c) 2016-2022 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,6 +32,7 @@ class QmlMetadata;
 class MetadataModel;
 class AttachedFiltersModel;
 class QmlProducer;
+class QMenu;
 
 class KeyframesDock : public QDockWidget
 {
@@ -50,12 +51,16 @@ public:
 
 signals:
     void changed(); /// Notifies when a filter parameter changes.
+    void setZoom(double value);
     void zoomIn();
     void zoomOut();
     void zoomToFit();
     void resetZoom();
     void seekPreviousSimple();
     void seekNextSimple();
+    void newFilter(); // Notifies when the filter itself has been changed
+    void timeScaleChanged();
+    void dockClicked();
 
 public slots:
     void setCurrentFilter(QmlFilter *filter, QmlMetadata *meta);
@@ -69,13 +74,21 @@ protected:
 
 private slots:
     void onVisibilityChanged(bool visible);
+    void onDockRightClicked();
+    void onKeyframeRightClicked();
+    void onClipRightClicked();
 
 private:
+    void setupActions();
     QQuickWidget m_qview;
-    QmlMetadata m_emptyQmlMetadata;
-    QmlFilter m_emptyQmlFilter;
     KeyframesModel m_model;
+    QmlMetadata *m_metadata;
+    QmlFilter *m_filter;
     QmlProducer *m_qmlProducer;
+    QHash<QString, QAction *> m_actions;
+    QMenu *m_mainMenu;
+    QMenu *m_keyMenu;
+    QMenu *m_clipMenu;
 };
 
 #endif // KEYFRAMESDOCK_H

src/docks/markersdock.cpp
@@ -157,37 +157,6 @@ MarkersDock::MarkersDock(QWidget *parent) :
     QHBoxLayout *buttonLayout = new QHBoxLayout();
     vboxLayout->addLayout(buttonLayout);
 
-    m_addButton = new QToolButton(this);
-    m_addButton->setIcon(QIcon::fromTheme("list-add",
-                                          QIcon(":/icons/oxygen/32x32/actions/list-add.png")));
-    m_addButton->setMaximumSize(22, 22);
-    m_addButton->setToolTip(tr("Add a marker at the current time"));
-    m_addButton->setAutoRaise(true);
-    if (!connect(m_addButton, &QAbstractButton::clicked, this, &MarkersDock::onAddRequested))
-        connect(m_addButton, SIGNAL(clicked()), SLOT(onAddRequested()));
-    buttonLayout->addWidget(m_addButton);
-
-    m_removeButton = new QToolButton(this);
-    m_removeButton->setIcon(QIcon::fromTheme("list-remove",
-                                             QIcon(":/icons/oxygen/32x32/actions/list-remove.png")));
-    m_removeButton->setMaximumSize(22, 22);
-    m_removeButton->setToolTip(tr("Remove the selected marker"));
-    m_removeButton->setAutoRaise(true);
-    if (!connect(m_removeButton, &QAbstractButton::clicked, this, &MarkersDock::onRemoveRequested))
-        connect(m_removeButton, SIGNAL(clicked()), SLOT(onRemoveRequested()));
-    buttonLayout->addWidget(m_removeButton);
-
-    m_clearButton = new QToolButton(this);
-    m_clearButton->setIcon(QIcon::fromTheme("window-close",
-                                            QIcon(":/icons/oxygen/32x32/actions/window-close.png")));
-    m_clearButton->setMaximumSize(22, 22);
-    m_clearButton->setToolTip(tr("Deselect the marker"));
-    m_clearButton->setAutoRaise(true);
-    if (!connect(m_clearButton, &QAbstractButton::clicked, this,
-                 &MarkersDock::onClearSelectionRequested))
-        connect(m_clearButton, SIGNAL(clicked()), SLOT(onClearSelectionRequested()));
-    buttonLayout->addWidget(m_clearButton);
-
     m_moreButton = new QToolButton(this);
     m_moreButton->setIcon(QIcon::fromTheme("show-menu",
                                            QIcon(":/icons/oxygen/32x32/actions/show-menu.png")));
@@ -221,6 +190,37 @@ MarkersDock::MarkersDock(QWidget *parent) :
     m_moreButton->setMenu(moreMenu);
     m_moreButton->setPopupMode(QToolButton::QToolButton::InstantPopup);
     buttonLayout->addWidget(m_moreButton);
+
+    m_addButton = new QToolButton(this);
+    m_addButton->setIcon(QIcon::fromTheme("list-add",
+                                          QIcon(":/icons/oxygen/32x32/actions/list-add.png")));
+    m_addButton->setMaximumSize(22, 22);
+    m_addButton->setToolTip(tr("Add a marker at the current time"));
+    m_addButton->setAutoRaise(true);
+    if (!connect(m_addButton, &QAbstractButton::clicked, this, &MarkersDock::onAddRequested))
+        connect(m_addButton, SIGNAL(clicked()), SLOT(onAddRequested()));
+    buttonLayout->addWidget(m_addButton);
+
+    m_removeButton = new QToolButton(this);
+    m_removeButton->setIcon(QIcon::fromTheme("list-remove",
+                                             QIcon(":/icons/oxygen/32x32/actions/list-remove.png")));
+    m_removeButton->setMaximumSize(22, 22);
+    m_removeButton->setToolTip(tr("Remove the selected marker"));
+    m_removeButton->setAutoRaise(true);
+    if (!connect(m_removeButton, &QAbstractButton::clicked, this, &MarkersDock::onRemoveRequested))
+        connect(m_removeButton, SIGNAL(clicked()), SLOT(onRemoveRequested()));
+    buttonLayout->addWidget(m_removeButton);
+
+    m_clearButton = new QToolButton(this);
+    m_clearButton->setIcon(QIcon::fromTheme("window-close",
+                                            QIcon(":/icons/oxygen/32x32/actions/window-close.png")));
+    m_clearButton->setMaximumSize(22, 22);
+    m_clearButton->setToolTip(tr("Deselect the marker"));
+    m_clearButton->setAutoRaise(true);
+    if (!connect(m_clearButton, &QAbstractButton::clicked, this,
+                 &MarkersDock::onClearSelectionRequested))
+        connect(m_clearButton, SIGNAL(clicked()), SLOT(onClearSelectionRequested()));
+    buttonLayout->addWidget(m_clearButton);
 
     m_searchField = new QLineEdit(this);
     m_searchField->setPlaceholderText(tr("search"));

commit 74139d78e58c23cef7a72a6e17145a87752e8218

fix toggle keyframe regression in 783b8112

src/docks/keyframesdock.cpp
@@ -359,7 +359,7 @@ void KeyframesDock::setupActions()
     action->setEnabled(m_qmlProducer && m_filter);
     connect(action, &QAction::triggered, this, [&]() {
         if (m_qmlProducer && m_filter && currentParameter() >= 0) {
-            auto position = m_qmlProducer->position() - m_filter->in() - m_qmlProducer->in();
+            auto position = m_qmlProducer->position() - (m_filter->in() - m_qmlProducer->in());
             auto parameterIndex = currentParameter();
             if (m_model.isKeyframe(parameterIndex, position)) {
                 auto keyframeIndex = m_model.keyframeIndex(parameterIndex, position);