mltframework/shotcut

commit 2905e65584a582d6f69ee2e604a788e34feee73d

fix Constrained VBR preset for nvenc

src/docks/encodedock.cpp
@@ -278,14 +278,10 @@ void EncodeDock::loadPresetFromProperties(Mlt::Properties &preset)
                 ui->audioRateControlCombo->setCurrentIndex(RateControlAverage);
             else
                 ui->audioRateControlCombo->setCurrentIndex(RateControlQuality);
-        } else if (name == "vq" || name == "vglobal_quality") {
+        } else if (name == "vq" || name == "vglobal_quality" || name == "qmin" || name == "qscale") {
             ui->videoRateControlCombo->setCurrentIndex(preset.get("vbufsize") ? RateControlConstrained :
                                                        RateControlQuality);
             videoQuality = preset.get_int(name.toUtf8().constData());
-        } else if (name == "qscale") {
-            ui->videoRateControlCombo->setCurrentIndex(preset.get("vbufsize") ? RateControlConstrained :
-                                                       RateControlQuality);
-            videoQuality = preset.get_int("qscale");
         } else if (name == "crf") {
             ui->videoRateControlCombo->setCurrentIndex(preset.get("vbufsize") ? RateControlConstrained
                                                        : ui->videoCodecCombo->currentText().endsWith("_ videotoolbox") ? RateControlAverage :
@@ -293,7 +289,7 @@ void EncodeDock::loadPresetFromProperties(Mlt::Properties &preset)
             videoQuality = preset.get_int("crf");
         } else if (name == "bufsize" || name == "vbufsize") {
             // traditionally "bufsize" means video only
-            if (preset.get("vq") || preset.get("qscale") || preset.get("crf"))
+            if (preset.get("vq") || preset.get("qmin") || preset.get("qscale") || preset.get("crf"))
                 ui->videoRateControlCombo->setCurrentIndex(RateControlConstrained);
             else
                 ui->videoRateControlCombo->setCurrentIndex(RateControlConstant);

commit 1e2d2f9878e5083dc27e904f345fb30bf38cdd8e

fix #1342 export preset regression in 2905e65
This affected any preset not for NVENC or AMF hardware encoder that uses
"qmin" including the stock VP9, D10, and XDCAM.

src/docks/encodedock.cpp
@@ -167,6 +167,7 @@ void EncodeDock::loadPresetFromProperties(Mlt::Properties &preset)
             other.append("vtag=hvc1");
         }
     }
+    bool qmin_nvenc_amf = preset.get("qmin") && (vcodec.contains("nvenc") || vcodec.endsWith("_amf"));
 
     for (int i = 0; i < preset.count(); i++) {
         QString name(preset.get_name(i));
@@ -278,7 +279,7 @@ void EncodeDock::loadPresetFromProperties(Mlt::Properties &preset)
                 ui->audioRateControlCombo->setCurrentIndex(RateControlAverage);
             else
                 ui->audioRateControlCombo->setCurrentIndex(RateControlQuality);
-        } else if (name == "vq" || name == "vglobal_quality" || name == "qmin" || name == "qscale") {
+        } else if (name == "vq" || name == "vglobal_quality" || name == "qscale" || qmin_nvenc_amf) {
             ui->videoRateControlCombo->setCurrentIndex(preset.get("vbufsize") ? RateControlConstrained :
                                                        RateControlQuality);
             videoQuality = preset.get_int(name.toUtf8().constData());
@@ -289,7 +290,7 @@ void EncodeDock::loadPresetFromProperties(Mlt::Properties &preset)
             videoQuality = preset.get_int("crf");
         } else if (name == "bufsize" || name == "vbufsize") {
             // traditionally "bufsize" means video only
-            if (preset.get("vq") || preset.get("qmin") || preset.get("qscale") || preset.get("crf"))
+            if (preset.get("vq") || preset.get("qscale") || preset.get("crf") || qmin_nvenc_amf)
                 ui->videoRateControlCombo->setCurrentIndex(RateControlConstrained);
             else
                 ui->videoRateControlCombo->setCurrentIndex(RateControlConstant);