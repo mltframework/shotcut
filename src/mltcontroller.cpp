mltframework/shotcut

commit fe9b6d1a99e02c574a66c4928cb838096084b52d

fix open MLT XML with '%' in path or name

src/mltcontroller.cpp
@@ -116,12 +116,17 @@ int Controller::open(const QString &url, const QString &urlToSave)
 
     close();
 
+    auto myUrl = url;
+    if (url.endsWith(".mlt")) {
+        // MLT xml producer does URL encoding; so if the URL contains % it must be encoded.
+        myUrl = QUrl::toPercentEncoding(url).constData();
+    }
     if (Settings.playerGPU() && !profile().is_explicit())
         // Prevent loading normalizing filters, which might be Movit ones that
         // may not have a proper OpenGL context when requesting a sample frame.
-        newProducer = new Mlt::Producer(profile(), "abnormal", url.toUtf8().constData());
+        newProducer = new Mlt::Producer(profile(), "abnormal", myUrl.toUtf8().constData());
     else
-        newProducer = new Mlt::Producer(profile(), url.toUtf8().constData());
+        newProducer = new Mlt::Producer(profile(), myUrl.toUtf8().constData());
     if (newProducer && newProducer->is_valid()) {
         double fps = profile().fps();
         if (!profile().is_explicit()) {
@@ -148,7 +153,7 @@ int Controller::open(const QString &url, const QString &urlToSave)
         if (isFpsDifferent(profile().fps(), fps) || (Settings.playerGPU() && !profile().is_explicit())) {
             // Reload with correct FPS or with Movit normalizing filters attached.
             delete newProducer;
-            newProducer = new Mlt::Producer(profile(), url.toUtf8().constData());
+            newProducer = new Mlt::Producer(profile(), myUrl.toUtf8().constData());
         }
         if (m_url.isEmpty() && QString(newProducer->get("xml")) == "was here") {
             if (newProducer->get_int("_original_type") != mlt_service_tractor_type ||
@@ -172,7 +177,12 @@ bool Controller::openXML(const QString &filename)
 {
     bool error = true;
     close();
-    Producer *producer = new Mlt::Producer(profile(), nullptr, filename.toUtf8().constData());
+    auto myFilename = filename;
+    if (myFilename.endsWith(".mlt")) {
+        // MLT xml producer does URL encoding; so if the URL contains % it must be encoded.
+        myFilename = QUrl::toPercentEncoding(filename).constData();
+    }
+    Producer *producer = new Mlt::Producer(profile(), myFilename.toUtf8().constData());
     if (producer->is_valid()) {
         double fps = profile().fps();
         if (!profile().is_explicit()) {
@@ -185,10 +195,10 @@ bool Controller::openXML(const QString &filename)
         if (isFpsDifferent(profile().fps(), fps)) {
             // reopen with the correct fps
             delete producer;
-            producer = new Mlt::Producer(profile(), nullptr, filename.toUtf8().constData());
+            producer = new Mlt::Producer(profile(), nullptr, myFilename.toUtf8().constData());
         }
         producer->set(kShotcutVirtualClip, 1);
-        producer->set("resource", filename.toUtf8().constData());
+        producer->set("resource", myFilename.toUtf8().constData());
         setProducer(new Producer(producer));
         error = false;
     }

src/mainwindow.cpp
@@ -1626,6 +1626,7 @@ bool MainWindow::open(QString url, const Mlt::Properties *properties, bool play)
     return true;
 }
 
+// This one is invoked from the command line.
 void MainWindow::openMultiple(const QStringList &paths)
 {
     if (paths.size() > 1) {
@@ -1638,6 +1639,7 @@ void MainWindow::openMultiple(const QStringList &paths)
     }
 }
 
+// This one is invoked from above (command line) or drag-n-drop.
 void MainWindow::openMultiple(const QList<QUrl> &urls)
 {
     if (urls.size() > 1) {
@@ -1650,6 +1652,7 @@ void MainWindow::openMultiple(const QList<QUrl> &urls)
     }
 }
 
+// This is one is invoked from the action.
 void MainWindow::openVideo()
 {
     QString path = Settings.openPath();

commit 287e3fae68c3509e3eddce934af91bcc3c9a3744

fix MLT XML as virtual clip broken by fe9b6d1
https://forum.shotcut.org/t/open-mlt-xml-as-clip-broken-path/36282

MLT XML files containing '%' are only supported for direct open now and
not embedding.

src/mltcontroller.cpp
@@ -118,7 +118,7 @@ int Controller::open(const QString &url, const QString &urlToSave)
 
     auto myUrl = url;
     if (url.endsWith(".mlt")) {
-        // MLT xml producer does URL encoding; so if the URL contains % it must be encoded.
+        // MLT xml producer does URL decoding; so if the URL contains % it must be encoded.
         myUrl = QUrl::toPercentEncoding(url).constData();
     }
     if (Settings.playerGPU() && !profile().is_explicit())
@@ -177,12 +177,7 @@ bool Controller::openXML(const QString &filename)
 {
     bool error = true;
     close();
-    auto myFilename = filename;
-    if (myFilename.endsWith(".mlt")) {
-        // MLT xml producer does URL encoding; so if the URL contains % it must be encoded.
-        myFilename = QUrl::toPercentEncoding(filename).constData();
-    }
-    Producer *producer = new Mlt::Producer(profile(), myFilename.toUtf8().constData());
+    Producer *producer = new Mlt::Producer(profile(), filename.toUtf8().constData());
     if (producer->is_valid()) {
         double fps = profile().fps();
         if (!profile().is_explicit()) {
@@ -195,10 +190,10 @@ bool Controller::openXML(const QString &filename)
         if (isFpsDifferent(profile().fps(), fps)) {
             // reopen with the correct fps
             delete producer;
-            producer = new Mlt::Producer(profile(), nullptr, myFilename.toUtf8().constData());
+            producer = new Mlt::Producer(profile(), filename.toUtf8().constData());
         }
         producer->set(kShotcutVirtualClip, 1);
-        producer->set("resource", myFilename.toUtf8().constData());
+        producer->set("resource", filename.toUtf8().constData());
         setProducer(new Producer(producer));
         error = false;
     }