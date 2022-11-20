mltframework/shotcut

commit 872ece3b75326e3de200fbb3fa6c2bf926526ac5

Use typeof to check for undefined in JavaScript

src/qml/views/timeline/timeline.qml
@@ -470,8 +470,8 @@ Rectangle {
                                 model: timeline.selection
                                 Rectangle {
                                     property var clip: trackAt(modelData.y).clipAt(modelData.x)
-                                    property var track: clip == undefined ? trackAt(clip.trackIndex + dragDelta.y) : 0
-                                    x: clip == undefined ? clip.x + dragDelta.x : 0
+                                    property var track: typeof clip === 'undefined' ? trackAt(clip.trackIndex + dragDelta.y) : 0
+                                    x: typeof clip === 'undefined' ? clip.x + dragDelta.x : 0
                                     y: track ? track.y : 0
                                     width: clip.width
                                     height: track ? track.height : 0

src/qml/filters/timer/ClockSpinner.qml
@@ -41,11 +41,11 @@ Item {
 
     function timeToSeconds(time) {
         var fields = time.split(":")
-        if (fields[1] == undefined)
+        if (typeof fields[1] === 'undefined')
             fields[1] = 0
-        if (fields[0] == undefined)
+        if (typeof fields[0] === 'undefined')
             fields[0] = 0
-        if (fields[2] == undefined)
+        if (typeof fields[2] === 'undefined')
             fields[2] = 0
         var hSeconds = fields[0] * 60.0 * 60.0
         var mSeconds = fields[1] * 60.0
@@ -75,7 +75,7 @@ Item {
         if (fractionLen == 0)
             return b[0]
         // Add trailing zeros to the fraction part (after decimal)
-        if (b[1] == undefined)
+        if (typeof b[1] === 'undefined')
             b[1] = ""
         while (b[1].length < fractionLen) {
             b[1] = b[1] + "0"

src/qml/modules/Shotcut/Controls/GradientControl.qml
@@ -47,7 +47,7 @@ RowLayout {
                stopIndex == colorList.length - 1 ? parent.width - width :
                stopIndex * (parent.width / (colorList.length - 1)) - (width / 2)
             y: 0
-            color: colorList[stopIndex] != undefined ? colorList[stopIndex] : "gray"
+            color: typeof colorList[stopIndex] !== 'undefined' ? colorList[stopIndex] : "gray"
             border.color: "gray"
             border.width: 1
             width: 10

src/qml/views/timeline/Track.qml
@@ -66,25 +66,25 @@ Rectangle {
     DelegateModel {
         id: trackModel
         Clip {
-            clipName: model.name != undefined ? model.name : ""
-            clipComment: model.comment != undefined ? model.comment : ""
-            clipResource: model.resource != undefined ? model.resource : ""
-            clipDuration: model.duration != undefined ? model.duration : 0
-            mltService: model.mlt_service != undefined ? model.mlt_service : ""
-            inPoint: model.in != undefined ? model.in : 0
-            outPoint: model.out != undefined ? model.out : 0
-            isBlank: model.blank != undefined ? model.blank : false
-            isAudio: model.audio != undefined ? model.audio : false
-            isTransition: model.isTransition != undefined ? model.isTransition : false
-            audioLevels: model.audioLevels != undefined ? model.audioLevels : ""
-            width: model.duration != undefined ? model.duration * timeScale : 0
+            clipName: typeof model.name !== 'undefined' ? model.name : ""
+            clipComment: typeof model.comment !== 'undefined' ? model.comment : ""
+            clipResource: typeof model.resource !== 'undefined' ? model.resource : ""
+            clipDuration: typeof model.duration !== 'undefined' ? model.duration : 0
+            mltService: typeof model.mlt_service !== 'undefined' ? model.mlt_service : ""
+            inPoint: typeof model.in !== 'undefined' ? model.in : 0
+            outPoint: typeof model.out !== 'undefined' ? model.out : 0
+            isBlank: typeof model.blank !== 'undefined' ? model.blank : false
+            isAudio: typeof model.audio !== 'undefined' ? model.audio : false
+            isTransition: typeof model.isTransition !== 'undefined' ? model.isTransition : false
+            audioLevels: typeof model.audioLevels !== 'undefined' ? model.audioLevels : ""
+            width: typeof model.duration !== 'undefined' ? model.duration * timeScale : 0
             height: trackRoot.height
             trackIndex: trackRoot.DelegateModel.itemsIndex
-            fadeIn: model.fadeIn != undefined ? model.fadeIn : 0
-            fadeOut: model.fadeOut != undefined ? model.fadeOut : 0
-            hash: model.hash != undefined ? model.hash : 0
-            speed: model.speed != undefined ? model.speed : 1
-            audioIndex: model.audioindex != undefined ? model.audioIndex : 0
+            fadeIn: typeof model.fadeIn !== 'undefined' ? model.fadeIn : 0
+            fadeOut: typeof model.fadeOut !== 'undefined' ? model.fadeOut : 0
+            hash: typeof model.hash !== 'undefined' ? model.hash : 0
+            speed: typeof model.speed !== 'undefined' ? model.speed : 1
+            audioIndex: typeof model.audioindex !== 'undefined' ? model.audioIndex : 0
             selected: Logic.selectionContains(timeline.selection, trackIndex, index)
             isTrackMute: trackRoot.isMute
 

commit c7c64f9dfc4e77c39140733b7f7d09fa29834d49

fix timeline multi-selection outlines when dragging
https://forum.shotcut.org/t/move-selected-clips/36331

regression in 872ece3

src/qml/views/timeline/timeline.qml
@@ -539,9 +539,9 @@ Rectangle {
 
                                 Rectangle {
                                     property var clip: trackAt(modelData.y).clipAt(modelData.x)
-                                    property var track: typeof clip === 'undefined' ? trackAt(clip.trackIndex + dragDelta.y) : 0
+                                    property var track: typeof clip !== 'undefined' && typeof dragDelta !== 'undefined' ? trackAt(clip.trackIndex + dragDelta.y) : 0
 
-                                    x: typeof clip === 'undefined' ? clip.x + dragDelta.x : 0
+                                    x: clip && typeof dragDelta !== 'undefined' ? clip.x + dragDelta.x : 0
                                     y: track ? track.y : 0
                                     width: clip.width
                                     height: track ? track.height : 0