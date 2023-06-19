bea706793801c73ad0470742c5770aef2b2b8a5d
Dan Dennedy <dan@dennedy.org>
2023-06-19 14:35:08 -0700

    use `notarytool` on macOS to notorize
    `altool` is deprecated.

--- scripts/notarize.sh
+++ scripts/notarize.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xcrun altool --notarize-app --primary-bundle-id "$(basename $1)" --username "dan@dennedy.org" --password "@keychain:altool" --file "$1"
-
+xcrun notarytool submit --wait --keychain-profile notarytool "$1"
