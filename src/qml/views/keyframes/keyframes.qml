mltframework/shotcut

commit 585a77c2cfad82e23f218af0b6680ebd04d4f7eb

also use continuous scroll when skimming

src/qml/views/keyframes/keyframes.qml
@@ -38,7 +38,7 @@ Rectangle {
     signal clipRightClicked
 
     function redrawWaveforms() {
-        Logic.scrollIfNeeded();
+        Logic.scrollIfNeeded(settings.timelineCenterPlayhead);
         beforeClip.generateWaveform();
         activeClip.generateWaveform();
         afterClip.generateWaveform();
@@ -574,7 +574,7 @@ Rectangle {
     Connections {
         function onPositionChanged() {
             if (!stopScrolling)
-                Logic.scrollIfNeeded(false, tracksAreaMouse.containsPress);
+                Logic.scrollIfNeeded(false, tracksAreaMouse.containsPress || tracksAreaMouse.skim);
         }
 
         target: producer

src/qml/views/timeline/timeline.qml
@@ -776,7 +776,7 @@ Rectangle {
     Connections {
         function onPositionChanged() {
             if (!stopScrolling)
-                Logic.scrollIfNeeded(false, scrubMouseArea.containsPress);
+                Logic.scrollIfNeeded(false, scrubMouseArea.containsPress || scrubMouseArea.skim);
         }
 
         function onDragging(pos, duration) {
@@ -845,7 +845,7 @@ Rectangle {
     Connections {
         function onScaleFactorChanged() {
             if (settings.timelineCenterPlayhead)
-                Logic.scrollIfNeeded();
+                Logic.scrollIfNeeded(true);
         }
 
         target: multitrack

src/qml/views/keyframes/Keyframes.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2021 Meltytech, LLC
+ * Copyright (c) 2017-2023 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@ function trackHeight(isCurves) {
 function scrollIfNeeded(center, continuous) {
     var x = producer.position * timeScale;
     if (!tracksFlickable) return;
-    if (settings.timelineCenterPlayhead || center) {
+    if (center) {
         if (x > tracksFlickable.contentX + tracksFlickable.width * 0.5)
             tracksFlickable.contentX = x - tracksFlickable.width * 0.5;
         else if (x < tracksFlickable.width * 0.5)

src/qml/views/timeline/Timeline.js
@@ -18,7 +18,7 @@
 function scrollIfNeeded(center, continouous) {
     var x = timeline.position * multitrack.scaleFactor;
     if (!tracksFlickable) return;
-    if (settings.timelineCenterPlayhead || center) {
+    if (center) {
         if (x > tracksFlickable.contentX + tracksFlickable.width * 0.5)
             tracksFlickable.contentX = x - tracksFlickable.width * 0.5;
         else if (x < tracksFlickable.width * 0.5)

commit a6d03ff7f04dd8d2c5187fb690709bca6206b704

fix Settings > Timeline > Center the Playhead
regression in 585a77c

src/qml/views/keyframes/keyframes.qml
@@ -574,7 +574,7 @@ Rectangle {
     Connections {
         function onPositionChanged() {
             if (!stopScrolling)
-                Logic.scrollIfNeeded(false, tracksAreaMouse.containsPress || tracksAreaMouse.skim);
+                Logic.scrollIfNeeded(settings.timelineCenterPlayhead, tracksAreaMouse.containsPress || tracksAreaMouse.skim);
         }
 
         target: producer

src/qml/views/timeline/timeline.qml
@@ -776,7 +776,7 @@ Rectangle {
     Connections {
         function onPositionChanged() {
             if (!stopScrolling)
-                Logic.scrollIfNeeded(false, scrubMouseArea.containsPress || scrubMouseArea.skim);
+                Logic.scrollIfNeeded(settings.timelineCenterPlayhead, scrubMouseArea.containsPress || scrubMouseArea.skim);
         }
 
         function onDragging(pos, duration) {