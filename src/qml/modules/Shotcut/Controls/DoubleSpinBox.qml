mltframework/shotcut

commit af61997721eb9849b8b3e2da2137159e68176b2c

fix VUI not does not update on up/down key
Also, add support for Ctrl+Wheel to spin Quick numeric fields by larger
amount - just like their QWidgets counterpart.

src/qml/modules/Shotcut/Controls/DoubleSpinBox.qml
@@ -174,13 +174,27 @@ Item {
                     text = spinbox.textFromValue(spinbox.value, spinbox.locale);
                     _blockTextUpdate = false;
                 }
-                Keys.onUpPressed: spinbox.increase()
-                Keys.onDownPressed: spinbox.decrease()
+                Keys.onUpPressed: {
+                    spinbox.increase();
+                    root.valueModified();
+                }
+                Keys.onDownPressed: {
+                    spinbox.decrease();
+                    root.valueModified();
+                }
 
                 MouseArea {
                     acceptedButtons: Qt.RightButton
                     anchors.fill: parent
                     onClicked: contextMenu.popup()
+                    onWheel: wheel => {
+                        spinbox.stepSize = root.stepSize * _factor * (wheel.modifiers & Qt.ControlModifier ? 10 : 1);
+                        if (wheel.angleDelta.y > 0)
+                            spinbox.increase();
+                        else
+                            spinbox.decrease();
+                        root.valueModified();
+                    }
                 }
 
                 Shotcut.EditMenu {

commit c4beae6ceb4d54f72c20c19d082dd94e9589535f

fix numeric field increment regression in af619977

src/qml/modules/Shotcut/Controls/DoubleSpinBox.qml
@@ -194,6 +194,7 @@ Item {
                         else
                             spinbox.decrease();
                         root.valueModified();
+                        spinbox.stepSize = root.stepSize * _factor;
                     }
                 }
 