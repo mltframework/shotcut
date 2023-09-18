mltframework/shotcut

commit b2b34f822c0cd5479054ef1adbbd2d70a06f9f30

fix Properties of saved proxy project when proxy off
Steps to reproduce:

1. turn on proxy
2. save timeline project
3. restart
4. turn off proxy
5. open project
6. check clip Properties and they may reflect the proxy instead of
original

Or

1. turn on proxy
2. load project of clips that already have proxies
3. turn off proxy
4. check clip Properties and they may reflect the proxy instead of
original

The solution is to save the "shotcut:proxy" property, clear it when
proxy is off, and discard all meta properties in MLT XML when
inconsistent.

src/mltxmlchecker.cpp
@@ -214,6 +214,8 @@ void MltXmlChecker::processProperties()
             mlt_service = p.second;
         } else if (p.first == kShotcutHashProperty) {
             m_resource.hash = p.second;
+        } else if (p.first == kIsProxyProperty) {
+            m_resource.isProxy = true;
         } else if (isNumericProperty(p.first)) {
             checkNumericString(p.second);
         } else if (p.first == "resource" && mlt_service == "webvfx" && fixWebVfxPath(p.second)) {
@@ -243,7 +245,8 @@ void MltXmlChecker::processProperties()
         replaceWebVfxCropFilters(mlt_service, newProperties);
         replaceWebVfxChoppyFilter(mlt_service, newProperties);
 #endif
-        if (Settings.proxyEnabled())
+        bool proxyEnabled = Settings.proxyEnabled();
+        if (proxyEnabled)
             checkForProxy(mlt_service, newProperties);
 
         // Second pass: amend property values.
@@ -262,6 +265,10 @@ void MltXmlChecker::processProperties()
                 p.second.clear();
             } else if (p.first == "audio_index" || p.first == "video_index") {
                 fixStreamIndex(p);
+            } else if (!proxyEnabled && m_resource.isProxy && (p.first == kIsProxyProperty ||
+                                                               p.first.startsWith("meta."))) {
+                p.second.clear();
+                m_isUpdated = true;
             }
 
             if (!p.second.isEmpty())
@@ -667,7 +674,7 @@ void MltXmlChecker::checkForProxy(const QString &mlt_service,
                 if (p.first == "resource") {
                     p.second = ProxyManager::GoProProxyFilePath(resource);
                     if (isTimewarp) {
-                        p.second = QString("%1:%2").arg(speed).arg(p.second);
+                        p.second = QString("%1:%2").arg(speed, p.second);
                     }
                     properties << MltProperty(kIsProxyProperty, "1");
                     properties << MltProperty(kOriginalResourceProperty, resource);
@@ -692,7 +699,7 @@ void MltXmlChecker::checkForProxy(const QString &mlt_service,
                         p.second = proxyDir.filePath(fileName);
                     }
                     if (isTimewarp) {
-                        p.second = QString("%1:%2").arg(speed).arg(p.second);
+                        p.second = QString("%1:%2").arg(speed, p.second);
                     }
                     break;
                 }

src/proxymanager.cpp
@@ -228,10 +228,8 @@ void ProxyManager::generateImageProxy(Mlt::Producer &producer, bool replace)
 {
     // Always regenerate per preview scaling or 540 if not specified
     QString resource = ProxyManager::resource(producer);
-    QStringList args;
     QString hash = Util::getHash(producer);
     QString fileName = ProxyManager::dir().filePath(hash + kProxyPendingImageExtension);
-    QString filters;
 
     // Touch file to make it in progress
     QFile file(fileName);
@@ -283,14 +281,14 @@ static void processProperties(QXmlStreamWriter &newXml, QVector<MltProperty> &pr
                     newResource = newResource.mid(root.size());
                 }
                 if (service == "timewarp") {
-                    newProperties << MltProperty(p.first, QString("%1:%2").arg(speed).arg(newResource));
+                    newProperties << MltProperty(p.first, QString("%1:%2").arg(speed, newResource));
                 } else {
                     newProperties << MltProperty(p.first, newResource);
                 }
             } else if (p.first == "warp_resource") {
                 newProperties << MltProperty(p.first, newResource);
                 // Remove special proxy and original resource properties
-            } else if (p.first != kIsProxyProperty && p.first != kOriginalResourceProperty) {
+            } else if (p.first != kOriginalResourceProperty) {
                 newProperties << MltProperty(p.first, p.second);
             }
         }

src/shotcut_mlt_properties.h
@@ -57,6 +57,7 @@
 #define kOriginalOutProperty "shotcut:originalOut"
 #define kDisableProxyProperty "shotcut:disableProxy"
 #define kBackupProperty "shotcut:backup"
+#define kIsProxyProperty "shotcut:proxy"
 
 /* Project specific properties */
 #define kShotcutProjectAudioChannels "shotcut:projectAudioChannels"
@@ -90,7 +91,6 @@
 #define kUuidProperty "_shotcut:uuid"
 #define kMultitrackItemProperty "_shotcut:multitrack-item"
 #define kExportFromProperty "_shotcut:exportFromDefault"
-#define kIsProxyProperty "shotcut:proxy"
 #define kTrackIndexProperty "_shotcut:trackIndex"
 #define kClipIndexProperty "_shotcut:clipIndex"
 #define kShotcutInProperty "_shotcut:in"

src/mltxmlchecker.h
@@ -124,6 +124,7 @@ private:
         QString prefix;
         QString suffix;
         int audio_index, video_index;
+        bool isProxy;
 
         void clear()
         {
@@ -134,6 +135,7 @@ private:
             prefix.clear();
             suffix.clear();
             audio_index = video_index = -1;
+            isProxy = false;
         }
     } m_resource;
     QVersionNumber m_mltVersion;

commit bd2778510496b4585a3cce2fe0c884c771a4487d

backward compatible fix for project saved in proxy mode
regression in b2b34f82 such that opening a project saved with that
change with a previous versions and proxy off shows "(PROXY)" labels in
Timeline and Properties. Instead of saving a "shotcut:proxy" property,
which previous versions do not know to filter out.

src/mltxmlchecker.cpp
@@ -214,7 +214,7 @@ void MltXmlChecker::processProperties()
             mlt_service = p.second;
         } else if (p.first == kShotcutHashProperty) {
             m_resource.hash = p.second;
-        } else if (p.first == kIsProxyProperty) {
+        } else if (p.first.startsWith(kIsProxyProperty)) {
             m_resource.isProxy = true;
         } else if (isNumericProperty(p.first)) {
             checkNumericString(p.second);
@@ -681,6 +681,7 @@ void MltXmlChecker::checkForProxy(const QString &mlt_service,
                         p.second = QString("%1:%2").arg(speed, p.second);
                     }
                     properties << MltProperty(kIsProxyProperty, "1");
+                    properties << MltProperty(kMetaProxyProperty, "1");
                     properties << MltProperty(kOriginalResourceProperty, resource);
                     m_resource.notProxyMeta = !m_resource.isProxy;
                     m_isUpdated = true;
@@ -710,6 +711,7 @@ void MltXmlChecker::checkForProxy(const QString &mlt_service,
                 }
             }
             properties << MltProperty(kIsProxyProperty, "1");
+            properties << MltProperty(kMetaProxyProperty, "1");
             properties << MltProperty(kOriginalResourceProperty, resource);
             m_resource.notProxyMeta = !m_resource.isProxy;
             m_isUpdated = true;
@@ -748,6 +750,7 @@ void MltXmlChecker::checkForProxy(const QString &mlt_service,
                 }
             }
             properties << MltProperty(kIsProxyProperty, "1");
+            properties << MltProperty(kMetaProxyProperty, "1");
             properties << MltProperty(kOriginalResourceProperty, resource);
             m_resource.notProxyMeta = !m_resource.isProxy;
             m_isUpdated = true;

src/proxymanager.cpp
@@ -287,8 +287,8 @@ static void processProperties(QXmlStreamWriter &newXml, QVector<MltProperty> &pr
                 }
             } else if (p.first == "warp_resource") {
                 newProperties << MltProperty(p.first, newResource);
+            } else if (p.first != kIsProxyProperty && p.first != kOriginalResourceProperty) {
                 // Remove special proxy and original resource properties
-            } else if (p.first != kOriginalResourceProperty) {
                 newProperties << MltProperty(p.first, p.second);
             }
         }
@@ -478,6 +478,7 @@ bool ProxyManager::generateIfNotExists(Mlt::Producer &producer, bool replace)
                 auto gopro = GoProProxyFilePath(producer.get("resource"));
                 if (QFile::exists(gopro)) {
                     producer.set(kIsProxyProperty, 1);
+                    producer.set(kMetaProxyProperty, 1);
                     producer.set(kOriginalResourceProperty, producer.get("resource"));
                     producer.set("resource", gopro.toUtf8().constData());
                     return true;
@@ -490,6 +491,7 @@ bool ProxyManager::generateIfNotExists(Mlt::Producer &producer, bool replace)
                 return false;
             }
             producer.set(kIsProxyProperty, 1);
+            producer.set(kMetaProxyProperty, 1);
             producer.set(kOriginalResourceProperty, producer.get("resource"));
             if (projectDir.exists(fileName)) {
                 ::utime(projectDir.filePath(fileName).toUtf8().constData(), nullptr);

src/shotcut_mlt_properties.h
@@ -57,7 +57,9 @@
 #define kOriginalOutProperty "shotcut:originalOut"
 #define kDisableProxyProperty "shotcut:disableProxy"
 #define kBackupProperty "shotcut:backup"
-#define kIsProxyProperty "shotcut:proxy"
+// "shotcut:proxy" is internal only because older versions do not know to hide it.
+// "shotcut:metaProxy" indicates whether the "meta." properties reflect source or proxy.
+#define kMetaProxyProperty "shotcut:proxy.meta"
 
 /* Project specific properties */
 #define kShotcutProjectAudioChannels "shotcut:projectAudioChannels"
@@ -97,6 +99,7 @@
 #define kShotcutOutProperty "_shotcut:out"
 #define kNewTrackIndexProperty "_shotcut:newTrackIndex"
 #define kShotcutFiltersClipboard "shotcut:filtersClipboard"
+#define kIsProxyProperty "shotcut:proxy"
 
 #define kDefaultMltProfile "atsc_1080p_25"
 