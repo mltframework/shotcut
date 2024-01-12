118f9b5a6803fcc05b717c8f4ee7b5db4948ad86
Dan Dennedy <dan@dennedy.org>
2024-01-11 17:22:36 -0800

    fix #1505 filter presets can break param values

--- src/qmltypes/qmlmetadata.cpp
+++ src/qmltypes/qmlmetadata.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2022 Meltytech, LLC
+ * Copyright (c) 2013-2024 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -219,5 +219,6 @@ QmlKeyframesParameter::QmlKeyframesParameter(QObject *parent)
     , m_units("")
     , m_isRectangle(false)
     , m_rangeType(MinMax)
+    , m_isColor(false)
 {
 }
