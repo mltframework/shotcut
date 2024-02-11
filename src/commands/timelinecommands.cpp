mltframework/shotcut

commit 066be7251ec575690a71581f9d01a7d1c4c4a417

Fix selection is incorrect after detach audio
As reported here:
https://forum.shotcut.org/t/detach-audio-grouping-audio-clip-not-immediately-linked-with-the-video-clip/42732/2

src/commands/timelinecommands.cpp
@@ -1685,16 +1685,16 @@ void UpdateCommand::undo()
     m_isFirstRedo = false;
 }
 
-DetachAudioCommand::DetachAudioCommand(MultitrackModel &model, int trackIndex, int clipIndex,
+DetachAudioCommand::DetachAudioCommand(TimelineDock &timeline, int trackIndex, int clipIndex,
                                        int position, const QString &xml, QUndoCommand *parent)
     : QUndoCommand(parent)
-    , m_model(model)
-    , m_trackIndex(qBound(0, trackIndex, qMax(model.rowCount() - 1, 0)))
+    , m_timeline(timeline)
+    , m_trackIndex(qBound(0, trackIndex, qMax(timeline.model()->rowCount() - 1, 0)))
     , m_clipIndex(clipIndex)
     , m_position(position)
     , m_targetTrackIndex(-1)
     , m_xml(xml)
-    , m_undoHelper(m_model)
+    , m_undoHelper(*timeline.model())
     , m_trackAdded(false)
 {
     setText(QObject::tr("Detach Audio"));
@@ -1704,9 +1704,11 @@ void DetachAudioCommand::redo()
 {
     LOG_DEBUG() << "trackIndex" << m_trackIndex << "clipIndex" << m_clipIndex << "position" <<
                 m_position;
+    TimelineSelectionSilencer selectionSilencer(m_timeline);
     Mlt::Producer audioClip(MLT.profile(), "xml-string", m_xml.toUtf8().constData());
     Mlt::Producer videoClip(MLT.profile(), "xml-string", m_xml.toUtf8().constData());
     if (audioClip.is_valid() && videoClip.is_valid()) {
+        auto model = m_timeline.model();
 
         // Disable audio on the video clip.
         videoClip.set("astream", -1);
@@ -1741,9 +1743,9 @@ void DetachAudioCommand::redo()
         }
 
         // Add an audio track if needed.
-        int n = m_model.trackList().size();
+        int n = model->trackList().size();
         for (int i = 0; i < n; i++) {
-            QScopedPointer<Mlt::Producer> track(m_model.tractor()->track(m_model.trackList()[i].mlt_index));
+            QScopedPointer<Mlt::Producer> track(model->tractor()->track(model->trackList()[i].mlt_index));
             if (!track->is_valid())
                 continue;
             if (track->get(kAudioTrackProperty)) {
@@ -1760,14 +1762,14 @@ void DetachAudioCommand::redo()
         }
         if (m_targetTrackIndex == -1) {
             // No target audio track
-            m_targetTrackIndex = m_model.addAudioTrack();
+            m_targetTrackIndex = model->addAudioTrack();
             m_trackAdded = m_targetTrackIndex > -1;
         }
 
         if (m_targetTrackIndex > -1) {
             // Set the producer UUID on the new track.
-            int mlt_index = m_model.trackList().at(m_targetTrackIndex).mlt_index;
-            Mlt::Producer producer(m_model.tractor()->multitrack()->track(mlt_index));
+            int mlt_index = model->trackList().at(m_targetTrackIndex).mlt_index;
+            Mlt::Producer producer(model->tractor()->multitrack()->track(mlt_index));
             if (producer.is_valid()) {
                 if (m_uuid.isNull()) {
                     m_uuid = MLT.ensureHasUuid(producer);
@@ -1777,24 +1779,26 @@ void DetachAudioCommand::redo()
             }
             m_undoHelper.recordBeforeState();
             // Add the clip to the new audio track.
-            m_model.overwrite(m_targetTrackIndex, audioClip, m_position, false);
+            model->overwrite(m_targetTrackIndex, audioClip, m_position, false);
             // Replace the original clip with the video only clip
-            m_model.overwrite(m_trackIndex, videoClip, m_position, false);
+            model->overwrite(m_trackIndex, videoClip, m_position, false);
             // Put the clips in a group
-            int groupNumber = getUniqueGroupNumber(m_model);
-            int audioClipIndex = m_model.clipIndex(m_targetTrackIndex, m_position);
-            auto audioClipInfo = m_model.getClipInfo(m_targetTrackIndex, audioClipIndex);
+            int groupNumber = getUniqueGroupNumber(*model);
+            int audioClipIndex = model->clipIndex(m_targetTrackIndex, m_position);
+            auto audioClipInfo = model->getClipInfo(m_targetTrackIndex, audioClipIndex);
             if (audioClipInfo && audioClipInfo->cut) {
                 audioClipInfo->cut->set(kShotcutGroupProperty, groupNumber);
             }
-            auto videoClipInfo = m_model.getClipInfo(m_trackIndex, m_clipIndex);
+            auto videoClipInfo = model->getClipInfo(m_trackIndex, m_clipIndex);
             if (videoClipInfo && videoClipInfo->cut) {
                 videoClipInfo->cut->set(kShotcutGroupProperty, groupNumber);
             }
             m_undoHelper.recordAfterState();
-            QModelIndex modelIndex = m_model.makeIndex(m_trackIndex, m_clipIndex);
-            emit m_model.dataChanged(modelIndex, modelIndex,
-                                     QVector<int>() << MultitrackModel::AudioIndexRole << MultitrackModel::GroupRole);
+            QModelIndex modelIndex = model->makeIndex(m_trackIndex, m_clipIndex);
+            emit model->dataChanged(modelIndex, modelIndex,
+                                    QVector<int>() << MultitrackModel::AudioIndexRole << MultitrackModel::GroupRole);
+            m_timeline.setSelection(QList<QPoint>() << QPoint(m_clipIndex,
+                                                              m_trackIndex) << QPoint(audioClipIndex, m_targetTrackIndex));
         }
     }
 }
@@ -1803,18 +1807,21 @@ void DetachAudioCommand::undo()
 {
     LOG_DEBUG() << "trackIndex" << m_trackIndex << "clipIndex" << m_clipIndex << "position" <<
                 m_position;
+    TimelineSelectionSilencer selectionSilencer(m_timeline);
+    auto model = m_timeline.model();
     m_undoHelper.undoChanges();
     if (m_trackAdded) {
         // Remove the new audio track.
-        m_model.removeTrack(m_targetTrackIndex);
+        model->removeTrack(m_targetTrackIndex);
         m_targetTrackIndex = -1;
     }
     Mlt::Producer originalClip(MLT.profile(), "xml-string", m_xml.toUtf8().constData());
-    m_model.overwrite(m_trackIndex, originalClip, m_position, true);
-    QModelIndex modelIndex = m_model.makeIndex(m_trackIndex, m_clipIndex);
-    emit m_model.dataChanged(modelIndex, modelIndex,
-                             QVector<int>() << MultitrackModel::AudioIndexRole << MultitrackModel::AudioLevelsRole <<
-                             MultitrackModel::GroupRole);
+    model->overwrite(m_trackIndex, originalClip, m_position, true);
+    QModelIndex modelIndex = model->makeIndex(m_trackIndex, m_clipIndex);
+    emit model->dataChanged(modelIndex, modelIndex,
+                            QVector<int>() << MultitrackModel::AudioIndexRole << MultitrackModel::AudioLevelsRole <<
+                            MultitrackModel::GroupRole);
+    m_timeline.setSelection(QList<QPoint>() << QPoint(m_clipIndex, m_trackIndex));
 }
 
 ReplaceCommand::ReplaceCommand(MultitrackModel &model, int trackIndex, int clipIndex,

src/docks/timelinedock.cpp
@@ -246,12 +246,9 @@ TimelineDock::TimelineDock(QWidget *parent) :
         }
     });
     connect(&m_model, SIGNAL(modified()), this, SLOT(clearSelectionIfInvalid()));
-    connect(&m_model, &MultitrackModel::appended, this, &TimelineDock::selectClip,
-            Qt::QueuedConnection);
-    connect(&m_model, &MultitrackModel::inserted, this, &TimelineDock::selectClip,
-            Qt::QueuedConnection);
-    connect(&m_model, &MultitrackModel::overWritten, this, &TimelineDock::selectClip,
-            Qt::QueuedConnection);
+    connect(&m_model, &MultitrackModel::appended, this, &TimelineDock::selectClip);
+    connect(&m_model, &MultitrackModel::inserted, this, &TimelineDock::selectClip);
+    connect(&m_model, &MultitrackModel::overWritten, this, &TimelineDock::selectClip);
     connect(&m_model, SIGNAL(rowsInserted(QModelIndex, int, int)), SLOT(onRowsInserted(QModelIndex, int,
                                                                                        int)));
     connect(&m_model, SIGNAL(rowsRemoved(QModelIndex, int, int)), SLOT(onRowsRemoved(QModelIndex, int,
@@ -2529,7 +2526,7 @@ void TimelineDock::detachAudio(int trackIndex, int clipIndex)
         Mlt::Producer clip(MLT.profile(), "xml-string", MLT.XML(info->producer).toUtf8().constData());
         clip.set_in_and_out(info->frame_in, info->frame_out);
         MAIN.undoStack()->push(
-            new Timeline::DetachAudioCommand(m_model, trackIndex, clipIndex, info->start, MLT.XML(&clip)));
+            new Timeline::DetachAudioCommand(*this, trackIndex, clipIndex, info->start, MLT.XML(&clip)));
     }
 }
 

src/commands/timelinecommands.h
@@ -692,12 +692,12 @@ private:
 class DetachAudioCommand: public QUndoCommand
 {
 public:
-    DetachAudioCommand(MultitrackModel &model, int trackIndex, int clipIndex, int position,
+    DetachAudioCommand(TimelineDock &timeline, int trackIndex, int clipIndex, int position,
                        const QString &xml, QUndoCommand *parent = 0);
     void redo();
     void undo();
 private:
-    MultitrackModel &m_model;
+    TimelineDock &m_timeline;
     int m_trackIndex;
     int m_clipIndex;
     int m_position;

commit 29ab463c7d85965605cfb56ad3274a5ddd7007de

Fix two recent regressions
Fix crash when changing speed on timeline clip.
Regression caused by:
066be72 - Fix selection is incorrect after detach audio

Fix failure to move clips with ripple all tracks on.
Regression caused by:
ae36d5b - Multiple fixes for move commands (#1508)

src/commands/timelinecommands.cpp
@@ -626,24 +626,44 @@ void MoveClipCommand::redo()
     QVector<Mlt::Producer> producers;
     QVector<QUuid> uuids;
 
-    // First, save each clip and remove it
+    // First, save each clip and uuid
     for (auto &clip : m_clips) {
+        uuids.append(clip.uuid);
+        auto info = m_model.getClipInfo(clip.trackIndex, clip.clipIndex);
+        if (info && info->producer && info->producer->is_valid() && info->cut) {
+            producers.append(info->producer);
+        } else {
+            LOG_ERROR() << "Unable to find clip to move" << clip.trackIndex << clip.clipIndex;
+            return;
+        }
+    }
+
+    // Next, remove each clip
+    for (auto &clip : m_clips) {
+        // Need to look up each one by UUID because indexes will change as clips are removed and
+        // some clips may be removed due to ripple.
         int trackIndex, clipIndex;
         auto info = m_model.findClipByUuid(clip.uuid, trackIndex, clipIndex);
         if (info && info->producer && info->producer->is_valid() && info->cut) {
-            producers.append(info->producer);
             if (m_ripple)
                 m_model.removeClip(trackIndex, clipIndex, m_rippleAllTracks);
             else
                 m_model.liftClip(trackIndex, clipIndex);
         } else {
-            LOG_ERROR() << "Unable to find clip to move" << trackIndex << clipIndex;
-            return;
+            // This can happen with ripple since a clip my be removed due to ripple
+            if (!m_ripple) {
+                LOG_ERROR() << "Unable to find clip to move" << trackIndex << clipIndex;
+                return;
+            }
         }
     }
 
-    // Then, place each clip in the new location
+    // Finally, place each clip in the new location
     for (auto &clip : m_clips) {
+        if (producers.size() == 0) {
+            LOG_ERROR() << "Missing producer in move command" << clip.trackIndex << clip.clipIndex;
+            return;
+        }
         Mlt::Producer &producer = producers.front();
         int newTrackIndex = qBound(0, clip.trackIndex + m_trackDelta,
                                    qMax(int(m_model.trackList().size()) - 1, 0));
@@ -1715,7 +1735,6 @@ void DetachAudioCommand::redo()
 {
     LOG_DEBUG() << "trackIndex" << m_trackIndex << "clipIndex" << m_clipIndex << "position" <<
                 m_position;
-    TimelineSelectionSilencer selectionSilencer(m_timeline);
     Mlt::Producer audioClip(MLT.profile(), "xml-string", m_xml.toUtf8().constData());
     Mlt::Producer videoClip(MLT.profile(), "xml-string", m_xml.toUtf8().constData());
     int groupNumber = -1;
@@ -1823,7 +1842,6 @@ void DetachAudioCommand::undo()
 {
     LOG_DEBUG() << "trackIndex" << m_trackIndex << "clipIndex" << m_clipIndex << "position" <<
                 m_position;
-    TimelineSelectionSilencer selectionSilencer(m_timeline);
     auto model = m_timeline.model();
     m_undoHelper.undoChanges();
     if (m_trackAdded) {

src/docks/timelinedock.cpp
@@ -59,9 +59,7 @@ TimelineDock::TimelineDock(QWidget *parent) :
     m_position(-1),
     m_ignoreNextPositionChange(false),
     m_trimDelta(0),
-    m_transitionDelta(0),
-    m_selectionSilenceCounter(0),
-    m_selectionChanged(false)
+    m_transitionDelta(0)
 {
     LOG_DEBUG() << "begin";
     m_selection.selectedTrack = -1;
@@ -236,6 +234,16 @@ TimelineDock::TimelineDock(QWidget *parent) :
 #ifndef Q_OS_MAC
     m_quickView.setAttribute(Qt::WA_AcceptTouchEvents);
 #endif
+
+    m_selectionSignalTimer.setSingleShot(true);
+    m_selectionSignalTimer.setInterval(1);
+    connect(&m_selectionSignalTimer, &QTimer::timeout, this, [&]() {
+        emit selectionChanged();
+        if (!m_selection.selectedClips.isEmpty())
+            emitSelectedFromSelection();
+        else
+            emit selected(nullptr);
+    });
 
     connect(&m_quickView, &QQuickWidget::statusChanged, this, [&]() {
         if ( m_quickView.status() == QQuickWidget::Ready) {
@@ -663,7 +671,6 @@ void TimelineDock::setupActions()
                 return;
             }
             auto clipInfo = m_model.getClipInfo(trackIndex, clipIndex);
-            TimelineSelectionSilencer selectionSilencer(*this);
             moveClip(trackIndex, trackIndex, clipIndex, clipInfo->start + 1, Settings.timelineRipple());
         }
     });
@@ -692,7 +699,6 @@ void TimelineDock::setupActions()
                 return;
             }
             auto clipInfo = m_model.getClipInfo(trackIndex, clipIndex);
-            TimelineSelectionSilencer selectionSilencer(*this);
             moveClip(trackIndex, trackIndex, clipIndex, clipInfo->start - 1, Settings.timelineRipple());
         }
     });
@@ -1603,18 +1609,6 @@ void TimelineDock::setSelectionFromJS(const QVariantList &list)
     setSelection(points);
 }
 
-void TimelineDock::reportSelectionChange()
-{
-    if (m_selectionSilenceCounter == 0 && m_selectionChanged) {
-        emit selectionChanged();
-        if (!m_selection.selectedClips.isEmpty())
-            emitSelectedFromSelection();
-        else
-            emit selected(nullptr);
-        m_selectionChanged = false;
-    }
-}
-
 void TimelineDock::setSelection(QList<QPoint> newSelection, int trackIndex, bool isMultitrack)
 {
     if (newSelection != selection()
@@ -1625,10 +1619,9 @@ void TimelineDock::setSelection(QList<QPoint> newSelection, int trackIndex, bool
         m_selection.selectedClips = newSelection;
         m_selection.selectedTrack = trackIndex;
         m_selection.isMultitrackSelected = isMultitrack;
-        m_selectionChanged = true;
-        reportSelectionChange();
         // Save the selection in case it will be restored later due to a player tab change.
         m_savedSelectionUuids = selectionUuids();
+        m_selectionSignalTimer.start();
     }
 }
 
@@ -2128,7 +2121,6 @@ void TimelineDock::append(int trackIndex)
             Mlt::ClipInfo info;
             MAIN.undoStack()->beginMacro(tr("Append multiple to timeline"));
             Mlt::Controller::RefreshBlocker blocker;
-            TimelineSelectionSilencer selectionSilencer(*this);
 
             // Loop over each source track
             for (int mltTrackIndex = 0; mltTrackIndex < tractor.count(); mltTrackIndex++) {
@@ -2931,7 +2923,6 @@ void TimelineDock::onClipMoved(int fromTrack, int toTrack, int clipIndex, int po
         setSelection();
         if (fromTrack == toTrack)
             disconnect(&m_model, &MultitrackModel::noMoreEmptyTracks, this, nullptr);
-        TimelineSelectionSilencer selectionSilencer(*this);
         MAIN.undoStack()->push(command);
         if (fromTrack == toTrack)
             connect(&m_model, &MultitrackModel::noMoreEmptyTracks, this, &TimelineDock::onNoMoreEmptyTracks,
@@ -3107,7 +3098,6 @@ void TimelineDock::insert(int trackIndex, int position, const QString &xml, bool
 
     if (MLT.isSeekableClip() || MLT.savedProducer() || !xml.isEmpty() || !xmlToUse.isEmpty()) {
         Mlt::Producer producer;
-        TimelineSelectionSilencer selectionSilencer(*this);
         if (xmlToUse.isEmpty() && xml.isEmpty()) {
             Mlt::Producer producer(MLT.isClip() ? MLT.producer() : MLT.savedProducer());
             ProxyManager::generateIfNotExists(producer);
@@ -3219,7 +3209,6 @@ void TimelineDock::overwrite(int trackIndex, int position, const QString &xml, b
 
     if (MLT.isSeekableClip() || MLT.savedProducer() || !xml.isEmpty() || !xmlToUse.isEmpty()) {
         Mlt::Producer producer;
-        TimelineSelectionSilencer selectionSilencer(*this);
         if (xmlToUse.isEmpty() && xml.isEmpty()) {
             Mlt::Producer producer(MLT.isClip() ? MLT.producer() : MLT.savedProducer());
             ProxyManager::generateIfNotExists(producer);

src/docks/timelinedock.h
@@ -259,12 +259,11 @@ private:
     int m_savedSelectedTrack;
     bool m_savedIsMultitrackSelected;
     QVector<QUuid> m_savedSelectionUuids;
+    QTimer m_selectionSignalTimer;
     std::unique_ptr<Timeline::TrimCommand> m_trimCommand;
     std::unique_ptr<UndoHelper> m_undoHelper;
     int m_trimDelta;
     int m_transitionDelta;
-    int m_selectionSilenceCounter;
-    bool m_selectionChanged;
     bool m_isRecording {false};
     std::unique_ptr<AbstractJob> m_recordJob;
     QTimer m_recordingTimer;
@@ -291,26 +290,6 @@ private slots:
     void onTimelineRightClicked();
     void onClipRightClicked();
     void onNoMoreEmptyTracks(bool isAudio);
-
-    friend class TimelineSelectionSilencer;
-};
-
-class TimelineSelectionSilencer
-{
-public:
-    TimelineSelectionSilencer(TimelineDock &timeline)
-        : m_timelineDock(timeline)
-    {
-        m_timelineDock.m_selectionSilenceCounter++;
-    }
-    ~TimelineSelectionSilencer()
-    {
-        m_timelineDock.m_selectionSilenceCounter--;
-        m_timelineDock.reportSelectionChange();
-    }
-
-private:
-    TimelineDock &m_timelineDock;
 };
 
 #endif // TIMELINEDOCK_H

src/mainwindow.cpp
@@ -2625,7 +2625,6 @@ void MainWindow::onProducerOpened(bool withReopen)
             m_player->switchToTab(Player::ProjectTabIndex);
         }
     } else if (MLT.isMultitrack()) {
-        TimelineSelectionSilencer selectionSilencer(*m_timelineDock);
         m_timelineDock->model()->load();
         if (isMultitrackValid()) {
             m_player->setIn(-1);
@@ -3138,13 +3137,11 @@ void MainWindow::updateThumbnails()
 
 void MainWindow::on_actionUndo_triggered()
 {
-    TimelineSelectionSilencer selectionSilencer(*m_timelineDock);
     m_undoStack->undo();
 }
 
 void MainWindow::on_actionRedo_triggered()
 {
-    TimelineSelectionSilencer selectionSilencer(*m_timelineDock);
     m_undoStack->redo();
 }
 