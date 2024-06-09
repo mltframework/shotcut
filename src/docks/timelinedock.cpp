mltframework/shotcut

commit 342893e1f060e4f7d6f74d10be9414154a8fd7e8

fix bulk add to timeline with Auto Add Tracks

src/docks/timelinedock.cpp
@@ -2884,11 +2884,15 @@ void TimelineDock::handleDrop(int trackIndex, int position, QString xml)
         }
     }
 
+    auto autoAddTracks = Settings.timelineAutoAddTracks();
+    Settings.setTimelineAutoAddTracks(false);
     if (Settings.timelineRipple()) {
         insert(trackIndex, position, xml, false);
     } else {
         overwrite(trackIndex, position, xml, false);
     }
+    Settings.setTimelineAutoAddTracks(true);
+    m_model.checkForEmptyTracks(trackIndex);
 }
 
 void TimelineDock::onLoopChanged(int start, int end)
@@ -3371,13 +3375,14 @@ void TimelineDock::appendFromPlaylist(Mlt::Playlist *playlist, bool skipProxy)
             m_model.removeClip(trackIndex, clipIndex, false);
     }
     disconnect(&m_model, &MultitrackModel::appended, this, &TimelineDock::selectClip);
-    disconnect(&m_model, &MultitrackModel::noMoreEmptyTracks, this, nullptr);
+    auto autoAddTracks = Settings.timelineAutoAddTracks();
+    Settings.setTimelineAutoAddTracks(false);
     MAIN.undoStack()->push(
         new Timeline::AppendCommand(m_model, trackIndex, MLT.XML(playlist), skipProxy));
     connect(&m_model, &MultitrackModel::appended, this, &TimelineDock::selectClip,
             Qt::QueuedConnection);
-    connect(&m_model, &MultitrackModel::noMoreEmptyTracks, this, &TimelineDock::onNoMoreEmptyTracks,
-            Qt::QueuedConnection);
+    Settings.setTimelineAutoAddTracks(true);
+    m_model.checkForEmptyTracks(trackIndex);
 }
 
 void TimelineDock::fadeIn(int trackIndex, int clipIndex, int duration)

src/models/multitrackmodel.cpp
@@ -2627,6 +2627,16 @@ int MultitrackModel::mltIndexForTrack(int trackIndex) const
     return i;
 }
 
+bool MultitrackModel::checkForEmptyTracks(int trackIndex)
+{
+    auto trackType = m_trackList.at(trackIndex).type;
+    if (!hasEmptyTrack(trackType)) {
+        emit noMoreEmptyTracks(AudioTrackType == trackType);
+        return true;
+    }
+    return false;
+}
+
 void MultitrackModel::adjustTrackFilters()
 {
     if (!m_tractor) return;
@@ -3714,10 +3724,6 @@ void MultitrackModel::removeBlankPlaceholder(Mlt::Playlist &playlist, int trackI
         beginRemoveRows(index(trackIndex), 0, 0);
         playlist.remove(0);
         endRemoveRows();
-
-        // Check for empty tracks
-        auto trackType = m_trackList.at(trackIndex).type;
-        if (!hasEmptyTrack(trackType))
-            emit noMoreEmptyTracks(AudioTrackType == trackType);
+        checkForEmptyTracks(trackIndex);
     }
 }

src/models/multitrackmodel.h
@@ -126,6 +126,7 @@ public:
     QString getTrackName(int trackIndex);
     int bottomVideoTrackIndex() const;
     int mltIndexForTrack(int trackIndex) const;
+    bool checkForEmptyTracks(int trackIndex);
 
 signals:
     void created();

src/settings.cpp
@@ -783,8 +783,10 @@ bool ShotcutSettings::timelineAutoAddTracks() const
 
 void ShotcutSettings::setTimelineAutoAddTracks(bool b)
 {
-    settings.setValue("timeline/autoAddTracks", b);
-    emit timelineAutoAddTracksChanged();
+    if (b != timelineAutoAddTracks()) {
+        settings.setValue("timeline/autoAddTracks", b);
+        emit timelineAutoAddTracksChanged();
+    }
 }
 
 bool ShotcutSettings::timelineRectangleSelect() const

commit a74d934b1f9f36c5a2fa2319a82c6b970ad0258a

fix auto add tracks regression in 342893e

src/docks/timelinedock.cpp
@@ -2891,7 +2891,7 @@ void TimelineDock::handleDrop(int trackIndex, int position, QString xml)
     } else {
         overwrite(trackIndex, position, xml, false);
     }
-    Settings.setTimelineAutoAddTracks(true);
+    Settings.setTimelineAutoAddTracks(autoAddTracks);
     m_model.checkForEmptyTracks(trackIndex);
 }
 
@@ -3381,7 +3381,7 @@ void TimelineDock::appendFromPlaylist(Mlt::Playlist *playlist, bool skipProxy)
         new Timeline::AppendCommand(m_model, trackIndex, MLT.XML(playlist), skipProxy));
     connect(&m_model, &MultitrackModel::appended, this, &TimelineDock::selectClip,
             Qt::QueuedConnection);
-    Settings.setTimelineAutoAddTracks(true);
+    Settings.setTimelineAutoAddTracks(autoAddTracks);
     m_model.checkForEmptyTracks(trackIndex);
 }
 