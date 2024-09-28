b091f27d7bd2e5270fd19948ef31fb2db8c9d4bb
dj-viking <anders.swedishviking@gmail.com>
2024-09-28 02:04:37 -0400

    rearrange

--- install.ps1
+++ install.ps1
@@ -64,131 +64,129 @@ function deletetarball {
 #EndRegion
 
 
+#Region MLT
+Write-Host -ForegroundColor Cyan "[INFO]: checking MLT"
+
+function getmlt {
+    if (-not (Test-Path $mltunpackpath)) {
+        Write-Host -ForegroundColor Cyan "[INFO]: MLT not found. installing to => $mltunpackpath"
+        New-Item -ItemType Directory -Path $mltunpackpath;
+
+        downloadtarball -url $mlttarballurl -outpath $mltoutfile; 
+        unpacktarball -outputfile $mltoutfile -unpackpath $mltunpackpath;
+        deletetarball -outputfile $mltoutfile;
+    }
+}
+#EndRegion
+
+#Region QT
+function getqt {
+    Write-Host -ForegroundColor Cyan "[INFO]: checking QT"
+    
+    if (-not (Test-Path "./src/Qt*")) {
+        Write-Host -ForegroundColor Cyan "[INFO]: QT not installed - installing to => ./src/Qt"
+        & "$($PSScriptRoot)/Get-Qt.ps1";
+    }
+}
+
+#EndRegion
+
+
+# FFTW
+#Region FFTW
+function getfftw {
+    Write-Host -ForegroundColor Cyan "[INFO]: checking FFTW"
+    
+    if (-not (Test-Path $fftwunpackpath)) {
+        New-Item -ItemType Directory -Path $fftwunpackpath;
+
+        Write-Host -ForegroundColor Cyan "[INFO]: FFTW not found... installing to this project to => $fftwunpackpath"
+        downloadtarball -url $fftwurl -outpath $fftwoutfile;
+        unpacktarball -outputfile $fftwoutfile -unpackpath $fftwunpackpath
+        deletetarball $fftwoutfile;
+    }
+}
+
+
+#EndRegion
+
+
+# FFmpeg: multimedia format and codec libraries
+#Region FFMPEG
+function getffmpeg {
+    Write-Host -ForegroundColor Cyan "[INFO]: checking FFmpeg: multimedia format and codec libraries"
+    if (-not (Test-Path $ffmpegunpackpath)) {
+        New-Item -ItemType Directory -Path $ffmpegunpackpath;
+
+        Write-Host -ForegroundColor Cyan "[INFO]: FFMPEG not found, installing to this project to => $($ffmpegunpackpath)..."
+        
+        downloadtarball -url $ffmpegurl -outpath $ffmpegoutfilexz;
+        
+        xz.exe -d -v $ffmpegoutfilexz;
+        unpacktarball -outputfile $ffmpegoutfiletar -unpackpath $ffmpegunpackpath;
+        
+        Remove-Item $ffmpegoutfiletar -Force -Recurse -Verbose;
+    
+    }
+}
+
+#EndRegion
+
+
+# Frei0r: video plugins
+#Region Frei0r
+function getfrei0r {
+    Write-Host -ForegroundColor Cyan "[INFO]: checking Frei0r: video plugins"
+    if (-not (Test-Path $Frei0runpackpath)) {
+
+        New-Item -ItemType Directory -Path $Frei0runpackpath;
+
+        Write-Host -ForegroundColor Cyan "[INFO]: Frei0r not found, installing to this project to => $Frei0runpackpath..."
+
+        Invoke-WebRequest $Frei0rurl -OutFile $Frei0routfile;
+        
+        Expand-Archive -Path $Frei0routfile -DestinationPath $Frei0runpackpath;
+        
+        Remove-Item $Frei0routfile -Force -Recurse -Verbose;
+    }
+}
+
+#EndRegion
+
+
+# SDL: cross-platform audio playback
+#Region SDL
+function getsdl {
+    Write-Host -ForegroundColor Cyan "[INFO]: checking SDL: cross-platform audio playback..."
+    if (-not (Test-Path $SDLunpackpath)) {
+        New-Item -ItemType Directory -Path $SDLunpackpath;
+        
+        Write-Host -ForegroundColor Cyan "[INFO]: SDL not found, installing to this project to => $SDLunpackpath..."
+        
+        Invoke-WebRequest $SDLurl -OutFile $SDLoutfile;
+        
+        Expand-Archive -Path $SDLoutfile -DestinationPath $SDLunpackpath;
+        
+        Remove-Item $SDLoutfile -Force -Recurse -Verbose;
+    }
+}
+
+#EndRegion
 try {
+    getmlt;
     
+    # this wont work - just cloned from git anyways in C:\qt6
     
-    #Region MLT
-    Write-Host -ForegroundColor Cyan "[INFO]: checking MLT"
+    # best I can do for qt6 at the moment is something like 
+    # local installation of qt6
+    # New-Item -Itemtype Junction -Path ./src/Qt -Target "$home\projects\qt6"
+    # getqt;
+    getfftw;
+    getffmpeg;
+    getfrei0r;
+    getsdl;
     
-    function getmlt {
-        if (-not (Test-Path $mltunpackpath)) {
-            Write-Host -ForegroundColor Cyan "[INFO]: MLT not found. installing to => $mltunpackpath"
-            New-Item -ItemType Directory -Path $mltunpackpath;
-
-            downloadtarball -url $mlttarballurl -outpath $mltoutfile; 
-            unpacktarball -outputfile $mltoutfile -unpackpath $mltunpackpath;
-            deletetarball -outputfile $mltoutfile;
-        }
-    }
-    #EndRegion
-    
-    #Region QT
-    function getqt {
-        Write-Host -ForegroundColor Cyan "[INFO]: checking QT"
-        
-        if (-not (Test-Path "./src/Qt*")) {
-            Write-Host -ForegroundColor Cyan "[INFO]: QT not installed - installing to => ./src/Qt"
-            & "$($PSScriptRoot)/Get-Qt.ps1";
-        }
-    }
-
-    #EndRegion
-    
-    
-    # FFTW
-    #Region FFTW
-    function getfftw {
-        Write-Host -ForegroundColor Cyan "[INFO]: checking FFTW"
-        
-        if (-not (Test-Path $fftwunpackpath)) {
-            New-Item -ItemType Directory -Path $fftwunpackpath;
-
-            Write-Host -ForegroundColor Cyan "[INFO]: FFTW not found... installing to this project to => $fftwunpackpath"
-            downloadtarball -url $fftwurl -outpath $fftwoutfile;
-            unpacktarball -outputfile $fftwoutfile -unpackpath $fftwunpackpath
-            deletetarball $fftwoutfile;
-        }
-    }
-    
-    
-    #EndRegion
-    
-    
-    # FFmpeg: multimedia format and codec libraries
-    #Region FFMPEG
-    function getffmpeg {
-        Write-Host -ForegroundColor Cyan "[INFO]: checking FFmpeg: multimedia format and codec libraries"
-        if (-not (Test-Path $ffmpegunpackpath)) {
-            New-Item -ItemType Directory -Path $ffmpegunpackpath;
-
-            Write-Host -ForegroundColor Cyan "[INFO]: FFMPEG not found, installing to this project to => $($ffmpegunpackpath)..."
-            
-            downloadtarball -url $ffmpegurl -outpath $ffmpegoutfilexz;
-            
-            xz.exe -d -v $ffmpegoutfilexz;
-            unpacktarball -outputfile $ffmpegoutfiletar -unpackpath $ffmpegunpackpath;
-            
-            Remove-Item $ffmpegoutfiletar -Force -Recurse -Verbose;
-        
-        }
-    }
-    
-    #EndRegion
-    
-    
-    # Frei0r: video plugins
-    #Region Frei0r
-    function getfrei0r {
-        Write-Host -ForegroundColor Cyan "[INFO]: checking Frei0r: video plugins"
-        if (-not (Test-Path $Frei0runpackpath)) {
-
-            New-Item -ItemType Directory -Path $Frei0runpackpath;
-
-            Write-Host -ForegroundColor Cyan "[INFO]: Frei0r not found, installing to this project to => $Frei0runpackpath..."
-
-            Invoke-WebRequest $Frei0rurl -OutFile $Frei0routfile;
-            
-            Expand-Archive -Path $Frei0routfile -DestinationPath $Frei0runpackpath;
-            
-            Remove-Item $Frei0routfile -Force -Recurse -Verbose;
-        }
-    }
-
-    #EndRegion
-    
-    
-    # SDL: cross-platform audio playback
-    #Region SDL
-    function getsdl {
-        Write-Host -ForegroundColor Cyan "[INFO]: checking SDL: cross-platform audio playback..."
-        if (-not (Test-Path $SDLunpackpath)) {
-            New-Item -ItemType Directory -Path $SDLunpackpath;
-            
-            Write-Host -ForegroundColor Cyan "[INFO]: SDL not found, installing to this project to => $SDLunpackpath..."
-            
-            Invoke-WebRequest $SDLurl -OutFile $SDLoutfile;
-            
-            Expand-Archive -Path $SDLoutfile -DestinationPath $SDLunpackpath;
-            
-            Remove-Item $SDLoutfile -Force -Recurse -Verbose;
-        }
-    }
-
-    #EndRegion
 }
 catch {
     throw $_;
 }
-
-getmlt;
-
-# this wont work - just cloned from git anyways in C:\qt6
-
-# best I can do for qt6 at the moment is something like 
-# local installation of qt6
-# New-Item -Itemtype Junction -Path ./src/Qt -Target "$home\projects\qt6"
-# getqt;
-getfftw;
-getffmpeg;
-getfrei0r;
-getsdl;
