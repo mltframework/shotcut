mltframework/shotcut

commit 0e85fcf7a5ff66d15952e61fa518bc9b592e73c4

add Mlt::Controller::isClosedClip()

src/models/attachedfiltersmodel.cpp
@@ -734,7 +734,7 @@ void AttachedFiltersModel::reset(Mlt::Producer *producer)
     m_event.reset();
     if (producer && producer->is_valid())
         m_producer.reset(new Mlt::Producer(producer));
-    else if (MLT.isClip() && qstrcmp("_hide", MLT.producer()->get("resource")))
+    else if (MLT.isClip() && MLT.isClosedClip())
         m_producer.reset(new Mlt::Producer(MLT.producer()));
     else
         m_producer.reset();

src/docks/encodedock.cpp
@@ -453,13 +453,13 @@ void EncodeDock::onProducerOpened()
         ui->fromCombo->addItem(tr("Playlist"), "playlist");
         ui->fromCombo->addItem(tr("Each Playlist Item"), "batch");
     }
-    if (MLT.isClip() && qstrcmp("_hide", MLT.producer()->get("resource"))) {
+    if (MLT.isClip() && !MLT.isClosedClip()) {
         ui->fromCombo->addItem(tr("Source"), "clip");
         if (MLT.producer()->get_int(kBackgroundCaptureProperty)
                 || MLT.producer()->get_int(kExportFromProperty))
             index = ui->fromCombo->count() - 1;
     } else if (MLT.savedProducer() && MLT.savedProducer()->is_valid()
-               && qstrcmp("_hide", MLT.savedProducer()->get("resource"))) {
+               && !MLT.isClosedClip(MLT.savedProducer())) {
         ui->fromCombo->addItem(tr("Source"), "clip");
     }
     if (MAIN.isMultitrackValid()) {

src/mltcontroller.cpp
@@ -601,12 +601,7 @@ void Controller::setProfile(const QString &profile_name)
         m_profile.set_explicit(true);
     } else {
         m_profile.set_explicit(false);
-        if (m_producer && m_producer->is_valid()
-                && (qstrcmp(m_producer->get("mlt_service"), "color")
-                    || qstrcmp(m_producer->get("resource"), "_hide"))) {
-            m_profile.from_producer(*m_producer);
-            m_profile.set_width(Util::coerceMultiple(m_profile.width()));
-        } else {
+        if (!m_producer || !m_producer->is_valid() && isClosedClip()) {
             // Use a default profile with the dummy hidden color producer.
             Mlt::Profile tmp(kDefaultMltProfile);
             m_profile.set_colorspace(tmp.colorspace());
@@ -616,6 +611,9 @@ void Controller::setProfile(const QString &profile_name)
             m_profile.set_sample_aspect(tmp.sample_aspect_num(), tmp.sample_aspect_den());
             m_profile.set_display_aspect(tmp.display_aspect_num(), tmp.display_aspect_den());
             m_profile.set_width(Util::coerceMultiple(tmp.width()));
+        } else {
+            m_profile.from_producer(*m_producer);
+            m_profile.set_width(Util::coerceMultiple(m_profile.width()));
         }
     }
     updatePreviewProfile();
@@ -676,6 +674,15 @@ bool Controller::isLiveProducer(Producer *p) const
 bool Controller::isClip() const
 {
     return producer() && producer()->is_valid() && !isPlaylist() && !isMultitrack();
+}
+
+bool Controller::isClosedClip(Producer *producer) const
+{
+    if (!producer)
+        producer = m_producer.data();
+    return producer && producer->is_valid() &&
+           !qstrcmp(producer->get("mlt_service"), "color") &&
+           !qstrcmp(producer->get("resource"), "_hide");
 }
 
 bool Controller::isSeekableClip()

src/mltcontroller.h
@@ -102,6 +102,7 @@ public:
     bool isSeekable(Mlt::Producer *p = nullptr) const;
     bool isLiveProducer(Mlt::Producer *p = nullptr) const;
     bool isClip() const;
+    bool isClosedClip(Producer *producer = nullptr) const;
     bool isSeekableClip();
     bool isPlaylist() const;
     bool isMultitrack() const;

commit 6d6172e00e37b99d4c5d71e7b215f6e03247cae3

fix adding filters to clip in Source
regression in 0e85fcf7 and b04b1819

src/mainwindow.cpp
@@ -1346,8 +1346,7 @@ void MainWindow::open(Mlt::Producer *producer)
 
     // no else here because open() will delete the producer if open fails
     if (!MLT.setProducer(producer)) {
-        auto play = MLT.isClip() && !MLT.isClosedClip();
-        emit producerOpened(play);
+        emit producerOpened();
         if (MLT.URL().endsWith(".mlt") || MLT.URL().endsWith(".xml")) {
             m_filterController->motionTrackerModel()->load();
             emit profileChanged();

src/models/attachedfiltersmodel.cpp
@@ -734,7 +734,7 @@ void AttachedFiltersModel::reset(Mlt::Producer *producer)
     m_event.reset();
     if (producer && producer->is_valid())
         m_producer.reset(new Mlt::Producer(producer));
-    else if (MLT.isClip() && MLT.isClosedClip())
+    else if (MLT.isClip() && !MLT.isClosedClip())
         m_producer.reset(new Mlt::Producer(MLT.producer()));
     else
         m_producer.reset();

src/player.cpp
@@ -942,11 +942,7 @@ void Player::onProducerOpened(bool play)
 
     connectTransport(MLT.transportControl());
 
-    // Closing the previous producer might call pause() milliseconds before
-    // calling play() here. Delays while purging the consumer on pause can
-    // interfere with the play() call. So, we delay play a little to let
-    // pause purging to complete.
-    if (play) {
+    if (play || (MLT.isClip() && !MLT.isClosedClip())) {
         if (m_pauseAfterOpen) {
             if (MLT.isClip()) {
                 pause();
@@ -954,13 +950,15 @@ void Player::onProducerOpened(bool play)
                 MLT.producer()->seek(0);
             }
         } else {
-            if (MLT.consumer()->is_stopped()) {
-                QTimer::singleShot(500, this, SLOT(play()));
-            } else {
+            // Closing the previous producer might call pause() milliseconds before
+            // calling play() here. Delays while purging the consumer on pause can
+            // interfere with the play() call. So, we delay play a little to let
+            // pause purging to complete.
+            if (!MLT.consumer()->is_stopped()) {
                 // This seek purges the consumer to prevent latent end-of-stream detection.
                 seek(0);
-                QTimer::singleShot(500, this, SLOT(play()));
             }
+            QTimer::singleShot(500, this, SLOT(play()));
         }
     } else {
         pause(0);