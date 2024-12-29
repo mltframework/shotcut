mltframework/shotcut

commit b04b18193c7f51a7cd5b64b08836b029879ad9a7

add Settings > Player > Pause After Seek
default is on, current behavior

src/player.cpp
@@ -371,6 +371,7 @@ Player::Player(QWidget *parent)
     connect(MLT.videoWidget(), SIGNAL(frameDisplayed(const SharedFrame &)), this,
             SLOT(onFrameDisplayed(const SharedFrame &)));
     connect(m_scrubber, SIGNAL(seeked(int)), this, SLOT(seek(int)));
+    connect(m_scrubber, SIGNAL(paused(int)), this, SLOT(pause(int)));
     connect(m_scrubber, SIGNAL(inChanged(int)), this, SLOT(onInChanged(int)));
     connect(m_scrubber, SIGNAL(outChanged(int)), this, SLOT(onOutChanged(int)));
     connect(m_positionSpinner, SIGNAL(valueChanged(int)), this, SLOT(seek(int)));
@@ -399,7 +400,7 @@ void Player::connectTransport(const TransportControllable *receiver)
         disconnect(m_currentTransport);
     m_currentTransport = receiver;
     connect(this, SIGNAL(played(double)), receiver, SLOT(play(double)));
-    connect(this, SIGNAL(paused()), receiver, SLOT(pause()));
+    connect(this, SIGNAL(paused(int)), receiver, SLOT(pause(int)));
     connect(this, SIGNAL(stopped()), receiver, SLOT(stop()));
     connect(this, SIGNAL(seeked(int)), receiver, SLOT(seek(int)));
     connect(this, SIGNAL(rewound(bool)), receiver, SLOT(rewind(bool)));
@@ -592,7 +593,7 @@ void Player::setupActions()
     action->setShortcut(QKeySequence(Qt::Key_Right));
     connect(action, &QAction::triggered, this, [&]() {
         if (MLT.producer())
-            seek(position() + 1);
+            pause(position() + 1);
     });
     Actions.add("playerNextFrameAction", action);
 
@@ -601,7 +602,7 @@ void Player::setupActions()
     action->setShortcut(QKeySequence(Qt::Key_Left));
     connect(action, &QAction::triggered, this, [&]() {
         if (MLT.producer())
-            seek(position() - 1);
+            pause(position() - 1);
     });
     Actions.add("playerPreviousFrameAction", action);
 
@@ -843,9 +844,9 @@ void Player::play(double speed)
     m_playPosition = m_position;
 }
 
-void Player::pause()
+void Player::pause(int position)
 {
-    emit paused();
+    emit paused(position);
     showPaused();
 }
 
@@ -862,9 +863,10 @@ void Player::seek(int position)
             emit seeked(qMin(position, MLT.isMultitrack() ? m_duration : m_duration - 1));
         }
     }
-    // Seek implies pause.
-    Actions["playerPlayPauseAction"]->setIcon(m_playIcon);
-    m_playPosition = std::numeric_limits<int>::max();
+    if (Settings.playerPauseAfterSeek()) {
+        Actions["playerPlayPauseAction"]->setIcon(m_playIcon);
+        m_playPosition = std::numeric_limits<int>::max();
+    }
 }
 
 void Player::reset()
@@ -940,9 +942,9 @@ void Player::onProducerOpened(bool play)
     // pause purging to complete.
     if (play) {
         if (m_pauseAfterOpen) {
-            m_pauseAfterOpen = false;
-            QTimer::singleShot(500, this, SLOT(postProducerOpened()));
-            if (!MLT.isClip()) {
+            if (MLT.isClip()) {
+                pause();
+            } else {
                 MLT.producer()->seek(0);
             }
         } else {
@@ -954,13 +956,15 @@ void Player::onProducerOpened(bool play)
                 QTimer::singleShot(500, this, SLOT(play()));
             }
         }
+    } else {
+        pause(0);
     }
 }
 
 void Player::postProducerOpened()
 {
     if (MLT.producer())
-        seek(MLT.producer()->position());
+        pause(MLT.producer()->position());
 }
 
 void Player::onMeltedUnitOpened()
@@ -1007,7 +1011,7 @@ void Player::onDurationChanged()
     if (MLT.producer()->get_speed() == 0)
         seek(m_position);
     else if (m_position >= m_duration)
-        seek(m_duration - 1);
+        pause(m_duration - 1);
 }
 
 void Player::onFrameDisplayed(const SharedFrame &frame)
@@ -1031,7 +1035,7 @@ void Player::onFrameDisplayed(const SharedFrame &frame)
         m_positionSpinner->blockSignals(false);
         m_scrubber->onSeek(position);
         if (m_playPosition < m_previousOut && m_position >= m_previousOut && !loop) {
-            seek(m_previousOut);
+            pause(m_previousOut);
         }
     }
     if (loop) {

src/mainwindow.cpp
@@ -302,7 +302,7 @@ void MainWindow::setupAndConnectPlayerWidget()
     connect(m_player, SIGNAL(tabIndexChanged(int)), SLOT(onPlayerTabIndexChanged(int)));
     connect(MLT.videoWidget(), SIGNAL(started()), SLOT(processMultipleFiles()));
     connect(MLT.videoWidget(), SIGNAL(started()), SLOT(processSingleFile()));
-    connect(MLT.videoWidget(), SIGNAL(paused()), m_player, SLOT(showPaused()));
+    // connect(MLT.videoWidget(), SIGNAL(paused()), m_player, SLOT(showPaused()));
     connect(MLT.videoWidget(), SIGNAL(playing()), m_player, SLOT(showPlaying()));
     connect(MLT.videoWidget(), SIGNAL(toggleZoom(bool)), m_player, SLOT(toggleZoom(bool)));
     ui->menuPlayer->addAction(Actions["playerPlayPauseAction"]);
@@ -1329,7 +1329,8 @@ void MainWindow::open(Mlt::Producer *producer)
 
     // no else here because open() will delete the producer if open fails
     if (!MLT.setProducer(producer)) {
-        emit producerOpened();
+        auto play = MLT.isClip() && !MLT.isClosedClip();
+        emit producerOpened(play);
         if (MLT.URL().endsWith(".mlt") || MLT.URL().endsWith(".xml")) {
             m_filterController->motionTrackerModel()->load();
             emit profileChanged();
@@ -1993,6 +1994,7 @@ void MainWindow::seekKeyframes(int position)
 void MainWindow::readPlayerSettings()
 {
     LOG_DEBUG() << "begin";
+    ui->actionPauseAfterSeek->setChecked(Settings.playerPauseAfterSeek());
     ui->actionRealtime->setChecked(Settings.playerRealtime());
     ui->actionProgressive->setChecked(Settings.playerProgressive());
     ui->actionScrubAudio->setChecked(Settings.playerScrubAudio());
@@ -2803,6 +2805,11 @@ void MainWindow::on_actionBackupSave_triggered()
         if (isWindowModified())
             on_actionSave_triggered();
     }
+}
+
+void MainWindow::on_actionPauseAfterSeek_triggered(bool checked)
+{
+    Settings.setPlayerPauseAfterSeek(checked);
 }
 
 void MainWindow::cropSource(const QRectF &rect)

src/mainwindow.h
@@ -406,6 +406,7 @@ private slots:
     void on_actionAudioVideoDevice_triggered();
     void on_actionReset_triggered();
     void on_actionBackupSave_triggered();
+    void on_actionPauseAfterSeek_triggered(bool checked);
 };
 
 #define MAIN MainWindow::singleton()

src/mainwindow.ui
@@ -260,6 +260,7 @@
       </property>
       <addaction name="actionExternalNone"/>
      </widget>
+     <addaction name="actionPauseAfterSeek"/>
      <addaction name="actionScrubAudio"/>
      <addaction name="actionJack"/>
      <addaction name="actionRealtime"/>
@@ -1407,6 +1408,17 @@
   <action name="actionShowProjectFolder">
    <property name="text">
     <string>Show Project in Folder</string>
+   </property>
+  </action>
+  <action name="actionPauseAfterSeek">
+   <property name="checkable">
+    <bool>true</bool>
+   </property>
+   <property name="checked">
+    <bool>true</bool>
+   </property>
+   <property name="text">
+    <string>Pause After Seek</string>
    </property>
   </action>
  </widget>

src/mltcontroller.cpp
@@ -262,12 +262,13 @@ static void fire_jack_seek_event(mlt_properties jack, int position)
     mlt_events_fire(jack, "jack-seek", mlt_event_data_from_int(position));
 }
 
-void Controller::pause()
+void Controller::pause(int position)
 {
     if (m_producer && !isPaused()) {
         m_producer->set_speed(0);
         if (m_consumer && m_consumer->is_valid()) {
-            m_producer->seek(m_consumer->position() + 1);
+            position = position > -1 ? position : m_consumer->position() + 1;
+            m_producer->seek(position);
             m_consumer->purge();
             m_consumer->start();
             // The following fixes a bug with frame-dropping. It is possible a video frame rendering
@@ -456,7 +457,8 @@ void Controller::seek(int position)
     setVolume(m_volume, false);
     if (m_producer) {
         // Always pause before seeking (if not already paused).
-        m_producer->set_speed(0);
+        if (Settings.playerPauseAfterSeek())
+            m_producer->set_speed(0);
         m_producer->seek(position);
         if (m_consumer && m_consumer->is_valid()) {
             if (m_consumer->is_stopped()) {
@@ -468,7 +470,8 @@ void Controller::seek(int position)
         }
     }
     if (m_jackFilter) {
-        stopJack();
+        if (Settings.playerPauseAfterSeek())
+            stopJack();
         ++m_skipJackEvents;
         fire_jack_seek_event(m_jackFilter->get_properties(), position);
     }
@@ -1705,9 +1708,9 @@ void TransportControl::play(double speed)
     MLT.play(speed);
 }
 
-void TransportControl::pause()
+void TransportControl::pause(int position)
 {
-    MLT.pause();
+    MLT.pause(position);
 }
 
 void TransportControl::stop()

src/mltcontroller.h
@@ -51,7 +51,7 @@ class TransportControl : public TransportControllable
     Q_OBJECT
 public slots:
     void play(double speed = 1.0) override;
-    void pause() override;
+    void pause(int position = -1) override;
     void stop() override;
     void seek(int position) override;
     void rewind(bool forceChangeDirection) override;
@@ -84,7 +84,7 @@ public:
     void closeConsumer();
     virtual void play(double speed = 1.0);
     bool isPaused() const;
-    virtual void pause();
+    virtual void pause(int position = -1);
     void stop();
     bool enableJack(bool enable = true);
     void setVolume(double volume, bool muteOnPause = true);

src/player.h
@@ -77,7 +77,7 @@ signals:
     void inChanged(int delta);
     void outChanged(int delta);
     void played(double speed);
-    void paused();
+    void paused(int position);
     void stopped();
     void seeked(int position);
     void rewound(bool forceChangeDirection);
@@ -97,7 +97,7 @@ signals:
 
 public slots:
     void play(double speed = 1.0);
-    void pause();
+    void pause(int position = -1);
     void stop();
     void seek(int position);
     void reset();

src/scrubbar.cpp
@@ -143,6 +143,8 @@ void ScrubBar::mousePressEvent(QMouseEvent *event)
             update(m_margin + x - offset, 0, w + 2 * offset, height());
         }
     }
+    if (m_activeControl >= CONTROL_IN && !Settings.playerPauseAfterSeek())
+        emit paused(pos);
     emit seeked(pos);
 }
 
@@ -170,6 +172,8 @@ void ScrubBar::mouseMoveEvent(QMouseEvent *event)
             update(m_margin + x - offset, 0, w + 2 * offset, height());
             m_head = pos;
         }
+        if (m_activeControl >= CONTROL_IN && !Settings.playerPauseAfterSeek())
+            emit paused(pos);
         emit seeked(pos);
     } else if (event->buttons() == Qt::NoButton && MLT.producer()) {
         QString text = QString::fromLatin1(MLT.producer()->frames_to_time(pos, Settings.timeFormat()));

commit 9403248f580ef460964f977bb1b8101b4ebc4f98

fix open project does not show start frame
Also, Open Other > Audio/Device not starting automatically.
A regression in b04b181

src/player.cpp
@@ -943,6 +943,11 @@ void Player::onProducerOpened(bool play)
 
     if (play || (MLT.isClip() && !MLT.isClosedClip())) {
         if (m_pauseAfterOpen) {
+            m_pauseAfterOpen = false;
+            QTimer::singleShot(500, this, [=](){
+                if (MLT.producer())
+                    pause(MLT.producer()->position());
+            });
             if (MLT.isClip()) {
                 pause();
             } else {