mltframework/shotcut

commit 272f880eecd342ab89ec51ed61567eb369084a72

change Export > From > Playlist to use current Bin

src/jobs/abstractjob.cpp
@@ -46,6 +46,7 @@ AbstractJob::AbstractJob(const QString &name, QThread::Priority priority)
                                                                                         int)));
     m_actionPause = new QAction(tr("Pause This Job"), this);
     m_standardActions << m_actionPause;
+    m_actionPause->setEnabled(false);
     m_actionResume = new QAction(tr("Resume This Job"), this);
     m_actionResume->setEnabled(false);
     m_standardActions << m_actionResume;
@@ -204,6 +205,8 @@ void AbstractJob::onFinished(int exitCode, QProcess::ExitStatus exitStatus)
         m_log.append(QStringLiteral("Failed with exit code %1\n").arg(exitCode));
         emit finished(this, false);
     }
+    m_actionPause->setEnabled(false);
+    m_actionResume->setEnabled(false);
 }
 
 void AbstractJob::onReadyRead()

src/docks/encodedock.cpp
@@ -16,6 +16,7 @@
  */
 
 #include "encodedock.h"
+#include "docks/timelinedock.h"
 #include "ui_encodedock.h"
 #include "dialogs/addencodepresetdialog.h"
 #include "dialogs/multifileexportdialog.h"
@@ -449,9 +450,10 @@ void EncodeDock::onProducerOpened()
     ui->fromCombo->clear();
     if (MAIN.isMultitrackValid())
         ui->fromCombo->addItem(tr("Timeline"), "timeline");
-    if (MAIN.playlist() && MAIN.playlist()->count() > 0) {
-        ui->fromCombo->addItem(tr("Playlist"), "playlist");
-        ui->fromCombo->addItem(tr("Each Playlist Item"), "batch");
+    auto playlist = MAIN.playlist();
+    if (playlist && playlist->is_valid() && playlist->count() > 0) {
+        ui->fromCombo->addItem(tr("Current Playlist Bin"), "playlist");
+        ui->fromCombo->addItem(tr("Each Playlist Bin Item"), "batch");
     }
     if (MLT.isClip() && !MLT.isClosedClip()) {
         ui->fromCombo->addItem(tr("Source"), "clip");
@@ -1307,9 +1309,10 @@ void EncodeDock::runMelt(const QString &target, int realtime)
     Mlt::Producer *service = fromProducer();
     if (!service) {
         // For each playlist item.
-        if (MAIN.playlist() && MAIN.playlist()->count() > 0) {
+        auto playlist = MAIN.binPlaylist();
+        if (playlist && playlist->is_valid() && playlist->count() > 0) {
             // Use the first playlist item.
-            QScopedPointer<Mlt::ClipInfo> info(MAIN.playlist()->clip_info(0));
+            QScopedPointer<Mlt::ClipInfo> info(playlist->clip_info(0));
             if (!info) return;
             QString xml = MLT.XML(info->producer);
             QScopedPointer<Mlt::Producer> producer(
@@ -1494,10 +1497,11 @@ void EncodeDock::enqueueMelt(const QStringList &targets, int realtime)
                 &&  ui->dualPassCheckbox->isEnabled() && ui->dualPassCheckbox->isChecked()) ? 1 : 0;
     if (!service) {
         // For each playlist item.
-        if (MAIN.playlist() && MAIN.playlist()->count() > 0) {
-            int n = MAIN.playlist()->count();
+        auto playlist = MAIN.binPlaylist();
+        if (playlist && playlist->is_valid() && playlist->count() > 0) {
+            int n = playlist->count();
             for (int i = 0; i < n; i++) {
-                QScopedPointer<Mlt::ClipInfo> info(MAIN.playlist()->clip_info(i));
+                QScopedPointer<Mlt::ClipInfo> info(playlist->clip_info(i));
                 if (!info) continue;
                 QString xml = MLT.XML(info->producer);
                 QScopedPointer<Mlt::Producer> producer(
@@ -1601,7 +1605,7 @@ Mlt::Producer *EncodeDock::fromProducer() const
     if (from == "clip")
         return MLT.isClip() ? MLT.producer() : MLT.savedProducer();
     else if (from == "playlist")
-        return MAIN.playlist();
+        return MAIN.binPlaylist();
     else if (from == "timeline" || from.startsWith("marker:"))
         return MAIN.multitrack();
     else
@@ -1797,20 +1801,10 @@ void EncodeDock::on_encodeButton_clicked()
 
     QString directory = Settings.encodePath();
     auto projectBaseName = QFileInfo(MAIN.fileName()).completeBaseName();
-    if (!m_extension.isEmpty()) {
-        if (!MAIN.fileName().isEmpty()) {
-            directory += QStringLiteral("/%1.%2").arg(projectBaseName, m_extension);
-        }
-    } else {
-        if (!MAIN.fileName().isEmpty()) {
-            directory += "/" + projectBaseName;
-        }
-    }
-
     QString caption = seekable ? tr("Export File") : tr("Capture File");
     if (ui->fromCombo->currentData().toString() == "batch") {
         caption = tr("Export Files");
-        MultiFileExportDialog dialog(tr("Export Each Playlist Item"), MAIN.playlist(),
+        MultiFileExportDialog dialog(tr("Export Each Playlist Bin Item"), MAIN.binPlaylist(),
                                      directory, projectBaseName, m_extension, this);
         if (dialog.exec() != QDialog::Accepted) {
             return;
@@ -1822,6 +1816,13 @@ void EncodeDock::on_encodeButton_clicked()
             nameFilter = tr("%1 (*.%2);;All Files (*)").arg(ui->formatCombo->currentText(), m_extension);
         else
             nameFilter = tr("Determined by Export (*)");
+        if (!m_extension.isEmpty()) {
+            if (!MAIN.fileName().isEmpty()) {
+                directory += QStringLiteral("/%1.%2").arg(projectBaseName, m_extension);
+            }
+        } else if (!MAIN.fileName().isEmpty()) {
+            directory += "/" + projectBaseName;
+        }
         QString newName = QFileDialog::getSaveFileName(this, caption, directory, nameFilter,
                                                        nullptr, Util::getFileDialogOptions());
         if (!newName.isEmpty() && !m_extension.isEmpty()) {
@@ -2744,7 +2745,7 @@ bool EncodeDock::checkForMissingFiles()
 {
     Mlt::Producer *service = fromProducer();
     if (!service) {
-        service = MAIN.playlist();
+        service = MAIN.binPlaylist();
     }
     if (!service) {
         LOG_ERROR() << "Encode: No service to encode";

src/docks/playlistdock.cpp
@@ -1058,6 +1058,28 @@ void PlaylistDock::getSelectionRange(int *start, int *end)
     }
 }
 
+Mlt::Playlist *PlaylistDock::binPlaylist()
+{
+    LongUiTask longTask(QObject::tr("Generating Playlist for Bin"));
+    auto items = ui->treeWidget->selectedItems();
+    if (ui->treeWidget->topLevelItem(0)->isSelected() || items.isEmpty()) {
+        // ALL
+        m_binPlaylist = *m_model.playlist();
+        return &m_binPlaylist;
+    }
+    auto bin = items.first()->text(0);
+    m_binPlaylist.clear();
+    auto count = m_model.playlist()->count();
+    for (int i = 0; i < count; ++i) {
+        Mlt::ClipInfo info;
+        m_model.playlist()->clip_info(i, &info);
+        if (info.producer && info.producer->is_valid() && bin == info.producer->get(kShotcutBinsProperty))
+            m_binPlaylist.append(*info.producer, info.frame_in, info.frame_out);
+        longTask.reportProgress(tr("Appending"), i, count);
+    }
+    return &m_binPlaylist;
+}
+
 void PlaylistDock::incrementIndex()
 {
     QModelIndex index = m_proxyModel->mapToSource(m_view->currentIndex());

src/docks/playlistdock.h
@@ -65,6 +65,7 @@ public:
     int position();
     void replaceClipsWithHash(const QString &hash, Mlt::Producer &producer);
     void getSelectionRange(int *start, int *end);
+    Mlt::Playlist *binPlaylist();
 
 signals:
     void clipOpened(Mlt::Producer *producer, bool play = false);
@@ -144,6 +145,7 @@ private:
     QMenu *m_mainMenu;
     bool m_blockResizeColumnsToContents;
     PlaylistProxyModel *m_proxyModel;
+    Mlt::Playlist m_binPlaylist;
 };
 
 #endif // PLAYLISTDOCK_H

src/mainwindow.cpp
@@ -2901,6 +2901,11 @@ void MainWindow::getSelectionRange(int *start, int *end)
     }
 }
 
+Mlt::Playlist *MainWindow::binPlaylist()
+{
+    return m_playlistDock->binPlaylist();
+}
+
 bool MainWindow::continueModified()
 {
     if (isWindowModified()) {

src/mainwindow.h
@@ -134,6 +134,7 @@ public:
     void cropSource(const QRectF &rect);
     void getMarkerRange(int position, int *start, int *end);
     void getSelectionRange(int *start, int *end);
+    Mlt::Playlist *binPlaylist();
 
 signals:
     void audioChannelsChanged();

commit 3116a7053a923a46e795468d6cbb605f74b3aeac

fix crash on Export > Stop Capture
regression in 272f880e

src/jobs/abstractjob.cpp
@@ -54,6 +54,10 @@ AbstractJob::AbstractJob(const QString &name, QThread::Priority priority)
 
     connect(m_actionPause, &QAction::triggered, this, &AbstractJob::pause);
     connect(m_actionResume, &QAction::triggered, this, &AbstractJob::resume);
+    connect(this, &AbstractJob::finished, this, [this]() {
+        m_actionPause->setEnabled(false);
+        m_actionResume->setEnabled(false);
+    });
 }
 
 void AbstractJob::start()
@@ -209,8 +213,6 @@ void AbstractJob::onFinished(int exitCode, QProcess::ExitStatus exitStatus)
         m_log.append(QStringLiteral("Failed with exit code %1\n").arg(exitCode));
         emit finished(this, false);
     }
-    m_actionPause->setEnabled(false);
-    m_actionResume->setEnabled(false);
     m_isPaused = false;
 }
 