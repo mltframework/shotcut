mltframework/shotcut

commit 1a10a04f89abe7607d2fc393f3252a638d658404

fix timeline clip when trimmed very short
https://forum.shotcut.org/t/if-you-shorten-images-in-timeline-too-much-
you-will-lose-control/47544

src/qml/views/timeline/Clip.qml
@@ -641,7 +641,7 @@ Rectangle {
     Rectangle {
         id: trimIn
 
-        enabled: !elided && !isBlank && !isTransition
+        enabled: parent.width > 1 && !isBlank && !isTransition
         anchors.left: parent.left
         anchors.leftMargin: 0
         height: parent.height
@@ -693,7 +693,7 @@ Rectangle {
     Rectangle {
         id: trimOut
 
-        enabled: !elided && !isBlank && !isTransition
+        enabled: parent.width > 1 && !isBlank && !isTransition
         anchors.right: parent.right
         anchors.rightMargin: 0
         height: parent.height

commit c667a40caf0157ce98f05fe8ecf0da7fe441454e

fix select, tooltip, and drag short timeline clip
https://forum.shotcut.org/t/not-happy-with-the-latest-update/48016

regression in 1a10a04f

src/qml/views/timeline/Clip.qml
@@ -647,7 +647,7 @@ Rectangle {
     Rectangle {
         id: trimIn
 
-        enabled: parent.width > 1 && !isBlank && !isTransition
+        enabled: trimInMouseArea.drag.active || (!elided && !isBlank && !isTransition)
         anchors.left: parent.left
         anchors.leftMargin: 0
         height: parent.height
@@ -699,7 +699,7 @@ Rectangle {
     Rectangle {
         id: trimOut
 
-        enabled: parent.width > 1 && !isBlank && !isTransition
+        enabled: trimOutMouseArea.drag.active || (!elided && !isBlank && !isTransition)
         anchors.right: parent.right
         anchors.rightMargin: 0
         height: parent.height