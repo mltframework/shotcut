mltframework/shotcut

commit ba1ec2740bfd5f07f3c3b086040a055a667d952f

added adjustable Timeline header width
https://forum.shotcut.org/t/make-the-width-of-tracks-names-adjustable/
48015

src/models/multitrackmodel.cpp
@@ -618,6 +618,23 @@ void MultitrackModel::setTrackHeight(int height)
         Settings.setTimelineTrackHeight(qBound(10, height, 150));
         m_tractor->set(kTrackHeightProperty, Settings.timelineTrackHeight());
         emit trackHeightChanged();
+        emit modified();
+    }
+}
+
+int MultitrackModel::trackHeaderWidth() const
+{
+    return (m_tractor && m_tractor->property_exists(kTrackHeaderWidthProperty))
+               ? m_tractor->get_int(kTrackHeaderWidthProperty)
+               : 140;
+}
+
+void MultitrackModel::setTrackHeaderWidth(int width)
+{
+    if (m_tractor) {
+        m_tractor->set(kTrackHeaderWidthProperty, width);
+        emit trackHeaderWidthChanged();
+        emit modified();
     }
 }
 
@@ -3517,6 +3534,7 @@ void MultitrackModel::load()
     emit created();
     emit filteredChanged();
     emit scaleFactorChanged();
+    emit trackHeaderWidthChanged();
 }
 
 void MultitrackModel::reload(bool asynchronous)
@@ -3620,6 +3638,7 @@ void MultitrackModel::close()
     endResetModel();
     emit closed();
     emit filteredChanged();
+    emit trackHeaderWidthChanged();
 }
 
 int MultitrackModel::clipIndex(int trackIndex, int position)

src/qml/views/timeline/timeline.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2024 Meltytech, LLC
+ * Copyright (c) 2013-2025 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@ import "Timeline.js" as Logic
 Rectangle {
     id: root
 
-    property int headerWidth: 140
+    property int headerWidth: multitrack.trackHeaderWidth
     property color selectedTrackColor: Qt.rgba(0.8, 0.8, 0, 0.3)
     property alias trackCount: tracksRepeater.count
     property bool stopScrolling: false
@@ -380,6 +380,19 @@ Rectangle {
                     anchors.top: parent.top
                     anchors.topMargin: -rulerFlickable.height
                     anchors.bottom: parent.bottom
+
+                    MouseArea {
+                        anchors.fill: parent
+                        anchors.leftMargin: -5
+                        anchors.rightMargin: -5
+                        acceptedButtons: Qt.LeftButton
+                        drag.target: parent
+                        drag.axis: Drag.XAxis
+                        drag.minimumX: 140
+                        drag.maximumX: 500
+                        cursorShape: Qt.SizeHorCursor
+                        onPositionChanged: multitrack.trackHeaderWidth = Math.max(drag.minimumX, Math.min(drag.maximumX, parent.x))
+                    }
                 }
             }
         }

src/models/multitrackmodel.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2024 Meltytech, LLC
+ * Copyright (c) 2013-2025 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -47,6 +47,8 @@ class MultitrackModel : public QAbstractItemModel
 {
     Q_OBJECT
     Q_PROPERTY(int trackHeight READ trackHeight WRITE setTrackHeight NOTIFY trackHeightChanged)
+    Q_PROPERTY(int trackHeaderWidth READ trackHeaderWidth WRITE setTrackHeaderWidth NOTIFY
+                   trackHeaderWidthChanged FINAL)
     Q_PROPERTY(double scaleFactor READ scaleFactor WRITE setScaleFactor NOTIFY scaleFactorChanged)
     Q_PROPERTY(bool filtered READ isFiltered NOTIFY filteredChanged)
 
@@ -109,6 +111,8 @@ public:
     bool trimClipOutValid(int trackIndex, int clipIndex, int delta, bool ripple);
     int trackHeight() const;
     void setTrackHeight(int height);
+    int trackHeaderWidth() const;
+    void setTrackHeaderWidth(int width);
     double scaleFactor() const;
     void setScaleFactor(double scale);
     bool isTransition(Mlt::Playlist &playlist, int clipIndex) const;
@@ -132,6 +136,7 @@ signals:
     void modified();
     void seeked(int position, bool seekPlayer = true);
     void trackHeightChanged();
+    void trackHeaderWidthChanged();
     void scaleFactorChanged();
     void showStatusMessage(QString);
     void durationChanged();

src/shotcut_mlt_properties.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2024 Meltytech, LLC
+ * Copyright (c) 2013-2025 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -38,6 +38,7 @@
 #define kShotcutVirtualClip "shotcut:virtual"
 #define kTimelineScaleProperty "shotcut:scaleFactor"
 #define kTrackHeightProperty "shotcut:trackHeight"
+#define kTrackHeaderWidthProperty "shotcut:trackHeaderWidth"
 #define kTrackNameProperty "shotcut:name"
 #define kTrackLockProperty "shotcut:lock"
 #define kVideoTrackProperty "shotcut:video"

commit 9a152d8c9015d9e94470bea03f7bfe31ee8a5afb

fix draggble divier conflicts with trim in
regression in ba1ec274

src/models/multitrackmodel.cpp
@@ -626,7 +626,7 @@ int MultitrackModel::trackHeaderWidth() const
 {
     return (m_tractor && m_tractor->property_exists(kTrackHeaderWidthProperty))
                ? m_tractor->get_int(kTrackHeaderWidthProperty)
-               : 140;
+               : 150;
 }
 
 void MultitrackModel::setTrackHeaderWidth(int width)

src/qml/views/timeline/TrackHead.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2023 Meltytech, LLC
+ * Copyright (c) 2013-2025 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -119,7 +119,7 @@ Rectangle {
 
         Rectangle {
             color: 'transparent'
-            width: trackHeadRoot.width - trackHeadColumn.anchors.margins * 2 - (trackHeadRoot.height < 50 ? 110 : 0)
+            width: trackHeadRoot.width - trackHeadColumn.anchors.margins * 2 - (trackHeadRoot.height < 50 ? 120 : 0)
             radius: 2
             border.color: (!timeline.isFloating() && trackNameMouseArea.containsMouse) ? activePalette.shadow : 'transparent'
             height: nameEdit.height

src/qml/views/timeline/timeline.qml
@@ -181,6 +181,7 @@ Rectangle {
 
                 MouseArea {
                     anchors.fill: parent
+                    anchors.rightMargin: 10
                     acceptedButtons: Qt.LeftButton | Qt.RightButton
                     onClicked: mouse => {
                         timeline.selectMultitrack();
@@ -383,12 +384,11 @@ Rectangle {
 
                     MouseArea {
                         anchors.fill: parent
-                        anchors.leftMargin: -5
-                        anchors.rightMargin: -5
+                        anchors.leftMargin: -10
                         acceptedButtons: Qt.LeftButton
                         drag.target: parent
                         drag.axis: Drag.XAxis
-                        drag.minimumX: 140
+                        drag.minimumX: 150
                         drag.maximumX: 500
                         cursorShape: Qt.SizeHorCursor
                         onPositionChanged: multitrack.trackHeaderWidth = Math.max(drag.minimumX, Math.min(drag.maximumX, parent.x))