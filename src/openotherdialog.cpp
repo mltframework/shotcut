b3420549c5f620d295870dd9b4a159878dfb52ae
Dan Dennedy <dan@dennedy.org>
2025-04-19 09:40:33 -0700

    Restore generators to Open Other dialog
    This reverts part of 0e7716a00

--- src/openotherdialog.cpp
+++ src/openotherdialog.cpp
@@ -29,6 +29,9 @@ OpenOtherDialog::OpenOtherDialog(QWidget *parent)
 {
     ui->setupUi(this);
     m_current = ui->networkWidget;
+    m_addTimelineButton = ui->buttonBox->addButton(tr("Add To Timeline"),
+                                                   QDialogButtonBox::ApplyRole);
+    connect(m_addTimelineButton, &QPushButton::clicked, this, [=]() { done(-1); });
 
     QScopedPointer<Mlt::Properties> mltProducers(MLT.repository()->producers());
     QScopedPointer<Mlt::Properties> mltFilters(MLT.repository()->filters());
@@ -57,6 +60,53 @@ OpenOtherDialog::OpenOtherDialog(QWidget *parent)
     item->setData(0, Qt::UserRole, ui->avfoundationTab->objectName());
 #endif
 
+    // populate the generators
+    group = new QTreeWidgetItem(ui->treeWidget, QStringList(tr("Generator")));
+    if (mltProducers->get_data("color")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Color")));
+        item->setData(0, Qt::UserRole, ui->colorTab->objectName());
+        if (mltProducers->get_data("qtext") && mltFilters->get_data("dynamictext")) {
+            QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Text")));
+            item->setData(0, Qt::UserRole, ui->textTab->objectName());
+        }
+    }
+    if (mltProducers->get_data("glaxnimate")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Animation")));
+        item->setData(0, Qt::UserRole, ui->glaxnimateTab->objectName());
+    }
+    if (mltProducers->get_data("noise")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Noise")));
+        item->setData(0, Qt::UserRole, ui->noiseTab->objectName());
+    }
+    if (mltProducers->get_data("frei0r.ising0r")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Ising")));
+        item->setData(0, Qt::UserRole, ui->isingTab->objectName());
+    }
+
+    if (mltProducers->get_data("frei0r.lissajous0r")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Lissajous")));
+        item->setData(0, Qt::UserRole, ui->lissajousTab->objectName());
+    }
+    if (mltProducers->get_data("frei0r.plasma")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Plasma")));
+        item->setData(0, Qt::UserRole, ui->plasmaTab->objectName());
+    }
+    if (mltProducers->get_data("frei0r.test_pat_B")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Color Bars")));
+        item->setData(0, Qt::UserRole, ui->colorbarsTab->objectName());
+    }
+    if (mltProducers->get_data("tone")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Audio Tone")));
+        item->setData(0, Qt::UserRole, ui->toneTab->objectName());
+    }
+    if (mltProducers->get_data("count")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Count")));
+        item->setData(0, Qt::UserRole, ui->countTab->objectName());
+    }
+    if (mltProducers->get_data("blipflash")) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(group, QStringList(tr("Blip Flash")));
+        item->setData(0, Qt::UserRole, ui->blipTab->objectName());
+    }
     ui->treeWidget->expandAll();
 }
 
@@ -93,6 +143,26 @@ void OpenOtherDialog::load(Mlt::Producer *producer)
             selectTreeWidget(tr("Network"));
         else if (service == "decklink" || resource.contains("decklink"))
             selectTreeWidget(tr("SDI/HDMI"));
+        else if (service == "color")
+            selectTreeWidget(tr("Color"));
+        else if (service == "glaxnimate")
+            selectTreeWidget(tr("Animation"));
+        else if (service == "noise")
+            selectTreeWidget(tr("Noise"));
+        else if (service == "frei0r.ising0r")
+            selectTreeWidget(tr("Ising"));
+        else if (service == "frei0r.lissajous0r")
+            selectTreeWidget(tr("Lissajous"));
+        else if (service == "frei0r.plasma")
+            selectTreeWidget(tr("Plasma"));
+        else if (service == "frei0r.test_pat_B")
+            selectTreeWidget(tr("Color Bars"));
+        else if (service == "tone")
+            selectTreeWidget(tr("Audio Tone"));
+        else if (service == "count")
+            selectTreeWidget(tr("Count"));
+        else if (service == "blipflash")
+            selectTreeWidget(tr("Blip Flash"));
         dynamic_cast<AbstractProducerWidget *>(m_current)->loadPreset(*producer);
     }
 }
@@ -114,6 +184,7 @@ void OpenOtherDialog::on_treeWidget_currentItemChanged(QTreeWidgetItem *current,
 {
     if (current->data(0, Qt::UserRole).isValid()) {
         QString currentData(current->data(0, Qt::UserRole).toString());
+        m_addTimelineButton->setVisible(true);
         for (int i = 0; i < ui->methodTabWidget->count(); i++) {
             QString tabName(ui->methodTabWidget->widget(i)->objectName());
             if (currentData == tabName) {
@@ -121,18 +192,47 @@ void OpenOtherDialog::on_treeWidget_currentItemChanged(QTreeWidgetItem *current,
                 QWidget *w = ui->methodTabWidget->currentWidget();
                 if (w == ui->networkTab) {
                     m_current = ui->networkWidget;
+                    m_addTimelineButton->setVisible(false);
                 } else if (w == ui->decklinkTab) {
                     m_current = ui->decklinkWidget;
+                    m_addTimelineButton->setVisible(false);
                 } else if (w == ui->v4lTab) {
                     m_current = ui->v4lWidget;
+                    m_addTimelineButton->setVisible(false);
+                } else if (w == ui->colorTab) {
+                    m_current = ui->colorWidget;
+                } else if (w == ui->textTab) {
+                    m_current = ui->textWidget;
+                } else if (w == ui->glaxnimateTab) {
+                    m_current = ui->glaxnimateWidget;
+                } else if (w == ui->noiseTab) {
+                    m_current = ui->noiseWidget;
+                } else if (w == ui->isingTab) {
+                    m_current = ui->isingWidget;
+                } else if (w == ui->lissajousTab) {
+                    m_current = ui->lissajousWidget;
+                } else if (w == ui->plasmaTab) {
+                    m_current = ui->plasmaWidget;
+                } else if (w == ui->colorbarsTab) {
+                    m_current = ui->colorbarsWidget;
                 } else if (w == ui->pulseTab) {
                     m_current = ui->pulseWidget;
+                    m_addTimelineButton->setVisible(false);
                 } else if (w == ui->alsaTab) {
                     m_current = ui->alsaWidget;
+                    m_addTimelineButton->setVisible(false);
                 } else if (w == ui->dshowVideoTab) {
                     m_current = ui->dshowVideoWidget;
+                    m_addTimelineButton->setVisible(false);
+                } else if (w == ui->toneTab) {
+                    m_current = ui->toneWidget;
+                } else if (w == ui->countTab) {
+                    m_current = ui->countWidget;
                 } else if (w == ui->avfoundationTab) {
                     m_current = ui->avfoundationWidget;
+                    m_addTimelineButton->setVisible(false);
+                } else if (w == ui->blipTab) {
+                    m_current = ui->blipWidget;
                 }
                 break;
             }
