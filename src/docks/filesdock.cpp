746669ca7f6485580f08040658abeb3063144e82
Dan Dennedy <dan@dennedy.org>
2025-06-29 11:50:25 -0700

    Revert "Cleanup some buttons in Files panel"
    This reverts commit 8c0c6e77ec9580e5b6a6132caf0ebf4b003d6384.

--- src/docks/filesdock.cpp
+++ src/docks/filesdock.cpp
@@ -639,17 +639,17 @@ FilesDock::FilesDock(QWidget *parent)
     ui->verticalLayout->addWidget(toolbar);
     ui->verticalLayout->addSpacing(2);
 
-    auto toolbar2 = new QToolBar(tr("Files Filters"));
-    toolbar2->addAction(Actions["filesFoldersView"]);
-    ui->filtersLayout->addWidget(toolbar2);
+    toolbar = new DockToolBar(tr("Files Filters"));
+    toolbar->addAction(Actions["filesFoldersView"]);
+    ui->filtersLayout->addWidget(toolbar);
 
-    toolbar2 = new QToolBar(ui->label->text());
-    toolbar2->addAction(Actions["filesRefreshFolders"]);
-    toolbar2->addAction(Actions["filesGoUp"]);
+    toolbar = new DockToolBar(ui->label->text());
+    toolbar->addAction(Actions["filesRefreshFolders"]);
+    toolbar->addAction(Actions["filesGoUp"]);
     delete ui->label;
-    ui->locationsLayout->insertWidget(1, toolbar2);
+    ui->locationsLayout->insertWidget(1, toolbar);
 
-    toolbar2 = new QToolBar(tr("Files Filters"));
+    auto toolbar2 = new QToolBar(tr("Files Filters"));
     QString styleSheet = QStringLiteral("QToolButton {"
                                         "    background-color: palette(background);"
                                         "    border-style: solid;"
