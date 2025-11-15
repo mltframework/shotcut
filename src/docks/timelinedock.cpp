3aea1acac328c34b7c6e37c0d1d91bbe1fdbb0f8
Dan Dennedy <dan@dennedy.org>
2025-11-15 14:42:42 -0800

    Ignore empty track in Seek Previous/Next
    https://forum.shotcut.org/t/video-track-starts-at-033-not-000/50319

--- src/docks/timelinedock.cpp
+++ src/docks/timelinedock.cpp
@@ -1710,6 +1710,11 @@ bool TimelineDock::isTransition(int trackIndex, int clipIndex)
                   .toBool();
 }
 
+bool TimelineDock::isEmptyTrack(Mlt::Playlist &playlist)
+{
+    return playlist.count() == 1 && playlist.is_blank(0);
+}
+
 void TimelineDock::onWarnTrackLocked()
 {
     emit showStatusMessage(tr("This track is locked"));
@@ -3847,7 +3852,7 @@ void TimelineDock::appendFromPlaylist(Mlt::Playlist *playlist, bool skipProxy, b
             QScopedPointer<Mlt::Producer> producer(m_model.tractor()->track(i));
             if (producer) {
                 Mlt::Playlist track(*producer);
-                if (!(track.count() == 1 && track.is_blank(0))) {
+                if (!isEmptyTrack(track)) {
                     trackIndex = addVideoTrack();
                 }
             }
@@ -3947,6 +3952,9 @@ void TimelineDock::seekPreviousEdit()
         QScopedPointer<Mlt::Producer> track(m_model.tractor()->track(i));
         if (track) {
             Mlt::Playlist playlist(*track);
+            // Skip empty tracks (tracks with only one blank clip)
+            if (isEmptyTrack(playlist))
+                continue;
             int clipIndex = playlist.get_clip_index_at(m_position);
             if (clipIndex >= 0 && m_position == playlist.clip_start(clipIndex))
                 --clipIndex;
@@ -3971,6 +3979,9 @@ void TimelineDock::seekNextEdit()
         QScopedPointer<Mlt::Producer> track(m_model.tractor()->track(i));
         if (track) {
             Mlt::Playlist playlist(*track);
+            // Skip empty tracks (tracks with only one blank clip)
+            if (isEmptyTrack(playlist))
+                continue;
             int clipIndex = playlist.get_clip_index_at(m_position) + 1;
             if (clipIndex < playlist.count())
                 newPosition = qMin(newPosition, playlist.clip_start(clipIndex));
