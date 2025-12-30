mltframework/shotcut

commit 089c47e195729ca24eb743d560aebd609e8b62e7

Fix #1768 memory and reference leak in Filters

src/controllers/filtercontroller.cpp
@@ -254,12 +254,13 @@ void FilterController::setCurrentFilter(int attachedIndex)
     }
 
     QmlMetadata *meta = m_attachedModel.getMetadata(m_currentFilterIndex);
-    QmlFilter *filter = 0;
+    QmlFilter *filter = nullptr;
     if (meta) {
         emit currentFilterChanged(nullptr, nullptr, QmlFilter::NoCurrentFilter);
-        m_mltService = m_attachedModel.getService(m_currentFilterIndex);
-        if (!m_mltService.is_valid())
+        std::unique_ptr<Mlt::Service> service(m_attachedModel.getService(m_currentFilterIndex));
+        if (!service || !service->is_valid())
             return;
+        m_mltService = *service;
         filter = new QmlFilter(m_mltService, meta);
         filter->setIsNew(m_mltService.get_int(kNewFilterProperty));
         m_mltService.clear(kNewFilterProperty);

commit e0bed3481bbc938a3ac92c6556d6e58720c3252f

Fix Filters broken by 089c47e

src/controllers/filtercontroller.cpp
@@ -260,7 +260,7 @@ void FilterController::setCurrentFilter(int attachedIndex)
         std::unique_ptr<Mlt::Service> service(m_attachedModel.getService(m_currentFilterIndex));
         if (!service || !service->is_valid())
             return;
-        m_mltService = *service;
+        m_mltService = Mlt::Service(service->get_service());
         filter = new QmlFilter(m_mltService, meta);
         filter->setIsNew(m_mltService.get_int(kNewFilterProperty));
         m_mltService.clear(kNewFilterProperty);