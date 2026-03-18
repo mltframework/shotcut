mltframework/shotcut

commit 7478055eec895a936fd68c2993751cd1753b057c

Refactor waveform data (#1788)
Related to #1786

This change moves the audio levels data out of QML, which was not only a source of trouble in Qt 6.10.1 but also inefficient.

src/qml/views/timeline/Clip.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2025 Meltytech, LLC
+ * Copyright (c) 2013-2026 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,7 +32,6 @@ Rectangle {
     property bool isAudio: false
     property bool isTransition: false
     property bool isFiltered: false
-    property var audioLevels
     property int fadeIn: 0
     property int fadeOut: 0
     property double gain: 0
@@ -75,8 +74,8 @@ Rectangle {
         // This is needed to make the model have the correct count.
         // Model as a property expression is not working in all cases.
         waveformRepeater.model = Math.ceil(clipRoot.width / waveform.maxWidth);
-        for (var i = 0; i < waveformRepeater.count; i++)
-            waveformRepeater.itemAt(0).update();
+        for (let i = 0; i < waveformRepeater.count; i++)
+            waveformRepeater.itemAt(i).update();
     }
 
     function updateThumbnails() {
@@ -105,7 +104,6 @@ Rectangle {
     Drag.active: mouseArea.drag.active
     Drag.proposedAction: Qt.MoveAction
     opacity: Drag.active ? 0.5 : 1
-    onAudioLevelsChanged: generateWaveform(false)
     states: [
         State {
             name: 'normal'
@@ -329,19 +327,18 @@ Rectangle {
             model: Math.ceil(clipRoot.width / waveform.maxWidth)
 
             Shotcut.TimelineWaveform {
-
-                // right edge
-                // left edge
-                // bottom edge
                 property int channels: 2
 
+                trackIndex: clipRoot.trackIndex
+                clipIndex: clipRoot.originalClipIndex
                 width: Math.min(clipRoot.width, waveform.maxWidth)
                 height: waveform.height
                 fillColor: clipColor
                 inPoint: Math.round((clipRoot.inPoint + index * waveform.maxWidth / timeScale) * speed) * channels
                 outPoint: inPoint + Math.round(width / timeScale * speed) * channels
-                levels: audioLevels
                 active: ((clipRoot.x + x + width) > tracksFlickable.contentX) && ((clipRoot.x + x) < tracksFlickable.contentX + tracksFlickable.width) && ((trackRoot.y + y + height) > tracksFlickable.contentY) && ((trackRoot.y + y) < tracksFlickable.contentY + tracksFlickable.height) // top edge
+
+                Component.onCompleted: connectToModel()
             }
         }
     }

src/models/multitrackmodel.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2025 Meltytech, LLC
+ * Copyright (c) 2013-2026 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -354,6 +354,26 @@ QHash<int, QByteArray> MultitrackModel::roleNames() const
     roles[GainRole] = "gain";
     roles[GainEnabledRole] = "gainEnabled";
     return roles;
+}
+
+const QVariantList *MultitrackModel::getAudioLevels(int trackIndex, int clipIndex) const
+{
+    if (!m_tractor || trackIndex < 0 || trackIndex >= m_trackList.size())
+        return nullptr;
+
+    int i = m_trackList.at(trackIndex).mlt_index;
+    QScopedPointer<Mlt::Producer> track(m_tractor->track(i));
+    if (track) {
+        Mlt::Playlist playlist(*track);
+        if (clipIndex >= 0 && clipIndex < playlist.count()) {
+            QScopedPointer<Mlt::ClipInfo> info(playlist.clip_info(clipIndex));
+            if (info && info->producer && info->producer->is_valid()) {
+                return static_cast<const QVariantList *>(
+                    info->producer->get_data(kAudioLevelsProperty));
+            }
+        }
+    }
+    return nullptr;
 }
 
 void MultitrackModel::setTrackName(int row, const QString &value)

src/models/multitrackmodel.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2025 Meltytech, LLC
+ * Copyright (c) 2013-2026 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -101,6 +101,7 @@ public:
     QModelIndex parent(const QModelIndex &index) const;
     QHash<int, QByteArray> roleNames() const;
     Q_INVOKABLE void audioLevelsReady(const QPersistentModelIndex &index);
+    const QVariantList *getAudioLevels(int trackIndex, int clipIndex) const;
     bool createIfNeeded();
     void addBackgroundTrack();
     int addAudioTrack();

src/qml/views/keyframes/KeyframeClip.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2025 Meltytech, LLC
+ * Copyright (c) 2016-2026 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,7 +30,6 @@ Rectangle {
     property int clipDuration: outPoint - inPoint + 1
     property bool isBlank: false
     property bool isAudio: false
-    property var audioLevels
     property int animateIn: 0
     property int animateOut: 0
     property int trackIndex: 0
@@ -58,8 +57,11 @@ Rectangle {
         // This is needed to make the model have the correct count.
         // Model as a property expression is not working in all cases.
         waveformRepeater.model = Math.ceil(waveform.innerWidth / waveform.maxWidth);
-        for (var i = 0; i < waveformRepeater.count; i++)
-            waveformRepeater.itemAt(0).update();
+        for (let i = 0; i < waveformRepeater.count; i++) {
+            var item = waveformRepeater.itemAt(i);
+            item.producer = producer;
+            item.update();
+        }
     }
 
     function imagePath(time) {
@@ -71,7 +73,6 @@ Rectangle {
     border.width: 1
     clip: true
     opacity: isBlank ? 0.5 : 1
-    onAudioLevelsChanged: generateWaveform()
     states: [
         State {
             name: 'normal'
@@ -160,10 +161,6 @@ Rectangle {
             model: Math.ceil(waveform.innerWidth / waveform.maxWidth)
 
             Shotcut.TimelineWaveform {
-
-                // right edge
-                // left edge
-                // bottom edge
                 property int channels: 2
 
                 width: Math.min(waveform.innerWidth, waveform.maxWidth)
@@ -171,7 +168,6 @@ Rectangle {
                 fillColor: getColor()
                 inPoint: Math.round((clipRoot.inPoint + index * waveform.maxWidth / timeScale) * speed) * channels
                 outPoint: inPoint + Math.round(width / timeScale * speed) * channels
-                levels: audioLevels
                 active: ((clipRoot.x + x + width) > tracksFlickable.contentX) && ((clipRoot.x + x) < tracksFlickable.contentX + tracksFlickable.width) && ((trackRoot.y + y + height) > tracksFlickable.contentY) && ((trackRoot.y + y) < tracksFlickable.contentY + tracksFlickable.height) // top edge
             }
         }
@@ -556,5 +552,13 @@ Rectangle {
             position: 1
             color: getColor()
         }
+    }
+
+    Connections {
+        function onAudioLevelsChanged() {
+            generateWaveform();
+        }
+
+        target: producer
     }
 }

src/qml/views/keyframes/keyframes.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2024 Meltytech, LLC
+ * Copyright (c) 2017-2026 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -368,12 +368,12 @@ Rectangle {
                                         mltService: producer.mlt_service
                                         inPoint: producer.in
                                         outPoint: filter !== null ? filter.in - 1 : 0
-                                        audioLevels: producer.audioLevels
                                         height: trackRoot.height
                                         hash: producer.hash
                                         speed: producer.speed
                                         outThumbnailVisible: false
                                         onRightClicked: root.clipRightClicked()
+                                        onOutPointChanged: generateWaveform()
                                     }
 
                                     KeyframeClip {
@@ -387,7 +387,6 @@ Rectangle {
                                         outPoint: filter !== null ? filter.out : 0
                                         animateIn: filter !== null ? filter.animateIn : 0
                                         animateOut: filter !== null ? filter.animateOut : 0
-                                        audioLevels: producer.audioLevels
                                         height: trackRoot.height
                                         hash: producer.hash
                                         speed: producer.speed
@@ -429,12 +428,12 @@ Rectangle {
                                         mltService: producer.mlt_service
                                         inPoint: filter !== null ? filter.out + 1 : 0
                                         outPoint: producer.out
-                                        audioLevels: producer.audioLevels
                                         height: trackRoot.height
                                         hash: producer.hash
                                         speed: producer.speed
                                         inThumbnailVisible: false
                                         onRightClicked: root.clipRightClicked()
+                                        onInPointChanged: generateWaveform()
                                     }
                                 }
                             }

src/qml/views/timeline/Track.qml
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2025 Meltytech, LLC
+ * Copyright (c) 2013-2026 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -87,7 +87,6 @@ Rectangle {
             isAudio: typeof model.audio !== 'undefined' ? model.audio : false
             isTransition: typeof model.isTransition !== 'undefined' ? model.isTransition : false
             isFiltered: typeof model.filtered !== 'undefined' ? model.filtered : false
-            audioLevels: typeof model.audioLevels !== 'undefined' ? model.audioLevels : ""
             width: typeof model.duration !== 'undefined' ? model.duration * timeScale : 0
             height: trackRoot.height
             trackIndex: trackRoot.DelegateModel.itemsIndex

src/qmltypes/qmlproducer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2023 Meltytech, LLC
+ * Copyright (c) 2016-2026 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,7 +22,6 @@
 #include "mltcontroller.h"
 #include "models/audiolevelstask.h"
 #include "qmltypes/qmlapplication.h"
-#include "settings.h"
 #include "util.h"
 #include "widgets/glaxnimateproducerwidget.h"
 
@@ -94,14 +93,11 @@ QString QmlProducer::name()
     return Util::producerTitle(m_producer);
 }
 
-QVariant QmlProducer::audioLevels()
+const QVariantList *QmlProducer::audioLevels()
 {
     if (!m_producer.is_valid())
-        return QVariant();
-    if (m_producer.get_data(kAudioLevelsProperty))
-        return QVariant::fromValue(*((QVariantList *) m_producer.get_data(kAudioLevelsProperty)));
-    else
-        return QVariant();
+        return nullptr;
+    return static_cast<const QVariantList *>(m_producer.get_data(kAudioLevelsProperty));
 }
 
 int QmlProducer::fadeIn()

src/qmltypes/qmlproducer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2023 Meltytech, LLC
+ * Copyright (c) 2016-2026 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -38,7 +38,6 @@ class QmlProducer : public QObject
     Q_PROPERTY(QString mlt_service READ mlt_service() NOTIFY producerChanged)
     Q_PROPERTY(QString hash READ hash() NOTIFY producerChanged)
     Q_PROPERTY(QString name READ name() NOTIFY producerChanged)
-    Q_PROPERTY(QVariant audioLevels READ audioLevels NOTIFY audioLevelsChanged)
     Q_PROPERTY(int fadeIn READ fadeIn NOTIFY producerChanged)
     Q_PROPERTY(int fadeOut READ fadeOut NOTIFY producerChanged)
     Q_PROPERTY(double speed READ speed NOTIFY producerChanged)
@@ -63,7 +62,7 @@ public:
         return m_producer.is_valid() ? m_producer.get(kShotcutHashProperty) : QString();
     }
     QString name();
-    QVariant audioLevels();
+    const QVariantList *audioLevels();
     int fadeIn();
     int fadeOut();
     double speed();
@@ -71,6 +70,7 @@ public:
     void setPosition(int position);
     void seek(int position);
     Mlt::Producer &producer() { return m_producer; }
+    Q_INVOKABLE Mlt::Producer *getMltProducer() { return &m_producer; }
     Q_INVOKABLE void audioLevelsReady(const QPersistentModelIndex &index);
     Q_INVOKABLE void remakeAudioLevels();
     double displayAspectRatio();

commit baa7a7a280e5a4176f9050e111431deeb3f69afd

Fix audio waveform sync/blank regression
regression in 7478055e

src/qml/views/timeline/Clip.qml
@@ -39,6 +39,7 @@ Rectangle {
     property int trackIndex
     property int originalTrackIndex: trackIndex
     property int originalClipIndex: index
+    readonly property int readonlyClipIndex: index
     property int originalX: x
     property bool selected: false
     property string hash: ''
@@ -332,7 +333,7 @@ Rectangle {
                 property int channels: 2
 
                 trackIndex: clipRoot.trackIndex
-                clipIndex: clipRoot.originalClipIndex
+                clipIndex: clipRoot.readonlyClipIndex
                 width: Math.min(clipRoot.width - 2 * clipRoot.border.width, waveform.maxWidth)
                 height: waveform.height
                 fillColor: clipColor