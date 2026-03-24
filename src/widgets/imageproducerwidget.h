64cfd8df2fa112b7101e470c86e3280141612de2
Dan Dennedy <dan@dennedy.org>
2026-03-24 13:02:03 -0700

    Change Properties > Duration to be editable

--- src/widgets/imageproducerwidget.h
+++ src/widgets/imageproducerwidget.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2025 Meltytech, LLC
+ * Copyright (c) 2012-2026 Meltytech, LLC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
