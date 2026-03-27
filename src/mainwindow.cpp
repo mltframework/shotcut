mltframework/shotcut

commit 4c8d634c29f37a9f6e6265c2ffb6d5593036dc02

Fix convert project GPU<->CPU to new location
Changing the document's location changes relative paths, quite possibly
needing to be made absolute. You had to first successfully save the
project before closing it. Now, that save operation is automated to fix
paths.

src/mainwindow.cpp
@@ -2198,6 +2198,7 @@ bool MainWindow::open(QString url, const Mlt::Properties *properties, bool play,
     // returns false when MLT is unable to open the file, possibly because it has percent sign in the path
     LOG_DEBUG() << url;
     bool modified = false;
+    bool converted = false;
     MltXmlChecker checker;
     QFileInfo info(url);
 
@@ -2213,7 +2214,8 @@ bool MainWindow::open(QString url, const Mlt::Properties *properties, bool play,
         }
         switch (checker.check(url)) {
         case QXmlStreamReader::NoError:
-            if (!isCompatibleWithGpuMode(checker, url)) {
+            converted = isCompatibleWithGpuMode(checker, url);
+            if (!converted) {
                 showStatusMessage(tr("Failed to open ").append(url));
                 return true;
             }
@@ -2242,7 +2244,8 @@ bool MainWindow::open(QString url, const Mlt::Properties *properties, bool play,
         modified = checkAutoSave(url);
         if (modified) {
             if (checker.check(url) == QXmlStreamReader::NoError) {
-                if (!isCompatibleWithGpuMode(checker, url))
+                converted = isCompatibleWithGpuMode(checker, url);
+                if (!converted)
                     return true;
             } else {
                 showStatusMessage(tr("Failed to open ").append(url));
@@ -2307,6 +2310,8 @@ bool MainWindow::open(QString url, const Mlt::Properties *properties, bool play,
             m_recentDock->add(url);
             LOG_INFO() << url;
         }
+        if (converted)
+            saveXML(url);
     } else if (url != untitledFileName()) {
         showStatusMessage(tr("Failed to open ") + url);
         emit openFailed(url);

commit 651ce57b3b74ec825eed43f67abc6dce20ddbea2

Fix open project may save it and cause extra backup
Regression in 4c8d634c2

src/mainwindow.cpp
@@ -1717,9 +1717,12 @@ void MainWindow::open(Mlt::Producer *producer, bool play)
     activateWindow();
 }
 
-bool MainWindow::isCompatibleWithGpuMode(MltXmlChecker &checker, QString &fileName)
+bool MainWindow::isCompatibleWithProcessingMode(MltXmlChecker &checker,
+                                                QString &fileName,
+                                                bool &converted)
 {
     bool result = true;
+    converted = false;
     if (checker.needsGPU() && !Settings.playerGPU()) {
         LOG_INFO() << "file uses GPU but GPU not enabled";
         QMessageBox dialog(
@@ -1735,7 +1738,7 @@ bool MainWindow::isCompatibleWithGpuMode(MltXmlChecker &checker, QString &fileNa
         dialog.setDefaultButton(QMessageBox::Yes);
         dialog.setEscapeButton(QMessageBox::No);
         if (dialog.exec() == QMessageBox::Yes)
-            result = saveConvertedXmlFile(checker, fileName);
+            converted = result = saveConvertedXmlFile(checker, fileName);
         else
             result = false;
     } else if (checker.needsCPU() && Settings.playerGPU()) {
@@ -1751,9 +1754,8 @@ bool MainWindow::isCompatibleWithGpuMode(MltXmlChecker &checker, QString &fileNa
         dialog.setWindowModality(QmlApplication::dialogModality());
         dialog.setDefaultButton(QMessageBox::Yes);
         dialog.setEscapeButton(QMessageBox::No);
-        int r = dialog.exec();
-        if (r == QMessageBox::Yes)
-            result = saveConvertedXmlFile(checker, fileName);
+        if (dialog.exec() == QMessageBox::Yes)
+            converted = result = saveConvertedXmlFile(checker, fileName);
         else
             result = false;
     }
@@ -2151,9 +2153,6 @@ static void autosaveTask(MainWindow *p)
 void MainWindow::onAutosaveTimeout()
 {
     if (isWindowModified()) {
-        // Automatic backup
-        backupPeriodically();
-
         // Auto-save to recovery file
         auto result = QtConcurrent::run(autosaveTask, this);
     }
@@ -2220,8 +2219,7 @@ bool MainWindow::open(QString url, const Mlt::Properties *properties, bool play,
         }
         switch (checker.check(url)) {
         case QXmlStreamReader::NoError:
-            converted = isCompatibleWithGpuMode(checker, url);
-            if (!converted) {
+            if (!isCompatibleWithProcessingMode(checker, url, converted)) {
                 showStatusMessage(tr("Failed to open ").append(url));
                 return true;
             }
@@ -2250,8 +2248,7 @@ bool MainWindow::open(QString url, const Mlt::Properties *properties, bool play,
         modified = checkAutoSave(url);
         if (modified) {
             if (checker.check(url) == QXmlStreamReader::NoError) {
-                converted = isCompatibleWithGpuMode(checker, url);
-                if (!converted)
+                if (!isCompatibleWithProcessingMode(checker, url, converted))
                     return true;
             } else {
                 showStatusMessage(tr("Failed to open ").append(url));
@@ -4953,8 +4950,12 @@ void MainWindow::on_actionOpenXML_triggered()
         QString url = filenames.first();
         MltXmlChecker checker;
         if (checker.check(url) == QXmlStreamReader::NoError) {
-            if (!isCompatibleWithGpuMode(checker, url))
+            bool converted = false;
+            if (!isCompatibleWithProcessingMode(checker, url, converted)) {
+                showStatusMessage(tr("Incompatible processing mode: ") + url);
                 return;
+            } else if (converted)
+                saveXML(url);
             isXmlRepaired(checker, url);
         } else {
             showStatusMessage(tr("Failed to open ").append(url));

src/mainwindow.h
@@ -225,7 +225,7 @@ private:
     ScreenCapture *m_screenCapture;
 
 public slots:
-    bool isCompatibleWithGpuMode(MltXmlChecker &checker, QString &fileName);
+    bool isCompatibleWithProcessingMode(MltXmlChecker &checker, QString &fileName, bool &converted);
     bool isXmlRepaired(MltXmlChecker &checker, QString &fileName);
     bool open(QString url,
               const Mlt::Properties * = nullptr,