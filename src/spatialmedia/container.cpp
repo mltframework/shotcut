fc07e6eea6a5f583da2d48283868b530e19510bc
Dan Dennedy <dan@dennedy.org>
2026-09-18 10:36:16 -0700

    cleanup copyrights

--- src/spatialmedia/container.cpp
+++ src/spatialmedia/container.cpp
@@ -1,7 +1,8 @@
 /*****************************************************************************
  * 
- * Copyright 2016-2026 Varol Okan. All rights reserved.
- * 
+ * Copyright 2016 Varol Okan. All rights reserved.
+ * Copyright (c) 2020-2026 Meltytech, LLC
+ *  
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
--- src/spatialmedia/mpeg4_container.h
+++ src/spatialmedia/mpeg4_container.h
@@ -2,7 +2,8 @@
 /*****************************************************************************
  * 
  * Copyright 2016 Varol Okan. All rights reserved.
- * 
+ * Copyright (c) 2026 Meltytech, LLC
+ *  
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
